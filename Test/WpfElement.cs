b3046f7cac916bb3a347b290ae7fa978b6790aaf
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-03-21 12:59:23 +0100

    Refactored window

---
+++ Test/WpfElement.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public class WpfElement : FrameworkElement, IElement
+    {
+        private readonly ILayer _layer;
+        private readonly IRenderer _renderer;
+
+        public WpfElement(ILayer layer, IRenderer renderer)
+        {
+            _layer = layer;
+            _renderer = renderer;
+        }
+
+        public void Invalidate()
+        {
+            this.InvalidateVisual();
+        }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+
+            _renderer.Render(drawingContext, _layer);
+        }
+    }
+}
