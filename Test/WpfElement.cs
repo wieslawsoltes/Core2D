19413b3bd77ed29a931038c9a23cb4687f4917cd
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-03-21 13:00:39 +0100

    Added missing references

--- Test/WpfElement.cs
+++ Test/WpfElement.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using Test.Core;
 
 namespace Test
 {
