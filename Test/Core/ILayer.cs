79b1efbb66568e1b0449254fea9d6e9f9d28560f
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2015-03-25 12:29:03 +0100

    Update ILayer.cs

--- Test/Core/ILayer.cs
+++ Test/Core/ILayer.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
