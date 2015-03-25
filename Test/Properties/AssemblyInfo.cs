2c1e5417a0e305e77af44428caf57aa31410ab82
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2015-03-25 12:27:37 +0100

    Update AssemblyInfo.cs

--- Test/Properties/AssemblyInfo.cs
+++ Test/Properties/AssemblyInfo.cs
@@ -1,4 +1,6 @@
-﻿using System.Reflection;
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
