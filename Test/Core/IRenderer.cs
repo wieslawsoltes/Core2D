06a5b0cdebac12fddb4ef4fa0d6d5d37b4a20045
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-04-04 17:28:53 +0200

    Added arc draw method to renderer interface

--- Test/Core/IRenderer.cs
+++ Test/Core/IRenderer.cs
@@ -16,6 +16,7 @@ namespace Test.Core
         void Draw(object dc, XLine line, double dx, double dy);
         void Draw(object dc, XRectangle rectangle, double dx, double dy);
         void Draw(object dc, XEllipse ellipse, double dx, double dy);
+        void Draw(object dc, XArc arc, double dx, double dy);
         void Draw(object dc, XBezier bezier, double dx, double dy);
         void Draw(object dc, XQBezier qbezier, double dx, double dy);
         void Draw(object dc, XText text, double dx, double dy);
