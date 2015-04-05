0a8339ccf7456586053a01bad28be6f624d337a9
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-04-05 09:00:43 +0200

    Added arc draw to pdf renderer

--- Test/Renderers/PdfRenderer.cs
+++ Test/Renderers/PdfRenderer.cs
@@ -10,6 +10,47 @@ using Core = Test.Core;
 
 namespace Test
 {
+    internal struct PdfArc
+    {
+        public double X;
+        public double Y;
+        public double Width;
+        public double Height;
+        public double StartAngle;
+        public double SweepAngle;
+
+        public static PdfArc FromXArc(Test.Core.XArc arc, double dx, double dy)
+        {
+            double x1 = arc.Point1.X + dx;
+            double y1 = arc.Point1.Y + dy;
+            double x2 = arc.Point2.X + dx;
+            double y2 = arc.Point2.Y + dy;
+
+            double x0 = (x1 + x2) / 2.0;
+            double y0 = (y1 + y2) / 2.0;
+
+            double r = Math.Sqrt((x1 - x0) * (x1 - x0) + (y1 - y0) * (y1 - y0));
+            double x = x0 - r;
+            double y = y0 - r;
+            double width = 2.0 * r;
+            double height = 2.0 * r;
+
+            double startAngle = 180.0 / Math.PI * Math.Atan2(y1 - y0, x1 - x0);
+            double endAngle = 180.0 / Math.PI * Math.Atan2(y2 - y0, x2 - x0);
+            double sweepAngle = Math.Abs(startAngle) + Math.Abs(endAngle);
+
+            return new PdfArc
+            {
+                X = x,
+                Y = y,
+                Width = width,
+                Height = height,
+                StartAngle = startAngle,
+                SweepAngle = sweepAngle
+            };
+        }
+    }
+
     public class PdfRenderer : Core.IRenderer
     {
         public bool DrawPoints { get; set; }
@@ -185,7 +226,16 @@ namespace Test
 
         public void Draw(object gfx, Core.XArc arc, double dx, double dy)
         {
-            throw new NotImplementedException();
+            var a = PdfArc.FromXArc(arc, dx, dy);
+
+            (gfx as XGraphics).DrawArc(
+                ToXPen(arc.Style),
+                ScaleToPage(a.X),
+                ScaleToPage(a.Y),
+                ScaleToPage(a.Width),
+                ScaleToPage(a.Height),
+                a.StartAngle,
+                a.SweepAngle);
         }
 
         public void Draw(object gfx, Core.XBezier bezier, double dx, double dy)
