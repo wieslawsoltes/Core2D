d0c170b968dc921f54c143e2c9574289a4be7a9a
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-04-21 17:47:45 +0200

    Added Rect2 struct

---
+++ Test2d/Editor/Rect2.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test2d
+{
+    public struct Rect2
+    {
+        public double X;
+        public double Y;
+        public double Width;
+        public double Height;
+        public double Left { get { return X; } }
+        public double Right { get { return Width + X; } }
+        public double Top { get { return Y; } }
+        public double Bottom { get { return Y + Height; } }
+
+        public Rect2(double x, double y, double width, double height)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public bool Contains(Point2 point)
+        {
+            return ((point.X >= X)
+                && (point.X - Width <= X)
+                && (point.Y >= Y)
+                && (point.Y - Height <= Y));
+        }
+
+        public bool IntersectsWith(Rect2 rect)
+        {
+            return (rect.Left <= Right)
+                && (rect.Right >= Left)
+                && (rect.Top <= Bottom)
+                && (rect.Bottom >= Top);
+        }
+    }
+}
