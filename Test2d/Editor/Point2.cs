c9fb592fc815f4b9a000b1e1a68e40cb9e332757
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-04-21 17:47:36 +0200

    Added Point2 struct

---
+++ Test2d/Editor/Point2.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test2d
+{
+    public struct Point2
+    {
+        public double X;
+        public double Y;
+        public Point2(double x, double y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+    }
+}
