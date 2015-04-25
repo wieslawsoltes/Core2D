b51bfc9b7e284e40614d7efa57ad7eb8c3af1e2f
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-04-25 12:40:57 +0200

    Added DxfViewMode enum

---
+++ TestDXF/Enums/DxfViewMode.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+
+namespace Dxf
+{
+    // Group code: 71
+    [Flags]
+    public enum DxfViewMode : int
+    {
+        Off = 0,
+        PerspectiveView = 1,
+        FrontClipping = 2,
+        BackClipping = 4,
+        UcsFollowMde = 8,
+        FrontClippingNotAtTheCamera = 16
+    }
+}
