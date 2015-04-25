2ef6accba097c48dd50ef42084301e503d2c725f
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-04-25 12:41:33 +0200

    Refactored DxfView class

--- TestDXF/Tables/DxfView.cs
+++ TestDXF/Tables/DxfView.cs
@@ -6,87 +6,129 @@ namespace Dxf
 {
     public class DxfView : DxfObject<DxfView>
     {
+        public string Name { get; set; }
+        public DxfViewStandardFlags ViewStandardFlags { get; set; }
+        public double Height { get; set; }
+        public double Width { get; set; }
+        public DxfVector2 Center { get; set; }
+        public DxfVector3 ViewDirection { get; set; }
+        public DxfVector3 TargetPoint { get; set; }
+        public double LensLength { get; set; }
+        public double FrontClippingPlane { get; set; }
+        public double BackClippingPlane { get; set; }
+        public double TwistAngle { get; set; }
+        public DxfViewMode ViewMode { get; set; }
+        public DxfViewRenderMode RenderMode { get; set; }
+        public bool HaveUcsAssociated { get; set; }
+        public DxfVector3 UcsOrigin { get; set; }
+        public DxfVector3 UcsXAxisDirection { get; set; }
+        public DxfVector3 UcsYAxisDirection { get; set; }
+        public DxfOrthographicViewType UcsOrthographicViewType { get; set; }
+        public string NamedUcsHandle { get; set; }
+        public string BaseUcsHandle { get; set; }
+
         public DxfView(DxfAcadVer version, int id)
             : base(version, id)
         {
+        }
+
+        public DxfView Defaults()
+        {
+            Name = string.Empty;
+            ViewStandardFlags = DxfViewStandardFlags.Default;
+            Height = 0;
+            Width = 0;
+            Center = new DxfVector2(0, 0);
+            ViewDirection = new DxfVector3(0, 0, 1);
+            TargetPoint = new DxfVector3(0, 0, 0);
+            LensLength = 0;
+            FrontClippingPlane = 0;
+            BackClippingPlane = 0;
+            TwistAngle = 0;
+            ViewMode = DxfViewMode.Off;
+            RenderMode = DxfViewRenderMode.Optimized2D;
+            HaveUcsAssociated = false;
+            UcsOrigin = new DxfVector3(0.0, 0.0, 0.0);
+            UcsXAxisDirection = new DxfVector3(0.0, 0.0, 0.0);
+            UcsYAxisDirection = new DxfVector3(0.0, 0.0, 0.0);
+            UcsOrthographicViewType = DxfOrthographicViewType.NotOrthographic;
+            NamedUcsHandle = null;
+            BaseUcsHandle = null;
+
+            return this;
+        }
+
+        public DxfView Create()
+        {
             Add(0, DxfCodeName.View);
 
-            if (version > DxfAcadVer.AC1009)
+            if (Version > DxfAcadVer.AC1009)
             {
-                Handle(id);
+                Handle(Id);
                 Subclass(DxfSubclassMarker.SymbolTableRecord);
                 Subclass(DxfSubclassMarker.ViewTableRecord);
             }
-        }
 
-        public DxfView Name(string name)
-        {
-            Add(2, name);
-            return this;
-        }
+            Add(2, Name);
+            Add(70, (int)ViewStandardFlags);
 
-        public DxfView StandardFlags(DxfViewStandardFlags flags)
-        {
-            Add(70, (int)flags);
-            return this;
-        }
+            Add(40, Height);
+            Add(41, Width);
 
-        public DxfView Height(double height)
-        {
-            Add(40, height);
-            return this;
-        }
+            Add(10, Center.X);
+            Add(20, Center.Y);
 
-        public DxfView Width(double width)
-        {
-            Add(41, width);
-            return this;
-        }
+            Add(11, ViewDirection.X);
+            Add(21, ViewDirection.Y);
+            Add(31, ViewDirection.Z);
 
-        public DxfView Center(DxfVector2 point)
-        {
-            Add(10, point.X);
-            Add(20, point.Y);
-            return this;
-        }
+            Add(12, TargetPoint.X);
+            Add(22, TargetPoint.Y);
+            Add(32, TargetPoint.Z);
 
-        public DxfView ViewDirection(DxfVector3 wcs)
-        {
-            Add(11, wcs.X);
-            Add(21, wcs.Y);
-            Add(31, wcs.Z);
-            return this;
-        }
+            Add(42, LensLength);
+            Add(43, FrontClippingPlane);
+            Add(44, BackClippingPlane);
 
-        public DxfView TargetPoint(DxfVector3 wcs)
-        {
-            Add(12, wcs.X);
-            Add(22, wcs.Y);
-            Add(32, wcs.Z);
-            return this;
-        }
+            Add(50, TwistAngle);
 
-        public DxfView LensLength(double length)
-        {
-            Add(42, length);
-            return this;
-        }
+            Add(71, (int)ViewMode);
 
-        public DxfView FrontClippingPlane(double offset)
-        {
-            Add(43, offset);
-            return this;
-        }
+            if (Version > DxfAcadVer.AC1009)
+            {
+                Add(281, (byte)RenderMode);
 
-        public DxfView BackClippingPlane(double offset)
-        {
-            Add(44, offset);
-            return this;
-        }
+                if (HaveUcsAssociated)
+                {
+                    Add(72, true); // HaveUcsAssociated = true
 
-        public DxfView Twist(double angle)
-        {
-            Add(50, angle);
+                    Add(110, UcsOrigin.X);
+                    Add(120, UcsOrigin.Y);
+                    Add(130, UcsOrigin.Z);
+
+                    Add(111, UcsXAxisDirection.X);
+                    Add(121, UcsXAxisDirection.Y);
+                    Add(131, UcsXAxisDirection.Z);
+
+                    Add(112, UcsYAxisDirection.X);
+                    Add(122, UcsYAxisDirection.Y);
+                    Add(132, UcsYAxisDirection.Z);
+
+                    Add(79, (int)UcsOrthographicViewType);
+
+                    if (NamedUcsHandle != null)
+                    {
+                        Add(345, NamedUcsHandle);
+                    }
+
+                    if (UcsOrthographicViewType != DxfOrthographicViewType.NotOrthographic
+                        && BaseUcsHandle != null)
+                    {
+                        Add(346, BaseUcsHandle);
+                    }
+                }
+            }
+
             return this;
         }
     }
