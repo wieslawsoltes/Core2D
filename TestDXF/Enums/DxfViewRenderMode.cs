93531a3e6e59212581601fa3788027f4f30c7301
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-04-25 12:40:46 +0200

    Added DxfViewRenderMode enum

---
+++ TestDXF/Enums/DxfViewRenderMode.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+
+namespace Dxf
+{
+    // Group code: 281
+    public enum DxfViewRenderMode : byte
+    {
+        Optimized2D = 0,
+        Wireframe = 1,
+        HiddenLine = 2,
+        FlatShaded= 3,
+        GouraudShaded = 4,
+        FlatShadedWithWireframe = 5,
+        GouraudShadedWithWireframe = 6
+    }
+}
