8526b47dd436f08640b8c3b4ba22340700d5facc
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-04-26 16:19:34 +0200

    Added DxfTable class

---
+++ TestDXF/Tables/DxfTable.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Dxf
+{
+    public class DxfTable<T>
+    {
+        public int Id { get; set; }
+        public IList<T> Items { get; set; }
+
+        public DxfTable()
+        {
+            Items = new List<T>();
+        }
+
+        public DxfTable(int id)
+            : this()
+        {
+            Id = id;
+        }
+    }
+}
