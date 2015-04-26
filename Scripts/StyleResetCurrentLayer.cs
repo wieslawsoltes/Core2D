783b667025cd5d1377dee3cb7a222f320914bf00
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-04-26 20:41:44 +0200

    Added new script

---
+++ Scripts/StyleResetCurrentLayer.cs
@@ -0,0 +1,15 @@
+﻿void SetStyle(IList<BaseShape> shapes, ShapeStyle style)
+{
+    if (shapes == null || style == null)
+        return;
+    foreach (var shape in shapes)
+    {
+        shape.Style = style;
+        if (shape is XGroup)
+            SetStyle((shape as XGroup).Shapes, style);
+    }
+}
+var layer = Context?.Editor?.Container?.CurrentLayer;
+var style = Context?.Editor?.Container?.CurrentStyle;
+SetStyle(layer?.Shapes, style);
+layer?.Invalidate();
