456b232d3cd770af614d6d0073a5d803651a36f2
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-04-27 19:31:24 +0200

    Added new script

---
+++ Scripts/StyleResetSelectedShapes.cs
@@ -0,0 +1,16 @@
+﻿void SetStyle(IEnumerable<BaseShape> shapes, ShapeStyle style)
+{
+    if (shapes == null || style == null)
+        return;
+    foreach (var shape in shapes)
+    {
+        shape.Style = style;
+        if (shape is XGroup)
+            SetStyle((shape as XGroup).Shapes, style);
+    }
+}
+var layer = Context?.Editor?.Container?.CurrentLayer;
+var style = Context?.Editor?.Container?.CurrentStyle;
+var shapes = Context?.Editor?.Renderer?.SelectedShapes;
+SetStyle(shapes, style);
+layer?.Invalidate();
