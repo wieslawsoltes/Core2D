ec3e33c06011efbd3b8fda9a8017c8c050347811
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2015-04-29 09:58:00 +0200

    Create UngroupSelectedShapes.cs

---
+++ Scripts/UngroupSelectedShapes.cs
@@ -0,0 +1,38 @@
+void Ungroup(IEnumerable<BaseShape> shapes, Layer layer, bool isGroup)
+{
+    if (shapes == null || layer == null)
+        return;
+    foreach (var shape in shapes)
+    {
+        if (shape is XGroup)
+        {
+            var g = shape as XGroup;
+            Ungroup(g.Shapes, layer, isGroup: true);
+            layer.Shapes.Remove(g);
+        }
+        else if (isGroup)
+        {
+            layer.Shapes.Add(shape);
+        }
+    }
+}
+
+var shapes = Context?.Editor?.Renderer?.SelectedShapes;
+var shape = Context?.Editor?.Renderer?.SelectedShape;
+var layer = Context?.Editor?.Container?.CurrentLayer;
+
+if (shape != null && shape is XGroup && layer != null)
+{
+    var g = shape as XGroup;
+    Ungroup(g.Shapes, layer, isGroup: true);
+    layer.Shapes.Remove(g);
+    layer.Invalidate();
+    Context.Editor.Renderer.SelectedShape = null;
+}
+
+if (shapes != null && layer != null)
+{
+    Ungroup(shapes, layer, isGroup: false);
+    layer.Invalidate();
+    Context.Editor.Renderer.SelectedShapes = null;
+}
