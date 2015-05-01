0a7bbfb4d5b29669c6662a7f2ddab5f2ed5480bd
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-05-01 19:17:55 +0200

    Fixed selected bezier point draw

--- Test2d/Shapes/XBezier.cs
+++ Test2d/Shapes/XBezier.cs
@@ -115,10 +115,10 @@ namespace Test2d
             {
                 if (renderer.SelectedShapes.Contains(this))
                 {
-                    _point1.Draw(dc, renderer, _point1.X + dx, _point1.Y + dy);
-                    _point2.Draw(dc, renderer, _point2.X + dx, _point2.Y + dy);
-                    _point3.Draw(dc, renderer, _point3.X + dx, _point3.Y + dy);
-                    _point4.Draw(dc, renderer, _point4.X + dx, _point4.Y + dy);
+                    _point1.Draw(dc, renderer, dx, dy);
+                    _point2.Draw(dc, renderer, dx, dy);
+                    _point3.Draw(dc, renderer, dx, dy);
+                    _point4.Draw(dc, renderer, dx, dy);
                 }
             }
         }
