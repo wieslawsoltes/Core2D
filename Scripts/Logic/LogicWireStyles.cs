e62790650d806c1f6a7dbf578953e7b0ae911544
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-05-01 14:11:08 +0200

    Added wire style

--- Scripts/Logic/LogicWireStyles.cs
+++ Scripts/Logic/LogicWireStyles.cs
@@ -2,6 +2,18 @@ var styles = Context.Editor.Container.Styles;
 var radiusX = 5.0;
 var radiusY = 5.0;
 var thickness = 2.0;
+// wire
+{
+    var name = "Wire";
+    var stroke = ArgbColor.Create(a: 0xFF, r: 0x00, g: 0x00, b: 0x00);
+    var fill = ArgbColor.Create(a: 0xFF, r: 0x00, g: 0x00, b: 0x00);
+    var startArrowStyle = ArrowStyle.Create(ArrowType.None, isFilled: false, radiusX: 0.0, radiusY: 0.0);
+    var endArrowStyle = ArrowStyle.Create(ArrowType.None, isFilled: false, radiusX: 0.0, radiusY: 0.0);
+    var lineStyle = LineStyle.Create(startArrowStyle, endArrowStyle);
+    var textStyle = TextStyle.Create();
+    var style = ShapeStyle.Create(name, stroke, fill, thickness, lineStyle, textStyle);
+    styles.Add(style);
+}
 // wire start inverted
 {
     var name = "Wire-Start-Inverted";
