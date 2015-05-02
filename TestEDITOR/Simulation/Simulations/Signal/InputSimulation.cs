4e58fde60e609dd6c87d1dc7d574713a297da1b6
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-05-02 13:28:15 +0200

    Added signal simulations

---
+++ TestEDITOR/Simulation/Simulations/Signal/InputSimulation.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Test2d;
+
+namespace TestSIM
+{
+    public class InputSimulation : BoolSimulation
+    {
+        public override string Key
+        {
+            get { return "INPUT"; }
+        }
+
+        public override Func<XGroup, BoolSimulation> Factory
+        {
+            get { return (block) => { return new InputSimulation(false); }; }
+        }
+
+        public InputSimulation()
+            : base()
+        {
+        }
+
+        public InputSimulation(bool? state)
+            : base()
+        {
+            base.State = state;
+        }
+
+        public override void Run(IClock clock)
+        {
+            int length = Inputs.Length;
+            if (length == 0)
+            {
+                // Do nothing.
+            }
+            else
+            {
+                throw new Exception("Input simulation can not have any inputs connected.");
+            }
+        }
+    }
+}
---
+++ TestEDITOR/Simulation/Simulations/Signal/OutputSimulation.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Test2d;
+
+namespace TestSIM
+{
+    public class OutputSimulation : BoolSimulation
+    {
+        public override string Key
+        {
+            get { return "OUTPUT"; }
+        }
+
+        public override Func<XGroup, BoolSimulation> Factory
+        {
+            get { return (block) => { return new OutputSimulation(false); }; }
+        }
+
+        public OutputSimulation()
+            : base()
+        {
+        }
+
+        public OutputSimulation(bool? state)
+            : base()
+        {
+            base.State = state;
+        }
+
+        public override void Run(IClock clock)
+        {
+            int length = Inputs.Length;
+            if (length == 0)
+            {
+                // Do nothing.
+            }
+            else if (length == 1)
+            {
+                var input = Inputs[0];
+                base.State = input.IsInverted ? !(input.Simulation.State) : input.Simulation.State;
+            }
+            else
+            {
+                throw new Exception("Output simulation can only have one input State.");
+            }
+        }
+    }
+}
