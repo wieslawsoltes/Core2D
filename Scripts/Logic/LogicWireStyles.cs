f17bdfb58bccabbbe02266690df7bb7cfeadf55d
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-05-02 12:30:39 +0200

    Updated logic wire style names

--- Scripts/Logic/LogicWireStyles.cs
+++ Scripts/Logic/LogicWireStyles.cs
@@ -12,7 +12,7 @@ var thickness = 2.0;
 
 // wire
 {
-    var name = "Wire";
+    var name = "Logic-Wire";
     var stroke = ArgbColor.Create(a: 0xFF, r: 0x00, g: 0x00, b: 0x00);
     var fill = ArgbColor.Create(a: 0xFF, r: 0x00, g: 0x00, b: 0x00);
     var startArrowStyle = ArrowStyle.Create(ArrowType.None, isFilled: false, radiusX: 0.0, radiusY: 0.0);
@@ -24,7 +24,7 @@ var thickness = 2.0;
 }
 // wire start inverted
 {
-    var name = "Wire-Start-Inverted";
+    var name = "Logic-Wire-Start-Inverted";
     var stroke = ArgbColor.Create(a: 0xFF, r: 0x00, g: 0x00, b: 0x00);
     var fill = ArgbColor.Create(a: 0xFF, r: 0x00, g: 0x00, b: 0x00);
     var startArrowStyle = ArrowStyle.Create(ArrowType.Ellipse, isFilled: false, radiusX: radiusX, radiusY: radiusY);
@@ -36,7 +36,7 @@ var thickness = 2.0;
 }
 // wire end inverted
 {
-    var name = "Wire-End-Inverted";
+    var name = "Logic-Wire-End-Inverted";
     var stroke = ArgbColor.Create(a: 0xFF, r: 0x00, g: 0x00, b: 0x00);
     var fill = ArgbColor.Create(a: 0xFF, r: 0x00, g: 0x00, b: 0x00);
     var startArrowStyle = ArrowStyle.Create(ArrowType.None, isFilled: false, radiusX: 0.0, radiusY: 0.0);
@@ -48,7 +48,7 @@ var thickness = 2.0;
 }
 // wire start & end inverted
 {
-    var name = "Wire-Start-End-Inverted";
+    var name = "Logic-Wire-Start/End-Inverted";
     var stroke = ArgbColor.Create(a: 0xFF, r: 0x00, g: 0x00, b: 0x00);
     var fill = ArgbColor.Create(a: 0xFF, r: 0x00, g: 0x00, b: 0x00);
     var startArrowStyle = ArrowStyle.Create(ArrowType.Ellipse, isFilled: false, radiusX: radiusX, radiusY: radiusY);
