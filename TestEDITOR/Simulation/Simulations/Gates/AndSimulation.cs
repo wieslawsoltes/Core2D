b96fc170f64dbf417903f7ea08240f3d8197f1fb
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-05-02 13:27:32 +0200

    Added gate simulations

---
+++ TestEDITOR/Simulation/Simulations/Gates/AndSimulation.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Test2d;
+
+namespace TestSIM
+{
+    public class AndSimulation : BoolSimulation
+    {
+        public override string Key
+        {
+            get { return "AND"; }
+        }
+
+        public override Func<XGroup, BoolSimulation> Factory
+        {
+            get { return (block) => { return new AndSimulation(null); }; }
+        }
+
+        public AndSimulation()
+            : base()
+        {
+        }
+
+        public AndSimulation(bool? state)
+            : base()
+        {
+            base.State = state;
+        }
+
+        public override void Run(IClock clock)
+        {
+            int length = Inputs.Length;
+            if (length == 1)
+            {
+                base.State = null;
+                return;
+            }
+
+            bool? result = null;
+            for (int i = 0; i < length; i++)
+            {
+                var input = Inputs[i];
+                if (i == 0)
+                {
+                    result = input.IsInverted ? !(input.Simulation.State) : input.Simulation.State;
+                }
+                else
+                {
+                    result &= input.IsInverted ? !(input.Simulation.State) : input.Simulation.State;
+                }
+            }
+            base.State = result;
+        }
+    }
+}
---
+++ TestEDITOR/Simulation/Simulations/Gates/InverterSimulation.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Test2d;
+
+namespace TestSIM
+{
+    public class InverterSimulation : BoolSimulation
+    {
+        public override string Key
+        {
+            get { return "INVERTER"; }
+        }
+
+        public override Func<XGroup, BoolSimulation> Factory
+        {
+            get { return (block) => { return new InverterSimulation(null); }; }
+        }
+
+        public InverterSimulation()
+            : base()
+        {
+        }
+
+        public InverterSimulation(bool? state)
+            : base()
+        {
+            base.State = state;
+        }
+
+        public override void Run(IClock clock)
+        {
+            int length = Inputs.Length;
+            if (length == 0)
+            {
+                // Do nothing.
+            }
+            else if (length == 1)
+            {
+                var input = Inputs[0];
+                base.State = input.IsInverted ? input.Simulation.State : !(input.Simulation.State);
+            }
+            else
+            {
+                throw new Exception("Inverter simulation can only have one input State.");
+            }
+        }
+    }
+}
