1e23eb0ad97c5bd1947908716f852250e92954e6
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-05-02 13:27:45 +0200

    Added memory simulations

---
+++ TestEDITOR/Simulation/Simulations/Memory/MemoryResetPriorityVSimulation.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Test2d;
+
+namespace TestSIM
+{
+    public class MemoryResetPriorityVSimulation : BoolSimulation
+    {
+        public override string Key
+        {
+            get { return "SR-RESET-V"; }
+        }
+
+        public override Func<XGroup, BoolSimulation> Factory
+        {
+            get { return (block) => { return new MemoryResetPriorityVSimulation(); }; }
+        }
+
+        public override void Run(IClock clock)
+        {
+            // TODO: Implement simulation
+        }
+    }
+}
