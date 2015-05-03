5a72aa5fd77c2da4d59f8642b2b7b56bb0140a26
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-05-04 01:20:42 +0200

    Added LineCap enum

---
+++ Test2d/Core/LineCap.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+
+namespace Test2d
+{
+    public enum LineCap
+    {
+        Flat = 0,
+        Square = 1,
+        Round = 2
+    }
+}
