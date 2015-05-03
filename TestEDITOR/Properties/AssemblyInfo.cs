e1510d74905ba260e6da2f6d12cd54b84d003621
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-05-03 16:25:43 +0200

    Updated assembly info

--- TestEDITOR/Properties/AssemblyInfo.cs
+++ TestEDITOR/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-using System;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -16,14 +16,23 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// This sets the default COM visibility of types in the assembly to invisible.
-// If you need to expose a type to COM, use [ComVisible(true)] on that type.
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The assembly version has following format :
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6c4f680a-4ea1-4b71-9ba9-3e7d26a2f0a1")]
+
+// Version information for an assembly consists of the following four values:
 //
-// Major.Minor.Build.Revision
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
 //
-// You can specify all the values or you can use the default the Revision and 
-// Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.*")]
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
