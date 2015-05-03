7a3d032f9ce62149297c1c1ce5fb76166b3678d9
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-05-03 16:23:22 +0200

    Added ITextClipboard interface

---
+++ Test2d/Editor/Core/ITextClipboard.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+
+namespace Test2d
+{
+    public interface ITextClipboard
+    {
+        bool ContainsText();
+        string GetText();
+        void SetText(string text);
+    }
+}
