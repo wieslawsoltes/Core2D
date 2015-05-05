6e669c875e239acbcb987fa4c7f9cb7fda4509a4
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-05-06 00:10:32 +0200

    Added converters

---
+++ Test/Converters/DoubleToStringConverter.cs
@@ -0,0 +1,65 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Test.Converters
+{
+    public class DoubleToStringConverter : IValueConverter
+    {
+        public object Convert(
+            object value,
+            Type targetType,
+            object parameter,
+            CultureInfo culture)
+        {
+            try
+            {
+                double[] a = value as double[];
+                if (a != null)
+                    return string.Join(
+                        " ",
+                        a.Select(x => x.ToString(culture)));
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.Print(ex.Message);
+                System.Diagnostics.Debug.Print(ex.StackTrace);
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(
+            object value,
+            Type targetType,
+            object parameter,
+            CultureInfo culture)
+        {
+            try
+            {
+                string s = value as string;
+                if (s != null)
+                {
+                    string[] a = s.Split(
+                        new char[] { ' ' },
+                        StringSplitOptions.RemoveEmptyEntries);
+                    if (a != null && a.Length > 0)
+                        return a.Select(x => double.Parse(x, culture)).ToArray();
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.Print(ex.Message);
+                System.Diagnostics.Debug.Print(ex.StackTrace);
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Test/Converters/ZoomScaleConverter.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Test.Converters
+{
+    public class ZoomScaleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double sliderValue = (double)value;
+            double displayValue = sliderValue;
+            return displayValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double displayValue = (double)value;
+            double sliderValue = displayValue;
+            return sliderValue;
+        }
+    }
+}
