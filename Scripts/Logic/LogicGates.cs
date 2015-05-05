77c0269d2ec556e9841d202b62b49c4b98acf199
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-05-05 21:45:41 +0200

    Added new logic gates

--- Scripts/Logic/LogicGates.cs
+++ Scripts/Logic/LogicGates.cs
@@ -1,4 +1,5 @@
 ﻿var c = Context.Editor.Container;
+
 var sg = c.StyleGroups.Where(x => x.Name == "Logic").FirstOrDefault();
 if (sg == null)
 {
@@ -6,6 +7,15 @@ if (sg == null)
     c.StyleGroups.Add(sg);
 }
 var styles = sg.Styles;
+
+var gl = c.GroupLibraries.Where(x => x.Name == "Logic").FirstOrDefault();
+if (gl == null)
+{
+    gl = GroupLibrary.Create("Logic");
+    c.GroupLibraries.Add(gl);
+}
+var groups = gl.Groups;
+
 var ps = c.PointShape;
 
 var styleTextMediumLC = ShapeStyle.Create(
@@ -46,33 +56,6 @@ styles.Add(styleLineThick);
 
 sg.CurrentStyle = sg.Styles.FirstOrDefault();
 
-void SetShapeState(BaseShape shape, XGroup owner)
-{
-    shape.Owner = owner;
-    shape.State &= ~ShapeState.Standalone;
-}
-
-void SetConnectorAsNone(XPoint point, XGroup owner)
-{
-    point.Owner = owner;
-    point.State |= ShapeState.Connector | ShapeState.None;
-    point.State &= ~ShapeState.Standalone;
-}
-
-void SetConnectorAsInput(XPoint point, XGroup owner)
-{
-    point.Owner = owner;
-    point.State |= ShapeState.Connector | ShapeState.Input;
-    point.State &= ~ShapeState.Standalone;
-}
-
-void SetConnectorAsOutput(XPoint point, XGroup owner)
-{
-    point.Owner = owner;
-    point.State |= ShapeState.Connector | ShapeState.Output;
-    point.State &= ~ShapeState.Standalone;
-}
-
 // INPUT
 XGroup CreateInputSignal()
 {
@@ -82,17 +65,13 @@ XGroup CreateInputSignal()
     var frame = XRectangle.Create(0, 0, 30, 30, styleLineThick, ps, false, "");
     var co = XPoint.Create(30, 15, ps, "O");
 
-    var labelProperty = ShapeProperty.Create("IN");
-    g.AddProperty("Label", labelProperty);
+    var labelProperty = ShapeProperty.Create("Label", "IN");
+    g.AddProperty(labelProperty);
     label.Properties = new [] { labelProperty };
 
-    SetShapeState(label, g);
-    SetShapeState(frame, g);
-    SetConnectorAsOutput(co, g);
-    
-    g.Shapes.Add(label);
-    g.Shapes.Add(frame);
-    g.Connectors.Add(co);
+    g.AddShape(label);
+    g.AddShape(frame);
+    g.AddConnectorAsOutput(co);
 
     return g;
 }
@@ -106,18 +85,14 @@ XGroup CreateOutputSignal()
     var frame = XRectangle.Create(0, 0, 30, 30, styleLineThick, ps, false, "");
     var ci = XPoint.Create(0, 15, ps, "I");
     
-    var labelProperty = ShapeProperty.Create("OUT");
-    g.AddProperty("Label", labelProperty);
+    var labelProperty = ShapeProperty.Create("Label", "OUT");
+    g.AddProperty(labelProperty);
     label.Properties = new [] { labelProperty };
 
-    g.Shapes.Add(label);
-    g.Shapes.Add(frame);
-    g.Connectors.Add(ci);
-    
-    SetShapeState(label, g);
-    SetConnectorAsInput(ci, g);
-    SetShapeState(frame, g);
-    
+    g.AddShape(label);
+    g.AddShape(frame);
+    g.AddConnectorAsInput(ci);
+
     return g;
 }
 
@@ -135,40 +110,31 @@ XGroup CreateSignal()
     var ci = XPoint.Create(0, 15, ps, "I");
     var co = XPoint.Create(300, 15, ps, "O");
    
-    var designationProperty = ShapeProperty.Create("Designation");
-    g.AddProperty("Designation", designationProperty);
+    var designationProperty = ShapeProperty.Create("Designation", "Designation");
+    g.AddProperty(designationProperty);
     labelDesignation.Properties = new [] { designationProperty };
 
-    var descriptionProperty = ShapeProperty.Create("Description");
-    g.AddProperty("Description", descriptionProperty);
+    var descriptionProperty = ShapeProperty.Create("Description", "Description");
+    g.AddProperty(descriptionProperty);
     labelDescription.Properties = new [] { descriptionProperty };
     
-    var signalProperty = ShapeProperty.Create("Signal");
-    g.AddProperty("Signal", signalProperty);
+    var signalProperty = ShapeProperty.Create("Signal", "Signal");
+    g.AddProperty(signalProperty);
     labelSignal.Properties = new [] { signalProperty };
     
-    var conditionProperty = ShapeProperty.Create("Condition");
-    g.AddProperty("Condition", conditionProperty);
+    var conditionProperty = ShapeProperty.Create("Condition", "Condition");
+    g.AddProperty(conditionProperty);
     labelCondition.Properties = new [] { conditionProperty };
     
-    SetShapeState(labelDesignation, g);
-    SetShapeState(labelDescription, g);
-    SetShapeState(labelSignal, g);
-    SetShapeState(labelCondition, g);
-    SetShapeState(frame, g);
-    SetShapeState(separator, g);
-    SetConnectorAsInput(ci, g);
-    SetConnectorAsOutput(co, g);
-    
-    g.Shapes.Add(labelDesignation);
-    g.Shapes.Add(labelDescription);
-    g.Shapes.Add(labelSignal);
-    g.Shapes.Add(labelCondition);
-    g.Shapes.Add(frame);
-    g.Shapes.Add(separator);
-    g.Connectors.Add(ci);
-    g.Connectors.Add(co);
-    
+    g.AddShape(labelDesignation);
+    g.AddShape(labelDescription);
+    g.AddShape(labelSignal);
+    g.AddShape(labelCondition);
+    g.AddShape(frame);
+    g.AddShape(separator);
+    g.AddConnectorAsInput(ci);
+    g.AddConnectorAsOutput(co);
+
     return g;
 }
 
@@ -184,24 +150,17 @@ XGroup CreateAndGate()
     var ct = XPoint.Create(15, 0, ps, "T");
     var cb = XPoint.Create(15, 30, ps, "B");
 
-    var labelProperty = ShapeProperty.Create("&");
-    g.AddProperty("Label", labelProperty);
+    var labelProperty = ShapeProperty.Create("Label", "&");
+    g.AddProperty(labelProperty);
     label.Properties = new [] { labelProperty };
     
-    SetShapeState(label, g);
-    SetShapeState(frame, g);
-    SetConnectorAsNone(cl, g);
-    SetConnectorAsNone(cr, g);
-    SetConnectorAsNone(ct, g); 
-    SetConnectorAsNone(cb, g);
-                
-    g.Shapes.Add(label);
-    g.Shapes.Add(frame);
-    g.Connectors.Add(cl);
-    g.Connectors.Add(cr);
-    g.Connectors.Add(ct);
-    g.Connectors.Add(cb);
-   
+    g.AddShape(label);
+    g.AddShape(frame);
+    g.AddConnectorAsNone(cl);
+    g.AddConnectorAsNone(cr);
+    g.AddConnectorAsNone(ct); 
+    g.AddConnectorAsNone(cb);
+
     return g;
 }
 
@@ -217,25 +176,18 @@ XGroup CreateOrGate()
     var ct = XPoint.Create(15, 0, ps, "T");
     var cb = XPoint.Create(15, 30, ps, "B");
 
-    var prefixProperty = ShapeProperty.Create("≥");
-    var counterProperty = ShapeProperty.Create("1");
-    g.AddProperty("Prefix", prefixProperty);
-    g.AddProperty("Counter", counterProperty);
+    var prefixProperty = ShapeProperty.Create("Prefix", "≥");
+    var counterProperty = ShapeProperty.Create("Counter", "1");
+    g.AddProperty(prefixProperty);
+    g.AddProperty(counterProperty);
     label.Properties = new [] { prefixProperty, counterProperty };
 
-    SetShapeState(label, g);
-    SetShapeState(frame, g);
-    SetConnectorAsNone(cl, g);
-    SetConnectorAsNone(cr, g);
-    SetConnectorAsNone(ct, g);
-    SetConnectorAsNone(cb, g);
-        
-    g.Shapes.Add(label);
-    g.Shapes.Add(frame);
-    g.Connectors.Add(cl);
-    g.Connectors.Add(cr);
-    g.Connectors.Add(ct);
-    g.Connectors.Add(cb);
+    g.AddShape(label);
+    g.AddShape(frame);
+    g.AddConnectorAsNone(cl);
+    g.AddConnectorAsNone(cr);
+    g.AddConnectorAsNone(ct);
+    g.AddConnectorAsNone(cb);
 
     return g;
 }
@@ -252,24 +204,17 @@ XGroup CreateXorGate()
     var ct = XPoint.Create(15, 0, ps, "T");
     var cb = XPoint.Create(15, 30, ps, "B");
 
-    var labelProperty = ShapeProperty.Create("=1");
-    g.AddProperty("Label", labelProperty);
+    var labelProperty = ShapeProperty.Create("Label", "=1");
+    g.AddProperty(labelProperty);
     label.Properties = new [] { labelProperty };
     
-    SetShapeState(label, g);
-    SetShapeState(frame, g);
-    SetConnectorAsNone(cl, g);
-    SetConnectorAsNone(cr, g);
-    SetConnectorAsNone(ct, g);
-    SetConnectorAsNone(cb, g);
-            
-    g.Shapes.Add(label);
-    g.Shapes.Add(frame);
-    g.Connectors.Add(cl);
-    g.Connectors.Add(cr);
-    g.Connectors.Add(ct);
-    g.Connectors.Add(cb);
- 
+    g.AddShape(label);
+    g.AddShape(frame);
+    g.AddConnectorAsNone(cl);
+    g.AddConnectorAsNone(cr);
+    g.AddConnectorAsNone(ct);
+    g.AddConnectorAsNone(cb);
+
     return g;
 }
 
@@ -285,24 +230,17 @@ XGroup CreateInverterGate()
     var ct = XPoint.Create(15, 0, ps, "T");
     var cb = XPoint.Create(15, 30, ps, "B");
 
-    var labelProperty = ShapeProperty.Create("1");
-    g.AddProperty("Label", labelProperty);
+    var labelProperty = ShapeProperty.Create("Label", "1");
+    g.AddProperty(labelProperty);
     label.Properties = new [] { labelProperty };
     
-    SetShapeState(label, g);
-    SetShapeState(frame, g);
-    SetConnectorAsNone(cl, g);
-    SetConnectorAsNone(cr, g);
-    SetConnectorAsNone(ct, g);
-    SetConnectorAsNone(cb, g);
-            
-    g.Shapes.Add(label);
-    g.Shapes.Add(frame);
-    g.Connectors.Add(cl);
-    g.Connectors.Add(cr);
-    g.Connectors.Add(ct);
-    g.Connectors.Add(cb);
- 
+    g.AddShape(label);
+    g.AddShape(frame);
+    g.AddConnectorAsNone(cl);
+    g.AddConnectorAsNone(cr);
+    g.AddConnectorAsNone(ct);
+    g.AddConnectorAsNone(cb);
+
     return g;
 }
 
@@ -323,54 +261,139 @@ XGroup CreateTimerOn()
     var ct = XPoint.Create(15, 0, ps, "T");
     var cb = XPoint.Create(15, 30, ps, "B");
 
-    var prefixProperty = ShapeProperty.Create("T=");
-    var delayProperty = ShapeProperty.Create("1");
-    var unitProperty = ShapeProperty.Create("s");
-    g.AddProperty("Prefix", prefixProperty);
-    g.AddProperty("Delay", delayProperty);
-    g.AddProperty("Unit", unitProperty);
+    var prefixProperty = ShapeProperty.Create("Prefix", "T=");
+    var delayProperty = ShapeProperty.Create("Delay", "1");
+    var unitProperty = ShapeProperty.Create("Unit", "s");
+    g.AddProperty(prefixProperty);
+    g.AddProperty(delayProperty);
+    g.AddProperty(unitProperty);
     label.Properties = new [] { prefixProperty, delayProperty, unitProperty };
 
-    var t0Property = ShapeProperty.Create("T");
-    g.AddProperty("T0", t0Property);
+    var t0Property = ShapeProperty.Create("T0", "T");
+    g.AddProperty(t0Property);
     t0.Properties = new [] { t0Property };
     
-    var t1Property = ShapeProperty.Create("0");
-    g.AddProperty("T1", t1Property);
+    var t1Property = ShapeProperty.Create("T1", "0");
+    g.AddProperty(t1Property);
     t1.Properties = new [] { t1Property };
 
-    SetShapeState(label, g);
-    SetShapeState(t0, g);
-    SetShapeState(t1, g);
-    SetShapeState(frame, g);
-    SetShapeState(l0, g);
-    SetShapeState(l1, g);
-    SetShapeState(l2, g);
-    SetConnectorAsNone(cl, g);
-    SetConnectorAsNone(cr, g);
-    SetConnectorAsNone(ct, g);
-    SetConnectorAsNone(cb, g);
-        
-    g.Shapes.Add(label);
-    g.Shapes.Add(t0);
-    g.Shapes.Add(t1);
-    g.Shapes.Add(frame);
-    g.Shapes.Add(l0);
-    g.Shapes.Add(l1);
-    g.Shapes.Add(l2);
-    g.Connectors.Add(cl);
-    g.Connectors.Add(cr);
-    g.Connectors.Add(ct);
-    g.Connectors.Add(cb);
+    g.AddShape(label);
+    g.AddShape(t0);
+    g.AddShape(t1);
+    g.AddShape(frame);
+    g.AddShape(l0);
+    g.AddShape(l1);
+    g.AddShape(l2);
+    g.AddConnectorAsNone(cl);
+    g.AddConnectorAsNone(cr);
+    g.AddConnectorAsNone(ct);
+    g.AddConnectorAsNone(cb);
 
     return g;
 }
 
-c.Groups.Add(CreateInputSignal());
-c.Groups.Add(CreateOutputSignal());
-c.Groups.Add(CreateSignal());
-c.Groups.Add(CreateAndGate());
-c.Groups.Add(CreateOrGate());
-c.Groups.Add(CreateXorGate());
-c.Groups.Add(CreateInverterGate());
-c.Groups.Add(CreateTimerOn());
+// TIMER-OFF
+XGroup CreateTimerOff()
+{
+    var g = XGroup.Create("TIMER-OFF");
+
+    var label = XText.Create(-15, -15, 45, 0, styleTextMediumCC, ps, "{0}{1}{2}", false, "");
+    var t0 = XText.Create(0, 3, 15, 18, styleTextMediumCC, ps, "{0}", false, "");
+    var t1 = XText.Create(15, 3, 30, 18, styleTextMediumCC, ps, "{0}", false, "");
+    var frame = XRectangle.Create(0, 0, 30, 30, styleLineThick, ps, false, "");
+    var l0 = XLine.Create(7, 18, 7, 22, styleLineThick, ps);
+    var l1 = XLine.Create(23, 18, 23, 22, styleLineThick, ps);
+    var l2 = XLine.Create(23, 20, 7, 20, styleLineThick, ps);
+    var cl = XPoint.Create(0, 15, ps, "L");
+    var cr = XPoint.Create(30, 15, ps, "R");
+    var ct = XPoint.Create(15, 0, ps, "T");
+    var cb = XPoint.Create(15, 30, ps, "B");
+
+    var prefixProperty = ShapeProperty.Create("Prefix", "T=");
+    var delayProperty = ShapeProperty.Create("Delay", "1");
+    var unitProperty = ShapeProperty.Create("Unit", "s");
+    g.AddProperty(prefixProperty);
+    g.AddProperty(delayProperty);
+    g.AddProperty(unitProperty);
+    label.Properties = new [] { prefixProperty, delayProperty, unitProperty };
+
+    var t0Property = ShapeProperty.Create("T0", "0");
+    g.AddProperty(t0Property);
+    t0.Properties = new [] { t0Property };
+    
+    var t1Property = ShapeProperty.Create("T1", "T");
+    g.AddProperty(t1Property);
+    t1.Properties = new [] { t1Property };
+
+    g.AddShape(label);
+    g.AddShape(t0);
+    g.AddShape(t1);
+    g.AddShape(frame);
+    g.AddShape(l0);
+    g.AddShape(l1);
+    g.AddShape(l2);
+    g.AddConnectorAsNone(cl);
+    g.AddConnectorAsNone(cr);
+    g.AddConnectorAsNone(ct);
+    g.AddConnectorAsNone(cb);
+
+    return g;
+}
+
+// TIMER-PULSE
+XGroup CreateTimerPulse()
+{
+    var g = XGroup.Create("TIMER-PULSE");
+
+    var label = XText.Create(-15, -15, 45, 0, styleTextMediumCC, ps, "{0}{1}{2}", false, "");
+    var t = XText.Create(7, 2, 23, 15, styleTextMediumCC, ps, "{0}", false, "");
+    var frame = XRectangle.Create(0, 0, 30, 30, styleLineThick, ps, false, "");
+    var l0 = XLine.Create(7, 24, 11, 24, styleLineThick, ps);
+    var l1 = XLine.Create(19, 24, 23, 24, styleLineThick, ps);
+    var l2 = XLine.Create(11, 16, 19, 16, styleLineThick, ps);
+    var l3 = XLine.Create(11, 16, 11, 24, styleLineThick, ps);
+    var l4 = XLine.Create(19, 16, 19, 24, styleLineThick, ps);
+
+    var cl = XPoint.Create(0, 15, ps, "L");
+    var cr = XPoint.Create(30, 15, ps, "R");
+    var ct = XPoint.Create(15, 0, ps, "T");
+    var cb = XPoint.Create(15, 30, ps, "B");
+
+    var prefixProperty = ShapeProperty.Create("Prefix", "T=");
+    var delayProperty = ShapeProperty.Create("Delay", "1");
+    var unitProperty = ShapeProperty.Create("Unit", "s");
+    g.AddProperty(prefixProperty);
+    g.AddProperty(delayProperty);
+    g.AddProperty(unitProperty);
+    label.Properties = new [] { prefixProperty, delayProperty, unitProperty };
+
+    var tProperty = ShapeProperty.Create("T", "T");
+    g.AddProperty(tProperty);
+    t.Properties = new [] { tProperty };
+
+    g.AddShape(label);
+    g.AddShape(t);
+    g.AddShape(frame);
+    g.AddShape(l0);
+    g.AddShape(l1);
+    g.AddShape(l2);
+    g.AddShape(l3);
+    g.AddShape(l4);
+    g.AddConnectorAsNone(cl);
+    g.AddConnectorAsNone(cr);
+    g.AddConnectorAsNone(ct);
+    g.AddConnectorAsNone(cb);
+
+    return g;
+}
+
+groups.Add(CreateInputSignal());
+groups.Add(CreateOutputSignal());
+groups.Add(CreateSignal());
+groups.Add(CreateAndGate());
+groups.Add(CreateOrGate());
+groups.Add(CreateXorGate());
+groups.Add(CreateInverterGate());
+groups.Add(CreateTimerOn());
+groups.Add(CreateTimerOff());
+groups.Add(CreateTimerPulse());
