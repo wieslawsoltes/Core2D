aebc6f580904de9cff2c3577818aa000166f3c28
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-05-08 00:11:37 +0200

    Updated controls to use project model

---
+++ Test/Controls/ProjectControl.xaml.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Test.Controls
+{
+    public partial class ProjectControl : UserControl
+    {
+        public ProjectControl()
+        {
+            InitializeComponent();
+        }
+
+        private void TreeViewItem_MouseRightButtonDown(object sender, MouseEventArgs e)
+        {
+            var item = sender as TreeViewItem;
+            if (item != null)
+            {
+                item.IsSelected = true;
+                item.BringIntoView();
+                e.Handled = true;
+            }
+        }
+    }
+}
