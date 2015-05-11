57f5bec46c1623ded2c64f79aadb2df58176690f
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-05-11 18:48:46 +0200

    Added compressors

---
+++ Test/Compressors/LZ4CodecCompressor.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+using LZ4;
+using Test2d;
+
+namespace Test.Compressors
+{
+    public class LZ4CodecCompressor : ICompressor
+    {
+        public byte[] Compress(byte[] data)
+        {
+            return LZ4Codec.Wrap(data, 0, data.Length);
+        }
+
+        public byte[] Decompress(byte[] data)
+        {
+            return LZ4Codec.Unwrap(data, 0);
+        }
+    }
+}
---
+++ Test/Compressors/LZ4StreamCompressor.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+using System.IO;
+using System.IO.Compression;
+using LZ4;
+using Test2d;
+
+namespace Test.Compressors
+{
+    public class LZ4StreamCompressor : ICompressor
+    {
+        public byte[] Compress(byte[] data)
+        {
+            using (var ms = new MemoryStream())
+            {
+                using (var cs = new LZ4Stream(ms, CompressionMode.Compress, true))
+                {
+                    cs.Write(data, 0, data.Length);
+                }
+                return ms.ToArray();
+            }
+        }
+
+        public byte[] Decompress(byte[] data)
+        {
+            using (var ms = new MemoryStream(data))
+            {
+                using (var cs = new LZ4Stream(ms, CompressionMode.Decompress))
+                {
+                    const int size = 4096;
+                    var buffer = new byte[size];
+                    using (var memory = new MemoryStream())
+                    {
+                        int count = 0;
+                        do
+                        {
+                            count = cs.Read(buffer, 0, size);
+                            if (count > 0)
+                            {
+                                memory.Write(buffer, 0, count);
+                            }
+                        }
+                        while (count > 0);
+                        return memory.ToArray();
+                    }
+                }
+            }
+        }
+    }
+}
