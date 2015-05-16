6d07913c334f05c74550a8e00c4d10ba2fad6a46
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-05-16 17:15:43 +0200

    Added FontStyle enum

---
+++ Test2d/Core/FontStyle.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+
+namespace Test2d
+{
+    /// <summary>
+    /// Specifies style information applied to text.
+    /// </summary>
+    [Flags]
+    public enum FontStyle
+    {
+        /// <summary>
+        /// Normal text.
+        /// </summary>
+        Regular = 0,
+        /// <summary>
+        /// Bold text.
+        /// </summary>
+        Bold = 1,
+        /// <summary>
+        /// Italic text.
+        /// </summary>
+        Italic = 2,
+        /// <summary>
+        /// Underlined text.
+        /// </summary>
+        Underline = 4,
+        /// <summary>
+        /// Text with a line through the middle.
+        /// </summary>
+        Strikeout = 8
+    }
+}
