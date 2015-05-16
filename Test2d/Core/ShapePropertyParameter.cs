8b0ac207043885ea7b8b1645ebd2b25ff96d869b
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-05-16 20:19:42 +0200

    Added ShapePropertyParameter class

---
+++ Test2d/Core/ShapePropertyParameter.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+
+namespace Test2d
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ShapePropertyParameter
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public object Owner { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public ShapeProperty Property { get; set; }
+    }
+}
