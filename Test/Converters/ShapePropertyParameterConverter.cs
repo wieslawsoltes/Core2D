e8209acab35008272e9d897b570de74d353752ea
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-05-16 20:19:58 +0200

    Added ShapePropertyParameterConverter class

---
+++ Test/Converters/ShapePropertyParameterConverter.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using Test2d;
+
+namespace Test.Converters
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ShapePropertyParameterConverter : IMultiValueConverter
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="values"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values != null && values.Length == 2)
+            {
+                return new ShapePropertyParameter()
+                {
+                    Owner = values[0],
+                    Property = values[1] as ShapeProperty
+                };
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetTypes"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
