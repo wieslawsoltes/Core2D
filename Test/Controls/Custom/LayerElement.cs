8913869c9dc7399b3b91b309a13b9912630760ac
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-05-19 16:52:51 +0200

    Moved LayerElement class

---
+++ Test/Controls/Custom/LayerElement.cs
@@ -0,0 +1,138 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Test2d;
+
+namespace Test.Controls
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    internal class LayerElement : FrameworkElement
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public static readonly DependencyProperty RendererProperty =
+            DependencyProperty.Register(
+                "Renderer",
+                typeof(IRenderer),
+                typeof(LayerElement),
+                new FrameworkPropertyMetadata(
+                    null,
+                    FrameworkPropertyMetadataOptions.AffectsRender |
+                    FrameworkPropertyMetadataOptions.AffectsMeasure |
+                    FrameworkPropertyMetadataOptions.AffectsArrange |
+                    FrameworkPropertyMetadataOptions.SubPropertiesDoNotAffectRender));
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public IRenderer Renderer
+        {
+            get { return (IRenderer)GetValue(RendererProperty); }
+            set { SetValue(RendererProperty, value); }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private Layer _layer = default(Layer);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public LayerElement()
+        {
+            DataContextChanged += (s, e) => Initialize();
+            Unloaded += (s, e) => DeInitialize();
+
+            RenderOptions.SetBitmapScalingMode(
+                this,
+                BitmapScalingMode.HighQuality);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Invalidate(object sender, InvalidateLayerEventArgs e)
+        {
+            this.InvalidateVisual();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void Initialize()
+        {
+            if (_layer != null)
+            {
+                DeInitialize();
+            }
+
+            var layer = DataContext as Layer;
+            if (layer != null)
+            {
+                _layer = layer;
+                _layer.InvalidateLayer += Invalidate;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void DeInitialize()
+        {
+            if (_layer != null)
+            {
+                _layer.InvalidateLayer -= Invalidate;
+                _layer = default(Layer);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="drawingContext"></param>
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+
+            Render(drawingContext);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="drawingContext"></param>
+        private void Render(DrawingContext drawingContext)
+        {
+            var layer = DataContext as Layer;
+            if (layer != null && layer.IsVisible)
+            {
+                if (Renderer != null)
+                {
+                    Renderer.Draw(
+                        drawingContext, 
+                        layer, 
+                        (Tag as Container).Properties);
+                }
+            }
+        }
+    }
+}
