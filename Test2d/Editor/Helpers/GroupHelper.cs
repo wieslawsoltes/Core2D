b48f7891f738c2feda4d3405b32c1fb269a49266
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-05-20 20:34:11 +0200

    Added Tool helper implementations

---
+++ Test2d/Editor/Helpers/GroupHelper.cs
@@ -0,0 +1,125 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test2d
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class GroupHelper : Helper
+    {
+        private Editor _editor;
+        //private State _currentState = State.None;
+        //private XGroup _shape;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="editor"></param>
+        public GroupHelper(Editor editor)
+        {
+            _editor = editor;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        public override void LeftDown(double x, double y)
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        public override void LeftUp(double x, double y)
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        public override void RightDown(double x, double y)
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        public override void RightUp(double x, double y)
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        public override void Move(double x, double y)
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override void ToStateOne()
+        {
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        public override void ToStateTwo()
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override void ToStateThree()
+        {
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        public override void ToStateFour()
+        {
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="shape"></param>
+        public override void Move(BaseShape shape)
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="shape"></param>
+        public override void Finalize(BaseShape shape)
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override void Remove()
+        {
+        }
+    }
+}
