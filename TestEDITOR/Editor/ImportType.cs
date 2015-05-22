b18a49ac1b87c622e5522e8f2a52665dd27ab6c6
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-05-22 18:08:56 +0200

    Added ImportType enum

---
+++ TestEDITOR/Editor/ImportType.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+
+namespace TestEDITOR
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public enum ImportType
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        Style,
+        /// <summary>
+        /// 
+        /// </summary>
+        Styles,
+        /// <summary>
+        /// 
+        /// </summary>
+        StyleGroup,
+        /// <summary>
+        /// 
+        /// </summary>
+        StyleGroups,
+        /// <summary>
+        /// 
+        /// </summary>
+        Group,
+        /// <summary>
+        /// 
+        /// </summary>
+        Groups,
+        /// <summary>
+        /// 
+        /// </summary>
+        GroupLibrary,
+        /// <summary>
+        /// 
+        /// </summary>
+        GroupLibraries,
+        /// <summary>
+        /// 
+        /// </summary>
+        Template,
+        /// <summary>
+        /// 
+        /// </summary>
+        Templates
+    }
+}
