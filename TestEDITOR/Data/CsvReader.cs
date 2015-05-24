4884ec5854a9e43bffb6dd87e2ba90012d30317b
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-05-25 00:10:20 +0200

    Added CsvReader class

---
+++ TestEDITOR/Data/CsvReader.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using Test2d;
+
+namespace TestEDITOR
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class CsvReader : ITextFieldReader
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public IEnumerable<string[]> Read(string path)
+        {
+            // TextFieldParser
+            // reference: Microsoft.VisualBasic
+            // namespace: Microsoft.VisualBasic.FileIO
+            using (var parser = new TextFieldParser(path))
+            {
+                parser.CommentTokens = new string[] { "#" };
+                parser.SetDelimiters(new string[] { ";" });
+                parser.HasFieldsEnclosedInQuotes = true;
+                while (!parser.EndOfData)
+                {
+                    string[] fields = parser.ReadFields();
+                    yield return fields;
+                }
+            }
+        }
+    }
+}
