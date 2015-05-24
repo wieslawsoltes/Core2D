c8a531bf1ba63f01e144a89b23aa03e51f5bd872
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-05-25 00:09:51 +0200

    Added ITextFieldReader interface

---
+++ Test2d/Data/ITextFieldReader.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+using System.Collections.Generic;
+
+namespace Test2d
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface ITextFieldReader
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        IEnumerable<string[]> Read(string path);
+    }
+}
