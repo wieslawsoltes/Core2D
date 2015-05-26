3d46547e1a98f2254f3857499db9a545dea34394
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-05-26 17:44:07 +0200

    Added CallerMemberName attribute and Update method

--- Test2d/Core/ObservableObject.cs
+++ Test2d/Core/ObservableObject.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 using System;
 using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace Test2d
 {
@@ -19,12 +20,28 @@ namespace Test2d
         /// 
         /// </summary>
         /// <param name="propertyName"></param>
-        public void Notify(string propertyName)
+        public void Notify([CallerMemberName] string propertyName = null)
         {
             var handler = PropertyChanged;
             if (handler != null)
             {
                 handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="field"></param>
+        /// <param name="value"></param>
+        /// <param name="propertyName"></param>
+        public void Update<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (!Equals(field, value))
+            {
+                field = value;
+                Notify(propertyName);
             }
         }
     }
