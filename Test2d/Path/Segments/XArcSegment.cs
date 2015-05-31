5f4192b75d3a2c59ea35b1724fd5d6d731ac7f16
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-05-31 16:32:44 +0200

    Added path segments

---
+++ Test2d/Path/Segments/XArcSegment.cs
@@ -0,0 +1,69 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+
+namespace Test2d
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class XArcSegment : XPathSegment
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public XPathPoint Point { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public XPathSize Size { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public double RotationAngle { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool IsLargeArc { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public XSweepDirection SweepDirection { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="point"></param>
+        /// <param name="size"></param>
+        /// <param name="rotationAngle"></param>
+        /// <param name="isLargeArc"></param>
+        /// <param name="sweepDirection"></param>
+        /// <param name="isStroked"></param>
+        /// <param name="isSmoothJoin"></param>
+        /// <returns></returns>
+        public static XArcSegment Create(
+            XPathPoint point,
+            XPathSize size,
+            double rotationAngle,
+            bool isLargeArc,
+            XSweepDirection sweepDirection,
+            bool isStroked,
+            bool isSmoothJoin)
+        {
+            return new XArcSegment()
+            {
+                Point = point,
+                Size = size,
+                RotationAngle = rotationAngle,
+                IsLargeArc = isLargeArc,
+                SweepDirection = sweepDirection,
+                IsStroked = isStroked,
+                IsSmoothJoin = isSmoothJoin
+            };
+        }
+    }
+}
---
+++ Test2d/Path/Segments/XQuadraticBezierSegment.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+
+namespace Test2d
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class XQuadraticBezierSegment : XPathSegment
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public XPathPoint Point1 { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public XPathPoint Point2 { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="point1"></param>
+        /// <param name="point2"></param>
+        /// <param name="isStroked"></param>
+        /// <param name="isSmoothJoin"></param>
+        /// <returns></returns>
+        public static XQuadraticBezierSegment Create(
+            XPathPoint point1,
+            XPathPoint point2,
+            bool isStroked,
+            bool isSmoothJoin)
+        {
+            return new XQuadraticBezierSegment()
+            {
+                Point1 = point1,
+                Point2 = point2,
+                IsStroked = isStroked,
+                IsSmoothJoin = isSmoothJoin
+            };
+        }
+    }
+}
