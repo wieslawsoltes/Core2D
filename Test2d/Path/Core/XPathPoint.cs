db1aa22c01ea2b6daaa8d1643df98f4565e42523
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-05-31 16:32:29 +0200

    Added path core

---
+++ Test2d/Path/Core/XPathPoint.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+
+namespace Test2d
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class XPathPoint
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public double X { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public double Y { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public static XPathPoint Create(double x, double y)
+        {
+            return new XPathPoint()
+            {
+                X = x,
+                Y = y
+            };
+        }
+    }
+}
