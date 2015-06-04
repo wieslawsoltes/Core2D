20571c4a1a68150016bc86fd59bbe91bc66c5bbd
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-06-04 23:32:51 +0200

    Added Serializer.Newtonsoft project

---
+++ Dependencies/Serializer.Newtonsoft/NewtonsoftSerializer.cs
@@ -0,0 +1,110 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Test2d;
+
+namespace TestEDITOR
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class NewtonsoftSerializer : ISerializer
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public string ToJson<T>(T value)
+        {
+            var settings = new JsonSerializerSettings()
+            {
+                Formatting = Formatting.Indented,
+                TypeNameHandling = TypeNameHandling.Objects,
+                PreserveReferencesHandling = PreserveReferencesHandling.Objects,
+                ReferenceLoopHandling = ReferenceLoopHandling.Serialize,
+                ContractResolver = new ProjectContractResolver()
+            };
+            settings.Converters.Add(new KeyValuePairConverter());
+            var json = JsonConvert.SerializeObject(value, settings);
+            return json;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public byte[] ToBson<T>(T value)
+        {
+            using (var ms = new System.IO.MemoryStream())
+            {
+                using (var writer = new BsonWriter(ms))
+                {
+                    var serializer = new JsonSerializer()
+                    {
+                        TypeNameHandling = TypeNameHandling.Objects,
+                        PreserveReferencesHandling = PreserveReferencesHandling.Objects,
+                        ReferenceLoopHandling = ReferenceLoopHandling.Serialize,
+                        ContractResolver = new ProjectContractResolver()
+                    };
+                    serializer.Serialize(writer, value);
+                }
+                return ms.ToArray();
+            }
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="bson"></param>
+        /// <returns></returns>
+        public T FromBson<T>(byte[] bson) 
+        {
+            using (var ms = new System.IO.MemoryStream(bson))
+            {
+                using (var reader = new BsonReader(ms))
+                {
+                    var serializer = new JsonSerializer()
+                    {
+                        TypeNameHandling = TypeNameHandling.Objects,
+                        PreserveReferencesHandling = PreserveReferencesHandling.Objects,
+                        ReferenceLoopHandling = ReferenceLoopHandling.Serialize,
+                        ContractResolver = new ProjectContractResolver()
+                    };
+                    var value = serializer.Deserialize<T>(reader);
+                    return value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="json"></param>
+        /// <returns></returns>
+        public T FromJson<T>(string json)
+        {
+            var settings = new JsonSerializerSettings()
+            {
+                Formatting = Formatting.Indented,
+                TypeNameHandling = TypeNameHandling.Objects,
+                PreserveReferencesHandling = PreserveReferencesHandling.Objects,
+                ReferenceLoopHandling = ReferenceLoopHandling.Serialize,
+                ContractResolver = new ProjectContractResolver()
+            };
+            settings.Converters.Add(new KeyValuePairConverter());
+            var value = JsonConvert.DeserializeObject<T>(json, settings);
+            return value;
+        }
+    }
+}
---
+++ Dependencies/Serializer.Newtonsoft/ProjectContractResolver.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestEDITOR
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    internal class ProjectContractResolver : DefaultContractResolver
+    {
+        /// <summary>
+        /// Use ImmutableArray for IList contract.
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public override JsonContract ResolveContract(Type type)
+        {
+            if (type.IsGenericType
+                && type.GetGenericTypeDefinition() == typeof(IList<>))
+            {
+                return base
+                    .ResolveContract(typeof(ImmutableArray<>)
+                    .MakeGenericType(type.GenericTypeArguments[0]));
+            }
+            else
+            {
+                return base.ResolveContract(type);
+            }
+        }
+
+        /// <summary>
+        /// Serialize only writable properties. 
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="memberSerialization"></param>
+        /// <returns></returns>
+        protected override IList<JsonProperty> CreateProperties(Type type, Newtonsoft.Json.MemberSerialization memberSerialization)
+        {
+            IList<JsonProperty> props = base.CreateProperties(type, memberSerialization);
+            return props.Where(p => p.Writable).ToList();
+        }
+    }
+}
