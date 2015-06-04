650faf558b1872b6246a2e165ca9f248863d38ef
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-06-04 14:19:28 +0200

    Removed using

--- TestEDITOR/Editor/EditorContext.cs
+++ TestEDITOR/Editor/EditorContext.cs
@@ -7,7 +7,6 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Globalization;
 using System.Linq;
-using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
