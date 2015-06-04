e0ea0426cdc5b0a62114f72dae2ef90fbd5d6d75
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-06-04 13:05:05 +0200

    Added ArgbColorToBrushMultiConverter class

---
+++ Test/Converters/ArgbColorToBrushMultiConverter.cs
@@ -0,0 +1,57 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+using Test2d;
+
+namespace Test.Converters
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ArgbColorToBrushMultiConverter : IMultiValueConverter
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="values"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values != null && values.Length == 4)
+            {
+                var brush = new SolidColorBrush(
+                    Color.FromArgb(
+                        (byte)values[0],
+                        (byte)values[1],
+                        (byte)values[2],
+                        (byte)values[3]));
+                brush.Freeze();
+                return brush;
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetTypes"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
