1073a82dcb92c34ca9d6c587b5e5424bcd60bf91
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-06-04 23:33:37 +0200

    Added TextFieldWriter.CsvHelper project

---
+++ Dependencies/TextFieldWriter.CsvHelper/CsvHelperWriter.cs
@@ -0,0 +1,62 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+using System.Globalization;
+using Test2d;
+
+namespace TestEDITOR
+{
+    public class CsvHelperWriter : ITextFieldWriter
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="database"></param>
+        public void Write(string path, Database database)
+        {
+            try
+            {
+                using (var writer = new System.IO.StringWriter())
+                {
+                    var configuration = new CsvHelper.Configuration.CsvConfiguration();
+
+                    configuration.Delimiter = CultureInfo.CurrentCulture.TextInfo.ListSeparator;
+                    configuration.CultureInfo = CultureInfo.CurrentCulture;
+
+                    using (var csv = new CsvHelper.CsvWriter(writer, configuration))
+                    {
+                        // columns
+                        csv.WriteField("Id");
+                        foreach (var column in database.Columns)
+                        {
+                            csv.WriteField(column.Name);
+                        }
+                        csv.NextRecord();
+
+                        // records
+                        foreach (var record in database.Records)
+                        {
+                            csv.WriteField(record.Id.ToString());
+                            foreach (var value in record.Values)
+                            {
+                                csv.WriteField(value.Content);
+                            }
+                            csv.NextRecord();
+                        }
+                    }
+
+                    using (var file = System.IO.File.CreateText(path))
+                    {
+                        file.Write(writer.ToString());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.Print(ex.Message);
+                System.Diagnostics.Debug.Print(ex.StackTrace);
+            }
+        }
+    }
+}
