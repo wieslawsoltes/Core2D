c88ab429a9c17a35edaf3fb178eb46ac865e4cbe
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-06-04 23:29:29 +0200

    Added FileWriter.Dxf project

---
+++ Dependencies/FileWriter.Dxf/DxfWriter.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+using Test2d;
+
+namespace TestEDITOR
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class DxfWriter : IFileWriter
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="item"></param>
+        /// <param name="options"></param>
+        public void Save(string path, object item, object options)
+        {
+            if (string.IsNullOrEmpty(path) || item == null)
+                return;
+
+            var renderer = new DxfRenderer()
+            {
+                DrawShapeState = ShapeState.Printable
+            };
+
+            if (item is Container)
+            {
+                renderer.Save(path, item as Container, (Dxf.DxfAcadVer)options);
+            }
+        }
+    }
+}
