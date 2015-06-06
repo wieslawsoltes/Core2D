1376d8ce9105458336cc85417e818fcb5a4caa43
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-06-07 00:45:54 +0200

    Make owner argument mandatory

--- Test2d/Editor/Project/Layer.cs
+++ Test2d/Editor/Project/Layer.cs
@@ -90,7 +90,7 @@ namespace Test2d
         /// <param name="owner"></param>
         /// <param name="isVisible"></param>
         /// <returns></returns>
-        public static Layer Create(string name, Container owner = null, bool isVisible = true)
+        public static Layer Create(string name, Container owner, bool isVisible = true)
         {
             return new Layer()
             {
