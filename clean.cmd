6ce9fc11cc84e586668168f6c0093b2a9e6a1588
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-06-08 15:36:21 +0200

    Added clean batch script

---
+++ clean.cmd
@@ -0,0 +1,62 @@
+@echo off
+
+rmdir /Q /S Dependencies\Compressor.GZip\bin
+rmdir /Q /S Dependencies\Compressor.GZip\obj
+
+rmdir /Q /S Dependencies\Compressor.LZ4\bin
+rmdir /Q /S Dependencies\Compressor.LZ4\obj
+
+rmdir /Q /S Dependencies\FileWriter.Dxf\bin
+rmdir /Q /S Dependencies\FileWriter.Dxf\obj
+
+rmdir /Q /S Dependencies\FileWriter.Emf\bin
+rmdir /Q /S Dependencies\FileWriter.Emf\obj
+
+rmdir /Q /S Dependencies\FileWriter.Pdf-wpf\bin
+rmdir /Q /S Dependencies\FileWriter.Pdf-wpf\obj
+
+rmdir /Q /S Dependencies\Renderer.Dxf\bin
+rmdir /Q /S Dependencies\Renderer.Dxf\obj
+
+rmdir /Q /S Dependencies\Renderer.EtoForms\bin
+rmdir /Q /S Dependencies\Renderer.EtoForms\obj
+
+rmdir /Q /S Dependencies\Renderer.PdfSharp-wpf\bin
+rmdir /Q /S Dependencies\Renderer.PdfSharp-wpf\obj
+
+rmdir /Q /S Dependencies\Renderer.WinForms\bin
+rmdir /Q /S Dependencies\Renderer.WinForms\obj
+
+rmdir /Q /S Dependencies\Renderer.Wpf\bin
+rmdir /Q /S Dependencies\Renderer.Wpf\obj
+
+rmdir /Q /S Dependencies\ScriptEngine.Roslyn\bin
+rmdir /Q /S Dependencies\ScriptEngine.Roslyn\obj
+
+rmdir /Q /S Dependencies\Serializer.Newtonsoft\bin
+rmdir /Q /S Dependencies\Serializer.Newtonsoft\obj
+
+rmdir /Q /S Dependencies\TextFieldReader.VisualBasic\bin
+rmdir /Q /S Dependencies\TextFieldReader.VisualBasic\obj
+
+rmdir /Q /S Dependencies\TextFieldWriter.CsvHelper\bin
+rmdir /Q /S Dependencies\TextFieldWriter.CsvHelper\obj
+
+rmdir /Q /S Test\bin
+rmdir /Q /S Test\obj
+
+rmdir /Q /S Test2d\bin
+rmdir /Q /S Test2d\obj
+
+rmdir /Q /S TestDirect2D\bin
+rmdir /Q /S TestDirect2D\obj
+
+rmdir /Q /S TestEtoForms\bin
+rmdir /Q /S TestEtoForms\obj
+
+rmdir /Q /S TestWinForms\bin
+rmdir /Q /S TestWinForms\obj
+
+rmdir /Q /S packages
+
+pause
