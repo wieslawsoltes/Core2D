5e8a241b44e651e7185040a299671e9eea508c83
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-06-15 00:13:32 +0200

    Added ICodeEngine interface

---
+++ Test2d.Interfaces/Interfaces/ICodeEngine.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+using System.Collections.Immutable;
+
+namespace Test2d
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface ICodeEngine
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="shapes"></param>
+        /// <param name="context"></param>
+        void Build(ImmutableArray<BaseShape> shapes, object context);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        void Run();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        void Reset();
+    }
+}
