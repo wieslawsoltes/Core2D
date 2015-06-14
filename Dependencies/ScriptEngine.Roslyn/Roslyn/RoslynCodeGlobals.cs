5e92eb7180a23eda10a865f8920a2e61f180a9c2
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-06-15 01:59:48 +0200

    Renamed class

---
+++ Dependencies/ScriptEngine.Roslyn/Roslyn/RoslynCodeGlobals.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Test2d;
+
+namespace Test2d
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class RoslynCodeGlobals
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public EditorContext Context;
+        /// <summary>
+        /// 
+        /// </summary>
+        public BaseShape[] Shapes;
+        /// <summary>
+        /// 
+        /// </summary>
+        public object State;
+    }
+}
