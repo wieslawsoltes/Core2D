06b65d5fe47db68efa041528af2e6fdbf76ae918
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-06-16 22:24:40 +0200

    Added support for persistent Data

--- Dependencies/ScriptEngine.Roslyn/Roslyn/RoslynCodeEngine.cs
+++ Dependencies/ScriptEngine.Roslyn/Roslyn/RoslynCodeEngine.cs
@@ -6,6 +6,7 @@ using Microsoft.CodeAnalysis.Scripting.CSharp;
 using System;
 using System.Collections.Immutable;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -35,35 +36,81 @@ namespace Test2d
         public void Build(ImmutableArray<BaseShape> shapes, object context)
         {
             _options = Helpers.GetOptions();
-            _shapes = shapes.Where(s => s.IsExecutable && !string.IsNullOrEmpty(s.Code)).ToArray();
+            _shapes = shapes.Where(s => s.IsExecutable).ToArray();
 
             _globals = new RoslynCodeGlobals<object>()
             {
                 Context = context as EditorContext,
                 Shapes = _shapes,
-                States = new object[_shapes.Length]
+                Runners = new CodeRunner[_shapes.Length]
             };
 
+            var sw = Stopwatch.StartNew();
+
             // merge all shapes code as one big script
-            var sb = new StringBuilder();
+            
             for (int i = 0; i < _shapes.Length; i++)
             {
-                // wrap shape Code in a block and define Shape variable as its own type
-                sb.AppendLine("{");
-                sb.AppendLine(string.Concat("var Id = ", i, ";"));
-                sb.AppendLine(string.Concat("var State = States[", i, "];"));
-                sb.AppendLine(string.Concat("var Shape = Shapes[", i, "] as ", _shapes[i].GetType().Name, ";"));
-                sb.AppendLine(_shapes[i].Code);
-                sb.AppendLine("}");
-            }
-            _code = sb.ToString();
+                // wrap shape Code and Data in a block and define Shape variable as its own type
+                var sb = new StringBuilder();
 
+                // class name
+                var name = string.Concat("Runner_", Guid.NewGuid().ToString("N").ToUpper());
+
+                // begin class
+                sb.AppendLine(string.Concat("public class ", name, " : CodeRunner"));
+                sb.AppendLine("{");
+
+                // shape property
+                sb.AppendLine(string.Concat("public ", name, "()"));
+                sb.AppendLine("{");
+                sb.AppendLine("}");
+
+                // shape data
+                if (!string.IsNullOrWhiteSpace(_shapes[i].Data))
+                {
+                    sb.AppendLine(_shapes[i].Data);
+                }
+
+                // begin run method
+                sb.AppendLine("public override void Run(int id, BaseShape[] shapes, EditorContext context)");
+                sb.AppendLine("{");
+
+                // define local shape variable with proper type
+                var type = _shapes[i].GetType().Name;
+                sb.AppendLine(string.Concat("var shape = shapes[id] as ", type, ";"));
+
+                // shape code
+                if (!string.IsNullOrWhiteSpace(_shapes[i].Code))
+                {
+                    sb.AppendLine(_shapes[i].Code);
+                }
+
+                // end run method
+                sb.AppendLine("}");
+
+                // end class
+                sb.AppendLine("}");
+
+                // create runner
+                sb.AppendLine(string.Concat("new ", name, "();"));
+
+                // evaluate shape code and set runner
+                var code = sb.ToString();
+
+                //Debug.Print(code);
+
+                var runner = CSharpScript.Eval(code, _options, _globals);
+                _globals.Runners[i] = runner as CodeRunner;
+            }
+
+            _code = "for (int i = 0; i < Runners.Length; i++) Runners[i].Run(i, Shapes, Context);";
             _runner = CSharpScript.Create(_code, _options)
                 .WithGlobalsType(typeof(RoslynCodeGlobals<object>))
                 .CreateDelegate();
 
-            // initialize States before first tick
-            _runner(_globals);
+            sw.Stop();
+            Debug.Print("Build: " + sw.Elapsed.TotalMilliseconds + "ms");
 
             _haveRunner = true;
         }
@@ -73,7 +120,12 @@ namespace Test2d
             if (!_haveRunner)
                 return;
 
+            var sw = Stopwatch.StartNew();
+
             _runner(_globals);
+
+            sw.Stop();
+            Debug.Print("Run: " + sw.Elapsed.TotalMilliseconds + "ms");
         }
 
         public void Reset()
