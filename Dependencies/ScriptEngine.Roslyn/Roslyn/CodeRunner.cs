10bef9181eb6eb2db7901e3d4fed429c2943227f
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-06-16 22:23:38 +0200

    Added CodeRunner class

---
+++ Dependencies/ScriptEngine.Roslyn/Roslyn/CodeRunner.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+
+namespace Test2d
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public abstract class CodeRunner
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="shapes"></param>
+        /// <param name="context"></param>
+        public abstract void Run(int id, BaseShape[] shapes, EditorContext context);
+    }
+}
