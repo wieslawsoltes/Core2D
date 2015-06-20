02ba2cb4e74c83c050e87325b87a7eeca5993a6b
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-06-20 18:43:12 +0200

    Added live code rebuild to engine

--- Dependencies/ScriptEngine.Roslyn/Roslyn/RoslynCodeEngine.cs
+++ Dependencies/ScriptEngine.Roslyn/Roslyn/RoslynCodeEngine.cs
@@ -4,15 +4,16 @@ using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Scripting;
 using Microsoft.CodeAnalysis.Scripting.CSharp;
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
 using System.Text;
-using Dxf;
 using Test2d;
-using TestSIM;
 
 namespace Test2d
 {
@@ -25,8 +26,150 @@ namespace Test2d
         private BaseShape[] _shapes;
         private RoslynCodeGlobals _globals;
         private string _code;
-        private bool _haveRunner;
-        private ScriptRunner _runner;
+        private bool _haveRunners;
+        private ScriptRunner _runners;
+        private IDictionary<ShapeCode, int> _ids;
+
+        private void FormatRunner(BaseShape shape, int id, StringBuilder sb)
+        {
+            // wrap shape Code into class object:
+
+            // public class Runner_{guid} : CodeRunner
+            // {
+            //     {Code.Definitions}
+            //
+            //     public Runner_{guid}(int id, BaseShape[] shapes, EditorContext context)
+            //     {
+            //         var shape = shapes[id] as {type};
+            //         {Code.Initialization}
+            //     }
+            //
+            //     public override void Run(int id, BaseShape[] shapes, EditorContext context)
+            //     {
+            //         var shape = shapes[id] as {type};
+            //         {Code.Script}
+            //     }
+            // }
+
+            // class name
+            var name = string.Concat("Runner_", Guid.NewGuid().ToString("N").ToUpper());
+            var type = shape.GetType().Name;
+            var code = shape.Code;
+
+            // begin class
+            sb.AppendLine(string.Concat("public class ", name, " : CodeRunner"));
+            sb.AppendLine("{");
+
+            // script definitions
+            if (!string.IsNullOrWhiteSpace(code.Definitions))
+            {
+                sb.AppendLine(code.Definitions);
+            }
+
+            // script initialization
+            sb.AppendLine(string.Concat("public ", name, "(int id, BaseShape[] shapes, EditorContext context)"));
+            sb.AppendLine("{");
+            if (!string.IsNullOrWhiteSpace(code.Initialization))
+            {
+                // define shape variable with proper type in constructor
+                sb.AppendLine(string.Concat("var shape = shapes[id] as ", type, ";"));
+                sb.AppendLine(code.Initialization);
+            }
+            sb.AppendLine("}");
+
+            // begin run method
+            sb.AppendLine("public override void Run(int id, BaseShape[] shapes, EditorContext context)");
+            sb.AppendLine("{");
+
+            // define shape variable with proper type in Run method
+            sb.AppendLine(string.Concat("var shape = shapes[id] as ", type, ";"));
+
+            // script code
+            if (!string.IsNullOrWhiteSpace(code.Script))
+            {
+                sb.AppendLine(code.Script);
+            }
+
+            // end run method
+            sb.AppendLine("}");
+
+            // end class
+            sb.AppendLine("}");
+
+            // create runner
+            sb.AppendLine(string.Concat("Runners[", id, "] = ", "new ", name, "(", id, ", Shapes, Context);"));
+        }
+
+        private void BuildRunner(ShapeCode code)
+        {
+            var sb = new StringBuilder();
+            var id = _ids[code];
+            var shape = _shapes[id];
+
+            FormatRunner(shape, id, sb);
+
+            var runner = sb.ToString();
+            CSharpScript.Eval(runner, _options, _globals);
+        }
+
+        private void CodeObserver(object sender, PropertyChangedEventArgs e)
+        {
+            var code = sender as ShapeCode;
+            if (code != null)
+            {
+                try
+                {
+                    var sw = Stopwatch.StartNew();
+
+                    BuildRunner(code);
+
+                    sw.Stop();
+                    Debug.Print("Build: " + sw.Elapsed.TotalMilliseconds + "ms");
+                }
+                catch (Exception ex)
+                {
+                    Debug.Print(ex.Message);
+                    Debug.Print(ex.StackTrace);
+                }
+            }
+        }
+
+        private void StartObservingCode(BaseShape shape)
+        {
+            shape.Code.PropertyChanged += CodeObserver;
+        }
+
+        private void StopObservingCode(BaseShape shape)
+        {
+            shape.Code.PropertyChanged -= CodeObserver;
+        }
+
+        private void BuildRunners()
+        {
+            // merge all shapes code as one big script
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < _shapes.Length; i++)
+            {
+                var shape = _shapes[i];
+                var id = i;
+                _ids.Add(shape.Code, id);
+
+                FormatRunner(shape, id, sb);
+                StartObservingCode(shape);
+            }
+
+            // compile runners
+            var runners = sb.ToString();
+            //Debug.Print(runners);
+            CSharpScript.Eval(runners, _options, _globals);
+
+            // create main runner script
+            _code = "for (int i = 0; i < Runners.Length; i++) if (Shapes[i].Code.IsExecutable) Runners[i].Run(i, Shapes, Context);";
+            _runners = CSharpScript.Create(_code, _options)
+                .WithGlobalsType(typeof(RoslynCodeGlobals))
+                .CreateDelegate();
+        }
 
         /// <summary>
         /// 
@@ -45,117 +188,54 @@ namespace Test2d
                 Runners = new CodeRunner[_shapes.Length]
             };
 
+            _ids = new Dictionary<ShapeCode, int>();
+
             var sw = Stopwatch.StartNew();
 
-            // merge all shapes code as one big script
-            var sb = new StringBuilder();
-
-            for (int i = 0; i < _shapes.Length; i++)
-            {
-                // wrap shape Code in class object:
-                //
-                // public class Runner_{guid} : CodeRunner
-                // {
-                //     {Code.Definitions}
-                //
-                //     public Runner_{guid}(int id, BaseShape[] shapes, EditorContext context)
-                //     {
-                //         var shape = shapes[id] as {type};
-                //         {Code.Initialization}
-                //     }
-                //
-                //     public override void Run(int id, BaseShape[] shapes, EditorContext context)
-                //     {
-                //         var shape = shapes[id] as {type};
-                //         {Code.Script}
-                //     }
-                // }
-                
-                // class name
-                var name = string.Concat("Runner_", Guid.NewGuid().ToString("N").ToUpper());
-                var type = _shapes[i].GetType().Name;
-
-                // begin class
-                sb.AppendLine(string.Concat("public class ", name, " : CodeRunner"));
-                sb.AppendLine("{");
-
-                // script definitions
-                if (!string.IsNullOrWhiteSpace(_shapes[i].Code.Definitions))
-                {
-                    sb.AppendLine(_shapes[i].Code.Definitions);
-                }
-
-                // script initialization
-                sb.AppendLine(string.Concat("public ", name, "(int id, BaseShape[] shapes, EditorContext context)"));
-                sb.AppendLine("{");
-                if (!string.IsNullOrWhiteSpace(_shapes[i].Code.Initialization))
-                {
-                    // define shape variable with proper type in constructor
-                    sb.AppendLine(string.Concat("var shape = shapes[id] as ", type, ";"));
-                    sb.AppendLine(_shapes[i].Code.Initialization);
-                }
-                sb.AppendLine("}");
-
-                // begin run method
-                sb.AppendLine("public override void Run(int id, BaseShape[] shapes, EditorContext context)");
-                sb.AppendLine("{");
-
-                // define shape variable with proper type in Run method
-                sb.AppendLine(string.Concat("var shape = shapes[id] as ", type, ";"));
-
-                // script code
-                if (!string.IsNullOrWhiteSpace(_shapes[i].Code.Script))
-                {
-                    sb.AppendLine(_shapes[i].Code.Script);
-                }
-
-                // end run method
-                sb.AppendLine("}");
-
-                // end class
-                sb.AppendLine("}");
-
-                // create runner
-                sb.AppendLine(string.Concat("Runners[", i, "] = ", "new ", name, "(", i, ", Shapes, Context);"));
-            }
-
-            // compile runners
-            var runners = sb.ToString();
-            //Debug.Print(runners);
-            CSharpScript.Eval(runners, _options, _globals);
-
-            // create main runner script
-            _code = "for (int i = 0; i < Runners.Length; i++) if (Shapes[i].Code.IsExecutable) Runners[i].Run(i, Shapes, Context);";
-            _runner = CSharpScript.Create(_code, _options)
-                .WithGlobalsType(typeof(RoslynCodeGlobals))
-                .CreateDelegate();
+            BuildRunners();
 
             sw.Stop();
             Debug.Print("Build: " + sw.Elapsed.TotalMilliseconds + "ms");
 
-            _haveRunner = true;
+            _haveRunners = true;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public void Run()
         {
-            if (!_haveRunner)
+            if (!_haveRunners)
                 return;
 
             var sw = Stopwatch.StartNew();
 
-            _runner(_globals);
+            _runners(_globals);
 
             sw.Stop();
             Debug.Print("Run: " + sw.Elapsed.TotalMilliseconds + "ms");
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public void Reset()
         {
-            _haveRunner = false;
+            if (_shapes != null)
+            {
+                for (int i = 0; i < _shapes.Length; i++)
+                {
+                    StopObservingCode(_shapes[i]);
+                }
+            }
+
+            _haveRunners = false;
             _options = null;
             _shapes = null;
             _globals = null;
             _code = null;
+            _runners = null;
+            _ids = null;
         }
     }
 }
