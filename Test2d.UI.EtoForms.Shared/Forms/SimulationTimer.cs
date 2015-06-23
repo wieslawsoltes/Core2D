05ab0f4df6e22e271b7482cd54cb87747a337dd4
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-06-23 17:50:21 +0200

    Added Copyright header

--- Test2d.UI.EtoForms.Shared/Forms/SimulationTimer.cs
+++ Test2d.UI.EtoForms.Shared/Forms/SimulationTimer.cs
@@ -1,4 +1,6 @@
-﻿using Eto.Forms;
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using Eto.Forms;
 using System;
 using Test2d;
 
