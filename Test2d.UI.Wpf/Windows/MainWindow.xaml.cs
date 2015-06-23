c66d11e1f8bb8b5ea00f27d7a44263485a15c868
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-06-23 18:01:50 +0200

    Moved SimulationTimer

--- Test2d.UI.Wpf/Windows/MainWindow.xaml.cs
+++ Test2d.UI.Wpf/Windows/MainWindow.xaml.cs
@@ -24,41 +24,6 @@ using Xceed.Wpf.AvalonDock.Layout.Serialization;
 namespace Test.Windows
 {
     /// <summary>
-    /// Wrapper class for System.Threading.Timer timer class.
-    /// </summary>
-    internal class SimulationTimer : ISimulationTimer
-    {
-        private System.Threading.Timer _timer = default(System.Threading.Timer);
-
-        /// <summary>
-        /// 
-        /// </summary>
-        public bool IsRunning
-        {
-            get { return _timer != null; }
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="callback"></param>
-        /// <param name="period"></param>
-        public void Start(Action callback, int period)
-        {
-            _timer = new System.Threading.Timer((_) => callback(), null, 0, period);
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        public void Stop()
-        {
-            _timer.Dispose();
-            _timer = default(System.Threading.Timer);
-        }
-    }
-
-    /// <summary>
     /// 
     /// </summary>
     public partial class MainWindow : Window, IView
