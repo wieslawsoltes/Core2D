00d17899c08b13743a5f91c7ab1f91e19ce04ad6
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-06-23 17:38:22 +0200

    Moved TextClipboard

--- Test2d.UI.WinForms/Forms/MainForm.cs
+++ Test2d.UI.WinForms/Forms/MainForm.cs
@@ -434,37 +434,4 @@ namespace TestWinForms
             _timer = default(System.Threading.Timer);
         }
     }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    internal class TextClipboard : ITextClipboard
-    {
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="text"></param>
-        public void SetText(string text)
-        {
-            Clipboard.SetText(text, TextDataFormat.UnicodeText);
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <returns></returns>
-        public string GetText()
-        {
-            return Clipboard.GetText(TextDataFormat.UnicodeText);
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <returns></returns>
-        public bool ContainsText()
-        {
-            return Clipboard.ContainsText(TextDataFormat.UnicodeText);
-        }
-    }
 }
---
+++ Test2d.UI.WinForms/Forms/TextClipboard.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+using System.Windows.Forms;
+using Test2d;
+
+namespace TestWinForms
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    internal class TextClipboard : ITextClipboard
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="text"></param>
+        public void SetText(string text)
+        {
+            Clipboard.SetText(text, TextDataFormat.UnicodeText);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public string GetText()
+        {
+            return Clipboard.GetText(TextDataFormat.UnicodeText);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public bool ContainsText()
+        {
+            return Clipboard.ContainsText(TextDataFormat.UnicodeText);
+        }
+    }
+}
