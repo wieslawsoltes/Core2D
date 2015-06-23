e2de75e5d8a5522361ba9a7ab39d9fd94d5b9c39
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-06-23 17:38:34 +0200

    Removed unused usings

--- Test2d.UI.WinForms/Forms/ContainerPanel.cs
+++ Test2d.UI.WinForms/Forms/ContainerPanel.cs
@@ -1,11 +1,8 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Drawing2D;
-using System.Drawing.Imaging;
 using System.Drawing.Text;
 using System.Windows.Forms;
 using Test2d;
