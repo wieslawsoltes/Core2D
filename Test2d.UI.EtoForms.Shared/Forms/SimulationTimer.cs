fef5d3dc88991da56761cfed45e11941bddffbf4
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-06-23 17:48:00 +0200

    Moved SimulationTimer

---
+++ Test2d.UI.EtoForms.Shared/Forms/SimulationTimer.cs
@@ -0,0 +1,44 @@
+﻿using Eto.Forms;
+using System;
+using Test2d;
+
+namespace TestEtoForms
+{
+    /// <summary>
+    /// Wrapper class for Eto.Forms.UITimer timer class.
+    /// </summary>
+    internal class SimulationTimer : ISimulationTimer
+    {
+        private UITimer _timer;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool IsRunning
+        {
+            get { return _timer != null; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="callback"></param>
+        /// <param name="period"></param>
+        public void Start(Action callback, int period)
+        {
+            _timer = new UITimer { Interval = period / 1000.0 };
+            _timer.Elapsed += delegate { callback(); };
+            _timer.Start();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Stop()
+        {
+            _timer.Stop();
+            _timer.Dispose();
+            _timer = default(UITimer);
+        }
+    }
+}
