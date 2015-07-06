ddf1b8b58a52625d52c1d6aa606f0f6bc48e42a1
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-07-06 18:53:47 +0200

    Removed unused code

--- Test2d.Editor/Editor/Helpers/RectangleHelper.cs
+++ Test2d.Editor/Editor/Helpers/RectangleHelper.cs
@@ -120,8 +120,6 @@ namespace Test2d
         /// <param name="y"></param>
         public override void LeftUp(double x, double y)
         {
-            double sx = _editor.Project.Options.SnapToGrid ? Editor.Snap(x, _editor.Project.Options.SnapX) : x;
-            double sy = _editor.Project.Options.SnapToGrid ? Editor.Snap(y, _editor.Project.Options.SnapY) : y;
         }
 
         /// <summary>
@@ -156,8 +154,6 @@ namespace Test2d
         /// <param name="y"></param>
         public override void RightUp(double x, double y)
         {
-            double sx = _editor.Project.Options.SnapToGrid ? Editor.Snap(x, _editor.Project.Options.SnapX) : x;
-            double sy = _editor.Project.Options.SnapToGrid ? Editor.Snap(y, _editor.Project.Options.SnapY) : y;
         }
 
         /// <summary>
