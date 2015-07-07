26e4f650ac2a83155722e925e0ecf3cb8755ff2e
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-07-07 19:42:44 +0200

    Added GetAllPoints method and point draw support

--- Test2d/Shapes/XPath.cs
+++ Test2d/Shapes/XPath.cs
@@ -55,6 +55,76 @@ namespace Test2d
         /// <summary>
         /// 
         /// </summary>
+        /// <returns></returns>
+        public ImmutableArray<XPoint> GetAllPoints()
+        {
+            var builder = ImmutableArray.CreateBuilder<XPoint>();
+
+            if (this.Geometry != null)
+            {
+                foreach (var figure in this.Geometry.Figures)
+                {
+                    builder.Add(figure.StartPoint);
+
+                    foreach (var segment in figure.Segments)
+                    {
+                        if (segment is XArcSegment)
+                        {
+                            var arcSegment = segment as XArcSegment;
+                            builder.Add(arcSegment.Point);
+                        }
+                        else if (segment is XBezierSegment)
+                        {
+                            var bezierSegment = segment as XBezierSegment;
+                            builder.Add(bezierSegment.Point1);
+                            builder.Add(bezierSegment.Point2);
+                            builder.Add(bezierSegment.Point3);
+                        }
+                        else if (segment is XLineSegment)
+                        {
+                            var lineSegment = segment as XLineSegment;
+                            builder.Add(lineSegment.Point);
+                        }
+                        else if (segment is XPolyBezierSegment)
+                        {
+                            var polyBezierSegment = segment as XPolyBezierSegment;
+                            foreach (var point in polyBezierSegment.Points)
+                            {
+                                builder.Add(point);
+                            }
+                        }
+                        else if (segment is XPolyLineSegment)
+                        {
+                            var polyLineSegment = segment as XPolyLineSegment;
+                            foreach (var point in polyLineSegment.Points)
+                            {
+                                builder.Add(point);
+                            }
+                        }
+                        else if (segment is XPolyQuadraticBezierSegment)
+                        {
+                            var polyQuadraticSegment = segment as XPolyQuadraticBezierSegment;
+                            foreach (var point in polyQuadraticSegment.Points)
+                            {
+                                builder.Add(point);
+                            }
+                        }
+                        else if (segment is XQuadraticBezierSegment)
+                        {
+                            var qbezierSegment = segment as XQuadraticBezierSegment;
+                            builder.Add(qbezierSegment.Point1);
+                            builder.Add(qbezierSegment.Point2);
+                        }
+                    }
+                }
+            }
+
+            return builder.ToImmutable();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
         /// <param name="dc"></param>
         /// <param name="renderer"></param>
         /// <param name="dx"></param>
@@ -68,6 +138,41 @@ namespace Test2d
             if (State.HasFlag(ShapeState.Visible))
             {
                 renderer.Draw(dc, this, dx, dy, db, record);
+            }
+
+            if (renderer.State.SelectedShape != null)
+            {
+                if (this == renderer.State.SelectedShape)
+                {
+                    var points = this.GetAllPoints();
+                    foreach (var point in points)
+                    {
+                        point.Draw(dc, renderer, dx, dy, db, record);
+                    }
+                }
+                else
+                {
+                    var points = this.GetAllPoints();
+                    foreach (var point in points)
+                    {
+                        if (point == renderer.State.SelectedShape)
+                        {
+                            point.Draw(dc, renderer, dx, dy, db, record);
+                        }
+                    }
+                }
+            }
+
+            if (renderer.State.SelectedShapes != null)
+            {
+                if (renderer.State.SelectedShapes.Contains(this))
+                {
+                    var points = this.GetAllPoints();
+                    foreach (var point in points)
+                    {
+                        point.Draw(dc, renderer, dx, dy, db, record);
+                    }
+                }
             }
         }
 
