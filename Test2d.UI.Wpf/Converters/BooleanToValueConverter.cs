8418ec489f87f633a000efb543e5bf79b6edac44
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-07-10 23:15:50 +0200

    Added BooleanToValueConverter class

---
+++ Test2d.UI.Wpf/Converters/BooleanToValueConverter.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+using Test2d;
+
+namespace Test.Converters
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class BooleanToValueConverter<T> : IValueConverter
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public T FalseValue { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public T TrueValue { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return FalseValue;
+            else
+                return (bool)value ? TrueValue : FalseValue;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value != null ? value.Equals(TrueValue) : false;
+        }
+    }
+}
