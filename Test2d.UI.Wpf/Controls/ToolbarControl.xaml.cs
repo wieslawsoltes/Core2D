4ddb4fec3cc739d06f5dffddd6090ad588dba24a
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-07-18 18:40:10 +0200

    Formatted code

--- Test2d.UI.Wpf/Controls/ToolbarControl.xaml.cs
+++ Test2d.UI.Wpf/Controls/ToolbarControl.xaml.cs
@@ -37,12 +37,14 @@ namespace Test.Controls
         /// <param name="e"></param>
         private void ToolBar_Loaded(object sender, RoutedEventArgs e)
         {
-            ToolBar toolBar = sender as ToolBar;
+            var toolBar = sender as ToolBar;
+            
             var overflowGrid = toolBar.Template.FindName("OverflowGrid", toolBar) as FrameworkElement;
             if (overflowGrid != null)
             {
                 overflowGrid.Visibility = Visibility.Collapsed;
             }
+
             var mainPanelBorder = toolBar.Template.FindName("MainPanelBorder", toolBar) as FrameworkElement;
             if (mainPanelBorder != null)
             {
