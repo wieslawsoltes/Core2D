69329f5365431e2584672ab13bc3f5716c449597
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-07-19 22:19:39 +0200

    Added Properties indexer for BaseShape class

--- Test2d/Shape/BaseShape.cs
+++ Test2d/Shape/BaseShape.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 using System;
 using System.Collections.Immutable;
+using System.Linq;
 
 namespace Test2d
 {
@@ -91,6 +92,40 @@ namespace Test2d
         {
             get { return _properties; }
             set { Update(ref _properties, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets property Value using Name as key for Properties array values. If property with the specified key does not exist it is created.
+        /// </summary>
+        /// <param name="name">The property name value.</param>
+        /// <returns>The property Value.</returns>
+        public object this[string name]
+        {
+            get
+            {
+                var result = _properties.FirstOrDefault(p => p.Name == name);
+                if (result != null)
+                {
+                    return result.Value;
+                }
+                return null;
+            }
+            set
+            {
+                if (value != null)
+                {
+                    var result = _properties.FirstOrDefault(p => p.Name == name);
+                    if (result != null)
+                    {
+                        result.Value = value;
+                    }
+                    else
+                    {
+                        var property = ShapeProperty.Create(name, value);
+                        Properties = Properties.Add(property);
+                    }
+                }
+            }
         }
 
         /// <summary>
