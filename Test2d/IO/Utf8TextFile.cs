85930fa2966e05c8086de564d91ced279b72d5fb
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-08-16 01:19:09 +0200

    Refactored Utf8TextFile class

--- Test2d/IO/Utf8TextFile.cs
+++ Test2d/IO/Utf8TextFile.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 using System;
+using System.IO;
+using System.IO.Compression;
 using System.Text;
 
 namespace Test2d
@@ -13,6 +15,38 @@ namespace Test2d
         /// <summary>
         /// 
         /// </summary>
+        /// <param name="fs"></param>
+        /// <param name="text"></param>
+        public static void Compress(Stream fs, string text)
+        {
+            using (var cs = new GZipStream(fs, CompressionMode.Compress))
+            {
+                using (var sw = new StreamWriter(cs, Encoding.UTF8))
+                {
+                    sw.Write(text);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fs"></param>
+        /// <returns></returns>
+        public static string Decompress(Stream fs)
+        {
+            using (var cs = new GZipStream(fs, CompressionMode.Decompress))
+            {
+                using (var sr = new StreamReader(cs, Encoding.UTF8))
+                {
+                    return sr.ReadToEnd();
+                }
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
         /// <param name="path"></param>
         /// <param name="compressed"></param>
         /// <returns></returns>
@@ -20,22 +54,16 @@ namespace Test2d
         {
             if (compressed)
             {
-                using (var fs = System.IO.File.OpenRead(path))
+                using (var fs = File.OpenRead(path))
                 {
-                    using (var cs = new System.IO.Compression.GZipStream(fs, System.IO.Compression.CompressionMode.Decompress))
-                    {
-                        using (var sr = new System.IO.StreamReader(cs, Encoding.UTF8))
-                        {
-                            return sr.ReadToEnd();
-                        }
-                    }
+                    return Decompress(fs);
                 }
             }
             else
             {
-                using (var fs = System.IO.File.OpenRead(path))
+                using (var fs = File.OpenRead(path))
                 {
-                    using (var sr = new System.IO.StreamReader(fs, Encoding.UTF8))
+                    using (var sr = new StreamReader(fs, Encoding.UTF8))
                     {
                         return sr.ReadToEnd();
                     }
@@ -53,22 +81,16 @@ namespace Test2d
         {
             if (compressed)
             {
-                using (var fs = System.IO.File.Create(path))
+                using (var fs = File.Create(path))
                 {
-                    using (var cs = new System.IO.Compression.GZipStream(fs, System.IO.Compression.CompressionMode.Compress))
-                    {
-                        using (var sw = new System.IO.StreamWriter(cs, Encoding.UTF8))
-                        {
-                            sw.Write(text);
-                        }
-                    }
+                    Compress(fs, text);
                 }
             }
             else
             {
-                using (var fs = System.IO.File.Create(path))
+                using (var fs = File.Create(path))
                 {
-                    using (var sw = new System.IO.StreamWriter(fs, Encoding.UTF8))
+                    using (var sw = new StreamWriter(fs, Encoding.UTF8))
                     {
                         sw.Write(text);
                     }
