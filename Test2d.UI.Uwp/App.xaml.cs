bb9be9456fb6f4f5ad6bdbb681052705d0245771
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-08-16 04:33:55 +0200

    Disable EnableFrameRateCounter

--- Test2d.UI.Uwp/App.xaml.cs
+++ Test2d.UI.Uwp/App.xaml.cs
@@ -44,7 +44,7 @@ namespace Test.Uwp
 #if DEBUG
             if (System.Diagnostics.Debugger.IsAttached)
             {
-                this.DebugSettings.EnableFrameRateCounter = true;
+                this.DebugSettings.EnableFrameRateCounter = false;
             }
 #endif
 
