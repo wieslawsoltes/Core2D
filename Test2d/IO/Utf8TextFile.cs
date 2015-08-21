4c8d0c67b1b7f62ae0e519cc7410591ba52fefb3
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-08-22 01:32:53 +0200

    Removed Compress and Decompress methods

--- Test2d/IO/Utf8TextFile.cs
+++ Test2d/IO/Utf8TextFile.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 using System;
 using System.IO;
-using System.IO.Compression;
 using System.Text;
 
 namespace Test2d
@@ -15,29 +14,13 @@ namespace Test2d
         /// <summary>
         /// 
         /// </summary>
-        /// <param name="fs"></param>
-        /// <param name="text"></param>
-        public static void Compress(Stream fs, string text)
-        {
-            using (var cs = new GZipStream(fs, CompressionMode.Compress))
-            {
-                using (var sw = new StreamWriter(cs, Encoding.UTF8))
-                {
-                    sw.Write(text);
-                }
-            }
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="fs"></param>
+        /// <param name="path"></param>
         /// <returns></returns>
-        public static string Decompress(Stream fs)
+        public static string Read(string path)
         {
-            using (var cs = new GZipStream(fs, CompressionMode.Decompress))
+            using (var fs = File.OpenRead(path))
             {
-                using (var sr = new StreamReader(cs, Encoding.UTF8))
+                using (var sr = new StreamReader(fs, Encoding.UTF8))
                 {
                     return sr.ReadToEnd();
                 }
@@ -48,52 +31,14 @@ namespace Test2d
         /// 
         /// </summary>
         /// <param name="path"></param>
-        /// <param name="compressed"></param>
-        /// <returns></returns>
-        public static string Read(string path, bool compressed = true)
-        {
-            if (compressed)
-            {
-                using (var fs = File.OpenRead(path))
-                {
-                    return Decompress(fs);
-                }
-            }
-            else
-            {
-                using (var fs = File.OpenRead(path))
-                {
-                    using (var sr = new StreamReader(fs, Encoding.UTF8))
-                    {
-                        return sr.ReadToEnd();
-                    }
-                }
-            }
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="path"></param>
         /// <param name="text"></param>
-        /// <param name="compressed"></param>
-        public static void Write(string path, string text, bool compressed = true)
+        public static void Write(string path, string text)
         {
-            if (compressed)
+            using (var fs = File.Create(path))
             {
-                using (var fs = File.Create(path))
+                using (var sw = new StreamWriter(fs, Encoding.UTF8))
                 {
-                    Compress(fs, text);
-                }
-            }
-            else
-            {
-                using (var fs = File.Create(path))
-                {
-                    using (var sw = new StreamWriter(fs, Encoding.UTF8))
-                    {
-                        sw.Write(text);
-                    }
+                    sw.Write(text);
                 }
             }
         }
