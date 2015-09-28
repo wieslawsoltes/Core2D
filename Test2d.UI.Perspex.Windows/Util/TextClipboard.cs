fd189cd93be3b6d539936662bf1a3b922d4964fd
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-09-29 00:30:40 +0200

    Added TextClipboard util

---
+++ Test2d.UI.Perspex.Windows/Util/TextClipboard.cs
@@ -0,0 +1,62 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+using System.Threading.Tasks;
+using Test2d;
+using Perspex;
+
+namespace Test2d.UI.Perspex.Windows
+{
+    /// <summary>
+    /// Wrapper class for App.Current.Clipboard clipboard class.
+    /// </summary>
+    internal class TextClipboard : ITextClipboard
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="text"></param>
+        public void SetText(string text)
+        {
+            App.Current.Clipboard.SetTextAsync(text);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        private async Task<string> GetTextAsync()
+        {
+            return await App.Current.Clipboard.GetTextAsync();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public string GetText()
+        {
+            // TODO: Do not use Result.
+            return GetTextAsync().Result;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        private async Task<bool> ContainsTextAsync()
+        {
+            return !string.IsNullOrEmpty(await GetTextAsync());
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public bool ContainsText()
+        {
+            // TODO: Do not use Result.
+            return ContainsTextAsync().Result;
+        }
+    }
+}
