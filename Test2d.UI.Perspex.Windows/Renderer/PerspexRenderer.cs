1106995f0988b37a915aa8c03a7514c34c921226
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-09-29 16:48:06 +0200

    Implemented renderer helper methods

--- Test2d.UI.Perspex.Windows/Renderer/PerspexRenderer.cs
+++ Test2d.UI.Perspex.Windows/Renderer/PerspexRenderer.cs
@@ -8,11 +8,15 @@ using System.Text;
 using System.Threading.Tasks;
 using Perspex;
 using Perspex.Media;
+using Perspex.Media.Imaging;
+using Test2d;
 
 namespace Test2d.UI.Perspex.Windows
 {
     public class PerspexRenderer : ObservableObject, IRenderer
     {
+        private bool _enableImageCache = true;
+        private IDictionary<string, Bitmap> _biCache;
         private RendererState _state = new RendererState();
 
         /// <summary>
@@ -27,7 +31,24 @@ namespace Test2d.UI.Perspex.Windows
         /// <summary>
         /// 
         /// </summary>
-        public PerspexRenderer() { }
+        private Func<double, float> _scaleToPage;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private double _textScaleFactor;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="textScaleFactor"></param>
+        public PerspexRenderer(double textScaleFactor = 1.0)
+        {
+            ClearCache(isZooming: false);
+
+            _textScaleFactor = textScaleFactor;
+            _scaleToPage = (value) => (float)(value);
+        }
         
         /// <summary>
         /// 
@@ -56,8 +77,9 @@ namespace Test2d.UI.Perspex.Windows
         /// 
         /// </summary>
         /// <param name="style"></param>
+        /// <param name="scale"></param>
         /// <returns></returns>
-        private Pen ToPen(BaseStyle style)
+        private Pen ToPen(BaseStyle style, Func<double, float> scale)
         {
             var lineCap = default(PenLineCap);
             var dashStyle = default(DashStyle);
@@ -83,8 +105,8 @@ namespace Test2d.UI.Perspex.Windows
             }
             
             var pen = new Pen(
-                ToSolidBrush(style.Stroke), 
-                style.Thickness / _state.Zoom,
+                ToSolidBrush(style.Stroke),
+                scale(style.Thickness / _state.Zoom),
                 dashStyle, lineCap, 
                 lineCap, lineCap);
   
@@ -104,10 +126,173 @@ namespace Test2d.UI.Perspex.Windows
         /// <summary>
         /// 
         /// </summary>
+        /// <param name="tl"></param>
+        /// <param name="br"></param>
+        /// <param name="dx"></param>
+        /// <param name="dy"></param>
+        /// <returns></returns>
+        private static Rect2 CreateRect(XPoint tl, XPoint br, double dx, double dy)
+        {
+            return Rect2.Create(tl, br, dx, dy);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="dc"></param>
+        /// <param name="pen"></param>
+        /// <param name="isStroked"></param>
+        /// <param name="p0"></param>
+        /// <param name="p1"></param>
+        private static void DrawLineInternal(
+            IDrawingContext dc,
+            Pen pen,
+            bool isStroked,
+            ref Point p0,
+            ref Point p1)
+        {
+            if (isStroked)
+            {
+                dc.DrawLine(pen, p0, p1);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="dc"></param>
+        /// <param name="brush"></param>
+        /// <param name="pen"></param>
+        /// <param name="isStroked"></param>
+        /// <param name="isFilled"></param>
+        /// <param name="rect"></param>
+        private static void DrawRectangleInternal(
+            IDrawingContext dc,
+            Brush brush,
+            Pen pen,
+            bool isStroked,
+            bool isFilled,
+            ref Rect rect)
+        {
+            if (isFilled)
+            {
+                dc.FillRectange(brush, rect);
+            }
+
+            if (isStroked)
+            {
+                dc.DrawRectange(pen, rect);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="dc"></param>
+        /// <param name="brush"></param>
+        /// <param name="pen"></param>
+        /// <param name="isStroked"></param>
+        /// <param name="isFilled"></param>
+        /// <param name="rect"></param>
+        private static void DrawEllipseInternal(
+            IDrawingContext dc,
+            Brush brush,
+            Pen pen,
+            bool isStroked,
+            bool isFilled,
+            ref Rect rect)
+        {
+            if (!isFilled && !isStroked)
+                return;
+
+            var g = new EllipseGeometry(rect);
+
+            dc.DrawGeometry(
+                isFilled ? brush : null,
+                isStroked ? pen : null,
+                g);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="dc"></param>
+        /// <param name="stroke"></param>
+        /// <param name="rect"></param>
+        /// <param name="offsetX"></param>
+        /// <param name="offsetY"></param>
+        /// <param name="cellWidth"></param>
+        /// <param name="cellHeight"></param>
+        /// <param name="isStroked"></param>
+        private void DrawGridInternal(
+            IDrawingContext dc,
+            Pen stroke,
+            ref Rect2 rect,
+            double offsetX, double offsetY,
+            double cellWidth, double cellHeight,
+            bool isStroked)
+        {
+            double ox = rect.X;
+            double oy = rect.Y;
+            double sx = ox + offsetX;
+            double sy = oy + offsetY;
+            double ex = ox + rect.Width;
+            double ey = oy + rect.Height;
+
+            for (double x = sx; x < ex; x += cellWidth)
+            {
+                var p0 = new Point(
+                    _scaleToPage(x),
+                    _scaleToPage(oy));
+                var p1 = new Point(
+                    _scaleToPage(x),
+                    _scaleToPage(ey));
+                DrawLineInternal(dc, stroke, isStroked, ref p0, ref p1);
+            }
+
+            for (double y = sy; y < ey; y += cellHeight)
+            {
+                var p0 = new Point(
+                    _scaleToPage(ox),
+                    _scaleToPage(y));
+                var p1 = new Point(
+                    _scaleToPage(ex),
+                    _scaleToPage(y));
+                DrawLineInternal(dc, stroke, isStroked, ref p0, ref p1);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="dc"></param>
+        /// <param name="container"></param>
+        private void DrawBackgroundInternal(IDrawingContext dc, Container container)
+        {
+            Brush brush = ToSolidBrush(container.Background);
+            var rect = new Rect(0, 0, container.Width, container.Height);
+            dc.FillRectange(brush, rect);
+            // TODO: brush.Dispose();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
         /// <param name="isZooming"></param>
         public void ClearCache(bool isZooming)
         {
-            // TODO:
+            if (!isZooming)
+            {
+                if (_biCache != null)
+                {
+                    foreach (var kvp in _biCache)
+                    {
+                        // TODO: kvp.Value.Dispose();
+                    }
+                    _biCache.Clear();
+                }
+                _biCache = new Dictionary<string, Bitmap>();
+            }
         }
 
         /// <summary>
@@ -160,7 +345,7 @@ namespace Test2d.UI.Perspex.Windows
             // TODO:
             var _dc = dc as IDrawingContext;
 
-            Pen strokeLine = ToPen(line.Style);
+            Pen strokeLine = ToPen(line.Style, _scaleToPage);
             
             _dc.DrawLine(
                 strokeLine,
