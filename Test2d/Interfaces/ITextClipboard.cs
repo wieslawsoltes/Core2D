bf0029c7dbaf5c20d9b04f073de54126af016efd
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-09-29 23:54:40 +0200

    Added async support to ITextClipboard interface

--- Test2d/Interfaces/ITextClipboard.cs
+++ Test2d/Interfaces/ITextClipboard.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 using System;
+using System.Threading.Tasks;
 
 namespace Test2d
 {
@@ -13,16 +14,19 @@ namespace Test2d
         /// 
         /// </summary>
         /// <returns></returns>
-        bool ContainsText();
+        Task<bool> ContainsText();
+
         /// <summary>
         /// 
         /// </summary>
         /// <returns></returns>
-        string GetText();
+        Task<string> GetText();
+
         /// <summary>
         /// 
         /// </summary>
         /// <param name="text"></param>
-        void SetText(string text);
+        /// <returns></returns>
+        Task SetText(string text);
     }
 }
