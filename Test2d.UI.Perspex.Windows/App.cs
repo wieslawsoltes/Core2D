303f66fe3af351bd85b6d01b93dca2eef2b2d640
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-09-30 20:19:12 +0200

    Moved Main to App class

--- Test2d.UI.Perspex.Windows/App.cs
+++ Test2d.UI.Perspex.Windows/App.cs
@@ -1,6 +1,10 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using Perspex;
 using Perspex.Controls;
 using Perspex.Diagnostics;
@@ -32,6 +36,18 @@ namespace TestPerspex
 #if DEBUG
             DevTools.Attach(window);
 #endif
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            var app = new App();
+            var window = new MainWindow();
+            window.Show();
+            app.Run(window);
         }
     }
 }
