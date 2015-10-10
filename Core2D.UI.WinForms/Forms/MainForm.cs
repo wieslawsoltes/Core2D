39f6c7c221e77c7152187ef0c1ea1551c9ecee70
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-10-10 23:34:33 +0200

    Set EnableAutofit flag

--- Core2D.UI.WinForms/Forms/MainForm.cs
+++ Core2D.UI.WinForms/Forms/MainForm.cs
@@ -85,6 +85,8 @@ namespace TestWinForms
                 CsvReader = new CsvHelperReader(),
                 CsvWriter = new CsvHelperWriter()
             };
+
+            context.Renderers[0].State.EnableAutofit = true;
             context.InitializeEditor(new TraceLog(), System.IO.Path.Combine(GetAssemblyPath(), _logFileName));
             context.Editor.Renderers[0].State.DrawShapeState.Flags = ShapeStateFlags.Visible;
             context.Editor.GetImageKey = async () => await GetImageKey();
