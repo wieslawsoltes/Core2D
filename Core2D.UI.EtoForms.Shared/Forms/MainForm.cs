b5f02095fa68362ec8b151cda9461af6e58e5307
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-10-12 18:33:22 +0200

    Do not set drawable size

--- Core2D.UI.EtoForms.Shared/Forms/MainForm.cs
+++ Core2D.UI.EtoForms.Shared/Forms/MainForm.cs
@@ -85,8 +85,6 @@ namespace TestEtoForms
         private void InitializeDrawable()
         {
             _drawable = new Drawable(false);
-            _drawable.Width = (int)_context.Editor.Project.CurrentContainer.Width;
-            _drawable.Height = (int)_context.Editor.Project.CurrentContainer.Height;
             _drawable.Paint += (s, e) => Draw(e.Graphics);
 
             _drawable.MouseDown +=
