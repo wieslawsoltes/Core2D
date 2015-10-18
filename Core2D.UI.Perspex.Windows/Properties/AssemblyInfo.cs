6670318d9469dde0e017d6b6fbccb5abd5fa3645
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-10-18 13:23:39 +0200

    Updated assembly info

--- Core2D.UI.Perspex.Windows/Properties/AssemblyInfo.cs
+++ Core2D.UI.Perspex.Windows/Properties/AssemblyInfo.cs
@@ -13,6 +13,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Core2D.UI.Perspex.Windows")]
 [assembly: AssemblyCulture("")]
 
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
