3b344ff4e0f0791ba46eecdbd3e2961044d56317
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-10-19 17:48:40 +0200

    Added using statement

--- Dependencies/Renderer.Perspex/XPathGeometryConverter.cs
+++ Dependencies/Renderer.Perspex/XPathGeometryConverter.cs
@@ -24,147 +24,124 @@ namespace TestPerspex
         public static StreamGeometry ToStreamGeometry(this XPathGeometry xpg)
         {
             var sg = new StreamGeometry();
-            var sgc = sg.Open();
 
-            var previous = default(XPoint);
-
-            foreach (var xpf in xpg.Figures)
+            using (var sgc = sg.Open())
             {
-                sgc.BeginFigure(
-                    new Point(xpf.StartPoint.X, xpf.StartPoint.Y),
-                    xpf.IsFilled);
+                var previous = default(XPoint);
 
-                previous = xpf.StartPoint;
-
-                foreach (var segment in xpf.Segments)
+                foreach (var xpf in xpg.Figures)
                 {
-                    if (segment is XArcSegment)
-                    {
-                        var arcSegment = segment as XArcSegment;
-                        sgc.ArcTo(
-                            new Point(arcSegment.Point.X, arcSegment.Point.Y),
-                            new Size(arcSegment.Size.Width, arcSegment.Size.Height),
-                            arcSegment.RotationAngle,
-                            arcSegment.IsLargeArc,
-                            arcSegment.SweepDirection == XSweepDirection.Clockwise ? SweepDirection.Clockwise : SweepDirection.CounterClockwise);
+                    sgc.BeginFigure(
+                        new Point(xpf.StartPoint.X, xpf.StartPoint.Y),
+                        xpf.IsFilled);
 
-                        previous = arcSegment.Point;
-                    }
-                    else if (segment is XBezierSegment)
-                    {
-                        var bezierSegment = segment as XBezierSegment;
-                        sgc.BezierTo(
-                            new Point(bezierSegment.Point1.X, bezierSegment.Point1.Y),
-                            new Point(bezierSegment.Point2.X, bezierSegment.Point2.Y),
-                            new Point(bezierSegment.Point3.X, bezierSegment.Point3.Y));
+                    previous = xpf.StartPoint;
 
-                        previous = bezierSegment.Point3;
-                    }
-                    else if (segment is XLineSegment)
+                    foreach (var segment in xpf.Segments)
                     {
-                        var lineSegment = segment as XLineSegment;
-                        sgc.LineTo(
-                            new Point(lineSegment.Point.X, lineSegment.Point.Y));
-
-                        previous = lineSegment.Point;
-                    }
-                    else if (segment is XPolyBezierSegment)
-                    {
-                        var polyBezierSegment = segment as XPolyBezierSegment;
-                        if (polyBezierSegment.Points.Count >= 3)
+                        if (segment is XArcSegment)
                         {
-                            sgc.BezierTo(
-                                new Point(
-                                    polyBezierSegment.Points[0].X,
-                                    polyBezierSegment.Points[0].Y),
-                                new Point(
-                                    polyBezierSegment.Points[1].X,
-                                    polyBezierSegment.Points[1].Y),
-                                new Point(
-                                    polyBezierSegment.Points[2].X,
-                                    polyBezierSegment.Points[2].Y));
+                            var arcSegment = segment as XArcSegment;
+                            sgc.ArcTo(
+                                new Point(arcSegment.Point.X, arcSegment.Point.Y),
+                                new Size(arcSegment.Size.Width, arcSegment.Size.Height),
+                                arcSegment.RotationAngle,
+                                arcSegment.IsLargeArc,
+                                arcSegment.SweepDirection == XSweepDirection.Clockwise ? SweepDirection.Clockwise : SweepDirection.CounterClockwise);
 
-                            previous = polyBezierSegment.Points[2];
+                            previous = arcSegment.Point;
                         }
-
-                        if (polyBezierSegment.Points.Count > 3
-                            && polyBezierSegment.Points.Count % 3 == 0)
+                        else if (segment is XBezierSegment)
                         {
-                            for (int i = 3; i < polyBezierSegment.Points.Count; i += 3)
+                            var bezierSegment = segment as XBezierSegment;
+                            sgc.BezierTo(
+                                new Point(bezierSegment.Point1.X, bezierSegment.Point1.Y),
+                                new Point(bezierSegment.Point2.X, bezierSegment.Point2.Y),
+                                new Point(bezierSegment.Point3.X, bezierSegment.Point3.Y));
+
+                            previous = bezierSegment.Point3;
+                        }
+                        else if (segment is XLineSegment)
+                        {
+                            var lineSegment = segment as XLineSegment;
+                            sgc.LineTo(
+                                new Point(lineSegment.Point.X, lineSegment.Point.Y));
+
+                            previous = lineSegment.Point;
+                        }
+                        else if (segment is XPolyBezierSegment)
+                        {
+                            var polyBezierSegment = segment as XPolyBezierSegment;
+                            if (polyBezierSegment.Points.Count >= 3)
                             {
                                 sgc.BezierTo(
                                     new Point(
-                                        polyBezierSegment.Points[i].X,
-                                        polyBezierSegment.Points[i].Y),
+                                        polyBezierSegment.Points[0].X,
+                                        polyBezierSegment.Points[0].Y),
                                     new Point(
-                                        polyBezierSegment.Points[i + 1].X,
-                                        polyBezierSegment.Points[i + 1].Y),
+                                        polyBezierSegment.Points[1].X,
+                                        polyBezierSegment.Points[1].Y),
                                     new Point(
-                                        polyBezierSegment.Points[i + 2].X,
-                                        polyBezierSegment.Points[i + 2].Y));
+                                        polyBezierSegment.Points[2].X,
+                                        polyBezierSegment.Points[2].Y));
 
-                                previous = polyBezierSegment.Points[i + 2];
+                                previous = polyBezierSegment.Points[2];
+                            }
+
+                            if (polyBezierSegment.Points.Count > 3
+                                && polyBezierSegment.Points.Count % 3 == 0)
+                            {
+                                for (int i = 3; i < polyBezierSegment.Points.Count; i += 3)
+                                {
+                                    sgc.BezierTo(
+                                        new Point(
+                                            polyBezierSegment.Points[i].X,
+                                            polyBezierSegment.Points[i].Y),
+                                        new Point(
+                                            polyBezierSegment.Points[i + 1].X,
+                                            polyBezierSegment.Points[i + 1].Y),
+                                        new Point(
+                                            polyBezierSegment.Points[i + 2].X,
+                                            polyBezierSegment.Points[i + 2].Y));
+
+                                    previous = polyBezierSegment.Points[i + 2];
+                                }
                             }
                         }
-                    }
-                    else if (segment is XPolyLineSegment)
-                    {
-                        var polyLineSegment = segment as XPolyLineSegment;
-                        if (polyLineSegment.Points.Count >= 1)
+                        else if (segment is XPolyLineSegment)
                         {
-                            sgc.LineTo(
-                                new Point(
-                                    polyLineSegment.Points[0].X,
-                                    polyLineSegment.Points[0].Y));
-
-                            previous = polyLineSegment.Points[0];
-                        }
-
-                        if (polyLineSegment.Points.Count > 1)
-                        {
-                            for (int i = 1; i < polyLineSegment.Points.Count; i++)
+                            var polyLineSegment = segment as XPolyLineSegment;
+                            if (polyLineSegment.Points.Count >= 1)
                             {
                                 sgc.LineTo(
                                     new Point(
-                                        polyLineSegment.Points[i].X,
-                                        polyLineSegment.Points[i].Y));
+                                        polyLineSegment.Points[0].X,
+                                        polyLineSegment.Points[0].Y));
 
-                                previous = polyLineSegment.Points[i];
+                                previous = polyLineSegment.Points[0];
+                            }
+
+                            if (polyLineSegment.Points.Count > 1)
+                            {
+                                for (int i = 1; i < polyLineSegment.Points.Count; i++)
+                                {
+                                    sgc.LineTo(
+                                        new Point(
+                                            polyLineSegment.Points[i].X,
+                                            polyLineSegment.Points[i].Y));
+
+                                    previous = polyLineSegment.Points[i];
+                                }
                             }
                         }
-                    }
-                    else if (segment is XPolyQuadraticBezierSegment)
-                    {
-                        var polyQuadraticSegment = segment as XPolyQuadraticBezierSegment;
-                        if (polyQuadraticSegment.Points.Count >= 2)
+                        else if (segment is XPolyQuadraticBezierSegment)
                         {
-                            var p1 = previous;
-                            var p2 = polyQuadraticSegment.Points[0];
-                            var p3 = polyQuadraticSegment.Points[1];
-                            double x1 = p1.X;
-                            double y1 = p1.Y;
-                            double x2 = p1.X + (2.0 * (p2.X - p1.X)) / 3.0;
-                            double y2 = p1.Y + (2.0 * (p2.Y - p1.Y)) / 3.0;
-                            double x3 = x2 + (p3.X - p1.X) / 3.0;
-                            double y3 = y2 + (p3.Y - p1.Y) / 3.0;
-                            double x4 = p3.X;
-                            double y4 = p3.Y;
-                            sgc.BezierTo(
-                                new Point(x2, y2),
-                                new Point(x3, y3),
-                                new Point(x4, y4));
-
-                            previous = polyQuadraticSegment.Points[1];
-                        }
-
-                        if (polyQuadraticSegment.Points.Count > 2
-                            && polyQuadraticSegment.Points.Count % 2 == 0)
-                        {
-                            for (int i = 3; i < polyQuadraticSegment.Points.Count; i += 3)
+                            var polyQuadraticSegment = segment as XPolyQuadraticBezierSegment;
+                            if (polyQuadraticSegment.Points.Count >= 2)
                             {
-                                var p1 = polyQuadraticSegment.Points[i - 1];
-                                var p2 = polyQuadraticSegment.Points[i];
-                                var p3 = polyQuadraticSegment.Points[i + 1];
+                                var p1 = previous;
+                                var p2 = polyQuadraticSegment.Points[0];
+                                var p3 = polyQuadraticSegment.Points[1];
                                 double x1 = p1.X;
                                 double y1 = p1.Y;
                                 double x2 = p1.X + (2.0 * (p2.X - p1.X)) / 3.0;
@@ -178,43 +155,64 @@ namespace TestPerspex
                                     new Point(x3, y3),
                                     new Point(x4, y4));
 
-                                previous = polyQuadraticSegment.Points[i + 1];
+                                previous = polyQuadraticSegment.Points[1];
+                            }
+
+                            if (polyQuadraticSegment.Points.Count > 2
+                                && polyQuadraticSegment.Points.Count % 2 == 0)
+                            {
+                                for (int i = 3; i < polyQuadraticSegment.Points.Count; i += 3)
+                                {
+                                    var p1 = polyQuadraticSegment.Points[i - 1];
+                                    var p2 = polyQuadraticSegment.Points[i];
+                                    var p3 = polyQuadraticSegment.Points[i + 1];
+                                    double x1 = p1.X;
+                                    double y1 = p1.Y;
+                                    double x2 = p1.X + (2.0 * (p2.X - p1.X)) / 3.0;
+                                    double y2 = p1.Y + (2.0 * (p2.Y - p1.Y)) / 3.0;
+                                    double x3 = x2 + (p3.X - p1.X) / 3.0;
+                                    double y3 = y2 + (p3.Y - p1.Y) / 3.0;
+                                    double x4 = p3.X;
+                                    double y4 = p3.Y;
+                                    sgc.BezierTo(
+                                        new Point(x2, y2),
+                                        new Point(x3, y3),
+                                        new Point(x4, y4));
+
+                                    previous = polyQuadraticSegment.Points[i + 1];
+                                }
                             }
                         }
-                    }
-                    else if (segment is XQuadraticBezierSegment)
-                    {
-                        var qbezierSegment = segment as XQuadraticBezierSegment;
-                        var p1 = previous;
-                        var p2 = qbezierSegment.Point1;
-                        var p3 = qbezierSegment.Point2;
-                        double x1 = p1.X;
-                        double y1 = p1.Y;
-                        double x2 = p1.X + (2.0 * (p2.X - p1.X)) / 3.0;
-                        double y2 = p1.Y + (2.0 * (p2.Y - p1.Y)) / 3.0;
-                        double x3 = x2 + (p3.X - p1.X) / 3.0;
-                        double y3 = y2 + (p3.Y - p1.Y) / 3.0;
-                        double x4 = p3.X;
-                        double y4 = p3.Y;
-                        sgc.BezierTo(
-                            new Point(x2, y2),
-                            new Point(x3, y3),
-                            new Point(x4, y4));
+                        else if (segment is XQuadraticBezierSegment)
+                        {
+                            var qbezierSegment = segment as XQuadraticBezierSegment;
+                            var p1 = previous;
+                            var p2 = qbezierSegment.Point1;
+                            var p3 = qbezierSegment.Point2;
+                            double x1 = p1.X;
+                            double y1 = p1.Y;
+                            double x2 = p1.X + (2.0 * (p2.X - p1.X)) / 3.0;
+                            double y2 = p1.Y + (2.0 * (p2.Y - p1.Y)) / 3.0;
+                            double x3 = x2 + (p3.X - p1.X) / 3.0;
+                            double y3 = y2 + (p3.Y - p1.Y) / 3.0;
+                            double x4 = p3.X;
+                            double y4 = p3.Y;
+                            sgc.BezierTo(
+                                new Point(x2, y2),
+                                new Point(x3, y3),
+                                new Point(x4, y4));
 
-                        previous = qbezierSegment.Point2;
+                            previous = qbezierSegment.Point2;
+                        }
+                        else
+                        {
+                            throw new NotSupportedException("Not supported segment type: " + segment.GetType());
+                        }
                     }
-                    else
-                    {
-                        throw new NotSupportedException("Not supported segment type: " + segment.GetType());
-                    }
+
+                    sgc.EndFigure(xpf.IsClosed);
                 }
-
-                sgc.EndFigure(xpf.IsClosed);
             }
-
-            // TODO: Perspex does not need to dispose StreamGeometryContext?
-            //sgc.Close();
-            sgc.Dispose();
 
             // TODO: Perspex has not yet implemented FillRule.
             //sg.FillRule = xpg.FillRule == XFillRule.Nonzero ? FillRule.Nonzero : FillRule.EvenOdd;
@@ -239,8 +237,7 @@ namespace TestPerspex
         /// <returns></returns>
         public static string ToSource(this XPathGeometry xpg)
         {
-            var sg = ToStreamGeometry(xpg);
-            return sg.ToString();
+            return ToStreamGeometry(xpg).ToString();
         }
 
         /// <summary>
