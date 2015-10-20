ccf31dbf9a866b59161b14de3a8a6990e1ade8ef
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2015-10-20 14:36:56 +0200

    Update restore.cmd

--- restore.cmd
+++ restore.cmd
@@ -1,2 +1,17 @@
 @echo off
+set retryNumber=0
+set maxRetries=5
+
+:RESTORE
+
 nuget restore -source "https://www.nuget.org/api/v2/;https://www.myget.org/F/eto/;https://www.myget.org/F/perspex-nightly/api/v2"
+
+IF NOT ERRORLEVEL 1 GOTO :EOF
+
+@echo Nuget restore exited with code %ERRORLEVEL%!
+set /a retryNumber=%retryNumber%+1
+IF %reTryNumber% LSS %maxRetries% (GOTO :RESTORE)
+
+@echo Restoring nuget packages %maxRetries% times was unsuccessful!
+
+EXIT /B 1
