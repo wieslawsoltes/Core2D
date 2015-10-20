d5280af030b073d76062bb3a989af9025e2b8cc6
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2015-10-20 13:47:10 +0200

    Update restore.cmd

--- restore.cmd
+++ restore.cmd
@@ -1,2 +1,2 @@
 @echo off
-nuget restore -source "https://www.myget.org/F/eto/;https://www.myget.org/F/perspex-nightly/api/v2"
+nuget restore -source "https://www.nuget.org/api/v2/;https://www.myget.org/F/eto/;https://www.myget.org/F/perspex-nightly/api/v2"
