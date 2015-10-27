2893ae8693f77bd925ba9c3a2f16971d1e7c0c28
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-10-27 23:04:03 +0100

    Added EditorConstants class

---
+++ Core2D/Editor/EditorConstants.cs
@@ -0,0 +1,92 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+
+namespace Core2D
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class EditorConstants
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string DefaultContainerName = "Container";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string DefaultDocumentName = "Document";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string ImportedStyleLibraryName = "Imported";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string ImportedDatabaseName = "Imported";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string ProjectExtension = ".project";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string CsvExtension = ".csv";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string StyleExtension = ".style";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string StylesExtension = ".styles";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string StyleLibraryExtension = ".StyleLibrary";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string StyleLibrariesExtension = ".StyleLibraries";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string GroupExtension = ".group";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string GroupsExtension = ".groups";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string GroupLibraryExtension = ".grouplibrary";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string GroupLibrariesExtension = ".grouplibraries";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string TemplateExtension = ".template";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string TemplatesExtension = ".templates";
+    }
+}
