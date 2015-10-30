e7d3867ef0e7f8af09ab41b389e31719a6aa7846
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-10-30 19:43:05 +0100

    Added ArgbColorToBrushMultiConverter converter

---
+++ Core2D.UI.Perspex.Desktop/Converters/ArgbColorToBrushMultiConverter.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Perspex;
+using Perspex.Markup;
+using Perspex.Media;
+
+namespace Core2D.UI.Perspex.Desktop.Converters
+{
+    /// <summary>
+    /// Converts multi-binding inputs to a final value.
+    /// </summary>
+    public class ArgbColorToBrushMultiConverter : IMultiValueConverter
+    {
+        /// <summary>
+        /// Gets an instance of a <see cref="ArgbColorToBrushMultiConverter"/>.
+        /// </summary>
+        public static readonly ArgbColorToBrushMultiConverter Instance = new ArgbColorToBrushMultiConverter();
+
+        /// <summary>
+        /// Converts multi-binding inputs to a final value.
+        /// </summary>
+        /// <param name="values">The values to convert.</param>
+        /// <param name="targetType">The type of the target.</param>
+        /// <param name="parameter">A user-defined parameter.</param>
+        /// <param name="culture">The culture to use.</param>
+        /// <returns>The converted value.</returns>
+        public object Convert(IList<object> values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Count() == 4)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    if (values[i].GetType() != typeof(int))
+                        return PerspexProperty.UnsetValue;
+                }
+
+                var color = Color.FromArgb(
+                    (byte)(int)values[0],
+                    (byte)(int)values[1],
+                    (byte)(int)values[2],
+                    (byte)(int)values[3]);
+                return new SolidColorBrush(color);
+            }
+            return PerspexProperty.UnsetValue;
+        }
+    }
+}
