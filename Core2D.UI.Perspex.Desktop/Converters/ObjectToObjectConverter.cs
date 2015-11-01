9f96b4044b1ffbeafd41c3f00e39809ebacc077e
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-11-01 16:09:12 +0100

    Added ObjectToObjectConverter class

---
+++ Core2D.UI.Perspex.Desktop/Converters/ObjectToObjectConverter.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Perspex;
+using Perspex.Markup;
+using Perspex.Media;
+
+namespace Core2D.UI.Perspex.Desktop.Converters
+{
+    /// <summary>
+    /// Pass-through a binding value <see cref="object"/> as <see cref="object"/> to avoid <see cref="PerspexProperty.UnsetValue"/>.
+    /// </summary>
+    public class ObjectToObjectConverter : IValueConverter
+    {
+        /// <summary>
+        /// Gets an instance of a <see cref="ObjectToObjectConverter"/>.
+        /// </summary>
+        public static readonly ObjectToObjectConverter Instance = new ObjectToObjectConverter();
+
+        /// <summary>
+        /// Converts a value.
+        /// </summary>
+        /// <param name="value">The value to convert.</param>
+        /// <param name="targetType">The type of the target.</param>
+        /// <param name="parameter">A user-defined parameter.</param>
+        /// <param name="culture">The culture to use.</param>
+        /// <returns>The converted value.</returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+
+        /// <summary>
+        /// Converts a value.
+        /// </summary>
+        /// <param name="value">The value to convert.</param>
+        /// <param name="targetType">The type of the target.</param>
+        /// <param name="parameter">A user-defined parameter.</param>
+        /// <param name="culture">The culture to use.</param>
+        /// <returns>The converted value.</returns>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
