06c64d96c84b8df614df9cbbf78f59eb5f2bc063
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-11-03 16:10:39 +0100

    Added DashboardControl control

---
+++ Core2D.UI.Perspex.Desktop/Controls/DashboardControl.paml.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Perspex.Controls;
+using Perspex.Markup.Xaml;
+
+namespace Core2D.UI.Perspex.Desktop.Controls
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class DashboardControl : UserControl
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public DashboardControl()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void InitializeComponent()
+        {
+            PerspexXamlLoader.Load(this);
+        }
+    }
+}
