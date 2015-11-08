eb40fb2ff7728eeda5b4c61d298eee30538c058b
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-11-08 13:54:40 +0100

    Added ImageKey struct

---
+++ Core2D/Renderer/ImageKey.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+
+namespace Core2D
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public struct ImageKey
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Key { get; set; }
+    }
+}
