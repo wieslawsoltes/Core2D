4f9f32853de793c6ac02970ac76673a596b298c0
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-11-15 21:26:40 +0100

    Added Cache class

---
+++ Core2D/Renderer/Cache.cs
@@ -0,0 +1,84 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+using System.Collections.Generic;
+
+namespace Core2D
+{
+    /// <summary>
+    /// Generic cache collection.
+    /// </summary>
+    /// <typeparam name="TKey">The input type.</typeparam>
+    /// <typeparam name="TValue">The output type.</typeparam>
+    public class Cache<TKey, TValue>
+        where TKey : class
+        where TValue : class
+    {
+        private IDictionary<TKey, TValue> _storage;
+        private Action<TValue> _dispose;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Cache{TIn, TOut}"/> class.
+        /// </summary>
+        /// <param name="dispose">The dispose action.</param>
+        public Cache(Action<TValue> dispose = null)
+        {
+            _dispose = dispose;
+            _storage = new Dictionary<TKey, TValue>();
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="Editor"/> instance.
+        /// </summary>
+        /// <param name="dispose">The dispose action.</param>
+        /// <returns>The new instance of the <see cref="Cache{TIn, TOut}"/> class.</returns>
+        public static Cache<TKey, TValue> Create(Action<TValue> dispose = null)
+        {
+            return new Cache<TKey, TValue>(dispose);
+        }
+
+        /// <summary>
+        /// Resets cache storage.
+        /// </summary>
+        public void Reset()
+        {
+            if (_storage != null)
+            {
+                if (_dispose != null)
+                {
+                    foreach (var data in _storage)
+                    {
+                        _dispose(data.Value);
+                    }
+                }
+                _storage.Clear();
+            }
+            _storage = new Dictionary<TKey, TValue>();
+        }
+
+        /// <summary>
+        /// Adds new value to storage.
+        /// </summary>
+        /// <param name="key">The key object.</param>
+        /// <param name="value">The value object.</param>
+        public void Add(TKey key, TValue value)
+        {
+            _storage.Add(key, value);
+        }
+
+        /// <summary>
+        /// Gets value from storage.
+        /// </summary>
+        /// <param name="key">The key object.</param>
+        /// <returns>The value from storage.</returns>
+        public TValue Get(TKey key)
+        {
+            TValue data;
+            if (_storage.TryGetValue(key, out data))
+            {
+                return data;
+            }
+            return default(TValue);
+        }
+    }
+}
