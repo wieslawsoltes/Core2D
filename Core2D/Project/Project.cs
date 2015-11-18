7066755e5bf20f44ffc6db4d4300bb4280c65a93
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-11-18 18:49:06 +0100

    Refactored project class

--- Core2D/Project/Project.cs
+++ Core2D/Project/Project.cs
@@ -18,14 +18,20 @@ namespace Core2D
         private static string ProjectEntryName = "Project.json";
         private static string ImageEntryNamePrefix = "Images\\";
         private IDictionary<string, byte[]> _images;
-
-        /// <summary>
-        /// Initializes a new <see cref="Project"/> instance.
-        /// </summary>
-        public Project()
-        {
-            _images = new Dictionary<string, byte[]>();
-        }
+        private string _name;
+        private Options _options;
+        private ImmutableArray<Database> _databases;
+        private Database _currentDatabase;
+        private ImmutableArray<Library<ShapeStyle>> _styleLibraries;
+        private Library<ShapeStyle> _currentStyleLibrary;
+        private ImmutableArray<Library<XGroup>> _groupLibraries;
+        private Library<XGroup> _currentGroupLibrary;
+        private ImmutableArray<Container> _templates;
+        private Container _currentTemplate;
+        private ImmutableArray<Document> _documents;
+        private Document _currentDocument;
+        private Container _currentContainer;
+        private object _selected;
 
         /// <summary>
         /// 
@@ -36,6 +42,162 @@ namespace Core2D
             {
                 return _images.Select(i => new ImageKey() { Key = i.Key }).ToList();
             }
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Name
+        {
+            get { return _name; }
+            set { Update(ref _name, value); }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Options Options
+        {
+            get { return _options; }
+            set { Update(ref _options, value); }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Database CurrentDatabase
+        {
+            get { return _currentDatabase; }
+            set { Update(ref _currentDatabase, value); }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public ImmutableArray<Database> Databases
+        {
+            get { return _databases; }
+            set { Update(ref _databases, value); }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public ImmutableArray<Library<ShapeStyle>> StyleLibraries
+        {
+            get { return _styleLibraries; }
+            set { Update(ref _styleLibraries, value); }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Library<ShapeStyle> CurrentStyleLibrary
+        {
+            get { return _currentStyleLibrary; }
+            set { Update(ref _currentStyleLibrary, value); }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public ImmutableArray<Library<XGroup>> GroupLibraries
+        {
+            get { return _groupLibraries; }
+            set { Update(ref _groupLibraries, value); }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Library<XGroup> CurrentGroupLibrary
+        {
+            get { return _currentGroupLibrary; }
+            set { Update(ref _currentGroupLibrary, value); }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public ImmutableArray<Container> Templates
+        {
+            get { return _templates; }
+            set { Update(ref _templates, value); }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Container CurrentTemplate
+        {
+            get { return _currentTemplate; }
+            set { Update(ref _currentTemplate, value); }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public ImmutableArray<Document> Documents
+        {
+            get { return _documents; }
+            set { Update(ref _documents, value); }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Document CurrentDocument
+        {
+            get { return _currentDocument; }
+            set { Update(ref _currentDocument, value); }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Container CurrentContainer
+        {
+            get { return _currentContainer; }
+            set { Update(ref _currentContainer, value); }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public object Selected
+        {
+            get { return _selected; }
+            set
+            {
+                var item = value;
+
+                if (item is Container && _documents != null)
+                {
+                    var container = item as Container;
+                    var document = _documents.FirstOrDefault(d => d.Containers.Contains(container));
+                    if (document != null)
+                    {
+                        CurrentDocument = document;
+                        CurrentContainer = container;
+                        CurrentContainer.Invalidate();
+                    }
+                }
+                else if (item is Document)
+                {
+                    var document = item as Document;
+                    CurrentDocument = document;
+                }
+
+                Update(ref _selected, value);
+            }
+        }
+        
+        /// <summary>
+        /// Initializes a new <see cref="Project"/> instance.
+        /// </summary>
+        public Project()
+        {
+            _images = new Dictionary<string, byte[]>();
         }
 
         /// <summary>
@@ -283,169 +445,38 @@ namespace Core2D
             }
         }
 
-        private string _name;
-        private Options _options;
-        private ImmutableArray<Database> _databases;
-        private Database _currentDatabase;
-        private ImmutableArray<StyleLibrary> _styleLibraries;
-        private StyleLibrary _currentStyleLibrary;
-        private ImmutableArray<GroupLibrary> _groupLibraries;
-        private GroupLibrary _currentGroupLibrary;
-        private ImmutableArray<Container> _templates;
-        private Container _currentTemplate;
-        private ImmutableArray<Document> _documents;
-        private Document _currentDocument;
-        private Container _currentContainer;
-        private object _selected;
-
         /// <summary>
         /// 
         /// </summary>
-        public string Name
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public static string ReadUtf8Text(string path)
         {
-            get { return _name; }
-            set { Update(ref _name, value); }
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        public Options Options
-        {
-            get { return _options; }
-            set { Update(ref _options, value); }
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        public Database CurrentDatabase
-        {
-            get { return _currentDatabase; }
-            set { Update(ref _currentDatabase, value); }
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        public ImmutableArray<Database> Databases
-        {
-            get { return _databases; }
-            set { Update(ref _databases, value); }
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        public ImmutableArray<StyleLibrary> StyleLibraries
-        {
-            get { return _styleLibraries; }
-            set { Update(ref _styleLibraries, value); }
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        public StyleLibrary CurrentStyleLibrary
-        {
-            get { return _currentStyleLibrary; }
-            set { Update(ref _currentStyleLibrary, value); }
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        public ImmutableArray<GroupLibrary> GroupLibraries
-        {
-            get { return _groupLibraries; }
-            set { Update(ref _groupLibraries, value); }
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        public GroupLibrary CurrentGroupLibrary
-        {
-            get { return _currentGroupLibrary; }
-            set { Update(ref _currentGroupLibrary, value); }
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        public ImmutableArray<Container> Templates
-        {
-            get { return _templates; }
-            set { Update(ref _templates, value); }
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        public Container CurrentTemplate
-        {
-            get { return _currentTemplate; }
-            set { Update(ref _currentTemplate, value); }
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        public ImmutableArray<Document> Documents
-        {
-            get { return _documents; }
-            set { Update(ref _documents, value); }
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        public Document CurrentDocument
-        {
-            get { return _currentDocument; }
-            set { Update(ref _currentDocument, value); }
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        public Container CurrentContainer
-        {
-            get { return _currentContainer; }
-            set { Update(ref _currentContainer, value); }
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        public object Selected
-        {
-            get { return _selected; }
-            set
+            using (var fs = File.OpenRead(path))
             {
-                var item = value;
-
-                if (item is Container && _documents != null)
+                using (var sr = new StreamReader(fs, Encoding.UTF8))
                 {
-                    var container = item as Container;
-                    var document = _documents.FirstOrDefault(d => d.Containers.Contains(container));
-                    if (document != null)
-                    {
-                        CurrentDocument = document;
-                        CurrentContainer = container;
-                        CurrentContainer.Invalidate();
-                    }
+                    return sr.ReadToEnd();
                 }
-                else if (item is Document)
-                {
-                    var document = item as Document;
-                    CurrentDocument = document;
-                }
-
-                Update(ref _selected, value);
             }
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="text"></param>
+        public static void WriteUtf8Text(string path, string text)
+        {
+            using (var fs = File.Create(path))
+            {
+                using (var sw = new StreamWriter(fs, Encoding.UTF8))
+                {
+                    sw.Write(text);
+                }
+            }
+        }
+        
         /// <summary>
         /// Creates a new <see cref="Project"/> instance.
         /// </summary>
@@ -458,8 +489,8 @@ namespace Core2D
                 Name = name,
                 Options = Options.Create(),
                 Databases = ImmutableArray.Create<Database>(),
-                StyleLibraries = ImmutableArray.Create<StyleLibrary>(),
-                GroupLibraries = ImmutableArray.Create<GroupLibrary>(),
+                StyleLibraries = ImmutableArray.Create<Library<ShapeStyle>>(),
+                GroupLibraries = ImmutableArray.Create<Library<XGroup>>(),
                 Templates = ImmutableArray.Create<Container>(),
                 Documents = ImmutableArray.Create<Document>(),
             };
