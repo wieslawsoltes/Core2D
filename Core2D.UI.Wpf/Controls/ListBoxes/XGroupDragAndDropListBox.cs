baf528e12b34a18609bf15f186e90294ac9161e2
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-11-22 18:14:50 +0100

    Removed unused code

--- Core2D.UI.Wpf/Controls/ListBoxes/XGroupDragAndDropListBox.cs
+++ Core2D.UI.Wpf/Controls/ListBoxes/XGroupDragAndDropListBox.cs
@@ -1,28 +1,14 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 using System;
-using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Core2D;
 
 namespace Core2D.UI.Wpf.Controls
 {
     /// <summary>
     /// 
     /// </summary>
-    public class XGroupDragAndDropListBox : DragAndDropListBox<Core2D.XGroup>
+    public class XGroupDragAndDropListBox : DragAndDropListBox<XGroup>
     {
         /// <summary>
         /// 
@@ -37,9 +23,9 @@ namespace Core2D.UI.Wpf.Controls
         /// Updates DataContext collection ImmutableArray property.
         /// </summary>
         /// <param name="array">The updated immutable array.</param>
-        public override void UpdateDataContext(ImmutableArray<Core2D.XGroup> array)
+        public override void UpdateDataContext(ImmutableArray<XGroup> array)
         {
-            var editor = (Core2D.Editor)this.Tag;
+            var editor = (Editor)this.Tag;
 
             var gl = editor.Project.CurrentGroupLibrary;
 
