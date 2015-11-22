4994abbc285114f370e25b30883f14210dd53649
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-11-22 20:33:35 +0100

    Updated release script

--- release.cmd
+++ release.cmd
@@ -1,10 +1,10 @@
 @echo off
 setlocal
 
-set ReleaseName=Core2D.UI.Wpf-YYYYMMDD-AnyCPU
+set ReleaseName=Core2D.Wpf-YYYYMMDD-AnyCPU
 
 set ProjectPath=.
-set ReleasePath=%ProjectPath%\Core2D.UI.Wpf\bin\Release
+set ReleasePath=%ProjectPath%\Core2D.Wpf\bin\Release
 set WinRAR=c:\Program Files\WinRAR\WinRAR.exe
 
 mkdir "%ReleaseName%"
@@ -12,7 +12,7 @@ mkdir "%ReleaseName%"
 copy "%ProjectPath%\*.txt" "%ReleaseName%"
 copy "%ProjectPath%\*.md" "%ReleaseName%"
 copy "%ReleasePath%\*.dll" "%ReleaseName%"
-copy "%ReleasePath%\Core2D.UI.Wpf.exe" "%ReleaseName%"
+copy "%ReleasePath%\Core2D.Wpf.exe" "%ReleaseName%"
 
 "%WinRAR%" a -ep1 -m5 -r -t "%ReleaseName%.zip" "%ReleaseName%\*"
 rmdir /S /Q "%ReleaseName%"
