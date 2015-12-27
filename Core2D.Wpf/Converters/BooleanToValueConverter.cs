a7b1a1633d1deddc0e9020b4b9c7de225250b434
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-12-28 00:28:08 +0100

    Updated comments

--- Core2D.Wpf/Converters/BooleanToValueConverter.cs
+++ Core2D.Wpf/Converters/BooleanToValueConverter.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 using System;
 using System.Globalization;
+using System.Windows;
 using System.Windows.Data;
 
 namespace Core2D.Wpf.Converters
@@ -28,7 +29,7 @@ namespace Core2D.Wpf.Converters
         /// <param name="targetType">The type of the binding target property.</param>
         /// <param name="parameter">The converter parameter to use.</param>
         /// <param name="culture">The culture to use in the converter.</param>
-        /// <returns>A converted value. If the method returns null, the valid null value is used.</returns>
+        /// <returns>A converted value.</returns>
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (value == null)
@@ -44,7 +45,7 @@ namespace Core2D.Wpf.Converters
         /// <param name="targetType">The type to convert to.</param>
         /// <param name="parameter">The converter parameter to use.</param>
         /// <param name="culture">The culture to use in the converter.</param>
-        /// <returns>A converted value. If the method returns null, the valid null value is used.</returns>
+        /// <returns>A converted value.</returns>
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             return value != null ? value.Equals(TrueValue) : false;
--- Core2D.Wpf/Converters/EnumToBooleanConverter.cs
+++ Core2D.Wpf/Converters/EnumToBooleanConverter.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 using System;
 using System.Globalization;
+using System.Windows;
 using System.Windows.Data;
 
 namespace Core2D.Wpf.Converters
@@ -18,7 +19,7 @@ namespace Core2D.Wpf.Converters
         /// <param name="targetType">The type of the binding target property.</param>
         /// <param name="parameter">The converter parameter to use.</param>
         /// <param name="culture">The culture to use in the converter.</param>
-        /// <returns>A converted value. If the method returns null, the valid null value is used.</returns>
+        /// <returns>A converted value.</returns>
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             return value.Equals(parameter);
@@ -31,7 +32,7 @@ namespace Core2D.Wpf.Converters
         /// <param name="targetType">The type to convert to.</param>
         /// <param name="parameter">The converter parameter to use.</param>
         /// <param name="culture">The culture to use in the converter.</param>
-        /// <returns>A converted value. If the method returns null, the valid null value is used.</returns>
+        /// <returns>A converted value.</returns>
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             return (bool)value ? parameter : Binding.DoNothing;
