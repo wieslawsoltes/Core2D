8e0545fb31bf6c4a46db313c9af0d05fbcf35766
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-12-29 17:55:37 +0100

    Added Template drop support

--- Core2D.Wpf/Controls/Project/PageControl.xaml.cs
+++ Core2D.Wpf/Controls/Project/PageControl.xaml.cs
@@ -1,5 +1,7 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace Core2D.Wpf.Controls.Project
@@ -15,6 +17,50 @@ namespace Core2D.Wpf.Controls.Project
         public PageControl()
         {
             InitializeComponent();
+            InitializeDrop();
+        }
+
+        /// <summary>
+        /// Initializes control drag and drop handler.
+        /// </summary>
+        public void InitializeDrop()
+        {
+            this.AllowDrop = true;
+
+            this.DragEnter +=
+                (s, e) =>
+                {
+                    if (!e.Data.GetDataPresent(typeof(Core2D.Template)))
+                    {
+                        e.Effects = DragDropEffects.None;
+                        e.Handled = true;
+                    }
+                };
+
+            this.Drop +=
+                (s, e) =>
+                {
+                    if (e.Data.GetDataPresent(typeof(Core2D.Template)))
+                    {
+                        try
+                        {
+                            var template = e.Data.GetData(typeof(Core2D.Template)) as Core2D.Template;
+                            if (template != null)
+                            {
+                                if (this.DataContext != null)
+                                {
+                                    var page = this.DataContext as Core2D.Page;
+                                    if (page != null)
+                                    {
+                                        page.Template = template;
+                                    }
+                                }
+                                e.Handled = true;
+                            }
+                        }
+                        catch (Exception) { }
+                    }
+                };
         }
     }
 }
