959e6b811fb5e551873153afbf5c0442bc586967
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-12-30 15:11:50 +0100

    Fixed UpdateDatabase method

--- Core2D/Editor/ProjectExtensions.cs
+++ Core2D/Editor/ProjectExtensions.cs
@@ -394,37 +394,39 @@ namespace Core2D
         /// <param name="project">The project instance.</param>
         /// <param name="destination">The destination database.</param>
         /// <param name="source">The source database.</param>
-        public static void ApplyDatabase(this Project project, Database destination, Database source)
+        public static void UpdateDatabase(this Project project, Database destination, Database source)
         {
             if (source.Columns.Length <= 1)
                 return;
 
-            // check for the Id column
+            // Check for the Id column.
             if (source.Columns[0].Name != Database.IdColumnName)
                 return;
 
-            // skip Id column for update
+            // Check columns length.
             if (source.Columns.Length - 1 != destination.Columns.Length)
                 return;
 
-            // check column names
+            // Check column names.
             for (int i = 1; i < source.Columns.Length; i++)
             {
                 if (source.Columns[i].Name != destination.Columns[i - 1].Name)
+                {
                     return;
+                }
             }
 
             bool isDirty = false;
             var recordsBuilder = destination.Records.ToBuilder();
 
+            // Update or remove existing records.
             for (int i = 0; i < destination.Records.Length; i++)
             {
                 var record = destination.Records[i];
-
                 var result = source.Records.FirstOrDefault(r => r.Id == record.Id);
                 if (result != null)
                 {
-                    // update existing record
+                    // Update existing record.
                     for (int j = 1; j < result.Values.Length; j++)
                     {
                         var valuesBuilder = record.Values.ToBuilder();
@@ -435,12 +437,25 @@ namespace Core2D
                 }
                 else
                 {
+                    // Remove existing record.
+                    recordsBuilder.Remove(record);
+                    isDirty = true;
+                }
+            }
+
+            // Add new records.
+            for (int i = 0; i < source.Records.Length; i++)
+            {
+                var record = source.Records[i];
+                var result = destination.Records.FirstOrDefault(r => r.Id == record.Id);
+                if (result == null)
+                {
                     var r = source.Records[i];
 
-                    // use existing columns
+                    // Use existing columns.
                     r.Columns = destination.Columns;
 
-                    // skip Id column
+                    // Skip Id column.
                     r.Values = r.Values.Skip(1).ToImmutableArray();
 
                     recordsBuilder.Add(r);
