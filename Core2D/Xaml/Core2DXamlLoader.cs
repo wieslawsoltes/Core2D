234c29caf248ccdd957ed0ee27cf680edd90a91b
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-01-03 01:38:48 +0100

    Added type converters support

--- Core2D/Xaml/Core2DXamlLoader.cs
+++ Core2D/Xaml/Core2DXamlLoader.cs
@@ -1,7 +1,9 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 using OmniXaml;
+using OmniXaml.Builder;
 using OmniXaml.Services.DotNetFx;
+using Core2D.Xaml.Converters;
 
 namespace Core2D.Xaml
 {
@@ -10,6 +12,12 @@ namespace Core2D.Xaml
         public static object Load(string path)
         {
             var wiringContext = WiringContext.FromAttributes(Assemblies.AssembliesInAppFolder);
+
+            var featureProvider = (TypeFeatureProvider)wiringContext.FeatureProvider;
+            featureProvider.AddTypeConverter(new TypeConverterRegistration(typeof(ArgbColor), new ArgbColorTypeConverter()));
+            featureProvider.AddTypeConverter(new TypeConverterRegistration(typeof(FontStyle), new FontStyleTypeConverter()));
+            featureProvider.AddTypeConverter(new TypeConverterRegistration(typeof(ShapeState), new ShapeStateTypeConverter()));
+
             var loader = new DefaultXamlLoader(wiringContext);
             return loader.FromPath(path);
         }
