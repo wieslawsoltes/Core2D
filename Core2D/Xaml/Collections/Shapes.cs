01aecaaa842e0e7bce87cb0726cf4e50db00fbee
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-01-03 01:38:09 +0100

    Added Content collections

---
+++ Core2D/Xaml/Collections/Shapes.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using OmniXaml.Attributes;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Core2D.Xaml.Collections
+{
+    [ContentProperty("Children")]
+    public class Shapes
+    {
+        public string Name { get; set; }
+        public ICollection<BaseShape> Children { get; set; }
+
+        public Shapes()
+        {
+            Children = new Collection<BaseShape>();
+        }
+    }
+}
