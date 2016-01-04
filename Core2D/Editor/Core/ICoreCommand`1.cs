f1a4037b29f7fd4e1213dfa6b46bd075c2f49b0c
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-01-05 00:00:35 +0100

    Added comments and inherit from ICoreCommand

--- Core2D/Editor/Core/ICoreCommand`1.cs
+++ Core2D/Editor/Core/ICoreCommand`1.cs
@@ -1,18 +1,13 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-using System.Windows.Input;
 
 namespace Core2D
 {
     /// <summary>
-    /// 
+    /// The generic core command interface.
     /// </summary>
-    /// <typeparam name="T"></typeparam>
-    public interface ICoreCommand<T> : ICommand
+    /// <typeparam name="T">The command parameter type.</typeparam>
+    public interface ICoreCommand<T> : ICoreCommand
     {
-        /// <summary>
-        /// 
-        /// </summary>
-        void NotifyCanExecuteChanged();
     }
 }
