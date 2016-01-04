9aa402ab373a8220fee994d54f3bab9b4d5dee0b
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-01-04 01:29:12 +0100

    Added ShapeStateConverter

---
+++ Dependencies/Serializer.Newtonsoft/Converters/ShapeStateConverter.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+using Newtonsoft.Json;
+using Core2D;
+
+namespace Dependencies
+{
+    /// <inheritdoc/>
+    internal class ShapeStateConverter : JsonConverter
+    {
+        /// <inheritdoc/>
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(ShapeState);
+        }
+
+        /// <inheritdoc/>
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            writer.WriteValue(((ShapeState)value).Flags.ToString());
+        }
+
+        /// <inheritdoc/>
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (objectType == typeof(ShapeState))
+            {
+                return ShapeState.Parse((string)reader.Value);
+            }
+            throw new ArgumentException("objectType");
+        }
+    }
+}
