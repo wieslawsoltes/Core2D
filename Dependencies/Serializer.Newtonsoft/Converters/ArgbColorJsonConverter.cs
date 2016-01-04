13e741f94656d3a90f2f4d31475b11a8f089427f
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-01-04 01:28:57 +0100

    Added ArgbColorJsonConverter

---
+++ Dependencies/Serializer.Newtonsoft/Converters/ArgbColorJsonConverter.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+using Newtonsoft.Json;
+using Core2D;
+
+namespace Dependencies
+{
+    /// <inheritdoc/>
+    internal class ArgbColorJsonConverter : JsonConverter
+    {
+        /// <inheritdoc/>
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(ArgbColor);
+        }
+
+        /// <inheritdoc/>
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            writer.WriteValue(ArgbColor.ToHtml((ArgbColor)value));
+        }
+
+        /// <inheritdoc/>
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (objectType == typeof(ArgbColor))
+            {
+                return ArgbColor.Parse((string)reader.Value);
+            }
+            throw new ArgumentException("objectType");
+        }
+    }
+}
