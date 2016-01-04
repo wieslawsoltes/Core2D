5b88a5fbe9f62bd228673c14275f772db233c032
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-01-04 01:29:04 +0100

    Added FontStyleConverter

---
+++ Dependencies/Serializer.Newtonsoft/Converters/FontStyleConverter.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+using Newtonsoft.Json;
+using Core2D;
+
+namespace Dependencies
+{
+    /// <inheritdoc/>
+    internal class FontStyleConverter : JsonConverter
+    {
+        /// <inheritdoc/>
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(FontStyle);
+        }
+
+        /// <inheritdoc/>
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            writer.WriteValue(((FontStyle)value).Flags.ToString());
+        }
+
+        /// <inheritdoc/>
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (objectType == typeof(FontStyle))
+            {
+                return FontStyle.Parse((string)reader.Value);
+            }
+            throw new ArgumentException("objectType");
+        }
+    }
+}
