8587b5c86bf8d054b92118eab48b9df9d3101626
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-01-06 23:11:41 +0100

    Added ObservableResource class

---
+++ Core2D/Core/ObservableResource.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Portable.Xaml.Markup;
+
+namespace Core2D
+{
+    /// <summary>
+    /// Base class for class with Xaml resources.
+    /// </summary>
+    [ContentProperty("Resources")]
+    public abstract class ObservableResource : ObservableObject
+    {
+        private IList<ObservableObject> _resources;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ObservableResource"/> class.
+        /// </summary>
+        public ObservableResource()
+            : base()
+        {
+            Resources = new ObservableCollection<ObservableObject>();
+        }
+
+        /// <summary>
+        /// Gets or sets shape resources.
+        /// </summary>
+        public IList<ObservableObject> Resources
+        {
+            get { return _resources; }
+            set { Update(ref _resources, value); }
+        }
+    }
+}
