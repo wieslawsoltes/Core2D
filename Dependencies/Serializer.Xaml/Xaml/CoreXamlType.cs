634affe25dafd4085f09330ce75b03a6145a425a
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-01-06 23:42:24 +0100

    Added missing Copyright header

--- Dependencies/Serializer.Xaml/Xaml/CoreXamlType.cs
+++ Dependencies/Serializer.Xaml/Xaml/CoreXamlType.cs
@@ -1,5 +1,6 @@
-﻿using System;
-using System.Reflection;
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
 using Portable.Xaml;
 using Portable.Xaml.Schema;
 
--- Dependencies/Serializer.Xaml/Xaml/CoreXamlTypeInvoker.cs
+++ Dependencies/Serializer.Xaml/Xaml/CoreXamlTypeInvoker.cs
@@ -1,4 +1,6 @@
-﻿using Portable.Xaml.Schema;
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using Portable.Xaml.Schema;
 
 namespace Dependencies
 {
