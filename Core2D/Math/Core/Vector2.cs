ce99b387103db835229a19942d466cf6594af6d9
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-01-11 16:03:49 +0100

    Use expression bodies

--- Core2D/Math/Core/Vector2.cs
+++ Core2D/Math/Core/Vector2.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 using System;
+using System.Globalization;
 
 namespace Core2D
 {
@@ -12,44 +13,32 @@ namespace Core2D
         /// <summary>
         /// 
         /// </summary>
-        public double X { get; private set; }
+        public double X { get; }
 
         /// <summary>
         /// 
         /// </summary>
-        public double Y { get; private set; }
+        public double Y { get; }
 
         /// <summary>
         /// 
         /// </summary>
-        public static Vector2 One
-        {
-            get { return new Vector2(1.0); }
-        }
+        public static Vector2 One => new Vector2(1.0);
 
         /// <summary>
         /// 
         /// </summary>
-        public static Vector2 Zero
-        {
-            get { return new Vector2(0.0); }
-        }
+        public static Vector2 Zero => new Vector2(0.0);
 
         /// <summary>
         /// 
         /// </summary>
-        public static Vector2 UnitX
-        {
-            get { return new Vector2(1.0, 0.0); }
-        }
+        public static Vector2 UnitX => new Vector2(1.0, 0.0);
 
         /// <summary>
         /// 
         /// </summary>
-        public static Vector2 UnitY
-        {
-            get { return new Vector2(0.0, 1.0); }
-        }
+        public static Vector2 UnitY => new Vector2(0.0, 1.0);
 
         /// <summary>
         /// Initializes a new <see cref="Vector2"/> instance.
@@ -58,8 +47,8 @@ namespace Core2D
         public Vector2(double value)
             : this()
         {
-            this.X = value;
-            this.Y = value;
+            X = value;
+            Y = value;
         }
 
         /// <summary>
@@ -70,18 +59,12 @@ namespace Core2D
         public Vector2(double x, double y)
             : this()
         {
-            this.X = x;
-            this.Y = y;
+            X = x;
+            Y = y;
         }
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <returns></returns>
-        public override string ToString()
-        {
-            return string.Concat(X, System.Globalization.CultureInfo.CurrentCulture.NumberFormat.NumberGroupSeparator, Y);
-        }
+        /// <inheritdoc/>
+        public override string ToString() => string.Concat(X, CultureInfo.CurrentCulture.NumberFormat.NumberGroupSeparator, Y);
 
         /// <summary>
         /// 
@@ -89,10 +72,7 @@ namespace Core2D
         /// <param name="v1"></param>
         /// <param name="v2"></param>
         /// <returns></returns>
-        public static bool operator <(Vector2 v1, Vector2 v2)
-        {
-            return v1.X < v2.X || (v1.X == v2.X && v1.Y < v2.Y);
-        }
+        public static bool operator <(Vector2 v1, Vector2 v2) => v1.X < v2.X || (v1.X == v2.X && v1.Y < v2.Y);
 
         /// <summary>
         /// 
@@ -100,115 +80,74 @@ namespace Core2D
         /// <param name="v1"></param>
         /// <param name="v2"></param>
         /// <returns></returns>
-        public static bool operator >(Vector2 v1, Vector2 v2)
-        {
-            return v1.X > v2.X || (v1.X == v2.X && v1.Y > v2.Y);
-        }
+        public static bool operator >(Vector2 v1, Vector2 v2) => v1.X > v2.X || (v1.X == v2.X && v1.Y > v2.Y);
 
         /// <summary>
         /// 
         /// </summary>
         /// <param name="v"></param>
         /// <returns></returns>
-        public int CompareTo(Vector2 v)
-        {
-            return (this > v) ? -1 : ((this < v) ? 1 : 0);
-        }
+        public int CompareTo(Vector2 v) => (this > v) ? -1 : ((this < v) ? 1 : 0);
 
         /// <summary>
         /// 
         /// </summary>
         /// <param name="v"></param>
         /// <returns></returns>
-        public bool Equals(Vector2 v)
-        {
-            return this.X == v.X && this.Y == v.Y;
-        }
+        public bool Equals(Vector2 v) => X == v.X && Y == v.Y;
 
         /// <summary>
         /// 
         /// </summary>
         /// <param name="obj"></param>
         /// <returns></returns>
-        public override bool Equals(object obj)
-        {
-            if (obj is Vector2)
-            {
-                return this.Equals((Vector2)obj);
-            }
-            return false;
-        }
+        public override bool Equals(object obj) => obj is Vector2 ? Equals((Vector2)obj) : false;
 
         /// <summary>
         /// 
         /// </summary>
         /// <returns></returns>
-        public override int GetHashCode()
-        {
-            return this.X.GetHashCode() + this.Y.GetHashCode();
-        }
+        public override int GetHashCode() => X.GetHashCode() + Y.GetHashCode();
 
         /// <summary>
         /// 
         /// </summary>
         /// <returns></returns>
-        public Vector2 Negate()
-        {
-            return new Vector2(-this.X, -this.Y);
-        }
+        public Vector2 Negate() => new Vector2(-X, -Y);
 
         /// <summary>
         /// 
         /// </summary>
         /// <returns></returns>
-        public Vector2 Perpendicular()
-        {
-            return new Vector2(-this.Y, this.X);
-        }
+        public Vector2 Perpendicular() => new Vector2(-Y, X);
 
         /// <summary>
         /// 
         /// </summary>
         /// <param name="v"></param>
         /// <returns></returns>
-        public Vector2 Subtract(Vector2 v)
-        {
-            return new Vector2(this.X - v.X, this.Y - v.Y);
-        }
+        public Vector2 Subtract(Vector2 v) => new Vector2(X - v.X, Y - v.Y);
 
         /// <summary>
         /// 
         /// </summary>
         /// <param name="v"></param>
         /// <returns></returns>
-        public Vector2 Add(Vector2 v)
-        {
-            return new Vector2(this.X + v.X, this.Y + v.Y);
-        }
+        public Vector2 Add(Vector2 v) => new Vector2(X + v.X, Y + v.Y);
 
         /// <summary>
         /// 
         /// </summary>
         /// <param name="scalar"></param>
         /// <returns></returns>
-        public Vector2 Multiply(double scalar)
-        {
-            return new Vector2(
-                this.X * scalar,
-                this.Y * scalar);
-        }
+        public Vector2 Multiply(double scalar) => new Vector2(X * scalar, Y * scalar);
 
         /// <summary>
         /// 
         /// </summary>
         /// <param name="v"></param>
         /// <returns></returns>
-        public Vector2 Multiply(Vector2 v)
-        {
-            return new Vector2(
-                this.X * v.X,
-                this.Y * v.Y);
-        }
+        public Vector2 Multiply(Vector2 v) => new Vector2(X * v.X, Y * v.Y);
 
         /// <summary>
         /// 
@@ -218,9 +157,7 @@ namespace Core2D
         public Vector2 Divide(double scalar)
         {
             double value = 1.0 / scalar;
-            return new Vector2(
-                this.X * value,
-                this.Y * value);
+            return new Vector2(X * value, Y * value);
         }
 
         /// <summary>
@@ -228,42 +165,28 @@ namespace Core2D
         /// </summary>
         /// <param name="v"></param>
         /// <returns></returns>
-        public Vector2 Divide(Vector2 v)
-        {
-            return new Vector2(
-                this.X / v.X,
-                this.Y / v.Y);
-        }
+        public Vector2 Divide(Vector2 v) => new Vector2(X / v.X, Y / v.Y);
 
         /// <summary>
         /// 
         /// </summary>
         /// <param name="v"></param>
         /// <returns></returns>
-        public double Dot(Vector2 v)
-        {
-            return (this.X * v.X) + (this.Y * v.Y);
-        }
+        public double Dot(Vector2 v) => (X * v.X) + (Y * v.Y);
 
         /// <summary>
         /// 
         /// </summary>
         /// <param name="v"></param>
         /// <returns></returns>
-        public double Angle(Vector2 v)
-        {
-            return Math.Acos(this.Dot(v));
-        }
+        public double Angle(Vector2 v) => Math.Acos(Dot(v));
 
         /// <summary>
         /// 
         /// </summary>
         /// <param name="v"></param>
         /// <returns></returns>
-        public double Cross(Vector2 v)
-        {
-            return (this.X * v.Y) - (this.Y * v.X);
-        }
+        public double Cross(Vector2 v) => (X * v.Y) - (Y * v.X);
 
         /// <summary>
         /// 
@@ -271,10 +194,7 @@ namespace Core2D
         /// <param name="v1"></param>
         /// <param name="v2"></param>
         /// <returns></returns>
-        public static bool operator ==(Vector2 v1, Vector2 v2)
-        {
-            return v1.X == v2.X && v1.Y == v2.Y;
-        }
+        public static bool operator ==(Vector2 v1, Vector2 v2) => v1.X == v2.X && v1.Y == v2.Y;
 
         /// <summary>
         /// 
@@ -282,20 +202,14 @@ namespace Core2D
         /// <param name="v1"></param>
         /// <param name="v2"></param>
         /// <returns></returns>
-        public static bool operator !=(Vector2 v1, Vector2 v2)
-        {
-            return v1.X != v2.X || v1.Y != v2.Y;
-        }
+        public static bool operator !=(Vector2 v1, Vector2 v2) => v1.X != v2.X || v1.Y != v2.Y;
 
         /// <summary>
         /// 
         /// </summary>
         /// <param name="v"></param>
         /// <returns></returns>
-        public static Vector2 operator -(Vector2 v)
-        {
-            return new Vector2(-v.X, -v.Y);
-        }
+        public static Vector2 operator -(Vector2 v) => new Vector2(-v.X, -v.Y);
 
         /// <summary>
         /// 
@@ -303,10 +217,7 @@ namespace Core2D
         /// <param name="v1"></param>
         /// <param name="v2"></param>
         /// <returns></returns>
-        public static Vector2 operator -(Vector2 v1, Vector2 v2)
-        {
-            return new Vector2(v1.X - v2.X, v1.Y - v2.Y);
-        }
+        public static Vector2 operator -(Vector2 v1, Vector2 v2) => new Vector2(v1.X - v2.X, v1.Y - v2.Y);
 
         /// <summary>
         /// 
@@ -314,10 +225,7 @@ namespace Core2D
         /// <param name="v1"></param>
         /// <param name="v2"></param>
         /// <returns></returns>
-        public static Vector2 operator +(Vector2 v1, Vector2 v2)
-        {
-            return new Vector2(v1.X + v2.X, v1.Y + v2.Y);
-        }
+        public static Vector2 operator +(Vector2 v1, Vector2 v2) => new Vector2(v1.X + v2.X, v1.Y + v2.Y);
 
         /// <summary>
         /// 
@@ -325,12 +233,7 @@ namespace Core2D
         /// <param name="v"></param>
         /// <param name="scalar"></param>
         /// <returns></returns>
-        public static Vector2 operator *(Vector2 v, double scalar)
-        {
-            return new Vector2(
-                v.X * scalar,
-                v.Y * scalar);
-        }
+        public static Vector2 operator *(Vector2 v, double scalar) => new Vector2(v.X * scalar, v.Y * scalar);
 
         /// <summary>
         /// 
@@ -338,12 +241,7 @@ namespace Core2D
         /// <param name="scalar"></param>
         /// <param name="v"></param>
         /// <returns></returns>
-        public static Vector2 operator *(double scalar, Vector2 v)
-        {
-            return new Vector2(
-                v.X * scalar,
-                v.Y * scalar);
-        }
+        public static Vector2 operator *(double scalar, Vector2 v) => new Vector2(v.X * scalar, v.Y * scalar);
 
         /// <summary>
         /// 
@@ -351,12 +249,7 @@ namespace Core2D
         /// <param name="v1"></param>
         /// <param name="v2"></param>
         /// <returns></returns>
-        public static Vector2 operator *(Vector2 v1, Vector2 v2)
-        {
-            return new Vector2(
-                v1.X * v2.X,
-                v1.Y * v2.Y);
-        }
+        public static Vector2 operator *(Vector2 v1, Vector2 v2) => new Vector2(v1.X * v2.X, v1.Y * v2.Y);
 
         /// <summary>
         /// 
@@ -367,9 +260,7 @@ namespace Core2D
         public static Vector2 operator /(Vector2 v, double scalar)
         {
             double value = 1.0 / scalar;
-            return new Vector2(
-                v.X * value,
-                v.Y * value);
+            return new Vector2(v.X * value, v.Y * value);
         }
 
         /// <summary>
@@ -378,78 +269,54 @@ namespace Core2D
         /// <param name="v1"></param>
         /// <param name="v2"></param>
         /// <returns></returns>
-        public static Vector2 operator /(Vector2 v1, Vector2 v2)
-        {
-            return new Vector2(
-                v1.X / v2.X,
-                v1.Y / v2.Y);
-        }
+        public static Vector2 operator /(Vector2 v1, Vector2 v2) => new Vector2(v1.X / v2.X, v1.Y / v2.Y);
 
         /// <summary>
         /// 
         /// </summary>
         /// <returns></returns>
-        public double Magnitude()
-        {
-            return Math.Sqrt(this.X * this.X + this.Y * this.Y);
-        }
+        public double Magnitude() => Math.Sqrt(X * X + Y * Y);
 
         /// <summary>
         /// 
         /// </summary>
         /// <returns></returns>
-        public double MagnitudeSquared()
-        {
-            return this.X * this.X + this.Y * this.Y;
-        }
+        public double MagnitudeSquared() => X * X + Y * Y;
 
         /// <summary>
         /// 
         /// </summary>
         /// <returns></returns>
-        public double Length()
-        {
-            return Math.Sqrt(this.X * this.X + this.Y * this.Y);
-        }
+        public double Length() => Math.Sqrt(X * X + Y * Y);
 
         /// <summary>
         /// 
         /// </summary>
         /// <returns></returns>
-        public double LengthSquared()
-        {
-            return this.X * this.X + this.Y * this.Y;
-        }
+        public double LengthSquared() => X * X + Y * Y;
 
         /// <summary>
         /// 
         /// </summary>
         /// <returns></returns>
-        public Vector2 Normalize()
-        {
-            return this / this.Length();
-        }
+        public Vector2 Normalize() => this / Length();
 
         /// <summary>
         /// 
         /// </summary>
         /// <param name="v"></param>
         /// <returns></returns>
-        public double Component(Vector2 v)
-        {
-            return this.Dot(v.Normalize());
-        }
+        public double Component(Vector2 v) => Dot(v.Normalize());
 
         /// <summary>
         /// 
         /// </summary>
         /// <param name="v"></param>
+        /// <remarks>
+        /// Alternative implementation: v.Normalize() * Component(v)
+        /// </remarks>
         /// <returns></returns>
-        public Vector2 Project(Vector2 v)
-        {
-            return v * (this.Dot(v) / v.Dot(v));
-            //return v.Normalize() * this.Component(v);
-        }
+        public Vector2 Project(Vector2 v) => v * (Dot(v) / v.Dot(v));
 
         /// <summary>
         /// 
@@ -458,10 +325,8 @@ namespace Core2D
         /// <returns></returns>
         public Vector2 Reflect(Vector2 normal)
         {
-            double dot = this.Dot(normal);
-            return new Vector2(
-                this.X - 2.0 * dot * normal.X,
-                this.Y - 2.0 * dot * normal.Y);
+            double dot = Dot(normal);
+            return new Vector2(X - 2.0 * dot * normal.X, Y - 2.0 * dot * normal.Y);
         }
 
         /// <summary>
@@ -469,24 +334,14 @@ namespace Core2D
         /// </summary>
         /// <param name="v"></param>
         /// <returns></returns>
-        public Vector2 Min(Vector2 v)
-        {
-            return new Vector2(
-                this.X < v.X ? this.X : v.X,
-                this.Y < v.Y ? this.Y : v.Y);
-        }
+        public Vector2 Min(Vector2 v) => new Vector2(X < v.X ? X : v.X, Y < v.Y ? Y : v.Y);
 
         /// <summary>
         /// 
         /// </summary>
         /// <param name="v"></param>
         /// <returns></returns>
-        public Vector2 Max(Vector2 v)
-        {
-            return new Vector2(
-                this.X > v.X ? this.X : v.X,
-                this.Y > v.Y ? this.Y : v.Y);
-        }
+        public Vector2 Max(Vector2 v) => new Vector2(X > v.X ? X : v.X, Y > v.Y ? Y : v.Y);
 
         /// <summary>
         /// 
@@ -494,10 +349,7 @@ namespace Core2D
         /// <param name="v"></param>
         /// <param name="amount"></param>
         /// <returns></returns>
-        public Vector2 Lerp(Vector2 v, double amount)
-        {
-            return this + (v - this) * amount;
-        }
+        public Vector2 Lerp(Vector2 v, double amount) => this + (v - this) * amount;
 
         /// <summary>
         /// 
@@ -519,10 +371,7 @@ namespace Core2D
         /// <param name="v"></param>
         /// <param name="amount"></param>
         /// <returns></returns>
-        public Vector2 Nlerp(Vector2 v, double amount)
-        {
-            return this.Lerp(v, amount).Normalize();
-        }
+        public Vector2 Nlerp(Vector2 v, double amount) => Lerp(v, amount).Normalize();
 
         /// <summary>
         /// 
@@ -531,8 +380,8 @@ namespace Core2D
         /// <returns></returns>
         public double Distance(Vector2 v)
         {
-            double dx = this.X - v.X;
-            double dy = this.Y - v.Y;
+            double dx = X - v.X;
+            double dy = Y - v.Y;
             return Math.Sqrt(dx * dx + dy * dy);
         }
 
@@ -541,12 +390,7 @@ namespace Core2D
         /// </summary>
         /// <param name="v"></param>
         /// <returns></returns>
-        public Vector2 Middle(Vector2 v)
-        {
-            return new Vector2(
-                (this.X + v.X) / 2.0,
-                (this.Y + v.Y) / 2.0);
-        }
+        public Vector2 Middle(Vector2 v) => new Vector2((X + v.X) / 2.0, (Y + v.Y) / 2.0);
 
         /// <summary>
         /// 
@@ -554,10 +398,7 @@ namespace Core2D
         /// <param name="a"></param>
         /// <param name="b"></param>
         /// <returns></returns>
-        public Vector2 NearestPointOnLine(Vector2 a, Vector2 b)
-        {
-            return (this - a).Project(b - a) + a;
-        }
+        public Vector2 NearestPointOnLine(Vector2 a, Vector2 b) => (this - a).Project(b - a) + a;
 
         /// <summary>
         /// 
@@ -576,9 +417,6 @@ namespace Core2D
         /// <param name="min"></param>
         /// <param name="max"></param>
         /// <returns></returns>
-        public static double Clamp(double value, double min, double max)
-        {
-            return value > max ? max : value < min ? min : value;
-        }
+        public static double Clamp(double value, double min, double max) => value > max ? max : value < min ? min : value;
     }
 }
