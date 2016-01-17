4c50929f7f15a0c4175f5bea05aea52d963e1fa1
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-01-17 14:43:08 +0100

    Added ShapeStateFlagsTests class

---
+++ Tests/Core2D.UnitTests/Shape/ShapeStateFlagsTests.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+using Xunit;
+
+namespace Core2D.UnitTests
+{
+    public class ShapeStateFlagsTests
+    {
+        [Fact]
+        public void Flags_Attribute_Not_Null()
+        {
+            Assert.NotNull(Attribute.GetCustomAttribute(typeof(ShapeStateFlags), typeof(FlagsAttribute)));
+        }
+    }
+}
