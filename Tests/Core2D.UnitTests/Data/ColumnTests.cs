c79499b92a8c053cdb4175c080fad06b75b3945d
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-01-18 19:36:17 +0100

    Added ColumnTests class

---
+++ Tests/Core2D.UnitTests/Data/ColumnTests.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using Xunit;
+
+namespace Core2D.UnitTests
+{
+    public class ColumnTests
+    {
+        [Fact]
+        public void Inherits_From_ObservableObject()
+        {
+            var target = new Column();
+            Assert.True(target is ObservableObject);
+        }
+    }
+}
