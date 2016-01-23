6d5bc6e55964f2f03452970d56a3aff77788b283
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-01-23 21:48:46 +0100

    Added ContainerTests class

---
+++ Tests/Core2D.UnitTests/Project/ContainerTests.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using Xunit;
+
+namespace Core2D.UnitTests
+{
+    public class ContainerTests
+    {
+        [Fact]
+        public void Inherits_From_ObservableResource()
+        {
+            var target = new Class1();
+            Assert.True(target is ObservableResource);
+        }
+
+        [Fact]
+        public void Layers_Not_Null()
+        {
+            var target = new Class1();
+            Assert.NotNull(target.Layers);
+        }
+
+        public class Class1 : Container
+        {
+        }
+    }
+}
