efb13895ad2a2c915cc888b2b67228d149ad4178
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-01-23 22:12:27 +0100

    Added new Container tests

--- Tests/Core2D.UnitTests/Project/ContainerTests.cs
+++ Tests/Core2D.UnitTests/Project/ContainerTests.cs
@@ -20,6 +20,91 @@ namespace Core2D.UnitTests
             Assert.NotNull(target.Layers);
         }
 
+        [Fact]
+        public void SetCurrentLayer_Sets_CurrentLayer()
+        {
+            var target = new Class1();
+
+            var layer = Layer.Create("Layer1", target);
+            target.Layers = target.Layers.Add(layer);
+
+            target.SetCurrentLayer(layer);
+
+            Assert.Equal(layer, target.CurrentLayer);
+        }
+
+        [Fact]
+        public void Invalidate_Should_Invalidate_All_Layers()
+        {
+            var target = new Class1();
+
+            var layer1 = Layer.Create("Layer1", target);
+            var layer2 = Layer.Create("Layer2", target);
+            target.Layers = target.Layers.Add(layer1);
+            target.Layers = target.Layers.Add(layer2);
+
+            var workingLayer = Layer.Create("WorkingLayer", target);
+            var helperLayer = Layer.Create("HelperLayer", target);
+            target.WorkingLayer = workingLayer;
+            target.HelperLayer = helperLayer;
+
+            int count = 0;
+
+            layer1.InvalidateLayer += (sender, e) => count++;
+            layer2.InvalidateLayer += (sender, e) => count++;
+            workingLayer.InvalidateLayer += (sender, e) => count++;
+            helperLayer.InvalidateLayer += (sender, e) => count++;
+
+            target.Invalidate();
+
+            Assert.Equal(4, count);
+        }
+
+        [Fact]
+        public void Template_Not_Null_Width_Returns_Template_Width()
+        {
+            var target = new Class1()
+            {
+                Width = 300,
+                Template = new Class1()
+                {
+                    Width = 400
+                }
+            };
+
+            Assert.Equal(400, target.Width);
+        }
+
+        [Fact]
+        public void Template_Not_Null_Width_Returns_Template_Height()
+        {
+            var target = new Class1()
+            {
+                Height = 300,
+                Template = new Class1()
+                {
+                    Height = 400
+                }
+            };
+
+            Assert.Equal(400, target.Height);
+        }
+
+        [Fact]
+        public void Template_Not_Null_Background_Returns_Template_Background()
+        {
+            var target = new Class1()
+            {
+                Background = ArgbColor.Create(),
+                Template = new Class1()
+                {
+                    Background = ArgbColor.Create()
+                }
+            };
+
+            Assert.Equal(target.Template.Background, target.Background);
+        }
+
         public class Class1 : Container
         {
         }
