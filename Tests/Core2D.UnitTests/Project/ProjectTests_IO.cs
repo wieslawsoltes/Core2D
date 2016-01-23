6f69bf6c0b6b86d9bddae270c27e2d107cae0d9b
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-01-24 00:14:13 +0100

    Added ProjectTests_IO class

---
+++ Tests/Core2D.UnitTests/Project/ProjectTests_IO.cs
@@ -0,0 +1,69 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using Xunit;
+
+namespace Core2D.UnitTests
+{
+    public class ProjectTests_IO
+    {
+        [Fact(Skip = "Need to write test.")]
+        public void Open_From_Path()
+        {
+
+        }
+
+        [Fact(Skip = "Need to write test.")]
+        public void Open_From_Stream()
+        {
+
+        }
+
+        [Fact(Skip = "Need to write test.")]
+        public void Save_To_Path()
+        {
+
+        }
+
+        [Fact(Skip = "Need to write test.")]
+        public void Save_To_Stream()
+        {
+
+        }
+
+        [Fact(Skip = "Need to write test.")]
+        public void ReadBinary_Read_All_Bytes_From_Stream()
+        {
+
+        }
+
+        [Fact(Skip = "Need to write test.")]
+        public void WriteBinary_Write_All_Bytes_To_Stream()
+        {
+
+        }
+
+        [Fact(Skip = "Need to write test.")]
+        public void ReadUtf8Text_Read_String_From_Stream()
+        {
+
+        }
+
+        [Fact(Skip = "Need to write test.")]
+        public void WriteUtf8Text_Write_String_To_Stream()
+        {
+
+        }
+
+        [Fact(Skip = "Need to write test.")]
+        public void ReadUtf8Text_Read_String_From_Path()
+        {
+
+        }
+
+        [Fact(Skip = "Need to write test.")]
+        public void WriteUtf8Text_Write_String_To_Path()
+        {
+
+        }
+    }
+}
