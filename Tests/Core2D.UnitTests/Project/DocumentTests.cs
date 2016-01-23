5064838b6c390aedaab37840c8694f71bbd8f34d
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-01-23 23:00:07 +0100

    Added DocumentTests class

---
+++ Tests/Core2D.UnitTests/Project/DocumentTests.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using Xunit;
+
+namespace Core2D.UnitTests
+{
+    public class DocumentTests
+    {
+        [Fact]
+        public void Inherits_From_ObservableResource()
+        {
+            var target = new Document();
+            Assert.True(target is ObservableResource);
+        }
+
+        [Fact]
+        public void Pages_Not_Null()
+        {
+            var target = new Document();
+            Assert.NotNull(target.Pages);
+        }
+    }
+}
