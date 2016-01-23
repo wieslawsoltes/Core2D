6450472b49193684904feef55c0e63e83a71eac1
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-01-23 15:25:13 +0100

    Added TextStyleTests class

---
+++ Tests/Core2D.UnitTests/Style/TextStyleTests.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using Xunit;
+
+namespace Core2D.UnitTests
+{
+    public class TextStyleTests
+    {
+        [Fact]
+        public void Inherits_From_ObservableObject()
+        {
+            var target = new TextStyle();
+            Assert.True(target is ObservableObject);
+        }
+    }
+}
