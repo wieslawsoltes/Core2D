d973ff3444e6d24f0d86855b845be6a0866f599d
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-01-24 21:38:42 +0100

    Added IFileSystem interface

---
+++ Core2D/IO/IFileSystem.cs
@@ -0,0 +1,68 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System.IO;
+
+namespace Core2D
+{
+    /// <summary>
+    /// Defines file system contract.
+    /// </summary>
+    public interface IFileSystem
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        Stream Open(string path);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        Stream Create(string path);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <returns></returns>
+        byte[] ReadBinary(Stream stream);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <param name="bytes"></param>
+        void WriteBinary(Stream stream, byte[] bytes);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <returns></returns>
+        string ReadUtf8Text(Stream stream);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <param name="text"></param>
+        void WriteUtf8Text(Stream stream, string text);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        string ReadUtf8Text(string path);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="text"></param>
+        void WriteUtf8Text(string path, string text);
+    }
+}
