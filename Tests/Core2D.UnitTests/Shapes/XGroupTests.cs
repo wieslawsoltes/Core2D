af61f39c206c45e0966b5e7a0e4640c1e0c44e68
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-01-26 20:44:48 +0100

    Added AddConnectorAsInput_Add_Point_To_Connectors_As_Input test

--- Tests/Core2D.UnitTests/Shapes/XGroupTests.cs
+++ Tests/Core2D.UnitTests/Shapes/XGroupTests.cs
@@ -135,11 +135,19 @@ namespace Core2D.UnitTests
             Assert.Contains(point, target.Connectors);
         }
 
-        [Fact(Skip = "Need to write test.")]
+        [Fact]
         [Trait("Core2D", "Shapes")]
         public void AddConnectorAsInput_Add_Point_To_Connectors_As_Input()
         {
+            var target = new XGroup();
+            var point = new XPoint();
 
+            target.AddConnectorAsInput(point);
+
+            Assert.Equal(point.Owner, target);
+            Assert.True(point.State.Flags.HasFlag(ShapeStateFlags.Connector | ShapeStateFlags.Input));
+            Assert.False(point.State.Flags.HasFlag(ShapeStateFlags.Standalone));
+            Assert.Contains(point, target.Connectors);
         }
 
         [Fact(Skip = "Need to write test.")]
