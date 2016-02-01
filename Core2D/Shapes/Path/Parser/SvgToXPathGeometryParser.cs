0dd0da3b3f27409a3f393791633deee9e22971ea
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-02-01 22:44:57 +0100

    Split classes and added Parser folder

--- Core2D/Shapes/Path/Parser/SvgToXPathGeometryParser.cs
+++ Core2D/Shapes/Path/Parser/SvgToXPathGeometryParser.cs
@@ -7,67 +7,9 @@ using System.Globalization;
 namespace Core2D
 {
     /// <summary>
-    /// <see cref="XPathGeometry"/> mini-language string representation parser.
-    /// </summary>
-    public static class XPathGeometryParser
-    {
-        /// <summary>
-        /// Parse a mini-language string representation of the <see cref="XPathGeometry"/>.
-        /// </summary>
-        /// <remarks>
-        /// The path geometry syntax may start with a "wsp*Fwsp*(0|1)" which indicate the winding mode (F0 is EvenOdd while F1 is NonZero).
-        /// </remarks>
-        /// <param name="source">The string with geometry data.</param>
-        /// <returns>The new instance of the <see cref="XPathGeometry"/> class.</returns>
-        public static XPathGeometry Parse(string source)
-        {
-            var fillRule = XFillRule.EvenOdd;
-            var geometry = XPathGeometry.Create(new List<XPathFigure>(), fillRule);
-
-            if (source != null)
-            {
-                int curIndex = 0;
-                while ((curIndex < source.Length) && Char.IsWhiteSpace(source, curIndex))
-                {
-                    curIndex++;
-                }
-
-                if (curIndex < source.Length)
-                {
-                    if (source[curIndex] == 'F')
-                    {
-                        curIndex++;
-
-                        while ((curIndex < source.Length) && Char.IsWhiteSpace(source, curIndex))
-                        {
-                            curIndex++;
-                        }
-
-                        if ((curIndex == source.Length) || ((source[curIndex] != '0') && (source[curIndex] != '1')))
-                        {
-                            throw new FormatException("Illegal token.");
-                        }
-
-                        fillRule = source[curIndex] == '0' ? XFillRule.EvenOdd : XFillRule.Nonzero;
-                        curIndex++;
-                    }
-                }
-
-                var parser = new SvgPathGeometryParser();
-
-                parser.Parse(geometry, source, curIndex);
-            }
-
-            geometry.FillRule = fillRule;
-
-            return geometry;
-        }
-    }
-
-    /// <summary>
     /// Parser for SVG path geometry http://www.w3.org/TR/SVG11/paths.html.
     /// </summary>
-    public class SvgPathGeometryParser
+    public class SvgToXPathGeometryParser
     {
         private const bool _allowSign = true;
         private const bool _allowComma = true;
