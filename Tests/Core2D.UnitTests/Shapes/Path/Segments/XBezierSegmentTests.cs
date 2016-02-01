67f6e201dff5d6aa42bf6f9fc6d13037eaa842b7
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-02-01 22:39:37 +0100

    Added path Segments test classes

---
+++ Tests/Core2D.UnitTests/Shapes/Path/Segments/XBezierSegmentTests.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System.Linq;
+using Xunit;
+
+namespace Core2D.UnitTests
+{
+    public class XBezierSegmentTests
+    {
+        [Fact]
+        [Trait("Core2D", "Path")]
+        public void GetPoints_Should_Return_All_Segment_Points()
+        {
+            var segment = new XBezierSegment()
+            {
+                Point1 = new XPoint(),
+                Point2 = new XPoint(),
+                Point3 = new XPoint()
+            };
+
+            var target = segment.GetPoints();
+
+            Assert.Equal(3, target.Count());
+
+            Assert.Contains(segment.Point1, target);
+            Assert.Contains(segment.Point2, target);
+            Assert.Contains(segment.Point3, target);
+        }
+
+        [Fact]
+        [Trait("Core2D", "Path")]
+        public void ToString_Should_Path_Markup()
+        {
+            var target = new XBezierSegment()
+            {
+                Point1 = new XPoint(),
+                Point2 = new XPoint(),
+                Point3 = new XPoint()
+            };
+
+            var actual = target.ToString();
+
+            Assert.Equal("C0,0 0,0 0,0", actual);
+        }
+    }
+}
---
+++ Tests/Core2D.UnitTests/Shapes/Path/Segments/XPolyQuadraticBezierSegmentTests.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System.Linq;
+using Xunit;
+
+namespace Core2D.UnitTests
+{
+    public class XPolyQuadraticBezierSegmentTests
+    {
+        [Fact]
+        [Trait("Core2D", "Path")]
+        public void Points_Not_Null()
+        {
+            var target = new XPolyQuadraticBezierSegment();
+            Assert.NotNull(target.Points);
+        }
+
+        [Fact]
+        [Trait("Core2D", "Path")]
+        public void GetPoints_Should_Return_All_Segment_Points()
+        {
+            var segment = new XPolyQuadraticBezierSegment();
+            segment.Points.Add(new XPoint());
+            segment.Points.Add(new XPoint());
+            segment.Points.Add(new XPoint());
+            segment.Points.Add(new XPoint());
+            segment.Points.Add(new XPoint());
+
+            var target = segment.GetPoints();
+
+            Assert.Equal(5, target.Count());
+
+            Assert.Equal(segment.Points, target);
+        }
+
+        [Fact]
+        [Trait("Core2D", "Path")]
+        public void ToString_Should_Path_Markup()
+        {
+            var target = new XPolyQuadraticBezierSegment();
+            target.Points.Add(new XPoint());
+            target.Points.Add(new XPoint());
+            target.Points.Add(new XPoint());
+            target.Points.Add(new XPoint());
+            target.Points.Add(new XPoint());
+
+            var actual = target.ToString();
+
+            Assert.Equal("Q0,0 0,0 0,0 0,0 0,0", actual);
+        }
+    }
+}
