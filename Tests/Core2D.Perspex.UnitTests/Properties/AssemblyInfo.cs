1ec8d8cec58f11cbe9a2335b607ec243f5e6dc31
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-02-02 21:53:10 +0100

    Added Core2D.Perspex.UnitTests project

---
+++ Tests/Core2D.Perspex.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Core2D.Perspex.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Wiesław Šoltés")]
+[assembly: AssemblyProduct("Core2D.Perspex.UnitTests")]
+[assembly: AssemblyCopyright("Copyright © Wiesław Šoltés 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("ad91e9dc-52e7-4883-b03c-7ba87a2b44a5")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
