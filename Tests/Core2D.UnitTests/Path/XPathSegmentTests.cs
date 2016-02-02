0256919e8a0c05d0658d60054e38cfa725c87161
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-02-02 19:28:33 +0100

    Added XPathSegmentTests tests

--- Tests/Core2D.UnitTests/Path/XPathSegmentTests.cs
+++ Tests/Core2D.UnitTests/Path/XPathSegmentTests.cs
@@ -1,11 +1,47 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace Core2D.UnitTests
 {
     public class XPathSegmentTests
     {
-        // TODO: Write XPathSegmentTests tests.
+        [Fact]
+        [Trait("Core2D", "Path")]
+        public void ToString_Should_Return_Empty()
+        {
+            var segment = new Class1();
+
+            var target = new List<XPoint>();
+            var actual = segment.ToString(target);
+
+            Assert.Equal(string.Empty, actual);
+        }
+
+        [Fact]
+        [Trait("Core2D", "Path")]
+        public void ToString_Should_Return_Points()
+        {
+            var segment = new Class1();
+
+            var target = new List<XPoint>();
+            target.Add(new XPoint());
+            target.Add(new XPoint());
+            target.Add(new XPoint());
+
+            var actual = segment.ToString(target);
+
+            Assert.Equal("0,0 0,0 0,0", actual);
+        }
+
+        public class Class1 : XPathSegment
+        {
+            public override IEnumerable<XPoint> GetPoints()
+            {
+                throw new NotImplementedException();
+            }
+        }
     }
 }
