3341e53ea3a7394aa6f2ac57606f3935fb7a9298
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-02-06 14:30:44 +0100

    Inherit from IDisposable

--- Core2D/Interfaces/ILog.cs
+++ Core2D/Interfaces/ILog.cs
@@ -1,12 +1,13 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
 
 namespace Core2D
 {
     /// <summary>
     /// Defines logger contract.
     /// </summary>
-    public interface ILog
+    public interface ILog : IDisposable
     {
         /// <summary>
         /// Gets or sets last log message.
