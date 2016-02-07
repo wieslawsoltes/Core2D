6e904ddc77b235dd67c1e4da6f57ae1eec615a9e
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-02-07 14:53:58 +0100

    Added SelectableTests class

---
+++ Tests/Core2D.UnitTests/Project/SelectableTests.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using Xunit;
+
+namespace Core2D.UnitTests
+{
+    public class SelectableTests
+    {
+        [Fact]
+        [Trait("Core2D.Project", "Project")]
+        public void Inherits_From_ObservableResource()
+        {
+            var target = new Class1();
+            Assert.True(target is ObservableResource);
+        }
+        
+        private class Class1 : Selectable
+        {
+        }
+    }
+}
