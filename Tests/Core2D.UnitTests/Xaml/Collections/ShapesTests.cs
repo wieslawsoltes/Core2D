87c6ee16d027f8d818dbfb82621c6d8556d48bd9
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-02-07 14:54:58 +0100

    Added ShapesTests tests

--- Tests/Core2D.UnitTests/Xaml/Collections/ShapesTests.cs
+++ Tests/Core2D.UnitTests/Xaml/Collections/ShapesTests.cs
@@ -6,6 +6,20 @@ namespace Core2D.UnitTests
 {
     public class ShapesTests
     {
-        // TODO: Write ShapesTests tests.
+        [Fact]
+        [Trait("Core2D.Xaml", "Collections")]
+        public void Inherits_From_ObservableResource()
+        {
+            var target = new Shapes();
+            Assert.True(target is ObservableResource);
+        }
+
+        [Fact]
+        [Trait("Core2D.Xaml", "Collections")]
+        public void Children_Not_Null()
+        {
+            var target = new Shapes();
+            Assert.NotNull(target.Children);
+        }
     }
 }
