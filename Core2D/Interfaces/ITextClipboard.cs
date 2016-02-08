2010191e7a0608601ae01c33e7b043ba1d800430
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-02-09 00:11:46 +0100

    Added Interfaces namespace

--- Core2D/Interfaces/ITextClipboard.cs
+++ Core2D/Interfaces/ITextClipboard.cs
@@ -2,7 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 using System.Threading.Tasks;
 
-namespace Core2D
+namespace Core2D.Interfaces
 {
     /// <summary>
     /// Defines text clipboard contract.
