756b72d687863090f361eb6055bd9a8905e6d54f
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-02-08 17:15:27 +0100

    Added base CommandManager class

---
+++ Core2D/Editor/Input/CommandManager.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System.Collections.Generic;
+
+namespace Core2D
+{
+    /// <summary>
+    /// Base commands manager.
+    /// </summary>
+    public class CommandManager : ICommandManager
+    {
+        /// <inheritdoc/>
+        public IDictionary<string, ICoreCommand> Registered { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CommandManager"/> class.
+        /// </summary>
+        public CommandManager()
+        {
+            Registered = new Dictionary<string, ICoreCommand>();
+        }
+
+        /// <inheritdoc/>
+        public void Register(string name, ICoreCommand command)
+        {
+            if (!Registered.ContainsKey(name))
+            {
+                Registered.Add(name, command);
+            }
+        }
+
+        /// <inheritdoc/>
+        public void NotifyCanExecuteChanged()
+        {
+            if (Registered != null)
+            {
+                foreach (var command in Registered)
+                {
+                    command.Value.NotifyCanExecuteChanged();
+                }
+            }
+        }
+    }
+}
