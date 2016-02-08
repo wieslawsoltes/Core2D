da68a645f76bd3c671be9b1d3bcd1c60d2ac9bda
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-02-09 00:15:20 +0100

    Added Shape namespace

--- Core2D/Shape/ShapeStateFlags.cs
+++ Core2D/Shape/ShapeStateFlags.cs
@@ -2,7 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 using System;
 
-namespace Core2D
+namespace Core2D.Shape
 {
     /// <summary>
     /// Specifies shape state flags.
