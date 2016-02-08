7b0b62038d03c39c49115d702dbaf1cddc68f48c
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-02-09 00:13:42 +0100

    Added Path namespace

--- Core2D/Path/XPathGeometry.cs
+++ Core2D/Path/XPathGeometry.cs
@@ -1,11 +1,12 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using Core2D.Xaml.Converters;
+using Portable.Xaml.ComponentModel;
+using Portable.Xaml.Markup;
 using System.Collections.Generic;
 using System.Text;
-using Portable.Xaml.Markup;
-using Portable.Xaml.ComponentModel;
 
-namespace Core2D
+namespace Core2D.Path
 {
     /// <summary>
     /// Path geometry.
