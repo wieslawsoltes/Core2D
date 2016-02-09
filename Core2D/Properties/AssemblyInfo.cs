f0b8adaaa9bd887fcf4527305428711b5ccdcd0a
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-02-09 19:40:08 +0100

    Reorderer usings

--- Core2D/Properties/AssemblyInfo.cs
+++ Core2D/Properties/AssemblyInfo.cs
@@ -1,9 +1,9 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-using System.Resources;
-using System.Reflection;
-using System.Runtime.CompilerServices;
 using Portable.Xaml.Markup;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("Core2D")]
 [assembly: AssemblyDescription("")]
