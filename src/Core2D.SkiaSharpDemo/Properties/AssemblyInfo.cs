1e4f82fe1b6eef60bdde0d8a2314d2456c97e834
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-02-25 17:32:46 +0100

    Added empty SkiaSharpDemo project

---
+++ src/Core2D.SkiaSharpDemo/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Core2D.SkiaSharpDemo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Core2D.SkiaSharpDemo")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("9311c318-31ec-467d-a285-65fa2f0f92b4")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
