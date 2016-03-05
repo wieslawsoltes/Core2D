337b74291dd32bb498f565dab7631baf6c95a09f
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-03-05 17:46:00 +0100

    Added custom behaviors

---
+++ src/Core2D.Perspex/Interactions/Behaviors/FocusOnAttachedToVisualTreeBehavior.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using Perspex;
+using Perspex.Controls;
+using Perspex.Xaml.Interactivity;
+
+namespace Core2D.Perspex.Interactions.Behaviors
+{
+    public class FocusOnAttachedToVisualTreeBehavior : Behavior<Control>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.AttachedToVisualTree += AttachedToVisualTree;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            AssociatedObject.AttachedToVisualTree -= AttachedToVisualTree;
+        }
+
+        private void AttachedToVisualTree(object sender, VisualTreeAttachmentEventArgs e)
+        {
+            AssociatedObject.Focus();
+        }
+    }
+}
