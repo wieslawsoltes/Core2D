5f39928f74ce07d8e1278b43d22c28afddd517ce
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-03-05 17:45:39 +0100

    Added FocusControlAction class

---
+++ src/Core2D.Perspex/Interactions/Actions/FocusControlAction.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using Perspex;
+using Perspex.Controls;
+using Perspex.Xaml.Interactivity;
+
+namespace Core2D.Perspex.Interactions.Actions
+{
+    public class FocusControlAction : PerspexObject, IAction
+    {
+        public object Execute(object sender, object parameter)
+        {
+            (sender as Control)?.Focus();
+            return null;
+        }
+    }
+}
