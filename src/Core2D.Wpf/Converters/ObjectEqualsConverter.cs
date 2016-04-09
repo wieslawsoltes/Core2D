c86e7916ae86121b9a8e594640279184d6962937
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-04-09 11:02:54 +0200

    Renamed EnumToBooleanConverter to ObjectEqualsConverter

--- src/Core2D.Wpf/Converters/ObjectEqualsConverter.cs
+++ src/Core2D.Wpf/Converters/ObjectEqualsConverter.cs
@@ -8,17 +8,17 @@ using System.Windows.Data;
 namespace Core2D.Wpf.Converters
 {
     /// <summary>
-    /// Enum to Boolean value converter.
+    /// Converts a binding value object from <see cref="object"/> to <see cref="bool"/> True if value is equal to parameter otherwise return False.
     /// </summary>
-    public class EnumToBooleanConverter : IValueConverter
+    public class ObjectEqualsConverter : IValueConverter
     {
         /// <summary>
-        /// Convert enum value to boolean, true if matches parameter.
+        /// Convert value to boolean, true if matches parameter.
         /// </summary>
-        /// <param name="value">The value produced by the binding source.</param>
-        /// <param name="targetType">The type of the binding target property.</param>
-        /// <param name="parameter">The converter parameter to use.</param>
-        /// <param name="culture">The culture to use in the converter.</param>
+        /// <param name="value">The value to convert.</param>
+        /// <param name="targetType">The type of the target.</param>
+        /// <param name="parameter">A user-defined parameter.</param>
+        /// <param name="culture">The culture to use.</param>
         /// <returns>A converted value.</returns>
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
@@ -30,12 +30,12 @@ namespace Core2D.Wpf.Converters
         }
 
         /// <summary>
-        /// Convert boolean to enum, returning parameter if true.
+        /// Convert boolean to parameter value, returning parameter if true.
         /// </summary>
-        /// <param name="value">The value that is produced by the binding target.</param>
-        /// <param name="targetType">The type to convert to.</param>
-        /// <param name="parameter">The converter parameter to use.</param>
-        /// <param name="culture">The culture to use in the converter.</param>
+        /// <param name="value">The value to convert.</param>
+        /// <param name="targetType">The type of the target.</param>
+        /// <param name="parameter">A user-defined parameter.</param>
+        /// <param name="culture">The culture to use.</param>
         /// <returns>A converted value.</returns>
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
