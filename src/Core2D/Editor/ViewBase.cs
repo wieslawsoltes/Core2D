62b944493ef80ba93400e703c0eaa93d9c659363
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-04-09 15:01:07 +0200

    Added ViewBase class

---
+++ src/Core2D/Editor/ViewBase.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Core2D.Editor
+{
+    /// <summary>
+    /// View base class.
+    /// </summary>
+    public abstract class ViewBase : ObservableObject
+    {
+        private object _dataContext;
+
+        /// <summary>
+        /// Gets or sets data context.
+        /// </summary>
+        public object DataContext
+        {
+            get { return _dataContext; }
+            set { Update(ref _dataContext, value); }
+        }
+    }
+}
