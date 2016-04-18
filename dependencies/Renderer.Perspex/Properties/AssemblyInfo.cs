2855e782dd575e06e8643105d91304a279da042d
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-04-18 17:28:01 +0200

    Make Renderer.Perspex protable

--- dependencies/Renderer.Perspex/Properties/AssemblyInfo.cs
+++ dependencies/Renderer.Perspex/Properties/AssemblyInfo.cs
@@ -1,12 +1,11 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System.Resources;
 using System.Reflection;
-using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Renderer.Perspex")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyProduct("Renderer.Perspex")]
 [assembly: AssemblyCulture("")]
-[assembly: ComVisible(false)]
-[assembly: Guid("dee86be6-43b1-45fb-8a56-7977c1d0964e")]
+[assembly: NeutralResourcesLanguage("en")]
