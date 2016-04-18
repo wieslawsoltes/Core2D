78b552c23df90b54e406ae30976ca71bc05ec46b
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-04-18 16:41:50 +0200

    Added Core2D.Perspex.Shared project

---
+++ src/Core2D.Perspex.Shared/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("Core2D.Perspex.Shared")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("Core2D.Perspex.Shared")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: InternalsVisibleTo("Core2D.Perspex.UnitTests")]
