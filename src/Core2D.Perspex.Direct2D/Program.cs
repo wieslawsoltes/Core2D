2a4718aafb69dd691a2f819d27ffe173e5503e01
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-04-18 23:00:12 +0200

    Added Core2D.Perspex.Direct2D project

---
+++ src/Core2D.Perspex.Direct2D/Program.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using Core2D.Interfaces;
+using FileSystem.DotNetFx;
+using Log.Trace;
+using Perspex;
+using Perspex.Logging.Serilog;
+using Serilog;
+
+namespace Core2D.Perspex.Direct2D
+{
+    /// <summary>
+    /// Encapsulates a Core2D Prespex program.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Program entry point.
+        /// </summary>
+        /// <param name="args">The program arguments.</param>
+        private static void Main(string[] args)
+        {
+            InitializeLogging();
+
+            using (ILog log = new TraceLog())
+            {
+                IFileSystem fileIO = new DotNetFxFileSystem();
+                new App().UseWin32().UseDirect2D().LoadFromXaml().Start(fileIO, log);
+            }
+        }
+
+        /// <summary>
+        /// Initialize the Serilog logger.
+        /// </summary>
+        private static void InitializeLogging()
+        {
+#if DEBUG
+            SerilogLogger.Initialize(new LoggerConfiguration()
+                .MinimumLevel.Warning()
+                .WriteTo.Trace(outputTemplate: "{Area}: {Message}")
+                .CreateLogger());
+#endif
+        }
+    }
+}
