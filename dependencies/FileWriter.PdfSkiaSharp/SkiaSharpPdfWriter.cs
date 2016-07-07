e75ecceda48dfd43f5dfe9e894b96269129c2b66
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-07-07 23:44:49 +0200

    Added FileWriter.PdfSkiaSharp project

---
+++ dependencies/FileWriter.PdfSkiaSharp/SkiaSharpPdfWriter.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using Core2D.Interfaces;
+using Core2D.Project;
+using Core2D.Renderer;
+using Core2D.Shape;
+using Renderer.SkiaSharp;
+
+namespace FileWriter.PdfSkiaSharp
+{
+    /// <summary>
+    /// Pdf file writer.
+    /// </summary>
+    public sealed class PdfWriter : IFileWriter
+    {
+        /// <inheritdoc/>
+        string IFileWriter.Name { get; } = "Pdf";
+
+        /// <inheritdoc/>
+        string IFileWriter.Extension { get; } = "pdf";
+
+        /// <inheritdoc/>
+        void IFileWriter.Save(string path, object item, object options)
+        {
+            if (string.IsNullOrEmpty(path) || item == null)
+                return;
+
+            var ic = options as IImageCache;
+            if (options == null)
+                return;
+
+            var r = new SkiaRenderer();
+            r.State.DrawShapeState.Flags = ShapeStateFlags.Printable;
+            r.State.ImageCache = ic;
+
+            if (item is XContainer)
+            {
+                r.Save(path, item as XContainer);
+            }
+            else if (item is XDocument)
+            {
+                r.Save(path, item as XDocument);
+            }
+            else if (item is XProject)
+            {
+                r.Save(path, item as XProject);
+            }
+        }
+    }
+}
