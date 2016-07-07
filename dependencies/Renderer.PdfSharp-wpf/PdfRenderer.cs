0b06bdd1827ba690db7aa46daf2b68cbb8b3428c
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-07-07 23:44:00 +0200

    Sorted usings

--- dependencies/Renderer.PdfSharp-wpf/PdfRenderer.cs
+++ dependencies/Renderer.PdfSharp-wpf/PdfRenderer.cs
@@ -1,11 +1,11 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-using PdfSharp;
-using PdfSharp.Drawing;
-using PdfSharp.Pdf;
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using PdfSharp;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
 #if WPF
 using System.Windows.Media.Imaging;
 #endif
