b848a603a393a423103cd1bb4e4f266d380e4c7d
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-07-10 13:35:17 +0200

    Added IProjectExporter interface

---
+++ src/Core2D/Interfaces/IProjectExporter.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using Core2D.Project;
+
+namespace Core2D.Interfaces
+{
+    /// <summary>
+    /// Defines project exporter contract.
+    /// </summary>
+    public interface IProjectExporter
+    {
+        /// <summary>
+        /// Save <see cref="XContainer"/> to a file.
+        /// </summary>
+        /// <param name="path">The file path.</param>
+        /// <param name="container">The container instance.</param>
+        void Save(string path, XContainer container);
+
+        /// <summary>
+        /// Save <see cref="XDocument"/> to a file.
+        /// </summary>
+        /// <param name="path">The file path.</param>
+        /// <param name="document">The project instance.</param>
+        void Save(string path, XDocument document);
+
+        /// <summary>
+        /// Save <see cref="XProject"/> to a file.
+        /// </summary>
+        /// <param name="path">The file path.</param>
+        /// <param name="project">The project instance.</param>
+        void Save(string path, XProject project);
+    }
+}
