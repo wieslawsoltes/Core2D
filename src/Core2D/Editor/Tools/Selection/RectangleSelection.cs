f425fe853ad300f3c4603039862432157c1f3bcc
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-07-23 23:59:18 +0200

    Added RectangleSelection class

---
+++ src/Core2D/Editor/Tools/Selection/RectangleSelection.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using Core2D.Project;
+using Core2D.Shape;
+using Core2D.Shapes;
+using Core2D.Style;
+
+namespace Core2D.Editor.Tools.Selection
+{
+    /// <summary>
+    /// Helper class for <see cref="XRectangle"/> shape selection.
+    /// </summary>
+    public class RectangleSelection
+    {
+        private XLayer _layer;
+        private XRectangle _shape;
+        private ShapeStyle _style;
+        private BaseShape _point;
+
+        /// <summary>
+        /// Initialize new instance of <see cref="RectangleSelection"/> class.
+        /// </summary>
+        /// <param name="layer">The selection shapes layer.</param>
+        /// <param name="shape">The selected shape.</param>
+        /// <param name="style">The selection shapes style.</param>
+        /// <param name="point">The selection point shape.</param>
+        public RectangleSelection(XLayer layer, XRectangle shape, ShapeStyle style, BaseShape point)
+        {
+            _layer = layer;
+            _shape = shape;
+            _style = style;
+            _point = point;
+        }
+
+        // TODO: Implement selection class.
+    }
+}
