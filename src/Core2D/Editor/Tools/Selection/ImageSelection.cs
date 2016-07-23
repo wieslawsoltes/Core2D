888e9bebe914b7df08523d0f578bc474d33ebcab
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-07-23 23:45:08 +0200

    Added constructor

--- src/Core2D/Editor/Tools/Selection/ImageSelection.cs
+++ src/Core2D/Editor/Tools/Selection/ImageSelection.cs
@@ -1,6 +1,9 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using Core2D.Project;
+using Core2D.Shape;
 using Core2D.Shapes;
+using Core2D.Style;
 
 namespace Core2D.Editor.Tools.Selection
 {
@@ -9,6 +12,26 @@ namespace Core2D.Editor.Tools.Selection
     /// </summary>
     public class ImageSelection
     {
+        private XLayer _layer;
+        private XText _shape;
+        private ShapeStyle _style;
+        private BaseShape _point;
+
+        /// <summary>
+        /// Initialize new instance of <see cref="ImageSelection"/> class.
+        /// </summary>
+        /// <param name="layer">The selection shapes layer.</param>
+        /// <param name="shape">The selected shape.</param>
+        /// <param name="style">The selection shapes style.</param>
+        /// <param name="point">The selection point shape.</param>
+        public ImageSelection(XLayer layer, XText shape, ShapeStyle style, BaseShape point)
+        {
+            _layer = layer;
+            _shape = shape;
+            _style = style;
+            _point = point;
+        }
+
         // TODO: Implement selection class.
     }
 }
