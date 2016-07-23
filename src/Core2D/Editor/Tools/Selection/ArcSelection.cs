677b17b7fcef9e314d36ef1c2150040a804386eb
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-07-23 23:40:07 +0200

    Added selection class stubs

---
+++ src/Core2D/Editor/Tools/Selection/ArcSelection.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using Core2D.Shapes;
+
+namespace Core2D.Editor.Tools.Selection
+{
+    /// <summary>
+    /// Helper class for <see cref="XArc"/> shape selection.
+    /// </summary>
+    public class ArcSelection
+    {
+        // TODO: Implement selection class.
+    }
+}
---
+++ src/Core2D/Editor/Tools/Selection/TextSelection.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using Core2D.Project;
+using Core2D.Shape;
+using Core2D.Shapes;
+using Core2D.Style;
+
+namespace Core2D.Editor.Tools.Selection
+{
+    /// <summary>
+    /// Helper class for <see cref="XText"/> shape selection.
+    /// </summary>
+    public class TextSelection
+    {
+        // TODO: Implement selection class.
+    }
+}
