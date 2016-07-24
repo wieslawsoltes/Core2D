16d988f8e413d7cd957f18e77e3182c37f528de6
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-07-24 13:38:57 +0200

    Added path tool selection class stubs

---
+++ src/Core2D/Editor/Tools/Path/Selection/ArcSelection.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Core2D.Editor.Tools.Path.Selection
+{
+    /// <summary>
+    /// Helper class for <see cref="PathTool.Arc"/> shape selection.
+    /// </summary>
+    internal class ArcSelection
+    {
+        // TODO: Implement selection class.
+    }
+}
---
+++ src/Core2D/Editor/Tools/Path/Selection/LineSelection.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Core2D.Editor.Tools.Path.Selection
+{
+    /// <summary>
+    /// Helper class for <see cref="PathTool.Line"/> shape selection.
+    /// </summary>
+    internal class LineSelection
+    {
+        // TODO: Implement selection class.
+    }
+}
