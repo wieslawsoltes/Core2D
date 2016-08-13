41b3b88632ad7edd3ea97eb2b867d205df5a9e51
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-08-14 00:31:04 +0200

    Added ContainerPresenter class

---
+++ src/Core2D/Renderer/Presenters/ContainerPresenter.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System.Collections.Immutable;
+using Core2D.Data;
+using Core2D.Data.Database;
+using Core2D.Project;
+
+namespace Core2D.Renderer.Presenters
+{
+    /// <summary>
+    /// Generic container presenter.
+    /// </summary>
+    public class ContainerPresenter
+    {
+        /// <summary>
+        /// Renders a container using provided drawing context.
+        /// </summary>
+        /// <param name="dc">The drawing context.</param>
+        /// <param name="renderer">The shape renderer.</param>
+        /// <param name="container">The container to render.</param>
+        public virtual void Render(object dc, ShapeRenderer renderer, XContainer container)
+        {
+            renderer.Fill(dc, 0, 0, container.Width, container.Height, container.Background);
+
+            var db = container.Data == null ? default(ImmutableArray<XProperty>) : container.Data.Properties;
+            var r = container.Data == null ? default(XRecord) : container.Data.Record;
+
+            renderer.Draw(dc, container.Template, db, r);
+            renderer.Draw(dc, container, db, r);
+
+            if (container.WorkingLayer != null)
+            {
+                renderer.Draw(dc, container.WorkingLayer, db, r);
+            }
+
+            if (container.HelperLayer != null)
+            {
+                renderer.Draw(dc, container.HelperLayer, db, r);
+            }
+        }
+    }
+}
