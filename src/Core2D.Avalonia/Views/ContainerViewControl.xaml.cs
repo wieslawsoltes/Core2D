2a421c000530cd949e53503bd7746d468975d8a4
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-08-14 00:31:37 +0200

    Use generic ContainerPresenter

--- src/Core2D.Avalonia/Views/ContainerViewControl.xaml.cs
+++ src/Core2D.Avalonia/Views/ContainerViewControl.xaml.cs
@@ -1,16 +1,13 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-using Core2D.Data;
-using Core2D.Data.Database;
-using Core2D.Project;
-using Core2D.Renderer;
-using Core2D.Style;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using Avalonia.Media;
+using Core2D.Project;
+using Core2D.Renderer;
+using Core2D.Renderer.Presenters;
 using Renderer.Avalonia;
-using System.Collections.Immutable;
 
 namespace Core2D.Avalonia.Views
 {
@@ -19,14 +16,22 @@ namespace Core2D.Avalonia.Views
     /// </summary>
     public class ContainerViewControl : UserControl
     {
+        private static ContainerPresenter _presenter = new ContainerPresenter();
+
+        /// <summary>
+        /// Gets or sets container property.
+        /// </summary>
         public static readonly AvaloniaProperty<XContainer> ContainerProperty =
             AvaloniaProperty.Register<ContainerViewControl, XContainer>(nameof(Container));
 
+        /// <summary>
+        /// Gets or sets renderer property.
+        /// </summary>
         public static readonly AvaloniaProperty<ShapeRenderer> RendererProperty =
             AvaloniaProperty.Register<ContainerViewControl, ShapeRenderer>(nameof(Renderer));
 
         /// <summary>
-        /// Gets or set container property.
+        /// Gets or sets container property.
         /// </summary>
         public XContainer Container
         {
@@ -35,7 +40,7 @@ namespace Core2D.Avalonia.Views
         }
 
         /// <summary>
-        ///  Gets or set renderer property.
+        ///  Gets or sets renderer property.
         /// </summary>
         public ShapeRenderer Renderer
         {
@@ -60,46 +65,6 @@ namespace Core2D.Avalonia.Views
         }
 
         /// <summary>
-        /// Draws background rectangle with specified color.
-        /// </summary>
-        /// <param name="dc">The drawing context.</param>
-        /// <param name="c">The background color.</param>
-        /// <param name="width">The width of background rectangle.</param>
-        /// <param name="height">The height of background rectangle.</param>
-        private void DrawBackground(DrawingContext dc, ArgbColor c, double width, double height)
-        {
-            dc.FillRectangle(
-                new SolidColorBrush(Color.FromArgb(c.A, c.R, c.G, c.B)), 
-                new Rect(0, 0, width, height));
-        }
-
-        /// <summary>
-        /// Draw container.
-        /// </summary>
-        /// <param name="dc">The drawing context.</param>
-        /// <param name="renderer">The shape renderer.</param>
-        /// <param name="container">The page to draw.</param>
-        private void Draw(DrawingContext dc, ShapeRenderer renderer, XContainer container)
-        {
-            DrawBackground(dc, container.Background, container.Width, container.Height);
-
-            var db = container.Data == null ? default(ImmutableArray<XProperty>) : container.Data.Properties;
-            var r = container.Data == null ? default(XRecord) : container.Data.Record;
-
-            renderer.Draw(dc, container, db, r);
-
-            if (container.WorkingLayer != null)
-            {
-                renderer.Draw(dc, container.WorkingLayer, db, r);
-            }
-
-            if (container.HelperLayer != null)
-            {
-                renderer.Draw(dc, container.HelperLayer, db, r);
-            }
-        }
-
-        /// <summary>
         /// Renders container control contents.
         /// </summary>
         /// <param name="context">The drawing context.</param>
@@ -111,14 +76,14 @@ namespace Core2D.Avalonia.Views
             {
                 if (Renderer != null)
                 {
-                    Draw(context, Renderer, Container);
+                    _presenter.Render(context, Renderer, Container);
                 }
                 else
                 {
                     var renderer = GetValue(RendererOptions.RendererProperty);
                     if (renderer != null)
                     {
-                        Draw(context, renderer, Container);
+                        _presenter.Render(context, renderer, Container);
                     }
                 }
             }
