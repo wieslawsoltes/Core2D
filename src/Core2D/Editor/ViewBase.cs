5c3ed09ec85f815f66106fb17124cce455de7b32
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-08-18 02:20:12 +0200

    Refactored view classes

--- src/Core2D/Editor/ViewBase.cs
+++ src/Core2D/Editor/ViewBase.cs
@@ -1,32 +1,19 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using Core2D.Editor.Interfaces;
 
 namespace Core2D.Editor
 {
     /// <summary>
     /// View base class.
     /// </summary>
-    public abstract class ViewBase : ObservableObject
+    /// <typeparam name="T">The type of <see cref="ViewBase{T}.Context"/> property.</typeparam>
+    public abstract class ViewBase<T> : ObservableObject, IView
     {
-        private string _name;
-        private object _context;
+        /// <inheritdoc/>
+        public abstract string Name { get; }
 
-        /// <summary>
-        /// Gets or sets view name.
-        /// </summary>
-        public string Name
-        {
-            get { return _name; }
-            set { Update(ref _name, value); }
-        }
-
-        /// <summary>
-        /// Gets or sets view context.
-        /// </summary>
-        public object Context
-        {
-            get { return _context; }
-            set { Update(ref _context, value); }
-        }
+        /// <inheritdoc/>
+        public abstract object Context { get; }
     }
 }
--- src/Core2D/Editor/Views/EditorView.cs
+++ src/Core2D/Editor/Views/EditorView.cs
@@ -1,12 +1,31 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
 
 namespace Core2D.Editor.Views
 {
     /// <summary>
     /// Editor view.
     /// </summary>
-    public class EditorView : ViewBase
+    public class EditorView : ViewBase<ProjectEditor>
     {
+        private readonly IServiceProvider _serviceProvider;
+        private Lazy<ProjectEditor> _context;
+
+        /// <inheritdoc/>
+        public override string Name => "Editor";
+
+        /// <inheritdoc/>
+        public override object Context => _context.Value;
+
+        /// <summary>
+        /// Initialize new instance of <see cref="EditorView"/> class.
+        /// </summary>
+        /// <param name="serviceProvider">The service provider.</param>
+        public EditorView(IServiceProvider serviceProvider) : base()
+        {
+            _serviceProvider = serviceProvider;
+            _context = serviceProvider.GetServiceLazily<ProjectEditor>();
+        }
     }
 }
