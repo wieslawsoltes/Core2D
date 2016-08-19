1a589cd0f536372bc67002e626d833900d6c3862
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-08-19 23:16:27 +0200

    Refactore app class

--- src/Core2D.Wpf/App.xaml.cs
+++ src/Core2D.Wpf/App.xaml.cs
@@ -15,14 +15,6 @@ namespace Core2D.Wpf
     /// </summary>
     public partial class App : Application
     {
-        private IServiceProvider _serviceProvider;
-
-        private static string RecentFileName = "Core2D.recent";
-
-        private static string LogFileName = "Core2D.log";
-
-        private bool _isLoaded = false;
-
         /// <summary>
         /// Raises the <see cref="Application.Startup"/> event.
         /// </summary>
@@ -42,8 +34,7 @@ namespace Core2D.Wpf
             {
                 using (var log = container.Resolve<ILog>())
                 {
-                    _serviceProvider = container.Resolve<IServiceProvider>();
-                    Start();
+                    Start(container.Resolve<IServiceProvider>());
                 }
             }
         }
@@ -51,25 +42,51 @@ namespace Core2D.Wpf
         /// <summary>
         /// Initialize application context and displays main window.
         /// </summary>
-        private void Start()
+        /// <param name="serviceProvider">The service provider.</param>
+        private void Start(IServiceProvider serviceProvider)
         {
-            var log = _serviceProvider.GetService<ILog>();
-            var fileIO = _serviceProvider.GetService<IFileSystem>();
-            var editor = _serviceProvider.GetService<ProjectEditor>();
+            var log = serviceProvider.GetService<ILog>();
+            var fileIO = serviceProvider.GetService<IFileSystem>();
 
-            log?.Initialize(System.IO.Path.Combine(fileIO.GetAssemblyPath(null), LogFileName));
+            log?.Initialize(System.IO.Path.Combine(fileIO.GetAssemblyPath(null), "Core2D.log"));
 
             try
             {
-                LoadRecent();
+                var editor = serviceProvider.GetService<ProjectEditor>();
+
+                var path = System.IO.Path.Combine(fileIO.GetAssemblyPath(null), "Core2D.recent");
+                if (System.IO.File.Exists(path))
+                {
+                    editor.OnLoadRecent(path);
+                }
 
                 editor.CurrentView = editor.Views.FirstOrDefault(view => view.Name == "Dashboard");
                 editor.CurrentTool = editor.Tools.FirstOrDefault(tool => tool.Name == "Selection");
                 editor.CurrentPathTool = editor.PathTools.FirstOrDefault(tool => tool.Name == "Line");
 
-                var window = _serviceProvider.GetService<Windows.MainWindow>();
-                window.Loaded += (sender, e) => OnLoaded();
-                window.Closed += (sender, e) => OnClosed();
+                var window = serviceProvider.GetService<Windows.MainWindow>();
+                bool isLoaded = false;
+
+                window.Loaded +=
+                    (sender, e) =>
+                    {
+                        if (isLoaded)
+                            return;
+                        else
+                            isLoaded = true;
+                    };
+
+                window.Closed +=
+                    (sender, e) =>
+                    {
+                        if (!isLoaded)
+                            return;
+                        else
+                            isLoaded = false;
+
+                        editor.OnSaveRecent(path);
+                    };
+
                 window.DataContext = editor;
                 window.ShowDialog();
             }
@@ -80,69 +97,6 @@ namespace Core2D.Wpf
                 {
                     log?.LogError($"{ex.InnerException.Message}{Environment.NewLine}{ex.InnerException.StackTrace}");
                 }
-            }
-        }
-
-        /// <summary>
-        /// Initialize main window after loaded.
-        /// </summary>
-        private void OnLoaded()
-        {
-            if (_isLoaded)
-                return;
-            else
-                _isLoaded = true;
-        }
-
-        /// <summary>
-        /// De-initialize main window after closed.
-        /// </summary>
-        private void OnClosed()
-        {
-            if (!_isLoaded)
-                return;
-            else
-                _isLoaded = false;
-
-            SaveRecent();
-        }
-
-        /// <summary>
-        /// Load recent project files list.
-        /// </summary>
-        private void LoadRecent()
-        {
-            try
-            {
-                var fileIO = _serviceProvider.GetService<IFileSystem>();
-                var editor = _serviceProvider.GetService<ProjectEditor>();
-                var path = System.IO.Path.Combine(fileIO.GetAssemblyPath(null), RecentFileName);
-                if (System.IO.File.Exists(path))
-                {
-                    editor?.OnLoadRecent(path);
-                }
-            }
-            catch (Exception ex)
-            {
-                _serviceProvider.GetService<ILog>()?.LogError($"{ex.Message}{Environment.NewLine}{ex.StackTrace}");
-            }
-        }
-
-        /// <summary>
-        /// Save recent project files list.
-        /// </summary>
-        private void SaveRecent()
-        {
-            try
-            {
-                var fileIO = _serviceProvider.GetService<IFileSystem>();
-                var editor = _serviceProvider.GetService<ProjectEditor>();
-                var path = System.IO.Path.Combine(fileIO.GetAssemblyPath(null), RecentFileName);
-                editor?.OnSaveRecent(path);
-            }
-            catch (Exception ex)
-            {
-                _serviceProvider.GetService<ILog>()?.LogError($"{ex.Message}{Environment.NewLine}{ex.StackTrace}");
             }
         }
     }
