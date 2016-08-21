5db1f0f8c33999c23fddf9beff1ed5aa1a7c526f
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-08-21 16:46:25 +0200

    Use new initialization system for Cairo backend

---
+++ src/Core2D.Avalonia.Cairo/Locator/ServiceProvider.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+using Autofac;
+
+namespace Core2D.Avalonia.Cairo.Locator
+{
+    /// <summary>
+    /// Service provider based on lifetime scope.
+    /// </summary>
+    public class ServiceProvider : IServiceProvider
+    {
+        private readonly ILifetimeScope _scope;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ServiceProvider"/> class.
+        /// </summary>
+        /// <param name="scope">The lifetime scope.</param>
+        public ServiceProvider(ILifetimeScope scope)
+        {
+            _scope = scope;
+        }
+
+        /// <summary>
+        /// Gets the service object of the specified type.
+        /// </summary>
+        /// <param name="serviceType">The type of resolved service object.</param>
+        /// <returns>The instance of type <paramref name="serviceType"/>.</returns>
+        object IServiceProvider.GetService(Type serviceType)
+        {
+            return _scope.Resolve(serviceType);
+        }
+    }
+}
