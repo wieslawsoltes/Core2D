a003ce3c3e1777ddead1495611a25de7014ab91c
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-08-21 16:47:02 +0200

    Use new initialization system for Droid backend

--- src/Core2D.Avalonia.Droid/MainActivity.cs
+++ src/Core2D.Avalonia.Droid/MainActivity.cs
@@ -1,31 +1,20 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-using System.Collections.Immutable;
+using System;
 using Android.App;
 using Android.Content.PM;
 using Android.OS;
-using Core2D.Data.Database;
-using Core2D.Editor;
-using Core2D.Editor.Factories;
-using Core2D.Editor.Input;
-using Core2D.Editor.Interfaces;
+using Autofac;
+using Core2D.Avalonia.Droid.Modules;
 using Core2D.Interfaces;
-using Core2D.Renderer;
-using FileSystem.DotNetFx;
-using Log.Trace;
-using Renderer.Avalonia;
-using Serializer.Newtonsoft;
-using Serializer.Xaml;
-using TextFieldReader.CsvHelper;
-using TextFieldWriter.CsvHelper;
-using Utilities.Avalonia;
 using A = Avalonia;
 using AAPS = Avalonia.Android.Platform.Specific;
+using AC = Avalonia.Controls;
 
 namespace Core2D.Avalonia.Droid
 {
-    [Activity
-        (Label = "Core2D",
+    [Activity(
+        Label = "Core2D",
         MainLauncher = true,
         Icon = "@drawable/icon",
         LaunchMode = LaunchMode.SingleInstance,
@@ -41,40 +30,24 @@ namespace Core2D.Avalonia.Droid
         {
             base.OnCreate(savedInstanceState);
 
-            RegisterServices();
+            var builder = new ContainerBuilder();
 
-            using (var log = ServiceLocator.Instance.Resolve<ILog>())
+            builder.RegisterModule<LocatorModule>();
+            builder.RegisterModule<CoreModule>();
+            builder.RegisterModule<DependenciesModule>();
+            builder.RegisterModule<AppModule>();
+
+            using (IContainer container = builder.Build())
             {
-                var app = A.Application.Current as App ?? new App();
-                ServiceLocator.Instance.RegisterSingleton<IEditorApplication>(() => app);
-
-                app.Start();
-            }
-        }
-
-        private static void RegisterServices()
-        {
-            ServiceLocator.Instance.RegisterSingleton<ITextFieldReader<ProjectEditor>>(() => new ProjectEditor());
-            ServiceLocator.Instance.RegisterSingleton<ILog>(() => new TraceLog());
-            ServiceLocator.Instance.RegisterSingleton<CommandManager>(() => new CommandManager());
-            ServiceLocator.Instance.RegisterSingleton<ShapeRenderer[]>(
-                () =>
+                using (var log = container.Resolve<ILog>())
                 {
-                    return new[]
-                    {
-                        new AvaloniaRenderer(),
-                        new AvaloniaRenderer()
-                    };
-                });
-            ServiceLocator.Instance.RegisterSingleton<IFileSystem>(() => new DotNetFxFileSystem());
-            ServiceLocator.Instance.RegisterSingleton<IProjectFactory>(() => new ProjectFactory());
-            ServiceLocator.Instance.RegisterSingleton<ITextClipboard>(() => new AvaloniaTextClipboard());
-            ServiceLocator.Instance.RegisterSingleton<IJsonSerializer>(() => new NewtonsoftJsonSerializer());
-            ServiceLocator.Instance.RegisterSingleton<IXamlSerializer>(() => new PortableXamlSerializer());
-            ServiceLocator.Instance.RegisterSingleton<ImmutableArray<IFileWriter>>(() => new IFileWriter[] { }.ToImmutableArray());
-            ServiceLocator.Instance.RegisterSingleton<ITextFieldReader<XDatabase>>(() => new CsvHelperReader());
-            ServiceLocator.Instance.RegisterSingleton<ITextFieldWriter<XDatabase>>(() => new CsvHelperWriter());
-            ServiceLocator.Instance.RegisterSingleton<Windows.MainWindow>(() => new Windows.MainWindow());
+                    var app = A.Application.Current as App ?? new App();
+                    AC.AppBuilder.Configure(app)
+                        .UsePlatformDetect()
+                        .SetupWithoutStarting();
+                    app.Start(container.Resolve<IServiceProvider>());
+                }
+            }
         }
     }
 }
---
+++ src/Core2D.Avalonia.Droid/Modules/LocatorModule.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+using Autofac;
+using Core2D.Avalonia.Droid.Locator;
+
+namespace Core2D.Avalonia.Droid.Modules
+{
+    /// <summary>
+    /// Locator components module.
+    /// </summary>
+    public class LocatorModule : Module
+    {
+        /// <inheritdoc/>
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ServiceProvider>().As<IServiceProvider>().InstancePerLifetimeScope();
+        }
+    }
+}
