935053a8c317f9af4d449a5a5ddfa051cc7b5ae8
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-08-21 13:32:08 +0200

    Changed commands namespace

--- src/Core2D.Wpf/Commands/Window/ResetWindowLayoutCommand.cs
+++ src/Core2D.Wpf/Commands/Window/ResetWindowLayoutCommand.cs
@@ -1,8 +1,10 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using Core2D.Editor;
+using Core2D.Editor.Commands;
 using Core2D.Editor.Input;
 
-namespace Core2D.Editor.Commands
+namespace Core2D.Wpf.Commands
 {
     /// <inheritdoc/>
     public class ResetWindowLayoutCommand : Command, IResetWindowLayoutCommand
