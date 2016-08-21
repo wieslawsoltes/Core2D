f27050e72f02c228b3bc3e504ae8092d043e51e5
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-08-21 13:19:10 +0200

    Added command implementations

---
+++ src/Core2D.Wpf/Commands/Window/SaveWindowLayoutCommand.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using Core2D.Editor.Input;
+
+namespace Core2D.Editor.Commands
+{
+    /// <inheritdoc/>
+    public class SaveWindowLayoutCommand : Command, ISaveWindowLayoutCommand
+    {
+        /// <inheritdoc/>
+        public override bool CanRun() => true;
+
+        /// <inheritdoc/>
+        public override void Run()
+            => ServiceProvider.GetService<ProjectEditor>().SaveLayout?.Invoke();
+    }
+}
