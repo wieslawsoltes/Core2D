a45cc42d4a67a9e190d687792cf633e7edee0c69
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-08-21 16:46:47 +0200

    Use new initialization system for Direct2D backend

---
+++ src/Core2D.Avalonia.Direct2D/Modules/DependenciesModule.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using Autofac;
+using Core2D.Data.Database;
+using Core2D.Interfaces;
+using Core2D.Renderer;
+using FileSystem.DotNetFx;
+using FileWriter.Dxf;
+using FileWriter.Emf;
+using FileWriter.Pdf_core;
+using Log.Trace;
+using Renderer.Avalonia;
+using Serializer.Newtonsoft;
+using Serializer.Xaml;
+using TextFieldReader.CsvHelper;
+using TextFieldWriter.CsvHelper;
+using Utilities.Avalonia;
+
+namespace Core2D.Avalonia.Direct2D.Modules
+{
+    /// <summary>
+    /// Dependencies components module.
+    /// </summary>
+    public class DependenciesModule : Module
+    {
+        /// <inheritdoc/>
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<TraceLog>().As<ILog>().SingleInstance();
+            builder.RegisterType<DotNetFxFileSystem>().As<IFileSystem>().InstancePerLifetimeScope();
+            builder.RegisterType<NewtonsoftJsonSerializer>().As<IJsonSerializer>().InstancePerLifetimeScope();
+            builder.RegisterType<PortableXamlSerializer>().As<IXamlSerializer>().InstancePerLifetimeScope();
+            builder.RegisterType<PdfWriter>().As<IFileWriter>().InstancePerLifetimeScope();
+            builder.RegisterType<DxfWriter>().As<IFileWriter>().InstancePerLifetimeScope();
+            builder.RegisterType<EmfWriter>().As<IFileWriter>().InstancePerLifetimeScope();
+            builder.RegisterType<CsvHelperReader>().As<ITextFieldReader<XDatabase>>().InstancePerLifetimeScope();
+            builder.RegisterType<CsvHelperWriter>().As<ITextFieldWriter<XDatabase>>().InstancePerLifetimeScope();
+            builder.RegisterType<AvaloniaRenderer>().As<ShapeRenderer>().InstancePerLifetimeScope();
+            builder.RegisterType<AvaloniaTextClipboard>().As<ITextClipboard>().InstancePerLifetimeScope();
+        }
+    }
+}
--- src/Core2D.Avalonia.Direct2D/Program.cs
+++ src/Core2D.Avalonia.Direct2D/Program.cs
@@ -1,100 +1,54 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-using System.Collections.Immutable;
+using System;
+using Autofac;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Logging.Serilog;
-using Core2D.Data.Database;
-using Core2D.Editor;
-using Core2D.Editor.Factories;
-using Core2D.Editor.Input;
-using Core2D.Editor.Interfaces;
+using Core2D.Avalonia.Direct2D.Modules;
 using Core2D.Interfaces;
-using Core2D.Renderer;
-using FileSystem.DotNetFx;
-using FileWriter.Dxf;
-using FileWriter.Emf;
-using FileWriter.Pdf_core;
-using Log.Trace;
-using Renderer.Avalonia;
-using Serializer.Newtonsoft;
-using Serializer.Xaml;
 using Serilog;
-using TextFieldReader.CsvHelper;
-using TextFieldWriter.CsvHelper;
-using Utilities.Avalonia;
 
 namespace Core2D.Avalonia.Direct2D
 {
     /// <summary>
     /// Encapsulates a Avalonia program.
     /// </summary>
-    internal class Program
+    class Program
     {
         /// <summary>
         /// Program entry point.
         /// </summary>
         /// <param name="args">The program arguments.</param>
-        private static void Main(string[] args)
+        static void Main(string[] args)
         {
             InitializeLogging();
 
-            RegisterServices();
+            var builder = new ContainerBuilder();
 
-            using (var log = ServiceLocator.Instance.Resolve<ILog>())
+            builder.RegisterModule<LocatorModule>();
+            builder.RegisterModule<CoreModule>();
+            builder.RegisterModule<DependenciesModule>();
+            builder.RegisterModule<AppModule>();
+
+            using (IContainer container = builder.Build())
             {
-                var app = new App();
-                ServiceLocator.Instance.RegisterSingleton<IEditorApplication>(() => app);
-
-                AppBuilder.Configure(app)
-                    .UseWin32()
-                    .UseDirect2D1()
-                    .SetupWithoutStarting();
-                app.Start();
+                using (var log = container.Resolve<ILog>())
+                {
+                    var app = new App();
+                    AppBuilder.Configure(app)
+                        .UseWin32()
+                        .UseDirect2D1()
+                        .SetupWithoutStarting();
+                    app.Start(container.Resolve<IServiceProvider>());
+                }
             }
-        }
-
-        /// <summary>
-        /// Register application services.
-        /// </summary>
-        private static void RegisterServices()
-        {
-            ServiceLocator.Instance.RegisterSingleton<ProjectEditor>(() => new ProjectEditor());
-            ServiceLocator.Instance.RegisterSingleton<ILog>(() => new TraceLog());
-            ServiceLocator.Instance.RegisterSingleton<CommandManager>(() => new CommandManager());
-            ServiceLocator.Instance.RegisterSingleton<ShapeRenderer[]>(
-                () =>
-                {
-                    return new[]
-                    {
-                        new AvaloniaRenderer(),
-                        new AvaloniaRenderer()
-                    };
-                });
-            ServiceLocator.Instance.RegisterSingleton<IFileSystem>(() => new DotNetFxFileSystem());
-            ServiceLocator.Instance.RegisterSingleton<IProjectFactory>(() => new ProjectFactory());
-            ServiceLocator.Instance.RegisterSingleton<ITextClipboard>(() => new AvaloniaTextClipboard());
-            ServiceLocator.Instance.RegisterSingleton<IJsonSerializer>(() => new NewtonsoftJsonSerializer());
-            ServiceLocator.Instance.RegisterSingleton<IXamlSerializer>(() => new PortableXamlSerializer());
-            ServiceLocator.Instance.RegisterSingleton<ImmutableArray<IFileWriter>>(
-                () =>
-                {
-                    return new IFileWriter[]
-                    {
-                        new PdfWriter(),
-                        new DxfWriter(),
-                        new EmfWriter()
-                    }.ToImmutableArray();
-                });
-            ServiceLocator.Instance.RegisterSingleton<ITextFieldReader<XDatabase>>(() => new CsvHelperReader());
-            ServiceLocator.Instance.RegisterSingleton<ITextFieldWriter<XDatabase>>(() => new CsvHelperWriter());
-            ServiceLocator.Instance.RegisterSingleton<Windows.MainWindow>(() => new Windows.MainWindow());
         }
 
         /// <summary>
         /// Initialize the Serilog logger.
         /// </summary>
-        private static void InitializeLogging()
+        static void InitializeLogging()
         {
 #if DEBUG
             SerilogLogger.Initialize(new LoggerConfiguration()
