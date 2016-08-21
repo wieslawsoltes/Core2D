b4d19567abdc602f0d91068e715d891215646648
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-08-21 17:48:53 +0200

    Added command implementations

---
+++ src/Core2D.Avalonia/Commands/Data/UpdateDataCommand.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System.Linq;
+using Avalonia.Controls;
+using Core2D.Avalonia.Windows;
+using Core2D.Data.Database;
+using Core2D.Editor;
+using Core2D.Editor.Commands;
+using Core2D.Editor.Input;
+
+namespace Core2D.Avalonia.Commands
+{
+    /// <inheritdoc/>
+    public class UpdateDataCommand : Command<XDatabase>, IUpdateDataCommand
+    {
+        /// <inheritdoc/>
+        public override bool CanRun(XDatabase db)
+        {
+            return ServiceProvider.GetService<ProjectEditor>().IsEditMode();
+        }
+
+        /// <inheritdoc/>
+        public override async void Run(XDatabase db)
+        {
+            if (db != null)
+            {
+                var dlg = new OpenFileDialog();
+                dlg.Filters.Add(new FileDialogFilter() { Name = "Csv", Extensions = { "csv" } });
+                dlg.Filters.Add(new FileDialogFilter() { Name = "All", Extensions = { "*" } });
+                var result = await dlg.ShowAsync(ServiceProvider.GetService<MainWindow>());
+                if (result != null)
+                {
+                    ServiceProvider.GetService<ProjectEditor>().OnUpdateData(result.FirstOrDefault(), db);
+                }
+            }
+        }
+    }
+}
---
+++ src/Core2D.Avalonia/Commands/File/Export/ExportXamlCommand.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using Avalonia.Controls;
+using Core2D.Avalonia.Windows;
+using Core2D.Editor;
+using Core2D.Editor.Commands;
+using Core2D.Editor.Input;
+
+namespace Core2D.Avalonia.Commands
+{
+    /// <inheritdoc/>
+    public class ExportXamlCommand : Command<object>, IExportXamlCommand
+    {
+        /// <inheritdoc/>
+        public override bool CanRun(object item)
+        {
+            return ServiceProvider.GetService<ProjectEditor>().IsEditMode();
+        }
+
+        /// <inheritdoc/>
+        public override async void Run(object item)
+        {
+            var editor = ServiceProvider.GetService<ProjectEditor>();
+            var dlg = new SaveFileDialog();
+            dlg.Filters.Add(new FileDialogFilter() { Name = "Xaml", Extensions = { "xaml" } });
+            dlg.Filters.Add(new FileDialogFilter() { Name = "All", Extensions = { "*" } });
+            dlg.InitialFileName = editor?.GetName(item);
+            dlg.DefaultExtension = "xaml";
+            var result = await dlg.ShowAsync(ServiceProvider.GetService<MainWindow>());
+            if (result != null)
+            {
+                editor.OnExportXaml(result, item);
+            }
+        }
+    }
+}
