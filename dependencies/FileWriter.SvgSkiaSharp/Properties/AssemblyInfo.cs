8e7fb66696c8115f8d276ab122a05ad19f46a086
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2017-02-21 20:07:14 +0100

    Added FileWriter.SvgSkiaSharp project

---
+++ dependencies/FileWriter.SvgSkiaSharp/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System.Resources;
+using System.Reflection;
+
+[assembly: AssemblyTitle("FileWriter.SvgSkiaSharp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("FileWriter.SvgSkiaSharp")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
