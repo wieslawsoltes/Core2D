196fc4b76c7a78c9411e4d39b0e2c221d2182534
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2017-03-04 15:30:36 +0100

    Added Utilities.Uwp project

---
+++ src/Utilities.Uwp/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Utilities.Uwp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("Utilities.Uwp")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
