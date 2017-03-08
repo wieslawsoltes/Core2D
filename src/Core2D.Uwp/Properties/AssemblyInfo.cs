a861538e4bf2f3422af12f906348ff91d45c7437
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2017-03-08 19:43:07 +0100

    Removed uwp

--- src/Core2D.Uwp/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-using System.Reflection;
-
-[assembly: AssemblyTitle("Core2D.Uwp")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("Core2D.Uwp")]
-[assembly: AssemblyCulture("")]
