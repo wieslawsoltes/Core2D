f08a4f1ef9f58b5af1bead43ffb0bf3728e1876e
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-03-08 12:19:55 +0100

    Update AssemblyInfo.cs
    [skip ci]

--- src/Core2D.Avalonia.Direct2D/Properties/AssemblyInfo.cs
+++ src/Core2D.Avalonia.Direct2D/Properties/AssemblyInfo.cs
@@ -3,10 +3,4 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Core2D.Avalonia.Direct2D")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("Core2D.Avalonia.Direct2D")]
-[assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: Guid("4868069e-3ad4-497e-9b8e-148d1eeacfd7")]
