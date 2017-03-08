885a256452f0e6fb1ae034641c37e4acc5ff03b7
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2017-03-08 21:24:38 +0100

    Added avalonia droid project

---
+++ apps/Core2D.Avalonia.Droid/Modules/DroidViewModule.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System.Reflection;
+using System.Windows.Input;
+using Autofac;
+using Core2D.Editor.Views.Interfaces;
+
+namespace Core2D.Avalonia.Droid.Modules
+{
+    /// <summary>
+    /// View components module.
+    /// </summary>
+    public class DroidViewModule : Autofac.Module
+    {
+        /// <inheritdoc/>
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterAssemblyTypes(typeof(App).GetTypeInfo().Assembly).AssignableTo<ICommand>().AsImplementedInterfaces().AsSelf().PropertiesAutowired().InstancePerLifetimeScope();
+            builder.RegisterAssemblyTypes(typeof(App).GetTypeInfo().Assembly).As<IView>().InstancePerLifetimeScope();
+        }
+    }
+}
