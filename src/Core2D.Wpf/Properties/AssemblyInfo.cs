f92ea7baef47c91b4cfdfb13707d046682de587a
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2017-03-08 19:39:54 +0100

    Removed projects

--- src/Core2D.Wpf/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-using System.Reflection;
-using System.Runtime.InteropServices;
-using System.Windows;
-
-[assembly: ComVisible(false)]
-[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
