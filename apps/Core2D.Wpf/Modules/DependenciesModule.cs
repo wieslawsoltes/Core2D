9fb5a7030fb135248c5aa75825b7eb94a9941e8a
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-03-09 09:09:22 +0100

    Update DependenciesModule.cs
    [skip ci]

--- apps/Core2D.Wpf/Modules/DependenciesModule.cs
+++ apps/Core2D.Wpf/Modules/DependenciesModule.cs
@@ -33,6 +33,8 @@ namespace Core2D.Wpf.Modules
             builder.RegisterType<NewtonsoftJsonSerializer>().As<IJsonSerializer>().InstancePerLifetimeScope();
             builder.RegisterType<PortableXamlSerializer>().As<IXamlSerializer>().InstancePerLifetimeScope();
             builder.RegisterType<PdfSharpWriter>().As<IFileWriter>().InstancePerLifetimeScope();
+            builder.RegisterType<PdfSkiaSharpWriter>().As<IFileWriter>().InstancePerLifetimeScope();
+            builder.RegisterType<SvgSkiaSharpWriter>().As<IFileWriter>().InstancePerLifetimeScope();
             builder.RegisterType<EmfWriter>().As<IFileWriter>().InstancePerLifetimeScope();
             builder.RegisterType<DxfWriter>().As<IFileWriter>().InstancePerLifetimeScope();
             builder.RegisterType<CsvHelperReader>().As<ITextFieldReader<XDatabase>>().InstancePerLifetimeScope();
