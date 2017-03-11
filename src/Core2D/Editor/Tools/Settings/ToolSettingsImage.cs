000acac378729d3584ead142c7d2cce7b76915e8
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2017-03-11 19:59:55 +0100

    Added tool settings classes

---
+++ src/Core2D/Editor/Tools/Settings/ToolSettingsImage.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Core2D.Editor.Tools
+{
+    /// <summary>
+    /// Image tool settings.
+    /// </summary>
+    public class ToolSettingsImage : ToolSettings
+    {
+    }
+}
