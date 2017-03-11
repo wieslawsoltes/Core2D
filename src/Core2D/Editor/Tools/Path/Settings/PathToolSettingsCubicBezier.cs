90b74acb1bc4f0a420b528227aa332ca0f7ca472
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2017-03-11 20:00:13 +0100

    Added path tool settings classes

---
+++ src/Core2D/Editor/Tools/Path/Settings/PathToolSettingsCubicBezier.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Core2D.Editor.Tools.Path.Settings
+{
+    /// <summary>
+    /// Cubic bezier path tool settings
+    /// </summary>
+    public class PathToolSettingsCubicBezier : ToolSettings
+    {
+    }
+}
