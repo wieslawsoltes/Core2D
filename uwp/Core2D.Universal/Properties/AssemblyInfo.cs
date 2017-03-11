6cc28270cf8dd1fc4d7c659a592b32f8946759b6
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2017-03-11 17:18:06 +0100

    Added universal windows app project

---
+++ uwp/Core2D.Universal/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Core2D.Universal")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Wiesław Šoltés")]
+[assembly: AssemblyProduct("Core2D.Universal")]
+[assembly: AssemblyCopyright("Copyright © Wiesław Šoltés 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("0.2.0")]
+[assembly: AssemblyFileVersion("0.2.0")]
+[assembly: AssemblyInformationalVersion("0.2.0")]
+[assembly: ComVisible(false)]
