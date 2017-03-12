32e925bde13b596caf5ac0a5041f9e9d19d28b8d
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2017-03-12 20:30:29 +0100

    Added android app project

---
+++ apps/Core2D.Avalonia.Android/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("Wiesław Šoltés")]
+[assembly: AssemblyCopyright("Copyright © Wiesław Šoltés 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyVersion("0.2.0")]
+[assembly: AssemblyFileVersion("0.2.0")]
+[assembly: AssemblyInformationalVersion("0.2.0")]
+
+[assembly: AssemblyTitle("Core2D.Avalonia.Android")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("Core2D.Avalonia.Android")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
