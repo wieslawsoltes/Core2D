305cd1d7c0ee4fe3fde09800324443a24d3f3cb3
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2017-03-12 14:01:20 +0100

    Refactored skia demo project

---
+++ apps/Core2D.SkiaDemo/Locator/ServiceProvider.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+using Autofac;
+
+namespace Core2D.SkiaDemo.Locator
+{
+    public class ServiceProvider : IServiceProvider
+    {
+        private readonly ILifetimeScope _scope;
+
+        public ServiceProvider(ILifetimeScope scope)
+        {
+            _scope = scope;
+        }
+
+        object IServiceProvider.GetService(Type serviceType)
+        {
+            return _scope.Resolve(serviceType);
+        }
+    }
+}
