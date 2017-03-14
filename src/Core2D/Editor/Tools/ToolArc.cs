18aae2e87b9862fb5f941052e5453070e5b6f73c
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-03-14 14:38:01 +0100

    Update ToolArc.cs
    [skip ci]

--- src/Core2D/Editor/Tools/ToolArc.cs
+++ src/Core2D/Editor/Tools/ToolArc.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 using System;
+using Core2D.Editor.Input;
 using Core2D.Editor.Tools.Selection;
 using Core2D.Editor.Tools.Settings;
 using Core2D.Spatial.Arc;
