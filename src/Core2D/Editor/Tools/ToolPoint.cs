4a9494cc732617842813b22cfef4157a5c9f6b02
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-03-14 12:45:33 +0100

    Update ToolPoint.cs
    [skip ci]

--- src/Core2D/Editor/Tools/ToolPoint.cs
+++ src/Core2D/Editor/Tools/ToolPoint.cs
@@ -40,12 +40,12 @@ namespace Core2D.Editor.Tools
         }
 
         /// <inheritdoc/>
-        public override void LeftDown(double x, double y, ModifierFlags modifier)
+        public override void LeftDown(InputArgs args)
         {
-            base.LeftDown(x, y, modifier);
+            base.LeftDown(args);
             var editor = _serviceProvider.GetService<ProjectEditor>();
-            double sx = editor.Project.Options.SnapToGrid ? ProjectEditor.Snap(x, editor.Project.Options.SnapX) : x;
-            double sy = editor.Project.Options.SnapToGrid ? ProjectEditor.Snap(y, editor.Project.Options.SnapY) : y;
+            double sx = editor.Project.Options.SnapToGrid ? ProjectEditor.Snap(args.X, editor.Project.Options.SnapX) : args.X;
+            double sy = editor.Project.Options.SnapToGrid ? ProjectEditor.Snap(args.Y, editor.Project.Options.SnapY) : args.Y;
             switch (_currentState)
             {
                 case State.Point:
@@ -54,7 +54,7 @@ namespace Core2D.Editor.Tools
 
                         if (editor.Project.Options.TryToConnect)
                         {
-                            if (!editor.TryToSplitLine(x, y, _point, true))
+                            if (!editor.TryToSplitLine(args.X, args.Y, _point, true))
                             {
                                 editor.Project.AddShape(editor.Project.CurrentContainer.CurrentLayer, _point);
                             }
@@ -69,12 +69,12 @@ namespace Core2D.Editor.Tools
         }
 
         /// <inheritdoc/>
-        public override void Move(double x, double y, ModifierFlags modifier)
+        public override void Move(InputArgs args)
         {
-            base.Move(x, y, modifier);
+            base.Move(args);
             var editor = _serviceProvider.GetService<ProjectEditor>();
-            double sx = editor.Project.Options.SnapToGrid ? ProjectEditor.Snap(x, editor.Project.Options.SnapX) : x;
-            double sy = editor.Project.Options.SnapToGrid ? ProjectEditor.Snap(y, editor.Project.Options.SnapY) : y;
+            double sx = editor.Project.Options.SnapToGrid ? ProjectEditor.Snap(args.X, editor.Project.Options.SnapX) : args.X;
+            double sy = editor.Project.Options.SnapToGrid ? ProjectEditor.Snap(args.Y, editor.Project.Options.SnapY) : args.Y;
             switch (_currentState)
             {
                 case State.Point:
