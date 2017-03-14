d4124eee968afe3d1b5a333f9b527962c65131ac
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-03-14 13:50:49 +0100

    Update PathToolCubicBezier.cs
    [skip ci]

--- src/Core2D/Editor/Tools/Path/PathToolCubicBezier.cs
+++ src/Core2D/Editor/Tools/Path/PathToolCubicBezier.cs
@@ -46,13 +46,12 @@ namespace Core2D.Editor.Tools.Path
         }
 
         /// <inheritdoc/>
-        public override void LeftDown(double x, double y, ModifierFlags modifier)
+        public override void LeftDown(InputArgs args)
         {
-            base.LeftDown(x, y, modifier);
+            base.LeftDown(args);
             var editor = _serviceProvider.GetService<ProjectEditor>();
             var pathTool = _serviceProvider.GetService<ToolPath>();
-            double sx = editor.Project.Options.SnapToGrid ? ProjectEditor.Snap(x, editor.Project.Options.SnapX) : x;
-            double sy = editor.Project.Options.SnapToGrid ? ProjectEditor.Snap(y, editor.Project.Options.SnapY) : y;
+            (double sx, double sy) = editor.TryToSnap(args);
             switch (_currentState)
             {
                 case State.Point1:
@@ -162,9 +161,9 @@ namespace Core2D.Editor.Tools.Path
         }
 
         /// <inheritdoc/>
-        public override void RightDown(double x, double y, ModifierFlags modifier)
+        public override void RightDown(InputArgs args)
         {
-            base.RightDown(x, y, modifier);
+            base.RightDown(args);
             var editor = _serviceProvider.GetService<ProjectEditor>();
             var pathTool = _serviceProvider.GetService<ToolPath>();
             switch (_currentState)
@@ -197,12 +196,11 @@ namespace Core2D.Editor.Tools.Path
         }
 
         /// <inheritdoc/>
-        public override void Move(double x, double y, ModifierFlags modifier)
+        public override void Move(InputArgs args)
         {
-            base.Move(x, y, modifier);
+            base.Move(args);
             var editor = _serviceProvider.GetService<ProjectEditor>();
-            double sx = editor.Project.Options.SnapToGrid ? ProjectEditor.Snap(x, editor.Project.Options.SnapX) : x;
-            double sy = editor.Project.Options.SnapToGrid ? ProjectEditor.Snap(y, editor.Project.Options.SnapY) : y;
+            (double sx, double sy) = editor.TryToSnap(args);
             switch (_currentState)
             {
                 case State.Point1:
