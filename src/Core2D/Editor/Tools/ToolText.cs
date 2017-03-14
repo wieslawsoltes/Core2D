1929dc63749ba8a7901dd96787310a9fe6b328f6
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-03-14 13:35:49 +0100

    Update ToolText.cs
    [skip ci]

--- src/Core2D/Editor/Tools/ToolText.cs
+++ src/Core2D/Editor/Tools/ToolText.cs
@@ -43,12 +43,11 @@ namespace Core2D.Editor.Tools
         }
 
         /// <inheritdoc/>
-        public override void LeftDown(double x, double y, ModifierFlags modifier)
+        public override void LeftDown(InputArgs args)
         {
-            base.LeftDown(x, y, modifier);
+            base.LeftDown(args);
             var editor = _serviceProvider.GetService<ProjectEditor>();
-            double sx = editor.Project.Options.SnapToGrid ? ProjectEditor.Snap(x, editor.Project.Options.SnapX) : x;
-            double sy = editor.Project.Options.SnapToGrid ? ProjectEditor.Snap(y, editor.Project.Options.SnapY) : y;
+            (double sx, double sy) = editor.TryToSnap(args);
             switch (_currentState)
             {
                 case State.TopLeft:
@@ -101,9 +100,9 @@ namespace Core2D.Editor.Tools
         }
 
         /// <inheritdoc/>
-        public override void RightDown(double x, double y, ModifierFlags modifier)
+        public override void RightDown(InputArgs args)
         {
-            base.RightDown(x, y, modifier);
+            base.RightDown(args);
             var editor = _serviceProvider.GetService<ProjectEditor>();
             switch (_currentState)
             {
@@ -122,12 +121,11 @@ namespace Core2D.Editor.Tools
         }
 
         /// <inheritdoc/>
-        public override void Move(double x, double y, ModifierFlags modifier)
+        public override void Move(InputArgs args)
         {
-            base.Move(x, y, modifier);
+            base.Move(args);
             var editor = _serviceProvider.GetService<ProjectEditor>();
-            double sx = editor.Project.Options.SnapToGrid ? ProjectEditor.Snap(x, editor.Project.Options.SnapX) : x;
-            double sy = editor.Project.Options.SnapToGrid ? ProjectEditor.Snap(y, editor.Project.Options.SnapY) : y;
+            (double sx, double sy) = editor.TryToSnap(args);
             switch (_currentState)
             {
                 case State.TopLeft:
