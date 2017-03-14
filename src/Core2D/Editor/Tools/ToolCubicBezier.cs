27ee5c4b708a371041f91e9ecaacd168ac31bfda
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-03-14 13:39:47 +0100

    Update ToolCubicBezier.cs
    [skip ci]

--- src/Core2D/Editor/Tools/ToolCubicBezier.cs
+++ src/Core2D/Editor/Tools/ToolCubicBezier.cs
@@ -47,8 +47,7 @@ namespace Core2D.Editor.Tools
         {
             base.LeftDown(args);
             var editor = _serviceProvider.GetService<ProjectEditor>();
-            double sx = editor.Project.Options.SnapToGrid ? ProjectEditor.Snap(args.X, editor.Project.Options.SnapX) : args.X;
-            double sy = editor.Project.Options.SnapToGrid ? ProjectEditor.Snap(args.Y, editor.Project.Options.SnapY) : args.Y;
+            (double sx, double sy) = editor.TryToSnap(args);
             switch (_currentState)
             {
                 case State.Point1:
@@ -170,8 +169,7 @@ namespace Core2D.Editor.Tools
         {
             base.Move(args);
             var editor = _serviceProvider.GetService<ProjectEditor>();
-            double sx = editor.Project.Options.SnapToGrid ? ProjectEditor.Snap(args.X, editor.Project.Options.SnapX) : args.X;
-            double sy = editor.Project.Options.SnapToGrid ? ProjectEditor.Snap(args.Y, editor.Project.Options.SnapY) : args.Y;
+            (double sx, double sy) = editor.TryToSnap(args);
             switch (_currentState)
             {
                 case State.Point1:
