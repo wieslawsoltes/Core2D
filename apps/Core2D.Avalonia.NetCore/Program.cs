10544cfd472069cd3d4f5b96de4a009f95832cda
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-04-03 11:38:08 +0200

    Update Program.cs

--- apps/Core2D.Avalonia.NetCore/Program.cs
+++ apps/Core2D.Avalonia.NetCore/Program.cs
@@ -1,11 +1,13 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 using System;
+using System.Linq;
 using Autofac;
 using Avalonia;
 using Avalonia.Logging.Serilog;
 using Core2D.Avalonia.Modules;
 using Core2D.Avalonia.NetCore.Modules;
+using Core2D.Avalonia.Views;
 using Core2D.Interfaces;
 using Serilog;
 
@@ -24,6 +26,7 @@ namespace Core2D.Avalonia.NetCore
         {
             InitializeLogging();
 
+            var fbdev = args.Contains("--fbdev");
             var builder = new ContainerBuilder();
 
             builder.RegisterModule<LocatorModule>();
@@ -32,17 +35,42 @@ namespace Core2D.Avalonia.NetCore
             builder.RegisterModule<AppModule>();
             builder.RegisterModule<ViewModule>();
 
-            using (IContainer container = builder.Build())
+            if (fbdev == true)
             {
-                using (var log = container.Resolve<ILog>())
+                IContainer container = builder.Build();
+                ILog log = container.Resolve<ILog>()
+                var app = new App();
+                AppBuilder.Configure(app)
+                    .InitializeWithLinuxFramebuffer(tl =>
+                    {
+                        tl.Content = app.CreateView(container.Resolve<IServiceProvider>());
+                        System.Threading.ThreadPool.QueueUserWorkItem(_ => ConsoleSilencer());
+                    });
+            }
+            else
+            {
+                using (IContainer container = builder.Build())
                 {
-                    var app = new App();
-                    AppBuilder.Configure(app)
-                        .UsePlatformDetect()
-                        .SetupWithoutStarting();
-                    app.Start(container.Resolve<IServiceProvider>());
+                    using (ILog log = container.Resolve<ILog>())
+                    {
+                        var app = new App();
+                        AppBuilder.Configure(app)
+                            .UsePlatformDetect()
+                            .SetupWithoutStarting();
+                        app.Start(container.Resolve<IServiceProvider>());
+                    }
                 }
             }
+        }
+
+        /// <summary>
+        /// Silence the console.
+        /// </summary>
+        static void ConsoleSilencer()
+        {
+            Console.CursorVisible = false;
+            while (true)
+                Console.ReadKey(true);
         }
 
         /// <summary>
