fd3340c7260bc6d99c0db07f0b5cfb3647e6251d
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-05-23 09:56:28 +0200

    Update XPathGeometryTypeConverter.cs
    [skip ci]

--- src/Serializer.Xaml/Converters/XPathGeometryTypeConverter.cs
+++ src/Serializer.Xaml/Converters/XPathGeometryTypeConverter.cs
@@ -4,11 +4,10 @@ using System;
 using System.Globalization;
 #if NETSTANDARD1_3
 using System.ComponentModel;
-#else
-using Portable.Xaml.ComponentModel;
 #endif
 using Core2D.Path;
 using Core2D.Path.Parser;
+using Portable.Xaml.ComponentModel;
 
 namespace Serializer.Xaml.Converters
 {
