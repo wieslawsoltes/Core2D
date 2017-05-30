72e18f627c620aa71c46865ce130dab59f3be50d
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2017-05-30 18:26:11 +0200

    Added XamlConstants class

---
+++ src/Serializer.Xaml/XamlConstants.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Serializer.Xaml
+{
+    public static class XamlConstants
+    {
+        public const string CoreNamespace = "https://github.com/Core2D";
+
+        public const string EditorNamespace = "https://github.com/Core2D.Editor";
+
+        public const string SpatialNamespace = "https://github.com/Math.Spatial";
+    }
+}
