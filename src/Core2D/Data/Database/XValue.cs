284b87d676aba1726879a1505cf5514d319af050
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-05-30 14:20:33 +0200

    Update XValue.cs
    [skip ci]

--- src/Core2D/Data/Database/XValue.cs
+++ src/Core2D/Data/Database/XValue.cs
@@ -27,5 +27,11 @@ namespace Core2D.Data.Database
         /// <param name="content">The value content.</param>
         /// <returns>The new instance of the <see cref="XValue"/> class.</returns>
         public static XValue Create(string content) => new XValue() { Content = content };
+
+        /// <summary>
+        /// Check whether the <see cref="Content"/> property has changed from its default value.
+        /// </summary>
+        /// <returns>Returns true if the property has changed; otherwise, returns false.</returns>
+        public bool ShouldSerializeContent() => !String.IsNullOrWhiteSpace(_content);
     }
 }
