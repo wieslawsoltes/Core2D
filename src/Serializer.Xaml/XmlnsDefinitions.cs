48a4f63e2a607f92212905cfe7a72d019cac611d
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-05-30 12:29:06 +0200

    Create XmlnsDefinitions.cs

---
+++ src/Serializer.Xaml/XmlnsDefinitions.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using Portable.Xaml.Markup;
+using Serializer.Xaml;
+using System.Reflection;
+using System.Resources;
+
+[assembly: XmlnsDefinition(CoreXamlSchemaContext.CoreNamespace, "Core2D", AssemblyName = "Core2D")]
+[assembly: XmlnsDefinition(CoreXamlSchemaContext.CoreNamespace, "Core2D.Collections", AssemblyName = "Core2D")]
+[assembly: XmlnsDefinition(CoreXamlSchemaContext.CoreNamespace, "Core2D.Data", AssemblyName = "Core2D")]
+[assembly: XmlnsDefinition(CoreXamlSchemaContext.CoreNamespace, "Core2D.Data.Database", AssemblyName = "Core2D")]
+[assembly: XmlnsDefinition(CoreXamlSchemaContext.CoreNamespace, "Core2D.Editor", AssemblyName = "Core2D")]
+[assembly: XmlnsDefinition(CoreXamlSchemaContext.CoreNamespace, "Core2D.Editor.Bounds", AssemblyName = "Core2D")]
+[assembly: XmlnsDefinition(CoreXamlSchemaContext.CoreNamespace, "Core2D.Editor.Designer", AssemblyName = "Core2D")]
+[assembly: XmlnsDefinition(CoreXamlSchemaContext.CoreNamespace, "Core2D.Editor.Factories", AssemblyName = "Core2D")]
+[assembly: XmlnsDefinition(CoreXamlSchemaContext.CoreNamespace, "Core2D.Editor.Input", AssemblyName = "Core2D")]
+[assembly: XmlnsDefinition(CoreXamlSchemaContext.CoreNamespace, "Core2D.Editor.Interfaces", AssemblyName = "Core2D")]
+[assembly: XmlnsDefinition(CoreXamlSchemaContext.CoreNamespace, "Core2D.Editor.Tools", AssemblyName = "Core2D")]
+[assembly: XmlnsDefinition(CoreXamlSchemaContext.CoreNamespace, "Core2D.Editor.Views", AssemblyName = "Core2D")]
+[assembly: XmlnsDefinition(CoreXamlSchemaContext.CoreNamespace, "Core2D.Editor.Recent", AssemblyName = "Core2D")]
+[assembly: XmlnsDefinition(CoreXamlSchemaContext.CoreNamespace, "Core2D.History", AssemblyName = "Core2D")]		
+[assembly: XmlnsDefinition(CoreXamlSchemaContext.CoreNamespace, "Core2D.Interfaces", AssemblyName = "Core2D")]
+[assembly: XmlnsDefinition(CoreXamlSchemaContext.CoreNamespace, "Core2D.Path", AssemblyName = "Core2D")]
+[assembly: XmlnsDefinition(CoreXamlSchemaContext.CoreNamespace, "Core2D.Path.Segments", AssemblyName = "Core2D")]
+[assembly: XmlnsDefinition(CoreXamlSchemaContext.CoreNamespace, "Core2D.Project", AssemblyName = "Core2D")]
+[assembly: XmlnsDefinition(CoreXamlSchemaContext.CoreNamespace, "Core2D.Renderer", AssemblyName = "Core2D")]
+[assembly: XmlnsDefinition(CoreXamlSchemaContext.CoreNamespace, "Core2D.Shape", AssemblyName = "Core2D")]
+[assembly: XmlnsDefinition(CoreXamlSchemaContext.CoreNamespace, "Core2D.Shapes", AssemblyName = "Core2D")]
+[assembly: XmlnsDefinition(CoreXamlSchemaContext.CoreNamespace, "Core2D.Style", AssemblyName = "Core2D")]
+
+[assembly: XmlnsPrefix(CoreXamlSchemaContext.CoreNamespace, "c")]
+
+[assembly: XmlnsDefinition(CoreXamlSchemaContext.SpatialNamespace, "Spatial", AssemblyName = "Math.Spatial")]
+[assembly: XmlnsDefinition(CoreXamlSchemaContext.SpatialNamespace, "Spatial.Arc", AssemblyName = "Math.Spatial")]
+[assembly: XmlnsDefinition(CoreXamlSchemaContext.SpatialNamespace, "Spatial.ConvexHull", AssemblyName = "Math.Spatial")]
+[assembly: XmlnsDefinition(CoreXamlSchemaContext.SpatialNamespace, "Spatial.DouglasPeucker", AssemblyName = "Math.Spatial")]
+[assembly: XmlnsDefinition(CoreXamlSchemaContext.SpatialNamespace, "Spatial.Sat", AssemblyName = "Math.Spatial")]
+
+[assembly: XmlnsPrefix(CoreXamlSchemaContext.SpatialNamespace, "s")]
