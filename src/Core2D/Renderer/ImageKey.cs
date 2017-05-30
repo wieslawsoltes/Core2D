afd50052fc96cb8b1f1f241363d63e4f4b2302b3
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-05-30 16:10:39 +0200

    Update ImageKey.cs
    [skip ci]

--- src/Core2D/Renderer/ImageKey.cs
+++ src/Core2D/Renderer/ImageKey.cs
@@ -1,5 +1,6 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
 
 namespace Core2D.Renderer
 {
@@ -12,5 +13,11 @@ namespace Core2D.Renderer
         /// Gets or sets image key.
         /// </summary>
         public string Key { get; set; }
+
+        /// <summary>
+        /// Check whether the <see cref="Key"/> property has changed from its default value.
+        /// </summary>
+        /// <returns>Returns true if the property has changed; otherwise, returns false.</returns>
+        public bool ShouldSerializeKey() => !String.IsNullOrWhiteSpace(Key);
     }
 }
