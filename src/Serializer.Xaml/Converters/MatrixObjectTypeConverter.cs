1e33caa53204c43ea5c91cc499b1492415977418
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-06-01 10:15:21 +0200

    Create MatrixObjectTypeConverter.cs

---
+++ src/Serializer.Xaml/Converters/MatrixObjectTypeConverter.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+using System.Globalization;
+#if NETSTANDARD1_3
+using System.ComponentModel;
+#else
+using Portable.Xaml.ComponentModel;
+#endif
+using Core2D.Renderer;
+
+namespace Serializer.Xaml.Converters
+{
+    /// <summary>
+    /// Defines <see cref="MatrixObject"/> type converter.
+    /// </summary>
+    internal class MatrixObjectTypeConverter : TypeConverter
+    {
+        /// <inheritdoc/>
+        public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+        {
+            return sourceType == typeof(string);
+        }
+
+        /// <inheritdoc/>
+        public override bool CanConvertTo(ITypeDescriptorContext context, Type destinationType)
+        {
+            return destinationType == typeof(string);
+        }
+
+        /// <inheritdoc/>
+        public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+        {
+            return MatrixObject.Parse((string)value);
+        }
+
+        /// <inheritdoc/>
+        public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
+        {
+            return value is MatrixObject matrix ? matrix.ToString() : throw new NotSupportedException();
+        }
+    }
+}
