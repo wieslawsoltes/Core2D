ec47333b52d3a1796d419f31bd59ecbb9eb8a264
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-06-02 09:50:44 +0200

    Update DocumentView.cs
    [skip ci]

--- src/Core2D.Editor/Views/DocumentView.cs
+++ src/Core2D.Editor/Views/DocumentView.cs
@@ -13,7 +13,7 @@ namespace Core2D.Editor.Views
         private Lazy<ProjectEditor> _context;
 
         /// <inheritdoc/>
-        public override string Name => "Document";
+        public override string Title => "Document";
 
         /// <inheritdoc/>
         public override object Context => _context.Value;
