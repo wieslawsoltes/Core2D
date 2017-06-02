0518986d1108a29f5436ba3ea289f28cc8293cea
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-06-02 07:38:59 +0200

    Update ObservableObject.cs
    [skip ci]

--- src/Core2D/ObservableObject.cs
+++ src/Core2D/ObservableObject.cs
@@ -1,15 +1,49 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System.Collections.Immutable;	
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using Core2D.Attributes;
 
 namespace Core2D
 {
     /// <summary>
-    /// Observable properties base class.
+    /// Observable object base class.
     /// </summary>
     public abstract class ObservableObject : INotifyPropertyChanged
     {
+        private string _id = null;
+        private string _name = null;
+        private ImmutableArray<ObservableObject> _resources = ImmutableArray.Create<ObservableObject>();
+
+        /// <summary>
+        /// Gets or sets resource name.
+        /// </summary>
+        [Name]
+        public string Id
+        {
+            get => _id;
+            set => Update(ref _id, value);
+        }
+
+        /// <summary>
+        /// Gets or sets resource name.
+        /// </summary>
+        public string Name
+        {
+            get => _name;
+            set => Update(ref _name, value);
+        }
+
+        /// <summary>
+        /// Gets or sets shape resources.
+        /// </summary>
+        public ImmutableArray<ObservableObject> Resources
+        {
+            get => _resources;
+            set => Update(ref _resources, value);
+        }
+
         /// <summary>
         /// Occurs when a property value changes.
         /// </summary>
@@ -42,5 +76,23 @@ namespace Core2D
             }
             return false;
         }
+
+        /// <summary>
+        /// Check whether the <see cref="Id"/> property has changed from its default value.
+        /// </summary>
+        /// <returns>Returns true if the property has changed; otherwise, returns false.</returns>
+        public bool ShouldSerializeId() => !String.IsNullOrWhiteSpace(_id);
+
+        /// <summary>
+        /// Check whether the <see cref="Name"/> property has changed from its default value.
+        /// </summary>
+        /// <returns>Returns true if the property has changed; otherwise, returns false.</returns>
+        public bool ShouldSerializeName() => !String.IsNullOrWhiteSpace(_name);
+
+        /// <summary>
+        /// Check whether the <see cref="Resources"/> property has changed from its default value.
+        /// </summary>
+        /// <returns>Returns true if the property has changed; otherwise, returns false.</returns>
+        public bool ShouldSerializeResources() => _resources.IsEmpty == false;
     }
 }
