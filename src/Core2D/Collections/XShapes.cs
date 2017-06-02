fc35d0fb920ac49c72e43b93908a4ccd1828aac1
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-06-02 09:26:58 +0200

    Update XShapes.cs
    [skip ci]

--- src/Core2D/Collections/XShapes.cs
+++ src/Core2D/Collections/XShapes.cs
@@ -10,14 +10,8 @@ namespace Core2D.Collections
     /// <summary>
     /// Observable <see cref="BaseShape"/> collection.
     /// </summary>
-    public class XShapes : ObservableResource
+    public class XShapes : ObservableObject
     {
-        /// <summary>
-        /// Gets or sets resource name.
-        /// </summary>
-        [Name]
-        public string Name { get; set; }
-
         /// <summary>
         /// Gets or sets children collection.
         /// </summary>
@@ -31,12 +25,6 @@ namespace Core2D.Collections
         {
             Children = ImmutableArray.Create<BaseShape>();
         }
-
-        /// <summary>
-        /// Check whether the <see cref="Name"/> property has changed from its default value.
-        /// </summary>
-        /// <returns>Returns true if the property has changed; otherwise, returns false.</returns>
-        public bool ShouldSerializeName() => !String.IsNullOrWhiteSpace(Name);
 
         /// <summary>
         /// Check whether the <see cref="Children"/> property has changed from its default value.
