7eb6bbc46cb47d7f1c6dda4ad4ef35bb50a98af9
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-06-02 09:25:27 +0200

    Update BaseShapeTests.cs
    [skip ci]

--- tests/Core2D.UnitTests/Shape/BaseShapeTests.cs
+++ tests/Core2D.UnitTests/Shape/BaseShapeTests.cs
@@ -16,10 +16,10 @@ namespace Core2D.UnitTests
     {
         [Fact]
         [Trait("Core2D.Shape", "Shape")]
-        public void Inherits_From_ObservableResource()
+        public void Inherits_From_ObservableObject()
         {
             var target = new Class1();
-            Assert.True(target is ObservableResource);
+            Assert.True(target is ObservableObject);
         }
 
         [Fact]
