39e343db3e7658e5ad6ec93b3716635f1e9de675
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-06-08 15:06:58 +0200

    Create App.xaml.cs
    [skip ci]

--- apps/Core2D.SkiaView/App.xaml.cs
+++ apps/Core2D.SkiaView/App.xaml.cs
@@ -2,24 +2,10 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 using System;
 using System.Windows;
-using Autofac;
 
 namespace Core2D.SkiaView
 {
     public partial class App : Application
     {
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            base.OnStartup(e);
-
-            var builder = new ContainerBuilder();
-
-            builder.RegisterAssemblyModules(typeof(MainWindow).Assembly);
-
-            using (IContainer container = builder.Build())
-            {
-                new MainWindow(container.Resolve<IServiceProvider>()).ShowDialog();
-            }
-        }
     }
 }
