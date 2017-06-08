e954a1e345b324d6e041d47f5fb1f311852397b9
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-06-08 15:21:34 +0200

    Create MainWindow.xaml.cs

--- apps/Core2D.SkiaView/MainWindow.xaml.cs
+++ apps/Core2D.SkiaView/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 using System;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Windows;
+using Autofac;
 using Core2D.Interfaces;
 using Core2D.Project;
 using Core2D.Renderer;
@@ -14,6 +16,7 @@ namespace Core2D.SkiaView
 {
     public partial class MainWindow : Window
     {
+        private readonly IContainer _container;
         private readonly IServiceProvider _serviceProvider;
         private ContainerPresenter _presenter;
         private ShapeRenderer _renderer;
@@ -22,92 +25,55 @@ namespace Core2D.SkiaView
         public MainWindow(IServiceProvider serviceProvider)
         {
             InitializeComponent();
+            InitializeContainer();
+            
+            Loaded += MainWindow_Loaded;
+            Closing += MainWindow_Closing;
+            
+            FileOpen.Click += FileOpen_Click;
+            FileClose.Click += FileClose_Click;
+            FileExit.Click += FileExit_Click;
+            
+            CanvasElement.PaintSurface += PaintSurface;
+        }
 
-            _serviceProvider = serviceProvider;
+        private void InitializeContainer()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterAssemblyModules(typeof(MainWindow).Assembly);
+
+            _container = builder.Build();
+
+            _serviceProvider = _container.Resolve<IServiceProvider>();
+
             _presenter = _serviceProvider.GetService<ContainerPresenter>();
             _renderer = _serviceProvider.GetService<ShapeRenderer>();
-
-            Loaded += MainWindow_Loaded;
         }
 
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            canvas.Focusable = true;
-            canvas.Focus();
+            CanvasElement.Focusable = true;
+            CanvasElement.Focus();
             OnRefreshRequested(null, null);
         }
 
-        private void OnPaintCanvas(object sender, SKPaintSurfaceEventArgs e)
+        private void MainWindow_Closing(object sender, CancelEventArgs e)
         {
-            OnPaintSurface(e.Surface.Canvas, e.Info.Width, e.Info.Height);
+            _container.Dispose();
         }
 
-        private void OnRefreshRequested(object sender, EventArgs e)
-        {
-            canvas.InvalidateVisual();
-        }
-
-        private void OnPaintSurface(SKCanvas canvas, int width, int height)
-        {
-            var container = _project?.CurrentContainer;
-            if (container != null)
-            {
-                var matrix = PresentationSource.FromVisual(this).CompositionTarget.TransformToDevice;
-                double offsetX = (this.canvas.ActualWidth * matrix.M11 - container?.Width ?? 0) / 2.0;
-                double offsetY = (this.canvas.ActualHeight * matrix.M22 - container?.Height ?? 0) / 2.0;
-                canvas.Clear(SKColors.White);
-                _presenter?.Render(canvas, _renderer, container, offsetX, offsetY);
-            }
-        }
-
-        public void OpenProject(string path)
+        private void FileOpen_Click(object sender, RoutedEventArgs e)
         {
             try
             {
-                var fileIO = _serviceProvider.GetService<IFileSystem>();
-                var jsonSerializer = _serviceProvider.GetService<IJsonSerializer>();
-                var project = XProject.Open(path, fileIO, jsonSerializer);
-                if (project != null)
-                {
-                    _project = project;
-                    _renderer.ClearCache(isZooming: false);
-                    _renderer.State.ImageCache = project;
-                    OnRefreshRequested(null, null);
-                }
+                OpenProject();
             }
             catch (Exception ex)
             {
                 Debug.WriteLine(ex.Message);
                 Debug.WriteLine(ex.StackTrace);
             }
-        }
-
-        private void OpenProject()
-        {
-            var dlg = new OpenFileDialog()
-            {
-                Filter = "Project (*.project)|*.project|All (*.*)|*.*",
-                FilterIndex = 0,
-                FileName = ""
-            };
-
-            if (dlg.ShowDialog(this) == true)
-            {
-                OpenProject(dlg.FileName);
-            }
-        }
-
-        public void CloseProject()
-        {
-            _project = null;
-            _renderer.ClearCache(isZooming: false);
-            _renderer.State.ImageCache = null;
-            OnRefreshRequested(null, null);
-        }
-
-        private void FileOpen_Click(object sender, RoutedEventArgs e)
-        {
-            OpenProject();
         }
 
         private void FileClose_Click(object sender, RoutedEventArgs e)
@@ -118,6 +84,66 @@ namespace Core2D.SkiaView
         private void FileExit_Click(object sender, RoutedEventArgs e)
         {
             Close();
+        }
+
+        private void PaintSurface(object sender, SKPaintSurfaceEventArgs e)
+        {
+            PaintSurface(e.Surface.Canvas, e.Info.Width, e.Info.Height);
+        }
+
+        private void RefreshRequested(object sender, EventArgs e)
+        {
+            CanvasElement.InvalidateVisual();
+        }
+
+        private void PaintSurface(SKCanvas canvas, int width, int height)
+        {
+            var container = _project?.CurrentContainer;
+            if (container != null)
+            {
+                var matrix = PresentationSource.FromVisual(this).CompositionTarget.TransformToDevice;
+                double offsetX = (this.canvas.ActualWidth * matrix.M11 - container?.Width ?? 0) / 2.0;
+                double offsetY = (this.canvas.ActualHeight * matrix.M22 - container?.Height ?? 0) / 2.0;
+
+                canvas.Clear(SKColors.White);
+                _presenter?.Render(canvas, _renderer, container, offsetX, offsetY);
+            }
+        }
+
+        private void SetProject(XProject project)
+        {
+            _project = project;
+            _renderer.ClearCache(isZooming: false);
+            _renderer.State.ImageCache = project;
+        }
+
+        private void OpenProject(string path)
+        {
+            var project = XProject.Open(path, 
+                _serviceProvider.GetService<IFileSystem>(), 
+                _serviceProvider.GetService<IJsonSerializer>());
+
+            SetProject(project);
+            RefreshRequested(null, null);
+        }
+
+        private void OpenProject()
+        {
+            var dlg = new OpenFileDialog()
+            {
+                Filter = "Project (*.project)|*.project|All (*.*)|*.*"
+            };
+
+            if (dlg.ShowDialog(this) == true)
+            {
+                OpenProject(dlg.FileName);
+            }
+        }
+
+        private void CloseProject()
+        {
+            SetProject(null);
+            RefreshRequested(null, null);
         }
     }
 }
