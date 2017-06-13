e577b85adafd9a14e8921beb3cbaffc1102950e8
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2017-06-13 15:54:54 +0200

    Added interfaces

---
+++ src/Core2D/IDrawable.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using Core2D.Renderer;
+using Core2D.Style;
+
+namespace Core2D
+{
+    /// <summary>
+    /// Defines drawable shape contract.
+    /// </summary>
+    public interface IDrawable
+    {
+        /// <summary>
+        /// Get or sets shape drawing style.
+        /// </summary>
+        ShapeStyle Style { get; set; }
+
+        /// <summary>
+        /// Get or sets shape matrix transform.
+        /// </summary>
+        MatrixObject Transform { get; set; }
+
+        /// <summary>
+        /// Begins matrix transform.
+        /// </summary>
+        /// <param name="dc">The generic drawing context object.</param>
+        /// <param name="renderer">The generic renderer object used to draw shape.</param>
+        /// <returns>The previous transform state.</returns>
+        object BeginTransform(object dc, ShapeRenderer renderer);
+
+        /// <summary>
+        /// Ends matrix transform.
+        /// </summary>
+        /// <param name="dc">The generic drawing context object.</param>
+        /// <param name="renderer">The generic renderer object used to draw shape.</param>
+        /// <param name="state">The previous transform state.</param>
+        void EndTransform(object dc, ShapeRenderer renderer, object state);
+
+        /// <summary>
+        /// Draws shape using current <see cref="ShapeRenderer"/>.
+        /// </summary>
+        /// <param name="dc">The generic drawing context object.</param>
+        /// <param name="renderer">The generic renderer object used to draw shape.</param>
+        /// <param name="dx">The X axis draw position offset.</param>
+        /// <param name="dy">The Y axis draw position offset.</param>
+        /// <param name="db">The properties database.</param>
+        /// <param name="r">The database record.</param>
+        void Draw(object dc, ShapeRenderer renderer, double dx, double dy, object db, object r);
+    }
+}
---
+++ src/Core2D/ISelectable.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System.Collections.Generic;
+using Core2D.Shape;
+
+namespace Draw2D.Core
+{
+    /// <summary>
+    /// Defines selectable shape contract.
+    /// </summary>
+    public interface ISelectable
+    {
+        /// <summary>
+        /// Moves shape to new position using X and Y axis offset.
+        /// </summary>
+        /// <param name="selected">The selected shapes set.</param>
+        /// <param name="dx">The X axis position offset.</param>
+        /// <param name="dy">The Y axis position offset.</param>
+        void Move(ISet<BaseShape> selected, double dx, double dy);
+
+        /// <summary>
+        /// Selects the shape.
+        /// </summary>
+        /// <param name="selected">The selected shapes set.</param>
+        void Select(ISet<BaseShape> selected);
+
+        /// <summary>
+        /// Deselects the shape.
+        /// </summary>
+        /// <param name="selected">The selected shapes set.</param>
+        void Deselect(ISet<BaseShape> selected);
+    }
+}
