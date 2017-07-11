9e319b43cc960794bdb8bbf9726ed732a187cb65
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2017-07-11 16:49:58 +0200

    Added ConnectableShape extensions

--- src/Core2D/Shapes/ConnectableShape.cs
+++ src/Core2D/Shapes/ConnectableShape.cs
@@ -115,42 +115,6 @@ namespace Core2D.Shapes
         }
 
         /// <summary>
-        /// Adds <see cref="PointShape"/> to <see cref="Connectors"/> collection with <see cref="ShapeStateFlags.None"/> flag set.
-        /// </summary>
-        /// <param name="point">The point object.</param>
-        public void AddConnectorAsNone(PointShape point)
-        {
-            point.Owner = this;
-            point.State.Flags |= ShapeStateFlags.Connector | ShapeStateFlags.None;
-            point.State.Flags &= ~ShapeStateFlags.Standalone;
-            Connectors = Connectors.Add(point);
-        }
-
-        /// <summary>
-        /// Adds <see cref="PointShape"/> to <see cref="Connectors"/> collection with <see cref="ShapeStateFlags.Input"/> flag set.
-        /// </summary>
-        /// <param name="point">The point object.</param>
-        public void AddConnectorAsInput(PointShape point)
-        {
-            point.Owner = this;
-            point.State.Flags |= ShapeStateFlags.Connector | ShapeStateFlags.Input;
-            point.State.Flags &= ~ShapeStateFlags.Standalone;
-            Connectors = Connectors.Add(point);
-        }
-
-        /// <summary>
-        /// Adds <see cref="PointShape"/> to <see cref="Connectors"/> collection with <see cref="ShapeStateFlags.Output"/> flag set.
-        /// </summary>
-        /// <param name="point">The point object.</param>
-        public void AddConnectorAsOutput(PointShape point)
-        {
-            point.Owner = this;
-            point.State.Flags |= ShapeStateFlags.Connector | ShapeStateFlags.Output;
-            point.State.Flags &= ~ShapeStateFlags.Standalone;
-            Connectors = Connectors.Add(point);
-        }
-
-        /// <summary>
         /// Check whether the <see cref="Connectors"/> property has changed from its default value.
         /// </summary>
         /// <returns>Returns true if the property has changed; otherwise, returns false.</returns>
