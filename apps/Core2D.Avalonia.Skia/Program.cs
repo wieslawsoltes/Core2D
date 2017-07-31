516e625875604a3ce8d7affa0a38b45d85631455
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2017-07-31 21:13:03 +0200

    Added correct using

--- apps/Core2D.Avalonia.Skia/Program.cs
+++ apps/Core2D.Avalonia.Skia/Program.cs
@@ -1,9 +1,9 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 using System;
+using System.Diagnostics;
 using Autofac;
 using Avalonia;
-using Avalonia.Diagnostics;
 using Avalonia.Logging.Serilog;
 using Core2D.Avalonia.Modules;
 using Core2D.Avalonia.Skia.Modules;
