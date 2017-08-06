aa9aa758dff2f07cdb53143bc5af60c56c42ae2f
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2017-08-07 01:08:51 +0200

    Added missing Copyright header

--- src/Core2D/Renderer/ContainerPresenter.cs
+++ src/Core2D/Renderer/ContainerPresenter.cs
@@ -1,4 +1,6 @@
-﻿using Core2D.Containers;
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using Core2D.Containers;
 
 namespace Core2D.Renderer
 {
