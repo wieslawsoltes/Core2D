58242b9a30e00c700b40473adc5143f5685882a1
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-09-28 10:59:15 +0200

    Update Program.cs

--- apps/Core2D.Avalonia/Program.cs
+++ apps/Core2D.Avalonia/Program.cs
@@ -12,7 +12,7 @@ using Serilog;
 namespace Core2D.Avalonia
 {
     /// <summary>
-    /// Encapsulates an Avalonia program.
+    /// Encapsulates an Core2D avalonia program.
     /// </summary>
     class Program
     {
