9c5a37a3b3af3860cbfef91f4c0ce60a25938d91
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2018-05-02 14:48:31 +0200

    Added drag and drop behaviors

---
+++ src/Core2D.Avalonia/Behaviors/DragBehavior.cs
@@ -0,0 +1,76 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Xaml.Interactivity;
+
+namespace Core2D.Avalonia.Behaviors
+{
+    public sealed class DragBehavior : Behavior<Control>
+    {
+        private double _minimumHorizontalDragDistance = 5;
+        private double _minimumVerticalDragDistance = 5;
+        private Point _dragStartPoint;
+        private bool _pressed;
+        private bool _drag;
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.PointerPressed += AssociatedObject_PointerPressed;
+            AssociatedObject.PointerReleased += AssociatedObject_PointerReleased;
+            AssociatedObject.PointerMoved += AssociatedObject_PointerMoved;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            AssociatedObject.PointerPressed -= AssociatedObject_PointerPressed;
+            AssociatedObject.PointerReleased -= AssociatedObject_PointerReleased;
+            AssociatedObject.PointerMoved -= AssociatedObject_PointerMoved;
+        }
+
+        private void AssociatedObject_PointerPressed(object sender, PointerPressedEventArgs e)
+        {
+            if (e.MouseButton == MouseButton.Left)
+            {
+                _dragStartPoint = e.GetPosition(AssociatedObject);
+                _pressed = true;
+                _drag = false;
+                Console.WriteLine("Pressed");
+            }
+        }
+
+        private void AssociatedObject_PointerReleased(object sender, PointerReleasedEventArgs e)
+        {
+            if (e.MouseButton == MouseButton.Left)
+            {
+                _pressed = false;
+                _drag = false;
+                Console.WriteLine("Released");
+            }
+        }
+
+        private void AssociatedObject_PointerMoved(object sender, PointerEventArgs e)
+        {
+            Point point = e.GetPosition(AssociatedObject);
+            Vector diff = _dragStartPoint - point;
+            if (_pressed == true && _drag == false &&
+                (Math.Abs(diff.X) > _minimumHorizontalDragDistance || Math.Abs(diff.Y) > _minimumVerticalDragDistance))
+            {
+                _drag = true;
+
+                var value = AssociatedObject.DataContext;
+                var format = value.GetType().ToString();
+
+                var data = new DataObject();
+                data.Set(format, value);
+
+                DragDrop.DoDragDrop(data, DragDropEffects.Link);
+                Console.WriteLine("DoDragDrop");
+            }
+        }
+    }
+}
---
+++ src/Core2D.Avalonia/Behaviors/DropBehavior.cs
@@ -0,0 +1,65 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Xaml.Interactivity;
+
+namespace Core2D.Avalonia.Behaviors
+{
+    public sealed class DropBehavior : Behavior<Control>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            DragDrop.SetAllowDrop(AssociatedObject, true);
+            AssociatedObject.AddHandler(DragDrop.DropEvent, Drop);
+            AssociatedObject.AddHandler(DragDrop.DragEnterEvent, DragEnter);
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            DragDrop.SetAllowDrop(AssociatedObject, false);
+            AssociatedObject.RemoveHandler(DragDrop.DropEvent, Drop);
+            AssociatedObject.RemoveHandler(DragDrop.DragEnterEvent, DragEnter);
+        }
+
+        private void DragEnter(object sender, DragEventArgs e)
+        {
+            e.DragEffects = e.DragEffects & (DragDropEffects.Copy | DragDropEffects.Link);
+
+            //if (!e.Data.Contains(DataFormats.Text) && !e.Data.Contains(DataFormats.FileNames))
+            //    e.DragEffects = DragDropEffects.None;
+
+            Console.WriteLine("DragEnter");
+        }
+
+        private void Drop(object sender, DragEventArgs e)
+        {
+            Console.WriteLine("Drop");
+
+            foreach (var format in e.Data.GetDataFormats())
+            {
+                Console.WriteLine(format);
+            }
+
+            if (e.Data.Contains(DataFormats.Text))
+            {
+                var text = e.Data.GetText();
+                Console.WriteLine(text);
+            }
+
+            if (e.Data.Contains(DataFormats.FileNames))
+            {
+                foreach (var file in e.Data.GetFileNames())
+                {
+                    Console.WriteLine(file);
+                }
+            }
+
+            e.DragEffects = DragDropEffects.None;
+            e.Handled = true;
+        }
+    }
+}
