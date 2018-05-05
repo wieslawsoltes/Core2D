f70b05760abc125b9b7fe30702a19375bc22dbb7
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2018-05-05 13:48:09 +0200

    Handle list box drag and drop

--- src/Core2D.Avalonia/Behaviors/DropBehavior.cs
+++ src/Core2D.Avalonia/Behaviors/DropBehavior.cs
@@ -1,12 +1,14 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 using System;
+using System.Collections.Immutable;
 using System.Linq;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.VisualTree;
 using Avalonia.Xaml.Interactivity;
+using Core2D.Containers;
 using Core2D.Data;
 using Core2D.Editor;
 using Core2D.Shape;
@@ -14,15 +16,30 @@ using Core2D.Style;
 
 namespace Core2D.Avalonia.Behaviors
 {
+    public enum ListBoxDropMode
+    {
+        Move,
+        Swap
+    }
+
     public sealed class DropBehavior : Behavior<Control>
     {
         public static readonly AvaloniaProperty EditorProperty =
             AvaloniaProperty.Register<DropBehavior, ProjectEditor>(nameof(Editor));
 
+        public static readonly AvaloniaProperty ListBoxDropModeProperty =
+            AvaloniaProperty.Register<DropBehavior, ListBoxDropMode>(nameof(ListBoxDropMode));
+
         public ProjectEditor Editor
         {
             get => (ProjectEditor)GetValue(EditorProperty);
             set => SetValue(EditorProperty, value);
+        }
+
+        public ListBoxDropMode ListBoxDropMode
+        {
+            get => (ListBoxDropMode)GetValue(ListBoxDropModeProperty);
+            set => SetValue(ListBoxDropModeProperty, value);
         }
 
         protected override void OnAttached()
@@ -79,6 +96,89 @@ namespace Core2D.Avalonia.Behaviors
             Console.WriteLine($"Drop sender: {sender}, source: {e.Source}");
 
             var point = GetPoint(sender);
+
+            if (sender is ListBox list)
+            {
+                var parent = e.Data.Get(CustomDataFormats.Parent);
+                if (parent is ListBoxItem source)
+                {
+                    if ((e.Source as IControl).Parent is ListBoxItem target)
+                    {
+                        int sourceIndex = list.ItemContainerGenerator.IndexFromContainer(source);
+                        int targetIndex = list.ItemContainerGenerator.IndexFromContainer(target);
+                        Console.WriteLine($"sourceIndex : {sourceIndex}");
+                        Console.WriteLine($"targetIndex : {targetIndex}");
+
+                        switch (list.Items)
+                        {
+                            case ImmutableArray<ShapeStyle> styles:
+                                {
+                                    if (list.DataContext is Library<ShapeStyle> library)
+                                    {
+                                        switch (ListBoxDropMode)
+                                        {
+                                            case ListBoxDropMode.Move:
+                                                {
+                                                    if (sourceIndex < targetIndex)
+                                                    {
+                                                        var item1 = library.Items[sourceIndex];
+                                                        var builder = library.Items.ToBuilder();
+                                                        builder.Insert(targetIndex + 1, item1);
+                                                        builder.RemoveAt(sourceIndex);
+
+                                                        var previous = library.Items;
+                                                        var next = builder.ToImmutable();
+                                                        Editor?.Project?.History?.Snapshot(previous, next, (p) => library.Items = p);
+                                                        library.Items = next;
+                                                    }
+                                                    else
+                                                    {
+                                                        int removeIndex = sourceIndex + 1;
+                                                        if (styles.Length + 1 > removeIndex)
+                                                        {
+                                                            var item1 = library.Items[sourceIndex];
+                                                            var builder = library.Items.ToBuilder();
+                                                            builder.Insert(targetIndex, item1);
+                                                            builder.RemoveAt(removeIndex);
+
+                                                            var previous = library.Items;
+                                                            var next = builder.ToImmutable();
+                                                            Editor?.Project?.History?.Snapshot(previous, next, (p) => library.Items = p);
+                                                            library.Items = next;
+                                                        }
+                                                    }
+                                                }
+                                                break;
+                                            case ListBoxDropMode.Swap:
+                                                {
+                                                    var item1 = library.Items[sourceIndex];
+                                                    var item2 = library.Items[targetIndex];
+                                                    var builder = library.Items.ToBuilder();
+                                                    builder[targetIndex] = item1;
+                                                    builder[sourceIndex] = item2;
+
+                                                    var previous = library.Items;
+                                                    var next = builder.ToImmutable();
+                                                    Editor?.Project?.History?.Snapshot(previous, next, (p) => library.Items = p);
+                                                    library.Items = next;
+                                                }
+                                                break;
+                                        }
+
+                                    }
+                                }
+                                break;
+                        }
+
+                        Console.WriteLine($"Items type : {list.Items.GetType()}");
+
+                        e.DragEffects = DragDropEffects.None;
+                        e.Handled = true;
+                        return;
+                    }
+                    
+                }
+            }
 
             foreach (var format in e.Data.GetDataFormats())
             {
