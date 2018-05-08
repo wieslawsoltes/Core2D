575dff7efd45a5f9cb3ae0fec6e98d58e45a3443
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2018-05-08 20:38:42 +0200

    Remove

--- src/Core2D.Avalonia/Behaviors/DropBehavior.cs
+++ src/Core2D.Avalonia/Behaviors/DropBehavior.cs
@@ -348,7 +348,7 @@ namespace Core2D.Avalonia.Behaviors
                         }
                     }
                     break;
-            };
+            }
 
             if (e.Data.Contains(DataFormats.Text))
             {
