f76b9e6a4424febdca8c83d9031c5cd1cce1dc0d
wieslawsoltes <wieslaw.soltes@gmail.com>
2018-05-13 12:23:21 +0000

    Added missing using

--- src/Dock.Model/ViewBase.cs
+++ src/Dock.Model/ViewBase.cs
@@ -1,5 +1,6 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System.Collections.Immutable;
 
 namespace Dock.Model
 {
