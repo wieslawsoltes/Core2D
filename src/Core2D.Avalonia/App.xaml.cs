be809cbfecc7bd9e050e57c364c2d01d22b0816d
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2018-05-20 19:46:16 +0200

    Remove using

--- src/Core2D.Avalonia/App.xaml.cs
+++ src/Core2D.Avalonia/App.xaml.cs
@@ -9,7 +9,6 @@ using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using Avalonia.Platform;
 using Core2D.Avalonia.Converters;
-using Core2D.Avalonia.Dock;
 using Core2D.Avalonia.Modules;
 using Core2D.Avalonia.Views;
 using Core2D.Editor;
