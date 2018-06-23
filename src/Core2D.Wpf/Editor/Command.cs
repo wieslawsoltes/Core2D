f82ee471e352175a671c4a908e822d7b0f153fce
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2018-06-23 15:23:35 +0200

    Added Command class

---
+++ src/Core2D.Wpf/Editor/Command.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+using System.Windows.Input;
+
+namespace Core2D.Wpf.Editor
+{
+    public class Command : ICommand
+    {
+        private readonly Func<object, bool> _canExecute;
+        private readonly Action<object> _execute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public Command(Func<object, bool> canExecute, Action<object> execute)
+        {
+            _canExecute = canExecute;
+            _execute = execute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute?.Invoke(parameter) == true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute?.Invoke(parameter);
+        }
+    }
+}
