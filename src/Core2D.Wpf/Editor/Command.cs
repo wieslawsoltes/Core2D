24fa4bf05b6124fa2d5e496cfd702adf560951ed
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2018-06-23 15:25:33 +0200

    Added xml docs

--- src/Core2D.Wpf/Editor/Command.cs
+++ src/Core2D.Wpf/Editor/Command.cs
@@ -5,24 +5,35 @@ using System.Windows.Input;
 
 namespace Core2D.Wpf.Editor
 {
+    /// <summary>
+    /// Command implementation.
+    /// </summary>
     public class Command : ICommand
     {
         private readonly Func<object, bool> _canExecute;
         private readonly Action<object> _execute;
 
+        /// <inheritdoc/>
         public event EventHandler CanExecuteChanged;
 
+        /// <summary>
+        /// Initialize new instance of <see cref="Command"/> class.
+        /// </summary>
+        /// <param name="canExecute">The can execute function.</param>
+        /// <param name="execute">The execute action.</param>
         public Command(Func<object, bool> canExecute, Action<object> execute)
         {
             _canExecute = canExecute;
             _execute = execute;
         }
 
+        /// <inheritdoc/>
         public bool CanExecute(object parameter)
         {
             return _canExecute?.Invoke(parameter) == true;
         }
 
+        /// <inheritdoc/>
         public void Execute(object parameter)
         {
             _execute?.Invoke(parameter);
