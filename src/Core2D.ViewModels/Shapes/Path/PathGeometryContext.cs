119fc0d59e87e22db9dbd6908c2341711d98e036
wieslawsoltes <wieslaw.soltes@gmail.com>
2018-08-29 11:10:40 +0000

    Add missing using

--- src/Core2D.ViewModels/Shapes/Path/PathGeometryContext.cs
+++ src/Core2D.ViewModels/Shapes/Path/PathGeometryContext.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 using System;
 using System.Collections.Immutable;
+using Core2D.Interfaces;
 using Core2D.Path.Segments;
 using Core2D.Shapes;
 
