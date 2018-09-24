5b07617cf0a931eaf85f8446839d34193dd47f45
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2018-09-24 17:10:53 +0200

    Added IBindable interface

---
+++ src/Core2D.Model/IBindable.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using Core2D.Data;
+
+namespace Core2D
+{
+    /// <summary>
+    /// Defines bindable contract.
+    /// </summary>
+    public interface IBindable
+    {
+        /// <summary>
+        /// Binds data using current <see cref="IDataFlow"/>.
+        /// </summary>
+        /// <param name="dataFlow">The generic data flow object used to bind data.</param>
+        /// <param name="db">The properties database.</param>
+        /// <param name="r">The database record.</param>
+        void Bind(IDataFlow dataFlow, object db, object r);
+    }
+}
