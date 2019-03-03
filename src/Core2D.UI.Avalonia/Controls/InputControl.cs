31fd6fd98c6b5107b20599f665160b75137d420c
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2019-03-03 18:52:27 +0100

    Update InputControl.cs

--- src/Core2D.UI.Avalonia/Controls/InputControl.cs
+++ src/Core2D.UI.Avalonia/Controls/InputControl.cs
@@ -2,8 +2,8 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Controls.PanAndZoom;
 using Avalonia.Input;
+using Avalonia.MatrixExtensions;
 using Avalonia.VisualTree;
 using Core2D.Editor;
 using Core2D.Editor.Input;
