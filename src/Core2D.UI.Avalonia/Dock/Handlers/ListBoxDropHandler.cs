75ebb7c3f5d4c614b59c94a48b5d079f8fbd68fd
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2019-04-07 14:10:02 +0200

    Update ListBoxDropHandler.cs

--- src/Core2D.UI.Avalonia/Dock/Handlers/ListBoxDropHandler.cs
+++ src/Core2D.UI.Avalonia/Dock/Handlers/ListBoxDropHandler.cs
@@ -1,6 +1,11 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using Avalonia.Controls;
 using Avalonia.Input;
+using Avalonia.VisualTree;
+using Core2D.Containers;
+using Core2D.Editor;
+using Core2D.Style;
 using Dock.Avalonia;
 
 namespace Core2D.UI.Avalonia.Dock.Handlers
@@ -12,138 +17,67 @@ namespace Core2D.UI.Avalonia.Dock.Handlers
     {
         public static IDropHandler Instance = new ListBoxDropHandler();
 
-        // FIXME:
-        /*
-        private bool ValidateListBox(IProjectEditor editor, DragEventArgs e, bool bExecute, ListBox list)
+        private bool Validate(object sender, DragEventArgs e, object sourceContext, object targetContext, object state, bool bExecute)
         {
-            var sourceItem = e.Data.Get(DragDataFormats.Parent);
-            var targetItem = (e.Source as IControl)?.Parent;
-
-            if (sourceItem is ListBoxItem source && targetItem is ListBoxItem target)
+            if (e.Source is IControl sourceControl
+                 && sender is ListBox listBox
+                 && sourceContext is IShapeStyle sourceStyle
+                 && targetContext is ILibrary<IShapeStyle> library)
             {
-                if (source.Parent == target.Parent)
+                var point = e.GetPosition(listBox);
+                var visual = listBox.GetVisualAt(point);
+                var root = listBox.GetVisualRoot();
+
+                if (visual is IControl targetControl
+                    && root is Window window
+                    && window.DataContext is IProjectEditor editor
+                    && targetControl.DataContext is IShapeStyle targetStyle)
                 {
-                    int sourceIndex = list.ItemContainerGenerator.IndexFromContainer(source);
-                    int targetIndex = list.ItemContainerGenerator.IndexFromContainer(target);
+                    int sourceIndex = library.Items.IndexOf(sourceStyle);
+                    int targetIndex = library.Items.IndexOf(targetStyle);
 
-                    switch (list.DataContext)
-                    {
-                        case Library<ShapeStyle> library:
-                            {
-                                if (e.DragEffects == DragDropEffects.Copy)
-                                {
-                                    if (bExecute)
-                                    {
-                                        // TODO: Clone item.
-                                    }
-                                    return true;
-                                }
-                                else if (e.DragEffects == DragDropEffects.Move)
-                                {
-                                    if (bExecute)
-                                    {
-                                        editor?.MoveItem(library, sourceIndex, targetIndex);
-                                    }
-                                    return true;
-                                }
-                                else if (e.DragEffects == DragDropEffects.Link)
-                                {
-                                    if (bExecute)
-                                    {
-                                        editor?.SwapItem(library, sourceIndex, targetIndex);
-                                    }
-                                    return true;
-                                }
-
-                                return false;
-                            }
-                        case Library<GroupShape> library:
-                            {
-                                if (e.DragEffects == DragDropEffects.Copy)
-                                {
-                                    if (bExecute)
-                                    {
-                                        // TODO: Clone item.
-                                    }
-                                    return true;
-                                }
-                                else if (e.DragEffects == DragDropEffects.Move)
-                                {
-                                    if (bExecute)
-                                    {
-                                        editor?.MoveItem(library, sourceIndex, targetIndex);
-                                    }
-                                    return true;
-                                }
-                                else if (e.DragEffects == DragDropEffects.Link)
-                                {
-                                    if (bExecute)
-                                    {
-                                        editor?.SwapItem(library, sourceIndex, targetIndex);
-                                    }
-                                    return true;
-                                }
-
-                                return false;
-                            }
-                        default:
-                            return false;
-                    }
-                }
-                else if (source.Parent is ListBox sourceList && target.Parent is ListBox targetList)
-                {
-                    if (sourceList.DataContext?.GetType() == targetList.DataContext?.GetType())
+                    if (e.DragEffects == DragDropEffects.Copy)
                     {
                         if (bExecute)
                         {
-                            // TODO: Exchange items between lists.
+                            var clone = (IShapeStyle)sourceStyle.Copy(null);
+                            clone.Name += "-copy";
+                            editor.InsertItem(library, clone, targetIndex + 1);
                         }
                         return true;
                     }
-
-                    return false;
+                    else if (e.DragEffects == DragDropEffects.Move)
+                    {
+                        if (bExecute)
+                        {
+                            editor.MoveItem(library, sourceIndex, targetIndex);
+                        }
+                        return true;
+                    }
+                    else if (e.DragEffects == DragDropEffects.Link)
+                    {
+                        if (bExecute)
+                        {
+                            editor.SwapItem(library, sourceIndex, targetIndex);
+                        }
+                        return true;
+                    }
                 }
-
-                return false;
             }
 
             return false;
         }
-
-        private bool Validate(IProjectEditor editor, object sender, DragEventArgs e, bool bExecute)
-        {
-            var point = DropHelper.GetPosition(sender, e);
-
-            switch (sender)
-            {
-                case ListBox list:
-                    return ValidateListBox(editor, e, bExecute, list);
-            }
-
-            return false;
-        }
-        */
 
         /// <inheritdoc/>
         public override bool Validate(object sender, DragEventArgs e, object sourceContext, object targetContext, object state)
         {
-            // FIXME:
-            //if (context is IProjectEditor editor)
-            //{
-            //    return Validate(editor, sender, e, false);
-            //}
-            return false;
+            return Validate(sender, e, sourceContext, targetContext, state, false);
         }
 
         /// <inheritdoc/>
         public override bool Execute(object sender, DragEventArgs e, object sourceContext, object targetContext, object state)
         {
-            // FIXME:
-            //if (context is IProjectEditor editor)
-            //{
-            //    return Validate(editor, sender, e, true);
-            //}
-            return false;
+            return Validate(sender, e, sourceContext, targetContext, state, true);
         }
     }
 }
