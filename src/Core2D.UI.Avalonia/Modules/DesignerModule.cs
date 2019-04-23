ecf15f3984d20a1e0c2f4ec89d83ddd6cb85bd2b
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2019-04-23 20:18:58 +0200

    Update DesignerModule.cs

--- src/Core2D.UI.Avalonia/Modules/DesignerModule.cs
+++ src/Core2D.UI.Avalonia/Modules/DesignerModule.cs
@@ -1,4 +1,4 @@
-// Copyright (c) Wiesław Šoltés. All rights reserved.
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 using Autofac;
 using Core2D.Interfaces;
