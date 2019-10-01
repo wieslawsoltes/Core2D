13a1309c10d18b0308d8a9a021d2656f78035486
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2019-10-01 22:49:35 +0200

    Update EditorView.cs

--- src/Core2D.UI.Avalonia/Dock/Views/EditorView.cs
+++ src/Core2D.UI.Avalonia/Dock/Views/EditorView.cs
@@ -1,13 +1,21 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-using DMC=Dock.Model.Controls;
+using DM=Dock.Model;
 
 namespace Core2D.UI.Avalonia.Dock.Views
 {
     /// <summary>
     /// Editor view.
     /// </summary>
-    public class EditorView : DMC.RootDock
+    public class EditorView : DM.DockBase
     {
+        public override DM.IDockable Clone()
+        {
+            var editorView = new EditorView();
+
+            DM.CloneHelper.CloneDockProperties(this, editorView);
+
+            return editorView;
+        }
     }
 }
