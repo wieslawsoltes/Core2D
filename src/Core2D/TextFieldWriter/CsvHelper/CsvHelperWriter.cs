8d2230045a90a325f6e4f15548ea32fb9b08e07a
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-03-18 20:13:33 +0100

    Update CsvHelperWriter.cs

--- src/Core2D/TextFieldWriter/CsvHelper/CsvHelperWriter.cs
+++ src/Core2D/TextFieldWriter/CsvHelper/CsvHelperWriter.cs
@@ -1,9 +1,9 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 using System;
+using System.Globalization;
 using Core2D.Data;
 using Core2D.Interfaces;
-using System.Globalization;
 using CSV = CsvHelper;
 
 namespace Core2D.TextFieldWriter.CsvHelper
