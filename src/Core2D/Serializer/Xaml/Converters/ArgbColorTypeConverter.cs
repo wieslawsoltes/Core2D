f3231a2aae49ef9958c862f938a984a1e3a32529
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-03-18 20:14:38 +0100

    Update ArgbColorTypeConverter.cs

--- src/Core2D/Serializer/Xaml/Converters/ArgbColorTypeConverter.cs
+++ src/Core2D/Serializer/Xaml/Converters/ArgbColorTypeConverter.cs
@@ -1,8 +1,8 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 using System;
-using System.Globalization;
 using System.ComponentModel;
+using System.Globalization;
 using Core2D.Style;
 
 namespace Core2D.Serializer.Xaml.Converters
