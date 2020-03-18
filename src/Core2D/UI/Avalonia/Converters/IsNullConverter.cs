d9397f06cf4f03599a4cd53cb53befed8446088f
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-03-18 20:13:23 +0100

    Update IsNullConverter.cs

--- src/Core2D/UI/Avalonia/Converters/IsNullConverter.cs
+++ src/Core2D/UI/Avalonia/Converters/IsNullConverter.cs
@@ -1,9 +1,9 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-using Avalonia;
-using Avalonia.Data.Converters;
 using System;
 using System.Globalization;
+using Avalonia;
+using Avalonia.Data.Converters;
 
 namespace Core2D.UI.Avalonia.Converters
 {
