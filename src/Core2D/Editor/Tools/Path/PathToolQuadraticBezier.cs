a52f586ff14f3899ccaf959ceee309040e7ac18a
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-03-18 20:19:26 +0100

    Update PathToolQuadraticBezier.cs

--- src/Core2D/Editor/Tools/Path/PathToolQuadraticBezier.cs
+++ src/Core2D/Editor/Tools/Path/PathToolQuadraticBezier.cs
@@ -1,8 +1,8 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 using System;
-using System.Linq;
 using System.Collections.Generic;
+using System.Linq;
 using Core2D.Editor.Input;
 using Core2D.Editor.Tools.Path.Settings;
 using Core2D.Editor.Tools.Selection;
