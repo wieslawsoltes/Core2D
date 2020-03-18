870cb69643d75724a7141d4493bdec852d5863e4
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-03-18 20:08:23 +0100

    Update BaseStyle.cs

--- src/Core2D/ViewModels/Style/BaseStyle.cs
+++ src/Core2D/ViewModels/Style/BaseStyle.cs
@@ -1,9 +1,5 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-using System;
-using System.Diagnostics;
-using System.Globalization;
-using System.Linq;
 
 namespace Core2D.Style
 {
