136cc6481538fd5ef92968bf9f449b94cfb846bf
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-03-18 20:21:36 +0100

    Update BoundsEllipse.cs

--- src/Core2D/Editor/Bounds/Shapes/BoundsEllipse.cs
+++ src/Core2D/Editor/Bounds/Shapes/BoundsEllipse.cs
@@ -14,7 +14,9 @@ namespace Core2D.Editor.Bounds.Shapes
         public IPointShape TryToGetPoint(IBaseShape shape, Point2 target, double radius, IDictionary<Type, IBounds> registered)
         {
             if (!(shape is IEllipseShape ellipse))
+            {
                 throw new ArgumentNullException(nameof(shape));
+            }
 
             var pointHitTest = registered[typeof(IPointShape)];
 
@@ -34,7 +36,9 @@ namespace Core2D.Editor.Bounds.Shapes
         public bool Contains(IBaseShape shape, Point2 target, double radius, IDictionary<Type, IBounds> registered)
         {
             if (!(shape is IEllipseShape ellipse))
+            {
                 throw new ArgumentNullException(nameof(shape));
+            }
 
             return Rect2.FromPoints(
                 ellipse.TopLeft.X,
@@ -46,7 +50,9 @@ namespace Core2D.Editor.Bounds.Shapes
         public bool Overlaps(IBaseShape shape, Rect2 target, double radius, IDictionary<Type, IBounds> registered)
         {
             if (!(shape is IEllipseShape ellipse))
+            {
                 throw new ArgumentNullException(nameof(shape));
+            }
 
             return Rect2.FromPoints(
                 ellipse.TopLeft.X,
