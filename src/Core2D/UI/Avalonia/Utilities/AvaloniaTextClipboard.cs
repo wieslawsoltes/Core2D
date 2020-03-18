9f538553714fdef807e712c3d3f446c9bbe2199e
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-03-18 20:10:34 +0100

    Update AvaloniaTextClipboard.cs

--- src/Core2D/UI/Avalonia/Utilities/AvaloniaTextClipboard.cs
+++ src/Core2D/UI/Avalonia/Utilities/AvaloniaTextClipboard.cs
@@ -1,8 +1,8 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-using Core2D.Interfaces;
 using System.Threading.Tasks;
 using Avalonia;
+using Core2D.Interfaces;
 
 namespace Core2D.UI.Avalonia.Utilities
 {
