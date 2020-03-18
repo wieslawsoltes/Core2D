7aea5832db354e0b41672d0f3146d06f9fe6ef18
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-03-18 20:14:07 +0100

    Update CoreXamlReader.cs

--- src/Core2D/Serializer/Xaml/CoreXamlReader.cs
+++ src/Core2D/Serializer/Xaml/CoreXamlReader.cs
@@ -1,8 +1,8 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-using Portable.Xaml;
 using System.IO;
 using System.Xml;
+using Portable.Xaml;
 
 namespace Core2D.Serializer.Xaml
 {
