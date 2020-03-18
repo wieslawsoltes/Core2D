65715dee438c831ef0090a94d6fa7a562b14fb1d
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-03-18 20:14:00 +0100

    Update XmlnsDefinitionsBase.cs

--- src/Core2D/Serializer/Xaml/XmlnsDefinitionsBase.cs
+++ src/Core2D/Serializer/Xaml/XmlnsDefinitionsBase.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-using Portable.Xaml.Markup;
 using Core2D.Serializer.Xaml;
+using Portable.Xaml.Markup;
 
 [assembly: XmlnsDefinition(XamlConstants.BaseNamespace, "Core2D", AssemblyName = "Core2D.Base")]
 [assembly: XmlnsDefinition(XamlConstants.BaseNamespace, "Core2D.Attributes", AssemblyName = "Core2D.Base")]
