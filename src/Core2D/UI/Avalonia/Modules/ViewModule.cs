d9116698fd0dd75d4b4a7d2dacb65d2d1d36094a
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-03-18 20:10:39 +0100

    Update ViewModule.cs

--- src/Core2D/UI/Avalonia/Modules/ViewModule.cs
+++ src/Core2D/UI/Avalonia/Modules/ViewModule.cs
@@ -1,13 +1,11 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-using System;
 using System.Reflection;
-using Avalonia.Data;
 using Autofac;
 using Core2D.UI.Avalonia.Dock.Factories;
 using Core2D.UI.Avalonia.Windows;
-using DM=Dock.Model;
-using DAC=Dock.Avalonia.Controls;
+using DAC = Dock.Avalonia.Controls;
+using DM = Dock.Model;
 
 namespace Core2D.UI.Avalonia.Modules
 {
