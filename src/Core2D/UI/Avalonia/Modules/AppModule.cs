c69bde2ec1406c037457fe6966d4711be9d49b2d
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-03-18 20:10:45 +0100

    Update AppModule.cs

--- src/Core2D/UI/Avalonia/Modules/AppModule.cs
+++ src/Core2D/UI/Avalonia/Modules/AppModule.cs
@@ -1,10 +1,10 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 using Autofac;
-using Core2D.UI.Avalonia.Editor;
-using Core2D.UI.Avalonia.Importers;
 using Core2D.Editor;
 using Core2D.Interfaces;
+using Core2D.UI.Avalonia.Editor;
+using Core2D.UI.Avalonia.Importers;
 
 namespace Core2D.UI.Avalonia.Modules
 {
