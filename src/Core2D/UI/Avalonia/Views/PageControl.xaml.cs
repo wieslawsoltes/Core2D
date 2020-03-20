90f7457d444ea72f983b48c9aab56c29547ac1e0
wieslawsoltes <wieslaw.soltes@gmail.com>
2020-03-20 09:47:59 +0000

    Try using behavior

--- src/Core2D/UI/Avalonia/Views/PageControl.xaml.cs
+++ src/Core2D/UI/Avalonia/Views/PageControl.xaml.cs
@@ -2,12 +2,135 @@
 using Avalonia.Controls;
 using Avalonia.Controls.PanAndZoom;
 using Avalonia.Markup.Xaml;
+using Avalonia.Xaml.Interactivity;
 using Core2D.Editor;
 using Core2D.Editor.Input;
 using Core2D.UI.Avalonia.Utilities;
 
 namespace Core2D.UI.Avalonia.Views
 {
+    /// <summary>
+    /// Attaches <see cref="IProjectEditor"/> to a <see cref="Control"/>.
+    /// </summary>
+    public sealed class AttachEditorBehavior : Behavior<Control>
+    {
+        private AvaloniaInputSource _inputSource = null;
+        private ProjectEditorInputTarget _inputTarget = null;
+        private InputProcessor _inputProcessor = null;
+        private bool _isLoaded = false;
+
+        /// <inheritdoc/>
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            if (AssociatedObject != null)
+            {
+                AttachEditor();
+            }
+        }
+
+        /// <inheritdoc/>
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            if (AssociatedObject != null)
+            {
+                DetachEditor();
+            }
+        }
+
+        private void InvalidateChild(double zoomX, double zoomY, double offsetX, double offsetY)
+        {
+            var control = AssociatedObject;
+
+            if (control.DataContext is IProjectEditor projectEditor)
+            {
+                var state = projectEditor.Renderers[0]?.State;
+                if (state != null)
+                {
+                    bool invalidateCache = state.ZoomX != zoomX || state.ZoomY != zoomY;
+
+                    state.ZoomX = zoomX;
+                    state.ZoomY = zoomY;
+                    state.PanX = offsetX;
+                    state.PanY = offsetY;
+
+                    if (invalidateCache)
+                    {
+                        projectEditor.OnInvalidateCache(isZooming: true);
+                    }
+                }
+            }
+        }
+
+        private void AttachEditor()
+        {
+            var control = AssociatedObject;
+
+            if (control.DataContext is IProjectEditor projectEditor)
+            {
+                var containerControlData = control.Find<ContainerControl>("containerControlData");
+                var containerControlTemplate = control.Find<ContainerControl>("containerControlTemplate");
+                var containerControlEditor = control.Find<ContainerControl>("containerControlEditor");
+                var zoomBorder = control.Find<ZoomBorder>("zoomBorder");
+
+                if (projectEditor.CanvasPlatform is IEditorCanvasPlatform canvasPlatform)
+                {
+                    canvasPlatform.Invalidate = () =>
+                    {
+                        containerControlData?.InvalidateVisual();
+                        containerControlTemplate?.InvalidateVisual();
+                        containerControlEditor?.InvalidateVisual();
+                    };
+                    canvasPlatform.ResetZoom = () => zoomBorder?.Reset();
+                    canvasPlatform.AutoFitZoom = () => zoomBorder?.AutoFit();
+                    canvasPlatform.Zoom = zoomBorder;
+                }
+
+                if (zoomBorder != null)
+                {
+                    zoomBorder.InvalidatedChild = InvalidateChild;
+                }
+
+                _inputSource = new AvaloniaInputSource(
+                        zoomBorder,
+                        containerControlEditor,
+                        p => p);
+                _inputTarget = new ProjectEditorInputTarget(projectEditor);
+                _inputProcessor = new InputProcessor();
+                _inputProcessor.Connect(_inputSource, _inputTarget);
+            }
+        }
+
+        private void DetachEditor()
+        {
+            var control = AssociatedObject;
+
+            if (control.DataContext is IProjectEditor projectEditor)
+            {
+                if (projectEditor.CanvasPlatform is IEditorCanvasPlatform canvasPlatform)
+                {
+                    canvasPlatform.Invalidate = null;
+                    canvasPlatform.ResetZoom = null;
+                    canvasPlatform.AutoFitZoom = null;
+                    canvasPlatform.Zoom = null;
+                }
+
+                var _zoomBorder = control.Find<ZoomBorder>("zoomBorder");
+
+                if (_zoomBorder != null)
+                {
+                    _zoomBorder.InvalidatedChild = null;
+                }
+
+                _inputProcessor.Dispose();
+                _inputTarget = null;
+                _inputProcessor = null;
+                _inputSource = null;
+            }
+        }
+    }
+
     /// <summary>
     /// Interaction logic for <see cref="PageControl"/> xaml.
     /// </summary>
@@ -24,8 +147,8 @@ namespace Core2D.UI.Avalonia.Views
         public PageControl()
         {
             InitializeComponent();
-            AttachedToVisualTree += PageControl_Loaded;
-            DetachedFromVisualTree += PageControl_Unloaded;
+            //AttachedToVisualTree += PageControl_Loaded;
+            //DetachedFromVisualTree += PageControl_Unloaded;
         }
 
         /// <summary>
