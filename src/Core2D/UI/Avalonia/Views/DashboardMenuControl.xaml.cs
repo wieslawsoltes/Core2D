ae667a447de8091153b23f660a9099589ed749af
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-03-21 14:14:40 +0100

    Removed debug menu

--- src/Core2D/UI/Avalonia/Views/DashboardMenuControl.xaml.cs
+++ src/Core2D/UI/Avalonia/Views/DashboardMenuControl.xaml.cs
@@ -1,6 +1,4 @@
-﻿using Avalonia;
-using Avalonia.Controls;
-using Avalonia.Interactivity;
+﻿using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 
 namespace Core2D.UI.Avalonia.Views
@@ -10,24 +8,6 @@ namespace Core2D.UI.Avalonia.Views
     /// </summary>
     public class DashboardMenuControl : UserControl
     {
-        public static readonly StyledProperty<bool> DrawDirtyRectsProperty =
-            AvaloniaProperty.Register<DashboardMenuControl, bool>(nameof(DrawDirtyRects));
-
-        public bool DrawDirtyRects
-        {
-            get { return GetValue(DrawDirtyRectsProperty); }
-            set { SetValue(DrawDirtyRectsProperty, value); }
-        }
-
-        public static readonly StyledProperty<bool> DrawFpsProperty =
-            AvaloniaProperty.Register<DashboardMenuControl, bool>(nameof(DrawFps));
-
-        public bool DrawFps
-        {
-            get { return GetValue(DrawFpsProperty); }
-            set { SetValue(DrawFpsProperty, value); }
-        }
-
         /// <summary>
         /// Initializes a new instance of the <see cref="DashboardMenuControl"/> class.
         /// </summary>
@@ -42,31 +22,6 @@ namespace Core2D.UI.Avalonia.Views
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
-        }
-
-        protected override void OnAttachedToVisualTree(VisualTreeAttachmentEventArgs e)
-        {
-            base.OnAttachedToVisualTree(e);
-
-            DrawDirtyRects = VisualRoot.Renderer.DrawDirtyRects;
-            DrawFps = VisualRoot.Renderer.DrawFps;
-        }
-
-        protected override void OnDetachedFromVisualTree(VisualTreeAttachmentEventArgs e)
-        {
-            base.OnDetachedFromVisualTree(e);
-        }
-
-        public void DebugDrawDirtyRects_Click(object sender, RoutedEventArgs e)
-        {
-            VisualRoot.Renderer.DrawDirtyRects = !VisualRoot.Renderer.DrawDirtyRects;
-            DrawDirtyRects = VisualRoot.Renderer.DrawDirtyRects;
-        }
-
-        public void DebugDrawFps_Click(object sender, RoutedEventArgs e)
-        {
-            VisualRoot.Renderer.DrawFps = !VisualRoot.Renderer.DrawFps;
-            DrawFps = VisualRoot.Renderer.DrawFps;
         }
     }
 }
