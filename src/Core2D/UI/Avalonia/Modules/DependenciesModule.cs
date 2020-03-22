3def3bd7cdf4920315ff16adb71b7c1dc6f1ac4f
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-03-22 11:39:14 +0100

    Added System.Text.Encoding.CodePages

--- src/Core2D/UI/Avalonia/Modules/DependenciesModule.cs
+++ src/Core2D/UI/Avalonia/Modules/DependenciesModule.cs
@@ -3,6 +3,7 @@ using Core2D.Data;
 using Core2D.FileSystem.DotNet;
 using Core2D.FileWriter.Dxf;
 using Core2D.FileWriter.Emf;
+using Core2D.FileWriter.PdfSharp;
 using Core2D.FileWriter.SkiaSharpJpeg;
 using Core2D.FileWriter.SkiaSharpPdf;
 using Core2D.FileWriter.SkiaSharpPng;
@@ -20,10 +21,6 @@ using Core2D.Serializer.Xaml;
 using Core2D.TextFieldReader.CsvHelper;
 using Core2D.TextFieldWriter.CsvHelper;
 using Core2D.UI.Avalonia.Utilities;
-
-#if NET461
-using Core2D.FileWriter.PdfSharp;
-#endif
 
 namespace Core2D.UI.Avalonia.Modules
 {
@@ -44,17 +41,14 @@ namespace Core2D.UI.Avalonia.Modules
 #endif
             builder.RegisterType<NewtonsoftJsonSerializer>().As<IJsonSerializer>().InstancePerLifetimeScope();
             builder.RegisterType<PortableXamlSerializer>().As<IXamlSerializer>().InstancePerLifetimeScope();
+            builder.RegisterType<PdfSharpWriter>().As<IFileWriter>().InstancePerLifetimeScope();
+            builder.RegisterType<PdfSkiaSharpWriter>().As<IFileWriter>().InstancePerLifetimeScope();
             builder.RegisterType<DxfWriter>().As<IFileWriter>().InstancePerLifetimeScope();
+            builder.RegisterType<SvgSkiaSharpWriter>().As<IFileWriter>().InstancePerLifetimeScope();
+            builder.RegisterType<PngSkiaSharpWriter>().As<IFileWriter>().InstancePerLifetimeScope();
             builder.RegisterType<EmfWriter>().As<IFileWriter>().InstancePerLifetimeScope();
             builder.RegisterType<JpegSkiaSharpWriter>().As<IFileWriter>().InstancePerLifetimeScope();
-            builder.RegisterType<PdfSkiaSharpWriter>().As<IFileWriter>().InstancePerLifetimeScope();
-            builder.RegisterType<PngSkiaSharpWriter>().As<IFileWriter>().InstancePerLifetimeScope();
-            builder.RegisterType<SvgSkiaSharpWriter>().As<IFileWriter>().InstancePerLifetimeScope();
-            //builder.RegisterType<WbmpSkiaSharpWriter>().As<IFileWriter>().InstancePerLifetimeScope();
             builder.RegisterType<WebpSkiaSharpWriter>().As<IFileWriter>().InstancePerLifetimeScope();
-#if NET461
-            builder.RegisterType<PdfSharpWriter>().As<IFileWriter>().InstancePerLifetimeScope();
-#endif
             builder.RegisterType<CsvHelperReader>().As<ITextFieldReader<IDatabase>>().InstancePerLifetimeScope();
             builder.RegisterType<CsvHelperWriter>().As<ITextFieldWriter<IDatabase>>().InstancePerLifetimeScope();
         }
