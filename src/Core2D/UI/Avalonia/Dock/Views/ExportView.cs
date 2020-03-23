aa6194a2b0d676b7bd53ce0a555b1536c0826dd4
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-03-23 22:26:46 +0100

    Create ExportView.cs

---
+++ src/Core2D/UI/Avalonia/Dock/Views/ExportView.cs
@@ -0,0 +1,19 @@
+﻿using DM = Dock.Model;
+
+namespace Core2D.UI.Avalonia.Dock.Views
+{
+    /// <summary>
+    /// Export view.
+    /// </summary>
+    public class ExportView : DM.DockBase
+    {
+        public override DM.IDockable Clone()
+        {
+            var exportView = new ExportView();
+
+            DM.CloneHelper.CloneDockProperties(this, exportView);
+
+            return exportView;
+        }
+    }
+}
