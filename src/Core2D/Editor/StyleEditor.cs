44c7f215a6f1ebe71cd58b9e9f44064e53a0b89d
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-03-23 18:38:30 +0100

    Update StyleEditor.cs

--- src/Core2D/Editor/StyleEditor.cs
+++ src/Core2D/Editor/StyleEditor.cs
@@ -941,7 +941,34 @@ namespace Core2D.Editor
         /// <inheritdoc/>
         public void OnStyleSetStartArrowStrokeTransparency(string alpha)
         {
-            // TODO:
+            if (!byte.TryParse(alpha, _numberStyles, CultureInfo.InvariantCulture, out var value))
+            {
+                return;
+            }
+
+            var editor = _serviceProvider.GetService<IProjectEditor>();
+
+            if (editor.Renderers[0]?.State?.SelectedShape != null)
+            {
+                var shape = editor.Renderers[0]?.State?.SelectedShape;
+                var style = shape.Style;
+                if (style != null && style.StartArrowStyle != null && style.StartArrowStyle.Stroke is IArgbColor argbColor)
+                {
+                    argbColor.A = value;
+                }
+            }
+
+            if (editor.Renderers?[0]?.State?.SelectedShapes?.Count > 0)
+            {
+                foreach (var shape in editor.Renderers[0].State.SelectedShapes)
+                {
+                    var style = shape.Style;
+                    if (style != null && style.StartArrowStyle != null && style.StartArrowStyle.Stroke is IArgbColor argbColor)
+                    {
+                        argbColor.A = value;
+                    }
+                }
+            }
         }
 
         /// <inheritdoc/>
