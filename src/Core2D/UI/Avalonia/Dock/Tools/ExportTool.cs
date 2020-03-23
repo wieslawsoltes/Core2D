9e7de7cf8a9bccbb4549cf1197eb88e52d64b87c
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-03-23 22:26:42 +0100

    Create ExportTool.cs

---
+++ src/Core2D/UI/Avalonia/Dock/Tools/ExportTool.cs
@@ -0,0 +1,11 @@
+﻿using DMC = Dock.Model.Controls;
+
+namespace Core2D.UI.Avalonia.Dock.Tools
+{
+    /// <summary>
+    /// Export view.
+    /// </summary>
+    public class ExportTool : DMC.Tool
+    {
+    }
+}
