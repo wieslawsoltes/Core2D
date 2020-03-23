49757d8f567da2fec60b66915331c817f2f8c627
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-03-23 18:39:32 +0100

    Update StyleEditor.cs

--- src/Core2D/Editor/StyleEditor.cs
+++ src/Core2D/Editor/StyleEditor.cs
@@ -1365,7 +1365,39 @@ namespace Core2D.Editor
         /// <inheritdoc/>
         public void OnStyleSetEndArrowFill(string color)
         {
-            // TODO:
+            IColor value;
+            try
+            {
+                value = ArgbColor.Parse(color);
+            }
+            catch
+            {
+                return;
+            }
+
+            var editor = _serviceProvider.GetService<IProjectEditor>();
+
+            if (editor.Renderers[0]?.State?.SelectedShape != null)
+            {
+                var shape = editor.Renderers[0]?.State?.SelectedShape;
+                var style = shape.Style;
+                if (style != null && style.EndArrowStyle != null)
+                {
+                    style.EndArrowStyle.Fill = value;
+                }
+            }
+
+            if (editor.Renderers?[0]?.State?.SelectedShapes?.Count > 0)
+            {
+                foreach (var shape in editor.Renderers[0].State.SelectedShapes)
+                {
+                    var style = shape.Style;
+                    if (style != null && style.EndArrowStyle != null)
+                    {
+                        style.EndArrowStyle.Fill = value;
+                    }
+                }
+            }
         }
 
         /// <inheritdoc/>
