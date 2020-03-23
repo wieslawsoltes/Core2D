3fbb5ae161c03d47edda852e48167120ffdd2f9d
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-03-23 18:15:17 +0100

    Update StyleEditor.cs

--- src/Core2D/Editor/StyleEditor.cs
+++ src/Core2D/Editor/StyleEditor.cs
@@ -654,7 +654,34 @@ namespace Core2D.Editor
         /// <inheritdoc/>
         public void OnStyleSetStartArrowType(string type)
         {
-            // TODO:
+            if (!Enum.TryParse<ArrowType>(type, true, out var value))
+            {
+                return;
+            }
+
+            var editor = _serviceProvider.GetService<IProjectEditor>();
+
+            if (editor.Renderers[0]?.State?.SelectedShape != null)
+            {
+                var shape = editor.Renderers[0]?.State?.SelectedShape;
+                var style = shape.Style;
+                if (style != null && style.StartArrowStyle != null)
+                {
+                    style.StartArrowStyle.ArrowType = value;
+                }
+            }
+
+            if (editor.Renderers?[0]?.State?.SelectedShapes?.Count > 0)
+            {
+                foreach (var shape in editor.Renderers[0].State.SelectedShapes)
+                {
+                    var style = shape.Style;
+                    if (style != null && style.StartArrowStyle != null)
+                    {
+                        style.StartArrowStyle.ArrowType = value;
+                    }
+                }
+            }
         }
 
         /// <inheritdoc/>
