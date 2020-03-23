ebbb95929763934ed6a8945de9253c7f625ab20f
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-03-23 18:36:31 +0100

    Update StyleEditor.cs

--- src/Core2D/Editor/StyleEditor.cs
+++ src/Core2D/Editor/StyleEditor.cs
@@ -715,7 +715,29 @@ namespace Core2D.Editor
         /// <inheritdoc/>
         public void OnStyleToggleStartArrowIsFilled()
         {
-            // TODO:
+            var editor = _serviceProvider.GetService<IProjectEditor>();
+
+            if (editor.Renderers[0]?.State?.SelectedShape != null)
+            {
+                var shape = editor.Renderers[0]?.State?.SelectedShape;
+                var style = shape.Style;
+                if (style != null && style.StartArrowStyle != null)
+                {
+                    style.StartArrowStyle.IsFilled = !style.StartArrowStyle.IsFilled;
+                }
+            }
+
+            if (editor.Renderers?[0]?.State?.SelectedShapes?.Count > 0)
+            {
+                foreach (var shape in editor.Renderers[0].State.SelectedShapes)
+                {
+                    var style = shape.Style;
+                    if (style != null && style.StartArrowStyle != null)
+                    {
+                        style.StartArrowStyle.IsFilled = !style.StartArrowStyle.IsFilled;
+                    }
+                }
+            }
         }
 
         /// <inheritdoc/>
