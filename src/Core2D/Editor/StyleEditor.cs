d9a55bbaeef65f5de5e5cbc1b19cd0c13a47b1b4
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-03-23 18:14:23 +0100

    Update StyleEditor.cs

--- src/Core2D/Editor/StyleEditor.cs
+++ src/Core2D/Editor/StyleEditor.cs
@@ -296,7 +296,34 @@ namespace Core2D.Editor
         /// <inheritdoc/>
         public void OnStyleSetFontSize(string fontSize)
         {
-            // TODO:
+            if (!double.TryParse(fontSize, _numberStyles, CultureInfo.InvariantCulture, out var value))
+            {
+                return;
+            }
+
+            var editor = _serviceProvider.GetService<IProjectEditor>();
+
+            if (editor.Renderers[0]?.State?.SelectedShape != null)
+            {
+                var shape = editor.Renderers[0]?.State?.SelectedShape;
+                var style = shape.Style;
+                if (style != null && style.TextStyle != null)
+                {
+                    style.TextStyle.FontSize = value;
+                }
+            }
+
+            if (editor.Renderers?[0]?.State?.SelectedShapes?.Count > 0)
+            {
+                foreach (var shape in editor.Renderers[0].State.SelectedShapes)
+                {
+                    var style = shape.Style;
+                    if (style != null && style.TextStyle != null)
+                    {
+                        style.TextStyle.FontSize = value;
+                    }
+                }
+            }
         }
 
         /// <inheritdoc/>
