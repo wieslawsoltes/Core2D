ff3e04a75ea12edcfeffbcd1b8d964017cc30f37
wieslawsoltes <wieslaw.soltes@gmail.com>
2020-03-27 11:01:33 +0000

    Moved menu item

---
+++ src/Core2D/UI/Avalonia/Views/Menus/FormatMenuItem.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Core2D.UI.Avalonia.Views.Menus
+{
+    /// <summary>
+    /// Interaction logic for <see cref="FormatMenuItem"/> xaml.
+    /// </summary>
+    public class FormatMenuItem : MenuItem
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FormatMenuItem"/> class.
+        /// </summary>
+        public FormatMenuItem()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Initialize the Xaml components.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
