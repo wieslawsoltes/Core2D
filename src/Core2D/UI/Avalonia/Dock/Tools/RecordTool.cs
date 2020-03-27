13c3c8c98d9d9a13a4ce58ac6d796358857a7df8
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-03-27 20:17:15 +0100

    Create RecordTool.cs

---
+++ src/Core2D/UI/Avalonia/Dock/Tools/RecordTool.cs
@@ -0,0 +1,11 @@
+﻿using DMC = Dock.Model.Controls;
+
+namespace Core2D.UI.Avalonia.Dock.Tools
+{
+    /// <summary>
+    /// Record view.
+    /// </summary>
+    public class RecordTool : DMC.Tool
+    {
+    }
+}
