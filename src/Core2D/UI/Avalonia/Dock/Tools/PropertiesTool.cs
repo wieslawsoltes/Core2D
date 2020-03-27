54b76023834599b074438593b114fa934225b008
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-03-27 20:17:11 +0100

    Create PropertiesTool.cs

---
+++ src/Core2D/UI/Avalonia/Dock/Tools/PropertiesTool.cs
@@ -0,0 +1,11 @@
+﻿using DMC = Dock.Model.Controls;
+
+namespace Core2D.UI.Avalonia.Dock.Tools
+{
+    /// <summary>
+    /// Properties view.
+    /// </summary>
+    public class PropertiesTool : DMC.Tool
+    {
+    }
+}
