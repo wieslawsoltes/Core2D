d833527f9ef660cad0131943f3820f8818d59d98
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-03-28 16:38:31 +0100

    Create BindingParser.cs

---
+++ src/Core2D/Model/Bindings/BindingParser.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace Core2D.Bindings
+{
+    internal static class BindingParser
+    {
+        public static char s_startChar = '{';
+        public static char s_endChar = '}';
+
+        public static List<Binding> Parse(string text)
+        {
+            var bindings = new List<Binding>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                var start = text.IndexOf(s_startChar, i);
+                if (start >= 0)
+                {
+                    var end = text.IndexOf(s_endChar, start);
+                    if (end >= start)
+                    {
+                        var length = end - start + 1;
+                        var path = text.Substring(start + 1, length - 2);
+                        var value = text.Substring(start, length);
+                        var binding = new Binding(start, end, length, path, value);
+                        bindings.Add(binding);
+                        i = end;
+                    }
+                }
+            }
+
+            return bindings;
+        }
+    }
+}
