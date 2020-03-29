c6e4680bd2734eab828334a2b849b6db20b2da01
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-03-29 22:49:02 +0200

    Update PathGeometryConverter.cs

--- src/Core2D/Renderer/SkiaSharp/PathGeometryConverter.cs
+++ src/Core2D/Renderer/SkiaSharp/PathGeometryConverter.cs
@@ -193,7 +193,7 @@ namespace Core2D.Renderer.SkiaSharp
                 return path;
             }
 
-            SkiaSharpRenderer.GetSKPaint(tbind, text.Style, text.TopLeft, text.BottomRight, dx, dy, scale, 96.0, 72.0, true, out var pen, out var origin);
+            SkiaSharpRenderer.GetSKPaint(tbind, text.Style, text.TopLeft, text.BottomRight, dx, dy, scale, 96.0, 96.0, true, out var pen, out var origin);
 
             using var outlinePath = pen.GetTextPath(tbind, origin.X, origin.Y);
             using var fillPath = pen.GetFillPath(outlinePath);
