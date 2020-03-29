b1922a423153c83aa28fa9572ac6d247596e92fb
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-03-29 21:23:50 +0200

    Create IPathConverter.cs

---
+++ src/Core2D/Model/Interfaces/IPathConverter.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using Core2D.Renderer;
+using Core2D.Shapes;
+
+namespace Core2D.Interfaces
+{
+    /// <summary>
+    /// Defines path converter contract.
+    /// </summary>
+    public interface IPathConverter
+    {
+        /// <summary>
+        /// Convert shape to path shape.
+        /// </summary>
+        /// <param name="shape">The shape to convert.</param>
+        /// <returns>The new instance of object of type <see cref="IPathShape"/>.</returns>
+        IPathShape ToPathShape(IBaseShape shape);
+
+        /// <summary>
+        /// Convert shape to stroke path shape.
+        /// </summary>
+        /// <param name="shape">The shape to convert.</param>
+        /// <returns>The new instance of object of type <see cref="IPathShape"/>.</returns>
+        IPathShape ToStrokePathShape(IBaseShape shape);
+
+        /// <summary>
+        /// Convert shape to fill path shape.
+        /// </summary>
+        /// <param name="shape">The shape to convert.</param>
+        /// <returns>The new instance of object of type <see cref="IPathShape"/>.</returns>
+        IPathShape ToFillPathShape(IBaseShape shape);
+
+        /// <summary>
+        /// Convert shapes to path shape.
+        /// </summary>
+        /// <param name="shapes">The shapes to convert.</param>
+        /// <param name="op">The convert operation.</param>
+        /// <returns>The new instance of object of type <see cref="IPathShape"/>.</returns>
+        IPathShape Op(IEnumerable<IBaseShape> shapes, PathOp op);
+    }
+}
