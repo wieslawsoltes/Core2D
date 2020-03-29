e9a768b1f596a0268b3e4001940a4d6bad858938
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-03-29 21:23:55 +0200

    Create PathOp.cs

---
+++ src/Core2D/Model/Renderer/PathOp.cs
@@ -0,0 +1,34 @@
+﻿
+namespace Core2D.Renderer
+{
+    /// <summary>
+    /// Specifies path op.
+    /// </summary>
+    public enum PathOp
+    {
+        /// <summary>
+        /// Difference.
+        /// </summary>
+        Difference = 0,
+
+        /// <summary>
+        /// Intersect.
+        /// </summary>
+        Intersect = 1,
+
+        /// <summary>
+        /// Union.
+        /// </summary>
+        Union = 2,
+
+        /// <summary>
+        /// Xor.
+        /// </summary>
+        Xor = 3,
+
+        /// <summary>
+        /// Reverse difference,
+        /// </summary>
+        ReverseDifference = 4
+    }
+}
