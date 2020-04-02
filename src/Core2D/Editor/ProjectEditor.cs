52912f8e959eb0ced4e3898f6cdbdcc681494d01
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-04-02 20:57:23 +0200

    Update ProjectEditor.cs

--- src/Core2D/Editor/ProjectEditor.cs
+++ src/Core2D/Editor/ProjectEditor.cs
@@ -311,7 +311,15 @@ namespace Core2D.Editor
         /// <inheritdoc/>
         public void OnNew(object item)
         {
-            if (item is IPageContainer page)
+            
+            if (item is ILayerContainer layer)
+            {
+                if (layer.Owner is IPageContainer page)
+                {
+                    OnAddLayer(page); 
+                }
+            }
+            else if (item is IPageContainer page)
             {
                 OnNewPage(page);
             }
