23be4730a2c64c713c75324d76df2631b0ae26ef
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-04-03 20:48:47 +0200

    Moved definitions

--- src/Core2D/Serializer/Xaml/XmlnsDefinitionsBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using Core2D.Serializer.Xaml;
-using Portable.Xaml.Markup;
-
-[assembly: XmlnsDefinition(XamlConstants.BaseNamespace, "Core2D", AssemblyName = "Core2D")]
-[assembly: XmlnsDefinition(XamlConstants.BaseNamespace, "Core2D.Attributes", AssemblyName = "Core2D")]
-
-[assembly: XmlnsPrefix(XamlConstants.ModelNamespace, "b")]
--- src/Core2D/Serializer/Xaml/XmlnsDefinitionsDock.cs
+++
@@ -1,6 +0,0 @@
-﻿using Core2D.Serializer.Xaml;
-using Portable.Xaml.Markup;
-
-[assembly: XmlnsDefinition(XamlConstants.DockNamespace, "Dock.Model", AssemblyName = "Dock.Model")]
-
-[assembly: XmlnsPrefix(XamlConstants.DockNamespace, "c")]
