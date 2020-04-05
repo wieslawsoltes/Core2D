eee879baa7fd274a0f0f8d80ec1493d90e011a41
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-04-05 16:08:18 +0200

    Create ISvgExporter.cs

---
+++ src/Core2D/Model/Interfaces/ISvgExporter.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Core2D.Interfaces
+{
+    /// <summary>
+    /// Defines svg exporter contract.
+    /// </summary>
+    public interface ISvgExporter
+    {
+        /// <summary>
+        /// Creates the svg string.
+        /// </summary>
+        /// <param name="item">The object to export.</param>
+        /// <param name="width">The svg width.</param>
+        /// <param name="height">The svg height.</param>
+        /// <returns>The new instance of the svg string.</returns>
+        string Create(object item, double width, double height);
+    }
+}
