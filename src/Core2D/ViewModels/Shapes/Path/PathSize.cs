57f258282f0758977709b02667cfdad4b50d5370
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-04-05 16:07:30 +0200

    Update PathSize.cs

--- src/Core2D/ViewModels/Shapes/Path/PathSize.cs
+++ src/Core2D/ViewModels/Shapes/Path/PathSize.cs
@@ -32,11 +32,13 @@ namespace Core2D.Path
             throw new NotImplementedException();
         }
 
-        /// <summary>
-        /// Returns a string that represents the current object.
-        /// </summary>
-        /// <returns>A string that represents the current object.</returns>
-        public override string ToString() => $"{Width.ToString(CultureInfo.InvariantCulture)},{Height.ToString(CultureInfo.InvariantCulture)}";
+        /// <inheritdoc/>
+        public string ToXamlString()
+            => $"{Width.ToString(CultureInfo.InvariantCulture)},{Height.ToString(CultureInfo.InvariantCulture)}";
+
+        /// <inheritdoc/>
+        public string ToSvgString()
+            => $"{Width.ToString(CultureInfo.InvariantCulture)},{Height.ToString(CultureInfo.InvariantCulture)}";
 
         /// <summary>
         /// Check whether the <see cref="Width"/> property has changed from its default value.
