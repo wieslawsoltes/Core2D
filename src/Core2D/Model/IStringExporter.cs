a86e681b4189b846231e25f53837444759cb08dd
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-04-05 16:06:35 +0200

    Create IStringExporter.cs

---
+++ src/Core2D/Model/IStringExporter.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Core2D
+{
+    public interface IStringExporter
+    {
+        /// <summary>
+        /// Creates xaml path data string.
+        /// </summary>
+        /// <returns>The xaml path data string.</returns>
+        string ToXamlString();
+
+        /// <summary>
+        /// Creates svg path data string.
+        /// </summary>
+        /// <returns>The svg path data string.</returns>
+        string ToSvgString();
+    }
+}
