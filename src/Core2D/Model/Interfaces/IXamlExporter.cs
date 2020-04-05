3129c9f75f3caaaca92eac2a10bd4b1b8d17766a
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-04-05 14:22:16 +0200

    Create IXamlExporter.cs

---
+++ src/Core2D/Model/Interfaces/IXamlExporter.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Core2D.Interfaces
+{
+    /// <summary>
+    /// Defines xaml exporter contract.
+    /// </summary>
+    public interface IXamlExporter
+    {
+        /// <summary>
+        /// Creates the xaml string.
+        /// </summary>
+        /// <param name="item">The object to export.</param>
+        /// <param name="key">The resouce key.</param>
+        /// <returns>The new instance of the xaml string.</returns>
+        string Create(object item, string key);
+    }
+}
