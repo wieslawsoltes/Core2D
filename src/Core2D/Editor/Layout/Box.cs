1a1e72328cfd008c429de84a5ee110cbd56627d5
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-04-15 19:56:53 +0200

    Create Box.cs

---
+++ src/Core2D/Editor/Layout/Box.cs
@@ -0,0 +1,14 @@
+﻿namespace Core2D.Editor.Layout
+{
+    public struct Box
+    {
+        public double Left;
+        public double Top;
+        public double Right;
+        public double Bottom;
+        public double CenterX;
+        public double CenterY;
+        public double Width;
+        public double Height;
+    }
+}
