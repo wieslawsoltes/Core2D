8890f36e46066acbb3849867627e5715eca5ca28
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-04-15 22:42:31 +0200

    Create IDecorator.cs

---
+++ src/Core2D/Editor/Interfaces/IDecorator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Core2D.Shapes;
+
+namespace Core2D.Editor
+{
+    /// <summary>
+    /// Defines decorator contract.
+    /// </summary>
+    public interface IDecorator : IObservableObject, IDrawable
+    {
+        /// <summary>
+        ///  Gets or sets decorated shapes.
+        /// </summary>
+        IList<IBaseShape> Shapes { get; set; }
+
+        /// <summary>
+        /// Update decorator.
+        /// </summary>
+        /// <param name="rebuild">The flag indicating whether to rebuild box.</param>
+        void Update(bool rebuild);
+
+        /// <summary>
+        /// Show decorator.
+        /// </summary>
+        void Show();
+
+        /// <summary>
+        /// Hide decorator.
+        /// </summary>
+        void Hide();
+    }
+}
