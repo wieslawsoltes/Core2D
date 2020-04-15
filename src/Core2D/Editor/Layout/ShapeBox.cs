0185d978cd2f94b9e9a1a1ea1b49ae2b6e93dcc1
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-04-15 19:56:59 +0200

    Update ShapeBox.cs

--- src/Core2D/Editor/Layout/ShapeBox.cs
+++ src/Core2D/Editor/Layout/ShapeBox.cs
@@ -1,87 +1,110 @@
 ﻿using System;
 using System.Collections.Generic;
+using Core2D.History;
 using Core2D.Shapes;
 
 namespace Core2D.Editor.Layout
 {
-    public readonly struct ShapeBox
+    public struct ShapeBox
     {
         public readonly IBaseShape Shape;
-        public readonly IEnumerable<IPointShape> Points;
-        public readonly double Left;
-        public readonly double Top;
-        public readonly double Right;
-        public readonly double Bottom;
-        public readonly double CenterX;
-        public readonly double CenterY;
-        public readonly double Width;
-        public readonly double Height;
+        public readonly List<IPointShape> Points;
+        public Box Bounds;
 
         public static int CompareLeft(ShapeBox box1, ShapeBox box2)
         {
-            return (box1.Left > box2.Left) ? 1 : ((box1.Left < box2.Left) ? -1 : 0);
+            return (box1.Bounds.Left > box2.Bounds.Left) ? 1 : ((box1.Bounds.Left < box2.Bounds.Left) ? -1 : 0);
         }
 
         public static int CompareRight(ShapeBox box1, ShapeBox box2)
         {
-            return (box1.Right > box2.Right) ? 1 : ((box1.Right < box2.Right) ? -1 : 0);
+            return (box1.Bounds.Right > box2.Bounds.Right) ? 1 : ((box1.Bounds.Right < box2.Bounds.Right) ? -1 : 0);
         }
 
         public static int CompareTop(ShapeBox box1, ShapeBox box2)
         {
-            return (box1.Top > box2.Top) ? 1 : ((box1.Top < box2.Top) ? -1 : 0);
+            return (box1.Bounds.Top > box2.Bounds.Top) ? 1 : ((box1.Bounds.Top < box2.Bounds.Top) ? -1 : 0);
         }
 
         public static int CompareBottom(ShapeBox box1, ShapeBox box2)
         {
-            return (box1.Bottom > box2.Bottom) ? 1 : ((box1.Bottom < box2.Bottom) ? -1 : 0);
+            return (box1.Bounds.Bottom > box2.Bounds.Bottom) ? 1 : ((box1.Bounds.Bottom < box2.Bounds.Bottom) ? -1 : 0);
         }
 
         public static int CompareCenterX(ShapeBox box1, ShapeBox box2)
         {
-            return (box1.CenterX > box2.CenterX) ? 1 : ((box1.CenterX < box2.CenterX) ? -1 : 0);
+            return (box1.Bounds.CenterX > box2.Bounds.CenterX) ? 1 : ((box1.Bounds.CenterX < box2.Bounds.CenterX) ? -1 : 0);
         }
 
         public static int CompareCenterY(ShapeBox box1, ShapeBox box2)
         {
-            return (box1.CenterY > box2.CenterY) ? 1 : ((box1.CenterY < box2.CenterY) ? -1 : 0);
+            return (box1.Bounds.CenterY > box2.Bounds.CenterY) ? 1 : ((box1.Bounds.CenterY < box2.Bounds.CenterY) ? -1 : 0);
         }
 
         public static int CompareWidth(ShapeBox box1, ShapeBox box2)
         {
-            return (box1.Width > box2.Width) ? 1 : ((box1.Width < box2.Width) ? -1 : 0);
+            return (box1.Bounds.Width > box2.Bounds.Width) ? 1 : ((box1.Bounds.Width < box2.Bounds.Width) ? -1 : 0);
         }
 
         public static int CompareHeight(ShapeBox box1, ShapeBox box2)
         {
-            return (box1.Height > box2.Height) ? 1 : ((box1.Height < box2.Height) ? -1 : 0);
+            return (box1.Bounds.Height > box2.Bounds.Height) ? 1 : ((box1.Bounds.Height < box2.Bounds.Height) ? -1 : 0);
         }
 
-        public static void GetBox(IEnumerable<IPointShape> points, out double left, out double top, out double right, out double bottom)
+        public static void Rotate(IPointShape point, double radians, double centerX, double centerY, out double x, out double y)
         {
-            left = double.MaxValue;
-            top = double.MaxValue;
-            right = double.MinValue;
-            bottom = double.MinValue;
-
-            foreach (var point in points)
-            {
-                left = Math.Min(left, point.X);
-                top = Math.Min(top, point.Y);
-                right = Math.Max(right, point.X);
-                bottom = Math.Max(bottom, point.Y);
-            }
+            x = (point.X - centerX) * Math.Cos(radians) - (point.Y - centerY) * Math.Sin(radians) + centerX;
+            y = (point.X - centerX) * Math.Sin(radians) + (point.Y - centerY) * Math.Cos(radians) + centerY;
         }
 
         public ShapeBox(IBaseShape shape)
         {
             Shape = shape;
-            Points = Shape.GetPoints();
-            GetBox(Points, out Left, out Top, out Right, out Bottom);
-            CenterX = (Left + Right) / 2.0;
-            CenterY = (Top + Bottom) / 2.0;
-            Width = Math.Abs(Right - Left);
-            Height = Math.Abs(Bottom - Top);
+
+            Points = new List<IPointShape>();
+
+            foreach (var point in Shape.GetPoints())
+            {
+                Points.Add(point);
+            }
+
+            Bounds = new Box();
+
+            Update(ref Bounds);
+        }
+
+        public void Update(ref Box bounds)
+        {
+            var left = double.MaxValue;
+            var top = double.MaxValue;
+            var right = double.MinValue;
+            var bottom = double.MinValue;
+
+            for (int i = 0; i < Points.Count; i++)
+            {
+                var point = Points[i];
+                left = Math.Min(left, point.X);
+                top = Math.Min(top, point.Y);
+                right = Math.Max(right, point.X);
+                bottom = Math.Max(bottom, point.Y);
+            }
+
+            bounds.Left = left;
+            bounds.Top = top;
+            bounds.Right = right;
+            bounds.Bottom = bottom;
+            bounds.CenterX = (bounds.Left + bounds.Right) / 2.0;
+            bounds.CenterY = (bounds.Top + bounds.Bottom) / 2.0;
+            bounds.Width = Math.Abs(bounds.Right - bounds.Left);
+            bounds.Height = Math.Abs(bounds.Bottom - bounds.Top);
+        }
+
+        public void MoveByWithHistory(double dx, double dy, IHistory history)
+        {
+            var previous = new { DeltaX = -dx, DeltaY = -dy, Shape };
+            var next = new { DeltaX = dx, DeltaY = dy, Shape };
+            history.Snapshot(previous, next, (s) => s.Shape.Move(null, s.DeltaX, s.DeltaY));
+            Shape.Move(null, dx, dy);
         }
     }
 }
