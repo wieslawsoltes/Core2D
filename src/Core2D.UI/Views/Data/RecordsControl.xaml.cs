b09c4b0b51f11695c349d6537318af5a3fe7b584
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-04-28 22:10:12 +0200

    Update RecordsControl.xaml.cs

--- src/Core2D.UI/Views/Data/RecordsControl.xaml.cs
+++ src/Core2D.UI/Views/Data/RecordsControl.xaml.cs
@@ -1,5 +1,11 @@
-﻿using Avalonia.Controls;
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Data;
 using Avalonia.Markup.Xaml;
+using Core2D.Data;
+using DocumentFormat.OpenXml.InkML;
+using System.ComponentModel;
+using System.Reactive;
 
 namespace Core2D.UI.Views.Data
 {
@@ -8,12 +14,19 @@ namespace Core2D.UI.Views.Data
     /// </summary>
     public class RecordsControl : UserControl
     {
+        private DataGrid _rowsDataGrid;
+        private IDatabase _database;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="RecordsControl"/> class.
         /// </summary>
         public RecordsControl()
         {
             InitializeComponent();
+
+            _rowsDataGrid = this.FindControl<DataGrid>("rowsDataGrid");
+
+            DataContextChanged += RecordsControl_DataContextChanged;
         }
 
         /// <summary>
@@ -22,6 +35,77 @@ namespace Core2D.UI.Views.Data
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+        }
+
+        private void RecordsControl_DataContextChanged(object sender, System.EventArgs e)
+        {
+            if (_database != null)
+            {
+                _database.PropertyChanged -= Database_PropertyChanged;
+                _database = null;
+            }
+
+            ResetColumns();
+
+            if (DataContext is IDatabase database)
+            {
+                _database = database;
+                _database.PropertyChanged += Database_PropertyChanged;
+                CreateColumns();
+            }
+        }
+
+        private void Database_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "Columns")
+            {
+                if (_database != null)
+                {
+                    ResetColumns();
+                    CreateColumns();
+                }
+            }
+        }
+
+        private void Column_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "Name")
+            {
+                if (_database != null)
+                {
+                    UpdateHeaders();
+                }
+            }
+        }
+
+        private void CreateColumns()
+        {
+            for (int i = 0; i < _database.Columns.Length; i++)
+            {
+                var column = _database.Columns[i];
+                var dataGridTextColumn = new DataGridTextColumn()
+                {
+                    Header = $"{column.Name}",
+                    Binding = new Binding($"Values[{i}].Content"),
+                    IsReadOnly = true
+                };
+                column.PropertyChanged += Column_PropertyChanged;
+                _rowsDataGrid.Columns.Add(dataGridTextColumn);
+            }
+        }
+
+        private void ResetColumns()
+        {
+            _rowsDataGrid.Columns.Clear();
+        }
+
+        private void UpdateHeaders()
+        {
+            for (int i = 0; i < _database.Columns.Length; i++)
+            {
+                var column = _database.Columns[i];
+                _rowsDataGrid.Columns[i].Header = column.Name;
+            }
         }
     }
 }
