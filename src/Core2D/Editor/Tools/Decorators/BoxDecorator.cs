eab2f4207b86fd828d3bb25ae20e22d533eb3f0f
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-05-30 18:14:44 +0200

    Update BoxDecorator.cs

--- src/Core2D/Editor/Tools/Decorators/BoxDecorator.cs
+++ src/Core2D/Editor/Tools/Decorators/BoxDecorator.cs
@@ -612,8 +612,25 @@ namespace Core2D.Editor.Tools.Decorators
                     break;
                 case Mode.TopLeft:
                     {
-                        _groupBox.ScaleTop(dy, _points);
-                        _groupBox.ScaleLeft(dx, _points);
+                        if (isProportionalResize)
+                        {
+                            var width = _groupBox.Bounds.Width;
+                            var height = _groupBox.Bounds.Height;
+
+                            var ratioWidth = (width + dx) / width;
+                            dy = (height * ratioWidth) - height;
+
+                            //var ratioHeight = (height + dy) / height;
+                            //dx = (width * ratioHeight) - width;
+
+                            _groupBox.ScaleTop(dy, _points);
+                            _groupBox.ScaleLeft(dx, _points);
+                        }
+                        else
+                        {
+                            _groupBox.ScaleTop(dy, _points);
+                            _groupBox.ScaleLeft(dx, _points);
+                        }
                     }
                     break;
                 case Mode.TopRight:
