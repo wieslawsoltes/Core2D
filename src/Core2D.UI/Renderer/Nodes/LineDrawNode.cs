d7750f30b97131b4ee7f30741bc043ffcd9edd4a
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-06-01 09:42:13 +0200

    Update LineDrawNode.cs

--- src/Core2D.UI/Renderer/Nodes/LineDrawNode.cs
+++ src/Core2D.UI/Renderer/Nodes/LineDrawNode.cs
@@ -1,14 +1,18 @@
-﻿using Core2D.Renderer;
+﻿using System;
+using Core2D.Renderer;
 using Core2D.Shapes;
 using Core2D.Style;
+using Spatial;
 using A = Avalonia;
 using AM = Avalonia.Media;
+using AME = Avalonia.MatrixExtensions;
 
 namespace Core2D.UI.Renderer
 {
     internal class LineDrawNode : DrawNode
     {
         public ILineShape Line { get; set; }
+
         public A.Point P0 { get; set; }
         public A.Point P1 { get; set; }
 
@@ -19,150 +23,135 @@ namespace Core2D.UI.Renderer
             UpdateGeometry();
         }
 
-        /*
-        private static void DrawLineInternal(AM.DrawingContext dc, AM.IPen pen, bool isStroked, ref A.Point p0, ref A.Point p1)
+        private void DrawLineInternal(AM.DrawingContext context, AM.IPen pen, bool isStroked, ref A.Point p0, ref A.Point p1)
         {
-            if (!isStroked)
+            if (isStroked)
             {
-                return;
+                context.DrawLine(pen, p0, p1);
             }
-            dc.DrawLine(pen, p0, p1);
         }
 
-        private static void DrawLineCurveInternal(AM.DrawingContext _dc, AM.IPen pen, bool isStroked, ref A.Point pt1, ref A.Point pt2, double curvature, CurveOrientation orientation, PointAlignment pt1a, PointAlignment pt2a)
+        private void DrawRectangleInternal(AM.DrawingContext context, AM.IBrush brush, AM.IPen pen, bool isStroked, bool isFilled, ref A.Rect rect)
         {
-            if (!isStroked)
+            if (isFilled)
             {
-                return;
+                context.FillRectangle(brush, rect);
             }
-            var sg = new AM.StreamGeometry();
-            using (var sgc = sg.Open())
+
+            if (isStroked)
             {
-                sgc.BeginFigure(new A.Point(pt1.X, pt1.Y), false);
-                double p1x = pt1.X;
-                double p1y = pt1.Y;
-                double p2x = pt2.X;
-                double p2y = pt2.Y;
-                LineShapeExtensions.GetCurvedLineBezierControlPoints(orientation, curvature, pt1a, pt2a, ref p1x, ref p1y, ref p2x, ref p2y);
-                sgc.CubicBezierTo(
-                    new A.Point(p1x, p1y),
-                    new A.Point(p2x, p2y),
-                    new A.Point(pt2.X, pt2.Y));
-                sgc.EndFigure(false);
+                context.DrawRectangle(pen, rect);
             }
-            _dc.DrawGeometry(null, pen, sg);
         }
 
-        private void DrawLineArrowsInternal(AM.DrawingContext dc, ILineShape line, IShapeStyle style, double dx, double dy, Func<double, float> scaleToPage, bool scaleStrokeWidth, out A.Point pt1, out A.Point pt2)
+        private void DrawEllipseInternal(AM.DrawingContext context, AM.IBrush brush, AM.IPen pen, bool isStroked, bool isFilled, AM.EllipseGeometry ellipseGeometry)
         {
-            GetCached(style.StartArrowStyle, out var fillStartArrow, out var strokeStartArrow, scaleStrokeWidth);
-            GetCached(style.EndArrowStyle, out var fillEndArrow, out var strokeEndArrow, scaleStrokeWidth);
-
-            double _x1 = line.Start.X + dx;
-            double _y1 = line.Start.Y + dy;
-            double _x2 = line.End.X + dx;
-            double _y2 = line.End.Y + dy;
-            line.GetMaxLength(ref _x1, ref _y1, ref _x2, ref _y2);
-
-            float x1 = (float)_x1;
-            float y1 = (float)_y1;
-            float x2 = (float)_x2;
-            float y2 = (float)_y2;
-            var sas = style.StartArrowStyle;
-            var eas = style.EndArrowStyle;
-            double a1 = Math.Atan2(y1 - y2, x1 - x2);
-            double a2 = Math.Atan2(y2 - y1, x2 - x1);
-
-            pt1 = DrawLineArrowInternal(dc, strokeStartArrow, fillStartArrow, x1, y1, a1, sas);
-            pt2 = DrawLineArrowInternal(dc, strokeEndArrow, fillEndArrow, x2, y2, a2, eas);
+            context.DrawGeometry(isFilled ? brush : null, isStroked ? pen : null, ellipseGeometry);
         }
 
-        private static A.Point DrawLineArrowInternal(AM.DrawingContext dc, AM.IPen pen, AM.IBrush brush, float x, float y, double angle, IArrowStyle style)
+        private void DrawLineCurveInternal(AM.DrawingContext context, AM.IPen pen, bool isStroked, AM.StreamGeometry curveGeometry)
         {
-            var rt = AME.MatrixHelper.Rotation(angle, new A.Vector(x, y));
+            context.DrawGeometry(null, isStroked ? pen : null, curveGeometry);
+        }
+
+        private AM.StreamGeometry CreateLineCurveGeometry(ref A.Point p0, ref A.Point p1, double curvature, CurveOrientation orientation, PointAlignment pt0a, PointAlignment pt1a)
+        {
+            var curveGeometry = new AM.StreamGeometry();
+
+            using (var geometryContext = curveGeometry.Open())
+            {
+                geometryContext.BeginFigure(new A.Point(p0.X, p0.Y), false);
+                double p0x = p0.X;
+                double p0y = p0.Y;
+                double p1x = p1.X;
+                double p1y = p1.Y;
+                LineShapeExtensions.GetCurvedLineBezierControlPoints(orientation, curvature, pt0a, pt1a, ref p0x, ref p0y, ref p1x, ref p1y);
+                var point1 = new A.Point(p0x, p0y);
+                var point2 = new A.Point(p1x, p1y);
+                var point3 = new A.Point(p1.X, p1.Y);
+                geometryContext.CubicBezierTo(point1, point2, point3);
+                geometryContext.EndFigure(false);
+            }
+
+            return curveGeometry;
+        }
+
+        private A.Point DrawLineArrowInternal(AM.DrawingContext dc, AM.IPen pen, AM.IBrush brush, double x, double y, double angle, IArrowStyle style)
+        {
+            var rotation = AME.MatrixHelper.Rotation(angle, new A.Vector(x, y));
             double rx = style.RadiusX;
             double ry = style.RadiusY;
             double sx = 2.0 * rx;
             double sy = 2.0 * ry;
-            A.Point pt;
+            A.Point point;
+
             switch (style.ArrowType)
             {
                 default:
                 case ArrowType.None:
                     {
-                        pt = new A.Point(x, y);
+                        point = new A.Point(x, y);
                     }
                     break;
                 case ArrowType.Rectangle:
                     {
-                        pt = AME.MatrixHelper.TransformPoint(rt, new A.Point(x - (float)sx, y));
-                        var rect = new Rect2(x - sx, y - ry, sx, sy);
-                        using var d = dc.PushPreTransform(rt);
+                        point = AME.MatrixHelper.TransformPoint(rotation, new A.Point(x - (float)sx, y));
+
+                        var rect2 = new Rect2(x - sx, y - ry, sx, sy);
+                        var rect = new A.Rect(rect2.X, rect2.Y, rect2.Width, rect2.Height);
+
+                        using var rotationDisposable = dc.PushPreTransform(rotation);
                         DrawRectangleInternal(dc, brush, pen, style.IsStroked, style.IsFilled, ref rect);
                     }
                     break;
                 case ArrowType.Ellipse:
                     {
-                        pt = AME.MatrixHelper.TransformPoint(rt, new A.Point(x - (float)sx, y));
-                        using var d = dc.PushPreTransform(rt);
-                        var rect = new Rect2(x - sx, y - ry, sx, sy);
-                        DrawEllipseInternal(dc, brush, pen, style.IsStroked, style.IsFilled, ref rect);
+                        point = AME.MatrixHelper.TransformPoint(rotation, new A.Point(x - (float)sx, y));
+
+                        var rect2 = new Rect2(x - sx, y - ry, sx, sy);
+                        var rect = new A.Rect(rect2.X, rect2.Y, rect2.Width, rect2.Height);
+                        var ellipseGeometry = new AM.EllipseGeometry(rect);
+
+                        using var rotationDisposable = dc.PushPreTransform(rotation);
+                        DrawEllipseInternal(dc, brush, pen, style.IsStroked, style.IsFilled, ellipseGeometry);
                     }
                     break;
                 case ArrowType.Arrow:
                     {
-                        var pts = new A.Point[]
-                        {
-                            new A.Point(x, y),
-                            new A.Point(x - (float)sx, y + (float)sy),
-                            new A.Point(x, y),
-                            new A.Point(x - (float)sx, y - (float)sy),
-                            new A.Point(x, y)
-                        };
-                        pt = AME.MatrixHelper.TransformPoint(rt, pts[0]);
-                        var p11 = AME.MatrixHelper.TransformPoint(rt, pts[1]);
-                        var p21 = AME.MatrixHelper.TransformPoint(rt, pts[2]);
-                        var p12 = AME.MatrixHelper.TransformPoint(rt, pts[3]);
-                        var p22 = AME.MatrixHelper.TransformPoint(rt, pts[4]);
+                        point = AME.MatrixHelper.TransformPoint(rotation, new A.Point(x, y));
+
+                        var p11 = AME.MatrixHelper.TransformPoint(rotation, new A.Point(x - (float)sx, y + (float)sy));
+                        var p21 = AME.MatrixHelper.TransformPoint(rotation, new A.Point(x, y));
+                        var p12 = AME.MatrixHelper.TransformPoint(rotation, new A.Point(x - (float)sx, y - (float)sy));
+                        var p22 = AME.MatrixHelper.TransformPoint(rotation, new A.Point(x, y));
+
                         DrawLineInternal(dc, pen, style.IsStroked, ref p11, ref p21);
                         DrawLineInternal(dc, pen, style.IsStroked, ref p12, ref p22);
                     }
                     break;
             }
-            return pt;
+
+            return point;
         }
 
-        private static void DrawRectangleInternal(AM.DrawingContext dc, AM.IBrush brush, AM.IPen pen, bool isStroked, bool isFilled, ref Rect2 rect)
+        private void DrawLineInternal(AM.DrawingContext dc, ILineShape line, IShapeStyle style, out A.Point p0, out A.Point p1)
         {
-            if (!isStroked && !isFilled)
-            {
-                return;
-            }
-            var r = new A.Rect(rect.X, rect.Y, rect.Width, rect.Height);
-            if (isFilled)
-            {
-                dc.FillRectangle(brush, r);
-            }
-            if (isStroked)
-            {
-                dc.DrawRectangle(pen, r);
-            }
-        }
+            double x1 = line.Start.X;
+            double y1 = line.Start.Y;
+            double x2 = line.End.X;
+            double y2 = line.End.Y;
+            line.GetMaxLength(ref x1, ref y1, ref x2, ref y2);
 
-        private static void DrawEllipseInternal(AM.DrawingContext dc, AM.IBrush brush, AM.IPen pen, bool isStroked, bool isFilled, ref Rect2 rect)
-        {
-            if (!isFilled && !isStroked)
-            {
-                return;
-            }
-            var r = new A.Rect(rect.X, rect.Y, rect.Width, rect.Height);
-            var g = new AM.EllipseGeometry(r);
-            dc.DrawGeometry(
-                isFilled ? brush : null,
-                isStroked ? pen : null,
-                g);
+            var fillStartArrow = ToBrush(style.StartArrowStyle.Fill);
+            var strokeStartArrow = ToPen(style.StartArrowStyle, style.StartArrowStyle.Thickness);
+            double a1 = Math.Atan2(y1 - y2, x1 - x2);
+            p0 = DrawLineArrowInternal(dc, strokeStartArrow, fillStartArrow, x1, y1, a1, style.StartArrowStyle);
+
+            var fillEndArrow = ToBrush(style.EndArrowStyle.Fill);
+            var strokeEndArrow = ToPen(style.EndArrowStyle, style.EndArrowStyle.Thickness);
+            double a2 = Math.Atan2(y2 - y1, x2 - x1);
+            p1 = DrawLineArrowInternal(dc, strokeEndArrow, fillEndArrow, x2, y2, a2, style.EndArrowStyle);
         }
-        */
 
         public override void UpdateGeometry()
         {
@@ -181,57 +170,28 @@ namespace Core2D.UI.Renderer
         {
             if (Line.IsStroked)
             {
-                context.DrawLine(Stroke, P0, P1);
+                if (Style.StartArrowStyle.ArrowType != ArrowType.None || Style.EndArrowStyle.ArrowType != ArrowType.None || Style.LineStyle.IsCurved)
+                {
+                    // TODO: Cache Arrows
+                    DrawLineInternal(context, Line, Style, out var p0, out var p1);
+
+                    if (Style.LineStyle.IsCurved)
+                    {
+                        var curveGeometry = CreateLineCurveGeometry(ref p0, ref p1, Style.LineStyle.Curvature, Style.LineStyle.CurveOrientation, Line.Start.Alignment, Line.End.Alignment);
+
+                        // TODO: Cache Line Curve
+                        DrawLineCurveInternal(context, Stroke, Line.IsStroked, curveGeometry);
+                    }
+                    else
+                    {
+                        DrawLineInternal(context, Stroke, Line.IsStroked, ref p0, ref p1);
+                    }
+                }
+                else
+                {
+                    context.DrawLine(Stroke, P0, P1);
+                }
             }
-
-            // TODO: Curved
-
-            // TODO: Arrows
-
-            /*
-            var _dc = dc as AM.DrawingContext;
-
-            var style = line.Style;
-            if (style == null)
-            {
-                return;
-            }
-
-            var scaleThickness = line.State.Flags.HasFlag(ShapeStateFlags.Thickness);
-            var scaleSize = line.State.Flags.HasFlag(ShapeStateFlags.Size);
-
-            var scale = scaleSize ? 1.0 / _state.ZoomX : 1.0;
-            var scaleToPage = scale == 1.0 ? _scaleToPage : (value) => (float)(_scaleToPage(value) / scale);
-            var center = new Point2((line.Start.X + line.End.X) / 2.0, (line.Start.Y + line.End.Y) / 2.0);
-            var translateX = 0.0 - (center.X * scale) + center.X;
-            var translateY = 0.0 - (center.Y * scale) + center.Y;
-
-            GetCached(style, out _, out var stroke, scaleThickness);
-
-            var translateDisposable = scale != 1.0 ? _dc.PushPreTransform(AME.MatrixHelper.Translate(translateX, translateY)) : default(IDisposable);
-            var scaleDisposable = scale != 1.0 ? _dc.PushPreTransform(AME.MatrixHelper.Scale(scale, scale)) : default(IDisposable);
-
-            DrawLineArrowsInternal(_dc, line, style, dx, dy, scaleToPage, scaleThickness, out var pt1, out var pt2);
-
-            if (style.LineStyle.IsCurved)
-            {
-                DrawLineCurveInternal(
-                    _dc,
-                    stroke, line.IsStroked,
-                    ref pt1, ref pt2,
-                    style.LineStyle.Curvature,
-                    style.LineStyle.CurveOrientation,
-                    line.Start.Alignment,
-                    line.End.Alignment);
-            }
-            else
-            {
-                DrawLineInternal(_dc, stroke, line.IsStroked, ref pt1, ref pt2);
-            }
-
-            scaleDisposable?.Dispose();
-            translateDisposable?.Dispose();
-            */
         }
     }
 }
