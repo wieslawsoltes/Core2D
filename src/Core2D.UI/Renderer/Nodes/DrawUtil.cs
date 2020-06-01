d898f4726279bff7ab8f60cc5768f6e82619e307
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-06-01 10:43:00 +0200

    Create DrawUtil.cs

---
+++ src/Core2D.UI/Renderer/Nodes/DrawUtil.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Core2D.Style;
+using AM = Avalonia.Media;
+
+namespace Core2D.UI.Renderer
+{
+    internal static class DrawUtil
+    {
+        public static AM.Color ToColor(IArgbColor argbColor)
+        {
+            return AM.Color.FromArgb(argbColor.A, argbColor.R, argbColor.G, argbColor.B);
+        }
+
+        public static AM.IBrush ToBrush(IColor color) => color switch
+        {
+            IArgbColor argbColor => new AM.Immutable.ImmutableSolidColorBrush(ToColor(argbColor)),
+            _ => throw new NotSupportedException($"The {color.GetType()} color type is not supported.")
+        };
+
+        public static AM.IPen ToPen(IBaseStyle style, double thickness)
+        {
+            var dashStyle = default(AM.Immutable.ImmutableDashStyle);
+            if (style.Dashes != null)
+            {
+                var dashes = StyleHelper.ConvertDashesToDoubleArray(style.Dashes, 1.0);
+                var dashOffset = style.DashOffset;
+                if (dashes != null)
+                {
+                    dashStyle = new AM.Immutable.ImmutableDashStyle(dashes, dashOffset);
+                }
+            }
+
+            var lineCap = style.LineCap switch
+            {
+                LineCap.Flat => AM.PenLineCap.Flat,
+                LineCap.Square => AM.PenLineCap.Square,
+                LineCap.Round => AM.PenLineCap.Round,
+                _ => throw new NotImplementedException()
+            };
+
+            var brush = ToBrush(style.Stroke);
+            var pen = new AM.Immutable.ImmutablePen(brush, thickness, dashStyle, lineCap);
+
+            return pen;
+        }
+    }
+}
