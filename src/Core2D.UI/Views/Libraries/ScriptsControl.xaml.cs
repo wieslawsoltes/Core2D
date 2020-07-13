226a5a5831ed59614647e1bf880c5152d61cf7af
wieslawsoltes <wieslaw.soltes@gmail.com>
2020-07-13 08:57:16 +0000

    Added compiled bindings support

--- src/Core2D.UI/Views/Libraries/ScriptsControl.xaml.cs
+++ src/Core2D.UI/Views/Libraries/ScriptsControl.xaml.cs
@@ -1,7 +1,7 @@
 ﻿using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 
-namespace Core2D.UI.Views.Scripting
+namespace Core2D.UI.Views.Libraries
 {
     /// <summary>
     /// Interaction logic for <see cref="ScriptsControl"/> xaml.
