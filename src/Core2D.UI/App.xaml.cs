61c4386d0b2e4bf1603b75a1f357344420911ea3
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-07-16 18:46:34 +0200

    Update App.xaml.cs

--- src/Core2D.UI/App.xaml.cs
+++ src/Core2D.UI/App.xaml.cs
@@ -257,8 +257,6 @@ namespace Core2D.UI
             var aboutInfo = CreateAboutInfo(runtimeInfo, windowingSubsystemName, renderingSubsystemName);
             editor.AboutInfo = aboutInfo;
 
-            //editor.OnNew(null);
-
             var mainWindow = serviceProvider.GetService<MainWindow>();
 
             if (windowSettings != null)
