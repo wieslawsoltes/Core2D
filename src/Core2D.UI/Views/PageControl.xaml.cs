0358fc69b45733522390ef396576f3f4e6bad659
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-07-18 15:53:09 +0200

    Update PageControl.xaml.cs

--- src/Core2D.UI/Views/PageControl.xaml.cs
+++ src/Core2D.UI/Views/PageControl.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Avalonia.Controls;
+using Avalonia.Controls.PanAndZoom;
 using Avalonia.Markup.Xaml;
 
 namespace Core2D.UI.Views
@@ -8,12 +9,26 @@ namespace Core2D.UI.Views
     /// </summary>
     public class PageControl : UserControl
     {
+        private ScrollViewer _scrollViewer;
+        private ZoomBorder _zoomBorder;
+        private Grid _gridTransformed;
+        private PresenterControl _presenterControlData;
+        private PresenterControl _presenterControlTemplate;
+        private PresenterControl _presenterControlEditor;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="PageControl"/> class.
         /// </summary>
         public PageControl()
         {
             InitializeComponent();
+
+            _scrollViewer = this.FindControl<ScrollViewer>("scrollViewer");
+            _zoomBorder = this.FindControl<ZoomBorder>("zoomBorder");
+            _gridTransformed = this.FindControl<Grid>("gridTransformed");
+            _presenterControlData = this.FindControl<PresenterControl>("presenterControlData");
+            _presenterControlTemplate = this.FindControl<PresenterControl>("presenterControlTemplate");
+            _presenterControlEditor = this.FindControl<PresenterControl>("presenterControlEditor");
         }
 
         /// <summary>
