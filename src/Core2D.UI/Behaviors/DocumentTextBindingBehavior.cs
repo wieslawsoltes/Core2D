d161787cc49ddea812cbda99274dda9cfabbc0c5
wieslawsoltes <wieslaw.soltes@gmail.com>
2020-07-24 08:02:50 +0000

    Use AvaloniaEdit

---
+++ src/Core2D.UI/Behaviors/DocumentTextBindingBehavior.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Media;
+using Avalonia.Xaml.Interactivity;
+using AvaloniaEdit;
+
+namespace Core2D.UI.Behaviors
+{
+    /// <summary>
+    /// Binds to a document text.
+    /// </summary>
+    public class DocumentTextBindingBehavior : Behavior<TextEditor>
+    {
+        private TextEditor _textEditor = null;
+
+        /// <summary>
+        /// Define <see cref="Context"/> property.
+        /// </summary>
+        public static readonly StyledProperty<string> TextProperty =
+            AvaloniaProperty.Register<DocumentTextBindingBehavior, string>(nameof(Text));
+
+        /// <summary>
+        /// Gets or sets text editor text.
+        /// </summary>
+        public string Text
+        {
+            get => GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
+        }
+
+        /// <inheritdoc/>
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            if (AssociatedObject is TextEditor textEditor)
+            {
+                _textEditor = textEditor;
+                _textEditor.TextChanged += TextChanged;
+                this.GetObservable(TextProperty).Subscribe(TextPropertyChanged);
+            }
+        }
+
+        /// <inheritdoc/>
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+
+            if (_textEditor != null)
+            {
+                _textEditor.TextChanged -= TextChanged;
+            }
+        }
+
+        private void TextChanged(object sender, EventArgs eventArgs)
+        {
+            if (_textEditor != null && _textEditor.Document != null)
+            {
+                Text = _textEditor.Document.Text;
+            }
+        }
+
+        private void TextPropertyChanged(string text)
+        {
+            if (_textEditor != null && _textEditor.Document != null && text != null)
+            {
+                var caretOffset = _textEditor.CaretOffset;
+                _textEditor.Document.Text = text;
+                _textEditor.CaretOffset = caretOffset;
+            }
+        }
+    }
+}
--- src/Core2D.UI/Views/Scripting/ScriptControl.xaml.cs
+++ src/Core2D.UI/Views/Scripting/ScriptControl.xaml.cs
@@ -1,5 +1,12 @@
 ﻿using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using AvaloniaEdit;
+using AvaloniaEdit.CodeCompletion;
+using AvaloniaEdit.Document;
+using AvaloniaEdit.Editing;
+using AvaloniaEdit.Highlighting;
+using AvaloniaEdit.Indentation.CSharp;
+using AvaloniaEdit.Rendering;
 
 namespace Core2D.UI.Views.Scripting
 {
@@ -8,12 +15,17 @@ namespace Core2D.UI.Views.Scripting
     /// </summary>
     public class ScriptControl : UserControl
     {
+        private readonly TextEditor _textEditor;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="ScriptControl"/> class.
         /// </summary>
         public ScriptControl()
         {
             InitializeComponent();
+            _textEditor = this.FindControl<TextEditor>("textCode");
+            _textEditor.ShowLineNumbers = true;
+            _textEditor.TextArea.IndentationStrategy = new CSharpIndentationStrategy();
         }
 
         /// <summary>
