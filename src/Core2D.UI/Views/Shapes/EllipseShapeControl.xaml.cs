1e2778e52f294028c2111e9e65d594a882f35e28
wieslawsoltes <wieslaw.soltes@gmail.com>
2020-08-03 11:34:04 +0000

    Added initial text binding editor

--- src/Core2D.UI/Views/Shapes/EllipseShapeControl.xaml.cs
+++ src/Core2D.UI/Views/Shapes/EllipseShapeControl.xaml.cs
@@ -1,5 +1,9 @@
 ﻿using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Core2D.Editor;
+using Core2D.Editors;
+using Core2D.Shapes;
+using Core2D.UI.Views.Editors;
 
 namespace Core2D.UI.Views.Shapes
 {
@@ -22,6 +26,27 @@ namespace Core2D.UI.Views.Shapes
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+        }
+
+        /// <summary>
+        /// Edit shape text binding.
+        /// </summary>
+        public void OnEditTextBinding(object shape)
+        {
+            if (this.VisualRoot is TopLevel topLevel 
+                && topLevel.DataContext is IProjectEditor editor
+                && shape is ITextShape text)
+            {
+                var window = new TextBindingEditorWindow()
+                {
+                    DataContext = new TextBindingEditor()
+                    {
+                        Editor = editor,
+                        Text = text
+                    }
+                };
+                window.ShowDialog(topLevel as Window);
+            }
         }
     }
 }
---
+++ src/Core2D/ViewModels/Editors/TextBindingEditor.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using Core2D.Data;
+using Core2D.Editor;
+using Core2D.Shapes;
+
+namespace Core2D.Editors
+{
+    /// <summary>
+    /// Text binding editor.
+    /// </summary>
+    public class TextBindingEditor : ObservableObject
+    {
+        private IProjectEditor _editor;
+        private ITextShape _text;
+
+        /// <summary>
+        /// Gets or sets project editor.
+        /// </summary>
+        public IProjectEditor Editor
+        {
+            get => _editor;
+            set => Update(ref _editor, value);
+        }
+
+        /// <summary>
+        /// Gets or sets text shape.
+        /// </summary>
+        public ITextShape Text
+        {
+            get => _text;
+            set => Update(ref _text, value);
+        }
+
+        /// <inheritdoc/>
+        public override object Copy(IDictionary<object, object> shared)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Use column name.
+        /// </summary>
+        public void OnUseColumnName(IColumn column)
+        {
+            if (_text != null && column != null)
+            {
+                _text.Text += $"{{{column.Name}}}";
+            }
+        }
+
+        /// <summary>
+        /// Use page property.
+        /// </summary>
+        public void OnUsePageProperty(IProperty property)
+        {
+            if (_text != null && property != null)
+            {
+                _text.Text += $"{{{property.Name}}}";
+            }
+        }
+
+        /// <summary>
+        /// Use shape property.
+        /// </summary>
+        public void OnUseShapeProperty(IProperty property)
+        {
+            if (_text != null && property != null)
+            {
+                _text.Text += $"{{{property.Name}}}";
+            }
+        }
+    }
+}
