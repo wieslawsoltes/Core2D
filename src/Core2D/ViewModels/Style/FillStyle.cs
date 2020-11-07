8b57ac7743e212d4da3691b2236348db0ac3032e
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-11-07 22:18:23 +0100

    Create FillStyle.cs

---
+++ src/Core2D/ViewModels/Style/FillStyle.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace Core2D.Style
+{
+    [DataContract(IsReference = true)]
+    public class FillStyle : ViewModelBase
+    {
+        private BaseColor _color;
+
+        [DataMember(IsRequired = false, EmitDefaultValue = true)]
+        public BaseColor Color
+        {
+            get => _color;
+            set => RaiseAndSetIfChanged(ref _color, value);
+        }
+
+        public override object Copy(IDictionary<object, object> shared)
+        {
+            return new FillStyle()
+            {
+                Name = this.Name,
+                Color = (BaseColor)this.Color.Copy(shared)
+            };
+        }
+
+        public override bool IsDirty()
+        {
+            var isDirty = base.IsDirty();
+
+            isDirty |= Color.IsDirty();
+
+            return isDirty;
+        }
+
+        public override void Invalidate()
+        {
+            base.Invalidate();
+            Color.Invalidate();
+        }
+    }
+}
