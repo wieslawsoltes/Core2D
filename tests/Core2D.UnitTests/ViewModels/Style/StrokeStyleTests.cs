d71ed04658169d8e7393f0f6763a76d577ceea32
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-11-08 12:14:06 +0100

    Create StrokeStyleTests.cs

---
+++ tests/Core2D.UnitTests/ViewModels/Style/StrokeStyleTests.cs
@@ -0,0 +1,18 @@
+﻿using Core2D;
+using Xunit;
+
+namespace Core2D.Style.UnitTests
+{
+    public class StrokeStyleTests
+    {
+        private readonly IFactory _factory = new Factory();
+
+        [Fact]
+        [Trait("Core2D.Style", "Style")]
+        public void Inherits_From_ViewModelBase()
+        {
+            var target = _factory.CreateStrokeStyle();
+            Assert.True(target is ViewModelBase);
+        }
+    }
+}
