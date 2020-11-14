7147cda3b4ddbf67dcfc818ca25fcf98a244f912
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-11-14 21:01:55 +0100

    Create ToolIconConverter.cs

---
+++ src/Core2D/Converters/ToolIconConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using Avalonia;
+using Avalonia.Data.Converters;
+using Core2D.Editor;
+
+namespace Core2D.Converters
+{
+    public class ToolIconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is ITool tool)
+            {
+                var key = $"{tool.Title}";
+
+                if (Application.Current.Styles.TryGetResource(key, out var resource))
+                {
+                    return resource;
+                }
+            }
+            return AvaloniaProperty.UnsetValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
