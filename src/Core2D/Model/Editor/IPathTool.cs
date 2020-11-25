2281f2e68a4fa62d55668c16efbed60e741586eb
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-11-25 19:47:43 +0100

    Revert

--- src/Core2D/Model/Editor/IPathTool.cs
+++ src/Core2D/Model/Editor/IPathTool.cs
@@ -1,24 +1,6 @@
-﻿using Core2D.Input;
-using Core2D.Shapes;
-
-namespace Core2D.Editor
+﻿namespace Core2D.Editor
 {
     public interface IPathTool : ITool
     {
-        void BeginDown(InputArgs args);
-
-        void BeginUp(InputArgs args);
-
-        void EndDown(InputArgs args);
-
-        void EndUp(InputArgs args);
-
-        void Move(InputArgs args);
-
-        void Move(BaseShape shape);
-
-        void Finalize(BaseShape shape);
-
-        void Reset();
     }
 }
--- src/Core2D/Model/Editor/ITool.cs
+++ src/Core2D/Model/Editor/ITool.cs
@@ -1,7 +1,26 @@
-﻿namespace Core2D.Editor
+﻿using Core2D.Input;
+using Core2D.Shapes;
+
+namespace Core2D.Editor
 {
     public interface ITool
     {
         string Title { get; }
+        
+        void BeginDown(InputArgs args);
+
+        void BeginUp(InputArgs args);
+
+        void EndDown(InputArgs args);
+
+        void EndUp(InputArgs args);
+
+        void Move(InputArgs args);
+
+        void Move(BaseShape shape);
+
+        void Finalize(BaseShape shape);
+
+        void Reset();
     }
 }
