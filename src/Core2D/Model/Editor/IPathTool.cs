d895343ef6c100493d4edd47a42dbaa26e7ef987
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-11-25 19:43:23 +0100

    Refactor

--- src/Core2D/Model/Editor/IPathTool.cs
+++ src/Core2D/Model/Editor/IPathTool.cs
@@ -1,6 +1,24 @@
-﻿namespace Core2D.Editor
+﻿using Core2D.Input;
+using Core2D.Shapes;
+
+namespace Core2D.Editor
 {
     public interface IPathTool : ITool
     {
+        void BeginDown(InputArgs args);
+
+        void BeginUp(InputArgs args);
+
+        void EndDown(InputArgs args);
+
+        void EndUp(InputArgs args);
+
+        void Move(InputArgs args);
+
+        void Move(BaseShape shape);
+
+        void Finalize(BaseShape shape);
+
+        void Reset();
     }
 }
--- src/Core2D/Model/Editor/ITool.cs
+++ src/Core2D/Model/Editor/ITool.cs
@@ -1,26 +1,7 @@
-﻿using Core2D.Input;
-using Core2D.Shapes;
-
-namespace Core2D.Editor
+﻿namespace Core2D.Editor
 {
     public interface ITool
     {
         string Title { get; }
-
-        void BeginDown(InputArgs args);
-
-        void BeginUp(InputArgs args);
-
-        void EndDown(InputArgs args);
-
-        void EndUp(InputArgs args);
-
-        void Move(InputArgs args);
-
-        void Move(BaseShape shape);
-
-        void Finalize(BaseShape shape);
-
-        void Reset();
     }
 }
