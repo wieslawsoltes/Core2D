0b9267c679cc4ffdb664b4dd6e31092285354467
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-12-17 19:23:57 +0100

    Update Util.cs

--- src/Core2D.Desktop/Util.cs
+++ src/Core2D.Desktop/Util.cs
@@ -5,6 +5,7 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Media;
 using Avalonia.Media.Imaging;
+using Avalonia.Platform;
 using Avalonia.Rendering;
 using Avalonia.Skia.Helpers;
 using Avalonia.Threading;
@@ -14,60 +15,85 @@ namespace Core2D.Desktop
 {
     public static class Util
     {
-        public static void RenderAsPng(Control? target, Size size, string path, double dpi = 96)
+        public static void RenderAsPng(Control target, Size size, string path, double dpi = 96)
         {
             var pixelSize = new PixelSize((int)size.Width, (int)size.Height);
             var dpiVector = new Vector(dpi, dpi);
             using var bitmap = new RenderTargetBitmap(pixelSize, dpiVector);
-            target?.Measure(size);
-            target?.Arrange(new Rect(size));
+            target.Measure(size);
+            target.Arrange(new Rect(size));
             bitmap.Render(target);
             bitmap.Save(path);
         }
 
-        public static void RenderAsSvg(Control? target, Size size, string path, double dpi = 96)
+        private class CustomRenderTarget : IRenderTarget
+        {
+            private readonly SKCanvas _canvas;
+            private readonly double _dpi;
+            
+            public CustomRenderTarget(SKCanvas canvas, double dpi)
+            {
+                _canvas = canvas;
+                _dpi = dpi;
+            }
+            
+            public IDrawingContextImpl CreateDrawingContext(IVisualBrushRenderer visualBrushRenderer)
+            {
+                return DrawingContextHelper.WrapSkiaCanvas(_canvas, new Vector(_dpi, _dpi), visualBrushRenderer);
+            }
+            
+            public void Dispose()
+            {
+                _canvas.Flush();
+            }
+        }
+        
+        public static void RenderAsSvg(Control target, Size size, string path, double dpi = 96)
         {
             using var stream = File.Create(path);
             using var wstream = new SKManagedWStream(stream);
             var bounds = SKRect.Create(new SKSize((float)size.Width, (float)size.Height));
             using var canvas = SKSvgCanvas.Create(bounds, wstream);
-            using var impl = DrawingContextHelper.WrapSkiaCanvas(canvas, new Vector(dpi, dpi));
-            using var context = new DrawingContext(impl);
-            target?.Measure(size);
-            target?.Arrange(new Rect(size));
-            target?.Render(context);
-            canvas.Flush();
+            using var renderer = new ImmediateRenderer(target);
+            target.Measure(size);
+            target.Arrange(new Rect(size));
+            using var renderTarget = new CustomRenderTarget(canvas, dpi);
+            ImmediateRenderer.Render(target, renderTarget);
         }
         
-        public static void RenderAsPdf(Control? target, Size size, string path, double dpi = 96)
+        public static void RenderAsPdf(Control target, Size size, string path, double dpi = 72)
         {
             using var stream = File.Create(path);
             using var wstream = new SKManagedWStream(stream);
             using var document = SKDocument.CreatePdf(stream, (float)dpi);
             using var canvas = document.BeginPage((float)size.Width, (float)size.Height);
-            using var impl = DrawingContextHelper.WrapSkiaCanvas(canvas, new Vector(dpi, dpi));
-            using var context = new DrawingContext(impl);
-            target?.Measure(size);
-            target?.Arrange(new Rect(size));
-            target?.Render(context);
-            canvas.Flush();
+            using var renderer = new ImmediateRenderer(target);
+            target.Measure(size);
+            target.Arrange(new Rect(size));
+            using var renderTarget = new CustomRenderTarget(canvas, dpi);
+            ImmediateRenderer.Render(target, renderTarget);
         }
         
         public static void Render(Control? control, Size size, string path)
         {
+            if (control is null)
+            {
+                return;
+            }
+            
             if (path.EndsWith("png", StringComparison.OrdinalIgnoreCase))
             {
-                Util.RenderAsPng(control, size, path);
+                RenderAsPng(control, size, path);
             }
 
             if (path.EndsWith("pdf", StringComparison.OrdinalIgnoreCase))
             {
-                Util.RenderAsPdf(control, size, path);
+                RenderAsPdf(control, size, path);
             }
 
             if (path.EndsWith("svg", StringComparison.OrdinalIgnoreCase))
             {
-                Util.RenderAsSvg(control, size, path);
+                RenderAsSvg(control, size, path);
             }
         }
 
