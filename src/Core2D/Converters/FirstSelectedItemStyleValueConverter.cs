dcaa8f1cbb7b4af26d0594c2ebcc34fa52c4166d
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-02-22 20:57:39 +0100

    Create FirstSelectedItemStyleValueConverter.cs

---
+++ src/Core2D/Converters/FirstSelectedItemStyleValueConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using Avalonia;
+using Avalonia.Data.Converters;
+using Core2D.ViewModels.Shapes;
+
+namespace Core2D.Converters
+{
+    public class FirstSelectedItemStyleValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is ISet<BaseShapeViewModel> items && items.Count == 1)
+            {
+                return items.First().Style;
+            }
+            return AvaloniaProperty.UnsetValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
