af5fd896f8c5f4f3c9d32962c46e3ee4e26e93b4
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-03-10 21:44:40 +0100

    Update PathGeometryConverter.cs

--- src/Core2D/Modules/Renderer/PathGeometryConverter.cs
+++ src/Core2D/Modules/Renderer/PathGeometryConverter.cs
@@ -16,16 +16,32 @@ namespace Core2D.Modules.Renderer
     {
         private static AP.IPlatformRenderInterface Factory => A.AvaloniaLocator.Current.GetService<AP.IPlatformRenderInterface>();
 
-        private static T GetService<T>(this A.IAvaloniaDependencyResolver resolver)
-        {
-            return (T) resolver.GetService(typeof (T));
-        }
+        private static T GetService<T>(this A.IAvaloniaDependencyResolver resolver) => (T) resolver.GetService(typeof (T));
 
         private static A.Point ToPoint(this PointShapeViewModel point) => new(point.X, point.Y);
 
         private static A.Size ToSize(this PathSizeViewModel size) => new( size.Width, size.Height);
 
         private static AM.FillRule ToFillRule(this FillRule fillRule) => fillRule == FillRule.Nonzero ? AM.FillRule.NonZero : AM.FillRule.EvenOdd;
+
+        private static A.Rect ToRect(EllipseShapeViewModel ellipse)
+        {
+            var rect2 = Rect2.FromPoints(
+                ellipse.TopLeft.X,
+                ellipse.TopLeft.Y,
+                ellipse.BottomRight.X,
+                ellipse.BottomRight.Y);
+            return new A.Rect(rect2.X, rect2.Y, rect2.Width, rect2.Height);
+        }
+
+        private static WpfArc ToWpfArc(ArcShapeViewModel arc)
+        {
+            return new WpfArc(
+                Point2.FromXY(arc.Point1.X, arc.Point1.Y),
+                Point2.FromXY(arc.Point2.X, arc.Point2.Y),
+                Point2.FromXY(arc.Point3.X, arc.Point3.Y),
+                Point2.FromXY(arc.Point4.X, arc.Point4.Y));
+        }
 
         public static AM.Geometry ToGeometry(PathGeometryViewModel path, bool isFilled)
         {
@@ -75,6 +91,59 @@ namespace Core2D.Modules.Renderer
                 context.EndFigure(figure.IsClosed);
             }
 
+            return geometry;
+        }
+
+        public static AM.Geometry ToGeometry(EllipseShapeViewModel ellipse)
+        {
+            var rect = ToRect(ellipse);
+            return new AM.EllipseGeometry(rect);
+        }
+
+        public static AM.Geometry ToGeometry(ArcShapeViewModel arc)
+        {
+            var geometry = new AM.StreamGeometry();
+            using var context = geometry.Open();
+            var wpfArc = ToWpfArc(arc);
+            context.BeginFigure(
+                new A.Point(wpfArc.Start.X, wpfArc.Start.Y),
+                arc.IsFilled);
+            context.ArcTo(
+                new A.Point(wpfArc.End.X, wpfArc.End.Y),
+                new A.Size(wpfArc.Radius.Width, wpfArc.Radius.Height),
+                0.0,
+                wpfArc.IsLargeArc,
+                AM.SweepDirection.Clockwise);
+            context.EndFigure(false);
+            return geometry;
+        }
+
+        public static AM.Geometry ToGeometry(CubicBezierShapeViewModel cubicBezier)
+        {
+            var geometry = new AM.StreamGeometry();
+            using var context = geometry.Open();
+            context.BeginFigure(
+                cubicBezier.Point1.ToPoint(),
+                cubicBezier.IsFilled);
+            context.CubicBezierTo(
+                cubicBezier.Point2.ToPoint(),
+                cubicBezier.Point3.ToPoint(),
+                cubicBezier.Point4.ToPoint());
+            context.EndFigure(false);
+            return geometry;
+        }
+
+        public static AM.Geometry ToGeometry(QuadraticBezierShapeViewModel quadraticBezier)
+        {
+            var geometry = new AM.StreamGeometry();
+            using var context = geometry.Open();
+            context.BeginFigure(
+                quadraticBezier.Point1.ToPoint(),
+                quadraticBezier.IsFilled);
+            context.QuadraticBezierTo(
+                quadraticBezier.Point2.ToPoint(),
+                quadraticBezier.Point3.ToPoint());
+            context.EndFigure(false);
             return geometry;
         }
 
@@ -129,54 +198,10 @@ namespace Core2D.Modules.Renderer
             return geometry;
         }
 
-        private static A.Rect ToRect(EllipseShapeViewModel ellipse)
-        {
-            var rect2 = Rect2.FromPoints(
-                ellipse.TopLeft.X,
-                ellipse.TopLeft.Y,
-                ellipse.BottomRight.X,
-                ellipse.BottomRight.Y);
-            var rect = new A.Rect(rect2.X, rect2.Y, rect2.Width, rect2.Height);
-            return rect;
-        }
-
-        public static AM.Geometry ToGeometry(EllipseShapeViewModel ellipse)
-        {
-            var rect = ToRect(ellipse);
-            return new AM.EllipseGeometry(rect);
-        }
-
         public static AP.IGeometryImpl ToGeometryImpl(EllipseShapeViewModel ellipse)
         {
             var rect = ToRect(ellipse);
             return Factory.CreateEllipseGeometry(rect);
-        }
-
-        private static WpfArc ToWpfArc(ArcShapeViewModel arc)
-        {
-            return new WpfArc(
-                Point2.FromXY(arc.Point1.X, arc.Point1.Y),
-                Point2.FromXY(arc.Point2.X, arc.Point2.Y),
-                Point2.FromXY(arc.Point3.X, arc.Point3.Y),
-                Point2.FromXY(arc.Point4.X, arc.Point4.Y));
-        }
-
-        public static AM.Geometry ToGeometry(ArcShapeViewModel arc)
-        {
-            var geometry = new AM.StreamGeometry();
-            using var context = geometry.Open();
-            var wpfArc = ToWpfArc(arc);
-            context.BeginFigure(
-                new A.Point(wpfArc.Start.X, wpfArc.Start.Y),
-                arc.IsFilled);
-            context.ArcTo(
-                new A.Point(wpfArc.End.X, wpfArc.End.Y),
-                new A.Size(wpfArc.Radius.Width, wpfArc.Radius.Height),
-                0.0,
-                wpfArc.IsLargeArc,
-                AM.SweepDirection.Clockwise);
-            context.EndFigure(false);
-            return geometry;
         }
 
         public static AP.IGeometryImpl ToGeometryImpl(ArcShapeViewModel arc)
@@ -197,21 +222,6 @@ namespace Core2D.Modules.Renderer
             return geometry;
         }
 
-        public static AM.Geometry ToGeometry(CubicBezierShapeViewModel cubicBezier)
-        {
-            var geometry = new AM.StreamGeometry();
-            using var context = geometry.Open();
-            context.BeginFigure(
-                cubicBezier.Point1.ToPoint(),
-                cubicBezier.IsFilled);
-            context.CubicBezierTo(
-                cubicBezier.Point2.ToPoint(),
-                cubicBezier.Point3.ToPoint(),
-                cubicBezier.Point4.ToPoint());
-            context.EndFigure(false);
-            return geometry;
-        }
-
         public static AP.IGeometryImpl ToGeometryImpl(CubicBezierShapeViewModel cubicBezier)
         {
             var geometry = Factory.CreateStreamGeometry();
@@ -223,20 +233,6 @@ namespace Core2D.Modules.Renderer
                 cubicBezier.Point2.ToPoint(),
                 cubicBezier.Point3.ToPoint(),
                 cubicBezier.Point4.ToPoint());
-            context.EndFigure(false);
-            return geometry;
-        }
-
-        public static AM.Geometry ToGeometry(QuadraticBezierShapeViewModel quadraticBezier)
-        {
-            var geometry = new AM.StreamGeometry();
-            using var context = geometry.Open();
-            context.BeginFigure(
-                quadraticBezier.Point1.ToPoint(),
-                quadraticBezier.IsFilled);
-            context.QuadraticBezierTo(
-                quadraticBezier.Point2.ToPoint(),
-                quadraticBezier.Point3.ToPoint());
             context.EndFigure(false);
             return geometry;
         }
