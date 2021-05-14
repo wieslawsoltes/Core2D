df5cf416a75f5cf4878d55efc8c79554e10070d9
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-05-14 09:08:36 +0200

    Refactor capture code

--- src/Core2D/Screenshot/Capture.cs
+++ src/Core2D/Screenshot/Capture.cs
@@ -1,21 +1,134 @@
 ﻿using System;
 using System.IO;
 using System.Threading.Tasks;
-using Avalonia;
+using SkiaSharp;
 using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Interactivity;
+using Avalonia.Rendering;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+using Avalonia.Skia.Helpers;
 
-namespace Core2D.Screenshot
+namespace Avalonia.Screenshot
 {
-    public static class Capture
+    public static class CanvasRenderer
     {
-        public static void AttachScreenshot(this TopLevel root)
+        public static void Render(Control target, SKCanvas canvas, double dpi = 96, bool useDeferredRenderer = false)
         {
-            AttachScreenshot(root, new KeyGesture(Key.F12));
+            var renderTarget = new CanvasRenderTarget(canvas, dpi);
+            if (useDeferredRenderer)
+            {
+                using var renderer = new DeferredRenderer(target, renderTarget);
+                renderer.Start();
+                var renderLoopTask = renderer as IRenderLoopTask;
+                renderLoopTask.Update(TimeSpan.Zero);
+                renderLoopTask.Render();
+            }
+            else
+            {
+                ImmediateRenderer.Render(target, renderTarget);
+            }
+        }
+    }
+
+    public class CanvasRenderTarget : IRenderTarget
+    {
+        private readonly SKCanvas _canvas;
+        private readonly double _dpi;
+
+        public CanvasRenderTarget(SKCanvas canvas, double dpi)
+        {
+            _canvas = canvas;
+            _dpi = dpi;
         }
 
-        public static void AttachScreenshot(this TopLevel root, KeyGesture gesture)
+        public IDrawingContextImpl CreateDrawingContext(IVisualBrushRenderer visualBrushRenderer)
+        {
+            return DrawingContextHelper.WrapSkiaCanvas(_canvas, new Vector(_dpi, _dpi), visualBrushRenderer);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+
+    public static class PdfRenderer
+    {
+        public static void Render(Control target, Size size, Stream stream, double dpi = 72, bool useDeferredRenderer = false)
+        {
+            using var managedWStream = new SKManagedWStream(stream);
+            using var document = SKDocument.CreatePdf(stream, (float)dpi);
+            using var canvas = document.BeginPage((float)size.Width, (float)size.Height);
+            target.Measure(size);
+            target.Arrange(new Rect(size));
+            CanvasRenderer.Render(target, canvas, dpi, useDeferredRenderer);
+        }
+    }
+
+    public static class PngRenderer
+    {
+        public static void Render(Control target, Size size, string path, double dpi = 96)
+        {
+            var pixelSize = new PixelSize((int)size.Width, (int)size.Height);
+            var dpiVector = new Vector(dpi, dpi);
+            using var bitmap = new RenderTargetBitmap(pixelSize, dpiVector);
+            target.Measure(size);
+            target.Arrange(new Rect(size));
+            bitmap.Render(target);
+            bitmap.Save(path);
+        }
+    }
+    
+    public static class SkpRenderer
+    {
+        public static void Render(Control target, Size size, Stream stream, double dpi = 96, bool useDeferredRenderer = false)
+        {
+            var bounds = SKRect.Create(new SKSize((float)size.Width, (float)size.Height));
+            using var pictureRecorder = new SKPictureRecorder();
+            using var canvas = pictureRecorder.BeginRecording(bounds);
+            target.Measure(size);
+            target.Arrange(new Rect(size));
+            CanvasRenderer.Render(target, canvas, dpi, useDeferredRenderer);
+            using var picture = pictureRecorder.EndRecording();
+            picture.Serialize(stream);
+        }
+    }
+
+    public static class SvgRenderer
+    {
+        public static void Render(Control target, Size size, Stream stream, double dpi = 96, bool useDeferredRenderer = false)
+        {
+            using var managedWStream = new SKManagedWStream(stream);
+            var bounds = SKRect.Create(new SKSize((float)size.Width, (float)size.Height));
+            using var canvas = SKSvgCanvas.Create(bounds, managedWStream);
+            target.Measure(size);
+            target.Arrange(new Rect(size));
+            CanvasRenderer.Render(target, canvas, dpi, useDeferredRenderer);
+        }
+    }
+
+    public static class XpsRenderer
+    {
+        public static void Render(Control target, Size size, Stream stream, double dpi = 72, bool useDeferredRenderer = false)
+        {
+            using var managedWStream = new SKManagedWStream(stream);
+            using var document = SKDocument.CreateXps(stream, (float)dpi);
+            using var canvas = document.BeginPage((float)size.Width, (float)size.Height);
+            target.Measure(size);
+            target.Arrange(new Rect(size));
+            CanvasRenderer.Render(target, canvas, dpi, useDeferredRenderer);
+        }
+    }
+
+    public static class Capture
+    {
+        public static void AttachCapture(this TopLevel root)
+        {
+            AttachCapture(root, new KeyGesture(Key.F12));
+        }
+
+        public static void AttachCapture(this TopLevel root, KeyGesture gesture)
         {
             root.AddHandler(InputElement.KeyDownEvent, async (sender, args) =>
             {
@@ -44,7 +157,7 @@ namespace Core2D.Screenshot
             }
         }
 
-        public static void Save(Control control, Size size, string path)
+        public static void Save(Control? control, Size size, string path)
         {
             if (control is null)
             {
