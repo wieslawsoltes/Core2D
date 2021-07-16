e90d18f739a47c2c34bb9af9a4dd55f6734a29ec
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-07-16 14:17:22 +0200

    Add docking views

---
+++ src/Core2D/Views/Docking/Tools/Options/ProjectOptionsView.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Core2D.Views.Docking.Tools.Options
+{
+    public class ProjectOptionsView : UserControl
+    {
+        public ProjectOptionsView()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
---
+++ src/Core2D/Views/Docking/Tools/Options/RendererOptionsView.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Core2D.Views.Docking.Tools.Options
+{
+    public class RendererOptionsView : UserControl
+    {
+        public RendererOptionsView()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
