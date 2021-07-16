68e996cae6e357b5549bb9b7564093be428e9f06
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-07-16 21:42:03 +0200

    Update ProjectEditorViewModel.cs

--- src/Core2D/ViewModels/Editor/ProjectEditorViewModel.cs
+++ src/Core2D/ViewModels/Editor/ProjectEditorViewModel.cs
@@ -161,7 +161,7 @@ namespace Core2D.ViewModels.Editor
             if (_rootDock is { })
             {
                 _dockFactory?.InitLayout(_rootDock);
-                _rootDock.Navigate.Execute("Dashboard");
+                _rootDock.Navigate.Execute("Home");
             }
         }
 
