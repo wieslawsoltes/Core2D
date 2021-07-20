f55435a9271c4640ea12eb363d20cf841d7e57cf
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-07-20 15:07:14 +0200

    Move and rename

---
+++ src/Core2D/Views/Renderer/RenderType.cs
@@ -0,0 +1,13 @@
+﻿#nullable enable
+
+namespace Core2D.Views.Renderer
+{
+    public enum RenderType
+    {
+        None = 0,
+        Data = 1,
+        Template = 2,
+        Editor = 3,
+        Export = 4
+    }
+}
