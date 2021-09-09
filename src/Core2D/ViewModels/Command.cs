7a1039bece5cd3211b02ccdc5f530cb61bc1407f
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-09-09 22:19:24 +0200

    Split

--- src/Core2D/ViewModels/Command.cs
+++ src/Core2D/ViewModels/Command.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿#nullable enable
+using System;
 using System.Windows.Input;
 
 namespace Core2D.ViewModels
@@ -24,30 +25,6 @@ namespace Core2D.ViewModels
         public void Execute(object? parameter)
         {
             _execute.Invoke();
-        }
-    }
-
-    public class Command<T> : ICommand
-    {
-        private readonly Action<T?> _execute;
-        private readonly Func<T?, bool> _canExecute;
-
-        public event EventHandler? CanExecuteChanged;
-
-        public Command(Action<T?> execute, Func<T?, bool>? canExecute = null)
-        {
-            _execute = execute;
-            _canExecute = canExecute ?? (_ => true);
-        }
-
-        public bool CanExecute(object? parameter)
-        {
-            return _canExecute.Invoke((T?)parameter);
-        }
-
-        public void Execute(object? parameter)
-        {
-            _execute.Invoke((T?)parameter);
         }
     }
 }
