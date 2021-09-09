ba97010062f6a2794e04a3810430c7c0f63ebdbe
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-09-09 11:37:05 +0200

    Create Command.cs

---
+++ src/Core2D/ViewModels/Command.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Core2D.ViewModels
+{
+    public class Command : ICommand
+    {
+        private readonly Action _execute;
+        private readonly Func<bool> _canExecute;
+
+        public event EventHandler? CanExecuteChanged;
+
+        public Command(Action execute, Func<bool>? canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute ?? (() => true);
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return _canExecute.Invoke();
+        }
+
+        public void Execute(object? parameter)
+        {
+            _execute.Invoke();
+        }
+    }
+
+    public class Command<T> : ICommand
+    {
+        private readonly Action<T?> _execute;
+        private readonly Func<T?, bool> _canExecute;
+
+        public event EventHandler? CanExecuteChanged;
+
+        public Command(Action<T?> execute, Func<T?, bool>? canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute ?? (_ => true);
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return _canExecute.Invoke((T?)parameter);
+        }
+
+        public void Execute(object? parameter)
+        {
+            _execute.Invoke((T?)parameter);
+        }
+    }
+}
