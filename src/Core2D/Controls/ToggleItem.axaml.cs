68fefe92437a838fa04e5cbc8fdefd81c40ae478
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-09-10 20:22:29 +0200

    Add ToggleItem

---
+++ src/Core2D/Controls/ToggleItem.axaml.cs
@@ -0,0 +1,27 @@
+#nullable enable
+using Avalonia;
+using Avalonia.Controls.Primitives;
+
+namespace Core2D.Controls
+{
+    public class ToggleItem : TemplatedControl
+    {
+        public static readonly StyledProperty<object?> ToggleContentProperty = 
+            AvaloniaProperty.Register<ToggleItem, object?>(nameof(ToggleContent));
+
+        public static readonly StyledProperty<object?> PopupContentProperty = 
+            AvaloniaProperty.Register<ToggleItem, object?>(nameof(PopupContent));
+
+        public object? ToggleContent
+        {
+            get => GetValue(ToggleContentProperty);
+            set => SetValue(ToggleContentProperty, value);
+        }
+
+        public object? PopupContent
+        {
+            get => GetValue(PopupContentProperty);
+            set => SetValue(PopupContentProperty, value);
+        }
+    }
+}
