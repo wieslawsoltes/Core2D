a90669574bca692f7c260e22f418c19d55920cd3
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-09-15 08:35:29 +0200

    Update RenderView.axaml.cs

--- src/Core2D/Views/Renderer/RenderView.axaml.cs
+++ src/Core2D/Views/Renderer/RenderView.axaml.cs
@@ -1,11 +1,9 @@
-﻿#nullable disable
+﻿#nullable enable
 using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Controls.PanAndZoom;
 using Avalonia.Markup.Xaml;
 using Avalonia.Media;
 using Core2D.Model.Renderer;
-using Core2D.Modules.Renderer;
 using Core2D.Model;
 using Core2D.Modules.Renderer.Avalonia;
 using Core2D.ViewModels.Containers;
@@ -15,49 +13,40 @@ namespace Core2D.Views.Renderer
 {
     public class RenderView : UserControl
     {
-        public static readonly StyledProperty<ZoomBorder> ZoomBorderProperty =
-            AvaloniaProperty.Register<RenderView, ZoomBorder>(nameof(ZoomBorder));
+        public static readonly StyledProperty<FrameContainerViewModel?> ContainerProperty =
+            AvaloniaProperty.Register<RenderView, FrameContainerViewModel?>(nameof(Container));
 
-        public static readonly StyledProperty<FrameContainerViewModel> ContainerProperty =
-            AvaloniaProperty.Register<RenderView, FrameContainerViewModel>(nameof(Container));
+        public static readonly StyledProperty<IShapeRenderer?> RendererProperty =
+            AvaloniaProperty.Register<RenderView, IShapeRenderer?>(nameof(Renderer));
 
-        public static readonly StyledProperty<IShapeRenderer> RendererProperty =
-            AvaloniaProperty.Register<RenderView, IShapeRenderer>(nameof(Renderer));
+        public static readonly StyledProperty<ISelection?> SelectionProperty =
+            AvaloniaProperty.Register<RenderView, ISelection?>(nameof(Selection));
 
-        public static readonly StyledProperty<ISelection> SelectionProperty =
-            AvaloniaProperty.Register<RenderView, ISelection>(nameof(Selection));
-
-        public static readonly StyledProperty<DataFlow> DataFlowProperty =
-            AvaloniaProperty.Register<RenderView, DataFlow>(nameof(DataFlow));
+        public static readonly StyledProperty<DataFlow?> DataFlowProperty =
+            AvaloniaProperty.Register<RenderView, DataFlow?>(nameof(DataFlow));
 
         public static readonly StyledProperty<RenderType> RenderTypeProperty =
             AvaloniaProperty.Register<RenderView, RenderType>(nameof(RenderType));
 
-        public ZoomBorder ZoomBorder
-        {
-            get => GetValue(ZoomBorderProperty);
-            set => SetValue(ZoomBorderProperty, value);
-        }
-
-        public FrameContainerViewModel Container
+        public FrameContainerViewModel? Container
         {
             get => GetValue(ContainerProperty);
             set => SetValue(ContainerProperty, value);
         }
 
-        public IShapeRenderer Renderer
+        public IShapeRenderer? Renderer
         {
             get => GetValue(RendererProperty);
             set => SetValue(RendererProperty, value);
         }
 
-        public ISelection Selection
+        public ISelection? Selection
         {
             get => GetValue(SelectionProperty);
             set => SetValue(SelectionProperty, value);
         }
 
-        public DataFlow DataFlow
+        public DataFlow? DataFlow
         {
             get => GetValue(DataFlowProperty);
             set => SetValue(DataFlowProperty, value);
@@ -83,13 +72,6 @@ namespace Core2D.Views.Renderer
         {
             base.Render(context);
 
-            var offset = this.TranslatePoint(new Point(0, 0), ZoomBorder) ?? default;
-            var bounds = new Rect(
-                offset.X > 0.0 ? -offset.X : 0.0,
-                offset.Y > 0.0 ? -offset.Y : 0.0,
-                ZoomBorder.Bounds.Width + (offset.X > 0.0 ? offset.X : -offset.X),
-                ZoomBorder.Bounds.Height + (offset.Y > 0.0 ? offset.Y : -offset.Y));
-
             var drawState = new RenderState()
             {
                 Container = Container,
@@ -102,10 +84,12 @@ namespace Core2D.Views.Renderer
             var customDrawOperation = new RenderDrawOperation
             {
                 RenderState = drawState,
-                Bounds = bounds
+                Bounds = Bounds
             };
 
-            context.Custom(customDrawOperation);
+            // TODO: context.Custom(customDrawOperation);
+
+            customDrawOperation.Render(context.PlatformImpl);
         }
     }
 }
