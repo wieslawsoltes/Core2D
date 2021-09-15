602bc6d0d78f96f8f8b7f36bbf0c790460f3773a
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-09-15 12:30:51 +0200

    Create IsCurrentToolConverter.cs

---
+++ src/Core2D/Converters/IsCurrentToolConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using Avalonia;
+using Avalonia.Data.Converters;
+using Core2D.Model.Editor;
+
+namespace Core2D.Converters
+{
+    public class IsCurrentToolConverter : IValueConverter
+    {
+        public static IsCurrentToolConverter Instance = new();
+
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is ITool tool && parameter is string title)
+            {
+                return tool.Title == title;
+            }
+            return AvaloniaProperty.UnsetValue;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
