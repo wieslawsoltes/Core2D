47c0c542ad64390e83eefa86354ec2238fa44e58
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-11-22 22:07:21 +0100

    Split into menu and tools view

---
+++ samples/Demo/Views/MenuView.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Demo.Views
+{
+    public class MenuView : UserControl
+    {
+        public MenuView()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
