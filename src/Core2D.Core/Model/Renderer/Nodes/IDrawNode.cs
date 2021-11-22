fe666cded39bcb232248d6a92c91e3e2dee8ae2e
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-11-22 19:04:49 +0100

    Split into separate projects

---
+++ src/Core2D.Core/Model/Renderer/Nodes/IDrawNode.cs
@@ -0,0 +1,22 @@
+﻿#nullable enable
+using System;
+using Core2D.ViewModels.Style;
+
+namespace Core2D.Model.Renderer.Nodes;
+
+public interface IDrawNode : IDisposable
+{
+    ShapeStyleViewModel? Style { get; set; }
+
+    bool ScaleThickness { get; set; }
+
+    bool ScaleSize { get; set; }
+
+    void UpdateGeometry();
+
+    void UpdateStyle();
+
+    void Draw(object? dc, double zoom);
+
+    void OnDraw(object? dc, double zoom);
+}
