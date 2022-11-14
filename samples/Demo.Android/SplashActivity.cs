ef605f605b229678bc6d513cad88b91d10c20c6d
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-11-14 20:49:05 +0100

    Add android demo

---
+++ samples/Demo.Android/SplashActivity.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.Content;
+using Avalonia.Android;
+
+namespace Demo;
+
+[Activity(Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true)]
+public class SplashActivity : AvaloniaSplashActivity<App>
+{
+    protected override void OnResume()
+    {
+        base.OnResume();
+
+        StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+    }
+}
