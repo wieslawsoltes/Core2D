99d545eb6db7f140604b4ff45ff6a252d03a08c5
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-11-17 14:29:07 +0100

    Update DesignerContext.cs

--- src/Core2D/ViewModels/Designer/DesignerContext.cs
+++ src/Core2D/ViewModels/Designer/DesignerContext.cs
@@ -21,105 +21,115 @@ namespace Core2D.ViewModels.Designer;
 
 public class DesignerContext
 {
-    public static ProjectEditorViewModel Editor { get; private set; }
+    public static ProjectEditorViewModel? Editor { get; private set; }
 
-    public static TemplateContainerViewModel Template { get; private set; }
+    public static TemplateContainerViewModel? Template { get; private set; }
 
-    public static PageContainerViewModel Page { get; private set; }
+    public static PageContainerViewModel? Page { get; private set; }
 
-    public static DocumentContainerViewModel Document { get; private set; }
+    public static DocumentContainerViewModel? Document { get; private set; }
 
-    public static LayerContainerViewModel Layer { get; private set; }
+    public static LayerContainerViewModel? Layer { get; private set; }
 
-    public static OptionsViewModel Options { get; private set; }
+    public static OptionsViewModel? Options { get; private set; }
 
-    public static ScriptViewModel Script { get; private set; }
+    public static ScriptViewModel? Script { get; private set; }
 
-    public static ProjectContainerViewModel Project { get; private set; }
+    public static ProjectContainerViewModel? Project { get; private set; }
 
-    public static LibraryViewModel CurrentStyleLibrary { get; private set; }
+    public static LibraryViewModel? CurrentStyleLibrary { get; private set; }
 
-    public static LibraryViewModel CurrentGroupLibrary { get; private set; }
+    public static LibraryViewModel? CurrentGroupLibrary { get; private set; }
 
     public static ShapeStateFlags State { get; private set; }
 
-    public static DatabaseViewModel Database { get; private set; }
+    public static DatabaseViewModel? Database { get; private set; }
 
-    public static RecordViewModel Record { get; private set; }
+    public static RecordViewModel? Record { get; private set; }
 
-    public static ArgbColorViewModel ArgbColor { get; private set; }
+    public static ArgbColorViewModel? ArgbColor { get; private set; }
 
-    public static ArrowStyleViewModel ArrowStyle { get; private set; }
+    public static ArrowStyleViewModel? ArrowStyle { get; private set; }
 
     public static FontStyleFlags FontStyle { get; private set; }
 
-    public static ShapeStyleViewModel ShapeStyle { get; private set; }
+    public static ShapeStyleViewModel? ShapeStyle { get; private set; }
 
-    public static StrokeStyleViewModel StrokeStyle { get; private set; }
+    public static StrokeStyleViewModel? StrokeStyle { get; private set; }
 
-    public static FillStyleViewModel FillStyle { get; private set; }
+    public static FillStyleViewModel? FillStyle { get; private set; }
 
-    public static TextStyleViewModel TextStyle { get; private set; }
+    public static TextStyleViewModel? TextStyle { get; private set; }
 
-    public static ArcShapeViewModel Arc { get; private set; }
+    public static ArcShapeViewModel? Arc { get; private set; }
 
-    public static CubicBezierShapeViewModel CubicBezier { get; private set; }
+    public static CubicBezierShapeViewModel? CubicBezier { get; private set; }
 
-    public static EllipseShapeViewModel Ellipse { get; private set; }
+    public static EllipseShapeViewModel? Ellipse { get; private set; }
 
-    public static GroupShapeViewModel Group { get; private set; }
+    public static GroupShapeViewModel? Group { get; private set; }
 
-    public static ImageShapeViewModel Image { get; private set; }
+    public static ImageShapeViewModel? Image { get; private set; }
 
-    public static LineShapeViewModel Line { get; private set; }
+    public static LineShapeViewModel? Line { get; private set; }
 
-    public static PathShapeViewModel Path { get; private set; }
+    public static PathShapeViewModel? Path { get; private set; }
 
-    public static PointShapeViewModel Point { get; private set; }
+    public static PointShapeViewModel? Point { get; private set; }
 
-    public static QuadraticBezierShapeViewModel QuadraticBezier { get; private set; }
+    public static QuadraticBezierShapeViewModel? QuadraticBezier { get; private set; }
 
-    public static RectangleShapeViewModel Rectangle { get; private set; }
+    public static RectangleShapeViewModel? Rectangle { get; private set; }
 
-    public static TextShapeViewModel Text { get; private set; }
+    public static TextShapeViewModel? Text { get; private set; }
 
-    public static ArcSegmentViewModel ArcSegment { get; private set; }
+    public static ArcSegmentViewModel? ArcSegment { get; private set; }
 
-    public static CubicBezierSegmentViewModel CubicBezierSegment { get; private set; }
+    public static CubicBezierSegmentViewModel? CubicBezierSegment { get; private set; }
 
-    public static LineSegmentViewModel LineSegment { get; private set; }
+    public static LineSegmentViewModel? LineSegment { get; private set; }
 
-    public static PathFigureViewModel PathFigure { get; private set; }
+    public static PathFigureViewModel? PathFigure { get; private set; }
 
-    public static PathSizeViewModel PathSize { get; private set; }
+    public static PathSizeViewModel? PathSize { get; private set; }
 
-    public static QuadraticBezierSegmentViewModel QuadraticBezierSegment { get; private set; }
+    public static QuadraticBezierSegmentViewModel? QuadraticBezierSegment { get; private set; }
 
-    public static ShapeRendererStateViewModel ShapeRendererState { get; private set; }
+    public static ShapeRendererStateViewModel? ShapeRendererState { get; private set; }
 
     public static void InitializeContext(IServiceProvider? serviceProvider)
     {
         var factory = serviceProvider.GetService<IViewModelFactory>();
+        var containerFactory = serviceProvider.GetService<IContainerFactory>();
+        if (factory is null || containerFactory is null)
+        {
+            return;
+        }
 
         // Editor
 
         Editor = serviceProvider.GetService<ProjectEditorViewModel>();
-        Editor.CurrentTool = Editor.Tools.FirstOrDefault(x => x.Title == "Selection");
-        Editor.CurrentPathTool = Editor.PathTools.FirstOrDefault(x => x.Title == "Line");
+        if (Editor is { })
+        {
+            Editor.CurrentTool = Editor.Tools.FirstOrDefault(x => x.Title == "Selection");
+            Editor.CurrentPathTool = Editor.PathTools.FirstOrDefault(x => x.Title == "Line");
+        }
 
         // Recent Projects
 
-        Editor.RecentProjects = Editor.RecentProjects.Add(RecentFileViewModel.Create(serviceProvider, "Test1", "Test1.project"));
-        Editor.RecentProjects = Editor.RecentProjects.Add(RecentFileViewModel.Create(serviceProvider, "Test2", "Test2.project"));
-
+        if (Editor is { })
+        {
+            Editor.RecentProjects = Editor.RecentProjects.Add(RecentFileViewModel.Create(serviceProvider, "Test1", "Test1.project"));
+            Editor.RecentProjects = Editor.RecentProjects.Add(RecentFileViewModel.Create(serviceProvider, "Test2", "Test2.project"));
+        }
         // New Project
 
-        Editor.OnNewProject();
+        Editor?.OnNewProject();
 
         // Data
 
         var db = factory.CreateDatabase("Db");
-        var fields = new string[] { "Column0", "Column1" };
+        var fields = new [] { "Column0", "Column1" };
         var columns = ImmutableArray.CreateRange(fields.Select(c => factory.CreateColumn(db, c)));
         db.Columns = columns;
         var values = Enumerable.Repeat("<empty>", db.Columns.Length).Select(c => factory.CreateValue(c));
@@ -134,25 +144,29 @@ public class DesignerContext
 
         // Project
 
-        var containerFactory = serviceProvider.GetService<IContainerFactory>();
-
         Project = containerFactory.GetProject();
 
         Template = factory.CreateTemplateContainer();
 
         Page = factory.CreatePageContainer();
         var layer = Page.Layers.FirstOrDefault();
-        layer.Shapes = layer.Shapes.Add(factory.CreateLineShape(0, 0, null));
-        Page.CurrentLayer = layer;
-        Page.CurrentShape = layer.Shapes.FirstOrDefault();
+        if (layer is { })
+        {
+            layer.Shapes = layer.Shapes.Add(factory.CreateLineShape(0, 0, null));
+            Page.CurrentLayer = layer;
+            Page.CurrentShape = layer.Shapes.FirstOrDefault();
+        }
         Page.Template = Template;
 
         Document = factory.CreateDocumentContainer();
         Layer = factory.CreateLayerContainer();
         Options = factory.CreateOptions();
 
-        CurrentStyleLibrary = Project.CurrentStyleLibrary;
-        CurrentGroupLibrary = Project.CurrentGroupLibrary;
+        if (Project is { })
+        {
+            CurrentStyleLibrary = Project.CurrentStyleLibrary;
+            CurrentGroupLibrary = Project.CurrentGroupLibrary;
+        }
 
         // Scripting
 
@@ -164,7 +178,7 @@ public class DesignerContext
 
         // Style
 
-        ArgbColor = factory.CreateArgbColor(128, 255, 0, 0);
+        ArgbColor = factory.CreateArgbColor(128, 255);
         ArrowStyle = factory.CreateArrowStyle();
         FontStyle = FontStyleFlags.Regular;
         ShapeStyle = factory.CreateShapeStyle("Default");
@@ -191,7 +205,7 @@ public class DesignerContext
         ArcSegment = factory.CreateArcSegment(factory.CreatePointShape(), factory.CreatePathSize(), 180, true, SweepDirection.Clockwise);
         CubicBezierSegment = factory.CreateCubicBezierSegment(factory.CreatePointShape(), factory.CreatePointShape(), factory.CreatePointShape());
         LineSegment = factory.CreateLineSegment(factory.CreatePointShape());
-        PathFigure = factory.CreatePathFigure(factory.CreatePointShape(), false);
+        PathFigure = factory.CreatePathFigure(factory.CreatePointShape());
         PathSize = factory.CreatePathSize();
         QuadraticBezierSegment = factory.CreateQuadraticBezierSegment(factory.CreatePointShape(), factory.CreatePointShape());
 
