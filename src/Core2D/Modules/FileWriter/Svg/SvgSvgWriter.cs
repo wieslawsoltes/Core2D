33ea5d3756a6d9922b2b1309a7e78def3d2762cf
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-11-18 12:01:16 +0100

    Update SvgSvgWriter.cs

--- src/Core2D/Modules/FileWriter/Svg/SvgSvgWriter.cs
+++ src/Core2D/Modules/FileWriter/Svg/SvgSvgWriter.cs
@@ -21,9 +21,9 @@ public sealed class SvgSvgWriter : IFileWriter
         _serviceProvider = serviceProvider;
     }
 
-    public string Name { get; } = "Svg (Svg)";
+    public string Name => "Svg (Svg)";
 
-    public string Extension { get; } = "svg";
+    public string Extension => "svg";
 
     public void Save(Stream stream, object? item, object? options)
     {
@@ -32,39 +32,43 @@ public sealed class SvgSvgWriter : IFileWriter
             return;
         }
 
-        var ic = options as IImageCache;
-        if (options is null)
+        if (options is not IImageCache _)
+        {
+            return;
+        }
+
+        var dataFlow = _serviceProvider.GetService<DataFlow>();
+        if (dataFlow is null)
         {
             return;
         }
 
         var exporter = new SvgSvgExporter(_serviceProvider);
 
-        if (item is PageContainerViewModel page)
+        if (item is PageContainerViewModel page && page.Template is { })
         {
-            var dataFlow = _serviceProvider.GetService<DataFlow>();
             var db = (object)page.Properties;
-            var record = (object)page.Record;
+            var record = (object?)page.Record;
 
             dataFlow.Bind(page.Template, db, record);
             dataFlow.Bind(page, db, record);
 
             var shapes = page.Layers.SelectMany(x => x.Shapes);
-            if (shapes is { })
+            if (shapes.Any())
             {
                 var xaml = exporter.Create(shapes, page.Template.Width, page.Template.Height);
                 if (!string.IsNullOrEmpty(xaml))
                 {
-                    byte[] bytes = Encoding.UTF8.GetBytes(xaml);
+                    var bytes = Encoding.UTF8.GetBytes(xaml);
                     stream.Write(bytes, 0, bytes.Length);
                 }
             }
         }
-        else if (item is DocumentContainerViewModel document)
+        else if (item is DocumentContainerViewModel _)
         {
             throw new NotSupportedException("Saving documents as svg drawing is not supported.");
         }
-        else if (item is ProjectContainerViewModel project)
+        else if (item is ProjectContainerViewModel _)
         {
             throw new NotSupportedException("Saving projects as svg drawing is not supported.");
         }
