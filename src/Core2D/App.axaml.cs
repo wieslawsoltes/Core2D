a1a524a6e2be7bda55b54c5c0a2555e168384f00
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-11-18 10:27:42 +0100

    Refactor themes

--- src/Core2D/App.axaml.cs
+++ src/Core2D/App.axaml.cs
@@ -9,6 +9,7 @@ using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 using Avalonia.Markup.Xaml.Styling;
+using Avalonia.Themes.Fluent;
 using Core2D.Configuration.Windows;
 using Core2D.ViewModels;
 using Core2D.ViewModels.Designer;
@@ -19,6 +20,10 @@ namespace Core2D;
 
 public class App : Application
 {
+    private static StyleInclude? s_fluentDark;
+
+    private static StyleInclude? s_fluentLight;
+
     public static string DefaultTheme { get; set; }
 
     public static ICommand? ChangeTheme { get; }
@@ -98,15 +103,36 @@ public class App : Application
             return;
         }
 
-        var theme = Current.Styles.Select(x => (StyleInclude)x).FirstOrDefault(x => x.Source is { } && x.Source.AbsolutePath.Contains("Themes"));
-        if (theme is { })
+        s_fluentDark ??= new StyleInclude(new Uri("avares://Core2D/App.axaml"))
         {
-            var index = Current.Styles.IndexOf(theme);
+            Source = new Uri("avares://Core2D/Themes/FluentDark.axaml")
+        };
 
-            Current.Styles[index] = new StyleInclude(new Uri("avares://Core2D/App.axaml"))
+        s_fluentLight ??= new StyleInclude(new Uri("avares://Core2D/App.axaml"))
+        {
+            Source = new Uri("avares://Core2D/Themes/FluentLight.axaml")
+        };
+
+        switch (themeName)
+        {
+            case "FluentLight":
             {
-                Source = new Uri($"avares://Core2D/Themes/{themeName}.axaml")
-            };
+                if (Current.Styles[0] is FluentTheme fluentTheme)
+                {
+                    fluentTheme.Mode = FluentThemeMode.Light;
+                }
+                Current.Styles[1] = s_fluentLight;
+                break;
+            }
+            case "FluentDark":
+            {
+                if (Current.Styles[0] is FluentTheme fluentTheme)
+                {
+                    fluentTheme.Mode = FluentThemeMode.Dark;
+                }
+                Current.Styles[1] = s_fluentDark;
+                break;
+            }
         }
     }
 
