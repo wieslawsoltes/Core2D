279eef146283d775b8d93a52b39af4b880848015
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-11-20 14:00:04 +0100

    Update RendererOptions.cs

--- src/Core2D/Views/Renderer/RendererOptions.cs
+++ src/Core2D/Views/Renderer/RendererOptions.cs
@@ -9,41 +9,41 @@ namespace Core2D.Views.Renderer;
 
 public class RendererOptions
 {
-    public static readonly AttachedProperty<IShapeRenderer> RendererProperty =
-        AvaloniaProperty.RegisterAttached<RendererOptions, AvaloniaObject, IShapeRenderer>("Renderer", null, true, BindingMode.TwoWay);
+    public static readonly AttachedProperty<IShapeRenderer?> RendererProperty =
+        AvaloniaProperty.RegisterAttached<RendererOptions, AvaloniaObject, IShapeRenderer?>("Renderer", null, true, BindingMode.TwoWay);
 
-    public static IShapeRenderer GetRenderer(AvaloniaObject obj)
+    public static IShapeRenderer? GetRenderer(AvaloniaObject obj)
     {
         return obj.GetValue(RendererProperty);
     }
 
-    public static void SetRenderer(AvaloniaObject obj, IShapeRenderer value)
+    public static void SetRenderer(AvaloniaObject obj, IShapeRenderer? value)
     {
         obj.SetValue(RendererProperty, value);
     }
 
-    public static readonly AttachedProperty<ISelection> SelectionProperty =
-        AvaloniaProperty.RegisterAttached<RendererOptions, AvaloniaObject, ISelection>("Selection", null, true, BindingMode.TwoWay);
+    public static readonly AttachedProperty<ISelection?> SelectionProperty =
+        AvaloniaProperty.RegisterAttached<RendererOptions, AvaloniaObject, ISelection?>("Selection", null, true, BindingMode.TwoWay);
 
-    public static ISelection GetSelection(AvaloniaObject obj)
+    public static ISelection? GetSelection(AvaloniaObject obj)
     {
         return obj.GetValue(SelectionProperty);
     }
 
-    public static void SetSelection(AvaloniaObject obj, ISelection value)
+    public static void SetSelection(AvaloniaObject obj, ISelection? value)
     {
         obj.SetValue(SelectionProperty, value);
     }
 
-    public static readonly AttachedProperty<DataFlow> DataFlowProperty =
-        AvaloniaProperty.RegisterAttached<RendererOptions, AvaloniaObject, DataFlow>("DataFlow", null, true, BindingMode.TwoWay);
+    public static readonly AttachedProperty<DataFlow?> DataFlowProperty =
+        AvaloniaProperty.RegisterAttached<RendererOptions, AvaloniaObject, DataFlow?>("DataFlow", null, true, BindingMode.TwoWay);
 
-    public static DataFlow GetDataFlow(AvaloniaObject obj)
+    public static DataFlow? GetDataFlow(AvaloniaObject obj)
     {
         return obj.GetValue(DataFlowProperty);
     }
 
-    public static void SetDataFlow(AvaloniaObject obj, DataFlow value)
+    public static void SetDataFlow(AvaloniaObject obj, DataFlow? value)
     {
         obj.SetValue(DataFlowProperty, value);
     }
